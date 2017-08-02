61ddfca7aa6cc9f02e5e4b5d19481f5bf6d2a2f4
Steve Kite <korshakov.stepan@gmail.com>
2017-08-02 05:26:47 +0300

    Code refactoring

--- src/api.tsx
+++ src/api.tsx
@@ -12,37 +12,19 @@ import {
 
 import * as Auth from './auth';
 
-const channelsListQuery = gql`
-   query Vote {
-     vote(id: "MTIz") {
-       id
-       count
-       own_set
-     }
-   }
- `;
+var client = new ApolloClient({
+    networkInterface: createNetworkInterface(
+        'http://localhost:9000/graphql',
+        {
+            opts: {
+                headers: Auth.headers()
+            }
+        })
+});
 
-const addChannelMutation = gql`
-  mutation vote($id: ID!) {
-    vote(id: $id) {
-      id
-      count
-      own_set
-    }
-  }
-`;
+// Example Mutation
 
-interface QueryVoteProps extends QueryProps {
-    vote: {
-        id: string;
-        count: number;
-    };
-}
-
-function AddChannel(props: { mutate: MutationFunc<{}> } ) {
-
-    console.warn(props);
-
+function AddChannel(props: { mutate: MutationFunc<{}> }) {
     function like() {
         props.mutate({
             variables: {
@@ -54,11 +36,26 @@ function AddChannel(props: { mutate: MutationFunc<{}> } ) {
     return <button onClick={like}>Like</button>;
 }
 
-const AddChannelWithMutation = graphql(
-    addChannelMutation
-)(AddChannel);
+const AddChannelWithMutation = graphql(gql`
+  mutation vote($id: ID!) {
+    vote(id: $id) {
+      id
+      count
+      own_set
+    }
+  }
+`)(AddChannel);
 
-function ChannelsList(props: { data: QueryVoteProps }) {
+// List
+
+interface Vote {
+    vote: {
+        id: string;
+        count: number;
+    };
+}
+
+function ChannelsList(props: { data: QueryProps & Vote }) {
     if (props.data.loading) {
         return (
             <div>
@@ -66,49 +63,23 @@ function ChannelsList(props: { data: QueryVoteProps }) {
             </div>
         );
     }
-
-    // function like() {
-
-    // }
-
-    console.warn(props.data.vote);
-    // var data = (props.data as {vote:any}).vote;
-    // console.warn((props.data as {vote:any}).vote);
     return (
         <div>
             Likes: {props.data.vote.count}
             <AddChannelWithMutation />
         </div>
     );
-    // if (props.data.loading) {
-    //     return (
-    //         <p>Loading ...</p>
-    //     );
-    // }
-    //     if (loading) {
-
-    //     }
-    // if (error) {
-    //     return (<p>{ error.message } < /p>);
-    //     }
-    // return (<ul>
-    //     {
-    //         channels.map(ch => <li key={ ch.id } > { ch.name } < /li> ) 
-    //         }
-    //     < /ul>);
 }
 
-var client = new ApolloClient({
-    networkInterface: createNetworkInterface(
-        'http://localhost:9000/graphql',
-        {
-            opts: {
-                headers: Auth.headers()
-            }
-        })
-});
-
-const ChannelsListWithData = graphql(channelsListQuery)(ChannelsList);
+const ChannelsListWithData = graphql(gql`
+   query Vote {
+     vote(id: "MTIz") {
+       id
+       count
+       own_set
+     }
+   }
+ `)(ChannelsList);
 
 export function TestComponent() {
     return (
