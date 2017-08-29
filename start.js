4dff26077584dd54b038604fe983fc4e88716e92
Steve Kite <korshakov.stepan@gmail.com>
2017-08-29 23:16:05 +0300

    Trying to redirect HTTP to HTTPS

---
+++ start.js
@@ -0,0 +1,20 @@
+'use strict';
+var redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
+var http = require("http")
+var pathUtils = require("path");
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 5000;
+var appDir = pathUtils.resolve(__dirname, "build");
+
+app.use(redirectToHTTPS());
+app.use(express.static(appDir));
+
+app.get("*", function (req, res) {
+    res.sendfile(pathUtils.resolve(appDir, "index.html"));
+});
+
+http.createServer(app).listen(PORT, function () {
+    console.log("Express server listening on port " + PORT);
+    console.log("http://localhost:" + PORT);
+});
