a1f232e365b6051eecbd6a30422323c1be3626a5
Steve Kite <korshakov.stepan@gmail.com>
2017-11-11 15:02:10 +0300

    Fixing request headers

--- src/api.ts
+++ src/api.ts
@@ -18,24 +18,26 @@ function buildId(typename: string, id: any) {
     return toIdValue(dataIdFromObject(typename, id));
 }
 
-var headers: string[][] = [];
-
+var headers: any = {};
+var headersArray: string[][] = [];
 if (Config.domain !== 'sandbox') {
-    headers = [['x-statecraft-domain', Config.domain]];
+    headers['x-statecraft-domain'] = Config.domain;
+    headersArray = [['x-statecraft-domain', Config.domain]];
 }
 
 if (Auth.authorizationHeader() != null) {
-    (<any>headers).authorization = Auth.authorizationHeader();
+    var h = Auth.authorizationHeader()!!;
+    headers.authorization = h;
+    headersArray = [['authorization', h], ...headersArray];
 }
 
 client = new ApolloClient({
-    networkInterface: createNetworkInterface(
-        endpoint,
-        {
-            opts: {
-                headers: headers
-            }
-        }),
+    networkInterface: createNetworkInterface({
+        uri: endpoint,
+        opts: {
+            headers: headers
+        }
+    }),
     customResolvers: {
         City: {
             segment: (_, args: { id: string }) => buildId('Segment', args.id)
@@ -48,7 +50,7 @@ export const apolloClient = client;
 export function sandboxResolver(args: any) {
     return fetch(endpoint, {
         method: 'post',
-        headers: [['Content-Type', 'application/json'], ...headers],
+        headers: [['Content-Type', 'application/json'], ...headersArray],
         body: JSON.stringify(args),
     }).then(response => response.json());
 }
