ca6db80e6af7c5823c6eb4797d6f3aec836aa99d
Steve Kite <korshakov.stepan@gmail.com>
2017-11-12 08:42:18 +0300

    Fixing /projects/ link

--- src/App/Projects/index.tsx
+++ src/App/Projects/index.tsx
@@ -2,11 +2,22 @@ import * as React from 'react';
 import { Route, Switch, Redirect } from 'react-router';
 import View from './View';
 import Edit from './Edit';
+import { withProjectsQuery } from '../../api/Project';
+import { withLoader } from '../Components/withLoader';
+
+const PickDefaultProject = withProjectsQuery(withLoader((props) => {
+    if (props.data.projects.length === 0) {
+        return <Redirect to={'/'} />;
+    }
+    return <Redirect to={'/projects/' + props.data.projects[0].slug} />;
+}));
+
 export default function () {
     return (
         <Switch>
             <Route path="/projects/:projectId/edit" component={Edit} />
             <Route path="/projects/:projectId" component={View} />
+            <Route path="/projects/" component={PickDefaultProject} />
             <Redirect to="/404" />
         </Switch>
     );
