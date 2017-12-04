7ec692955a471f7ed2ce489a2bb855ae5b75b674
Steve Kite <korshakov.stepan@gmail.com>
2017-12-04 19:04:21 +0300

    Fixing navigation urls

--- src/App/XComponents/Header.tsx
+++ src/App/XComponents/Header.tsx
@@ -1,20 +1,25 @@
 import * as React from 'react';
-
-export function Header() {
+import { withUser } from '../Components/UserProvider';
+import * as A from '../../auth';
+import { Link } from '../Components/Link';
+export const Header = withUser((props) => {
     return (
         <div className="x-top">
             <div className="x-container is-wide clearfix">
                 <a className="x-top--label" href="#">San Francisco Housing Forecast</a>
                 <div className="x-top--tabs">
-                    <a className="x-top--tab is-active" href="#">Dashboard</a>
-                    <a className="x-top--tab" href="#">Pipeline Explorer</a>
+                    <Link className="x-top--tab" path="/">Home</Link>
+                    <Link className="x-top--tab is-active" path="/">Pipeline Explorer</Link>
+                    {/* <a className="x-top--tab is-active" href="#">Dashboard</a>
+                    <a className="x-top--tab" href="#">Pipeline Explorer</a> */}
                 </div>
                 <ul className="x-top--nav">
-                    <li className="x-top--item"><span><img src="http://placehold.it/28" />Yury Lifshits</span></li>
-                    <li className="x-top--item"><button>Sign In</button></li>
-                    <li className="x-top--item is-join"><button>Join</button></li>
+                    {props.user && (<li className="x-top--item"><span><img src={props.user.picture} alt="" />{props.user.firstName} {' '} {props.user.lastName}</span></li>)}
+
+                    {!props.user && (<li className="x-top--item"><button onClick={e => { e.preventDefault(); A.login(); }}>Sign In</button></li>)}
+                    {!props.user && (<li className="x-top--item is-join"><button onClick={e => { e.preventDefault(); A.login(); }}>Join</button></li>)}
                 </ul>
             </div>
         </div>
     );
-}
+});
