65a6aa17ca52bc93b00b7ccccb747346dbfd79f8
Fred Morozov (iMac) <morfedor@hotmail.com>
2017-12-05 04:43:02 +0300

    Share links

--- src/App/XComponents/HeaderLargeSocial.tsx
+++ src/App/XComponents/HeaderLargeSocial.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 
 export function HeaderLargeSocial() {
+    var shareText: string = 'San Francisco Housing Forecast 2017-18 ' + window.location.href + ' #housing #sf';
+
     return (
         <div className="x-intro--tools">
             <form className="x-intro--form" action="https://one.us15.list-manage.com/subscribe/post?u=b588cb73ec7bd801c3b609670&amp;id=c943356ada" method="post">
@@ -11,8 +13,8 @@ export function HeaderLargeSocial() {
             </form>
             <div className="x-intro--socials">
                 <div className="x-intro--label">Share</div>
-                <a className="x-intro--social" href="#"><i className="icon-fb">{}</i></a>
-                <a className="x-intro--social" href="#"><i className="icon-tw">{}</i></a>
+                <a className="x-intro--social" target="_blank" href={'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(shareText)}><i className="icon-fb">{}</i></a>
+                <a className="x-intro--social" target="_blank" href={'https://twitter.com/intent/tweet?text=' + encodeURIComponent(shareText)}><i className="icon-tw">{}</i></a>
             </div>
         </div>
     );
