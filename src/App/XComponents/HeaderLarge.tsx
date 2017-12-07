56538e7e816e519e9c7fe6e512607102e65ca721
Fred Morozov (iMac) <morfedor@hotmail.com>
2017-12-07 06:34:29 +0300

    Открытие/закрытие меню в мобильной версии.

--- src/App/XComponents/HeaderLarge.tsx
+++ src/App/XComponents/HeaderLarge.tsx
@@ -2,53 +2,53 @@ import * as React from 'react';
 import { withUser } from '../Components/UserProvider';
 import { Link } from '../Components/Link';
 import * as A from '../../auth';
-export const HeaderLarge = withUser((props) => {
-    return (
-        <div className="x-intro">
-            <div className="x-container">
-                <div className="x-header">
-                    <a className="x-header--logo" href="https://statecraft.one/"><img src="/img/logotype.svg" alt="" /></a>
-                    <a className="x-header--open visible-xs" href="#">{}</a>
-                    <ul className="x-header--nav hidden-xs">
-                        <li className="x-header--item"><Link path="/pipeline">Explore Pipeline</Link></li>
 
-                        {props.user &&
-                            <li className="x-header--item"><span><img src={props.user.picture} alt="" />{props.user.firstName} {props.user.lastName}</span></li>
-                        }
-                        {props.user &&
-                            <li className="x-header--item is-join"><button onClick={e => { e.preventDefault(); A.logout(); }}>Sign Out</button></li>
-                        }
-                        {!props.user &&
-                            <li className="x-header--item"><button onClick={e => { e.preventDefault(); A.login(); }}>Sign In</button></li>
-                        }
-                        {!props.user &&
-                            (<li className="x-header--item is-join"><a target="_blank" href="https://goo.gl/forms/YX8LSpH6jWLzbEj02">Join</a></li>)
-                        }
-                    </ul>
 
-                    <div className="x-header--menu">
-                        <ul className="x-header--nav">
-                            <li className="x-header--item"><Link path="/pipeline">Explore Pipeline</Link></li>
+export class HeaderLarge extends React.Component<{children: any}, {isShown: boolean}> {
+    constructor (props: {children: any}) {
+        super(props);
 
-                            {props.user &&
-                                <li className="x-header--item"><span><img src={props.user.picture} alt="" />{props.user.firstName} {props.user.lastName}</span></li>
-                            }
-                            {props.user &&
-                                <li className="x-header--item is-join"><button onClick={e => { e.preventDefault(); A.logout(); }}>Sign Out</button></li>
-                            }
-                            {!props.user &&
-                                <li className="x-header--item"><button onClick={e => { e.preventDefault(); A.login(); }}>Sign In</button></li>
-                            }
-                            {!props.user &&
-                                (<li className="x-header--item is-join"><a target="_blank" href="https://goo.gl/forms/YX8LSpH6jWLzbEj02">Join</a></li>)
-                            }
-                        </ul>
+        this.state = {
+            isShown: false
+        };
+    }
+
+    render () {
+        return (
+            <div className="x-intro">
+                <div className="x-container">
+                    <div className={'x-header' + (this.state.isShown ? ' is-shown' : '')}>
+                        <a className="x-header--logo" href="https://statecraft.one/"><img src="/img/logotype.svg" alt="" /></a>
+                        <a className="x-header--open visible-xs" href="#" onClick={(e) => { e.preventDefault(); this.setState({ isShown: !this.state.isShown }); }}>{}</a>
+
+                        <div className="x-header--nav hidden-xs">
+                            <HeaderLargeNavItems />
+                        </div>
+
+                        <div className="x-header--menu">
+                            <div className="x-header--nav">
+                                <HeaderLargeNavItems />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="x-intro--in">
+                        {this.props.children}
                     </div>
                 </div>
-                <div className="x-intro--in">
-                    {props.children}
-                </div>
             </div>
-        </div>
+        );
+    }
+}
+
+export const HeaderLargeNavItems = withUser((props) => {
+    return (
+        <ul>
+            <li className="x-header--item is-block"><Link path="/pipeline">Explore Pipeline</Link></li>
+
+            {props.user && (<li className="x-header--item"><span><img src={props.user.picture} alt="" />{props.user.firstName} {props.user.lastName}</span></li>)}
+            {props.user && (<li className="x-header--item is-join"><button onClick={e => { e.preventDefault(); A.logout(); }}>Sign Out</button></li>)}
+            {!props.user && (<li className="x-header--item"><button onClick={e => { e.preventDefault(); A.login(); }}>Sign In</button></li>)}
+            {!props.user && (<li className="x-header--item is-join"><a target="_blank" href="https://goo.gl/forms/YX8LSpH6jWLzbEj02">Join</a></li>)}
+        </ul>
     );
 });
