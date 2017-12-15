6f95ae50b696b941dd4de2fe24880f303fdb9449
Steve Kite <korshakov.stepan@gmail.com>
2017-12-15 22:33:15 +0300

    Unified Paged List

--- src/components/withPagedList.tsx
+++ src/components/withPagedList.tsx
@@ -1,5 +1,7 @@
 import { ListQueryPagedData } from '../utils/graphqlList';
 import * as React from 'react';
+import { Dimmer, Loader } from 'semantic-ui-react';
+import { XPaging } from './X/XPaging';
 
 export function withPagedList<TResult extends { id: string }>(WrappedComponent: React.ComponentType<{
     items: TResult[],
@@ -11,16 +13,39 @@ export function withPagedList<TResult extends { id: string }>(WrappedComponent: 
     return function (props: ListQueryPagedData<TResult>) {
         if (props.data.items) {
             return (
-                <WrappedComponent
-                    items={props.data.items!!.edges.map((p) => p.node)}
-                    loading={props.data.loading}
-                    itemsCount={props.data.items.pageInfo.itemsCount}
-                    currentPage={props.data.items.pageInfo.currentPage}
-                    pagesCount={props.data.items.pageInfo.pagesCount}
-                />
+                <div>
+                    <div style={{ position: 'relative' }}>
+                        <Dimmer active={props.data.loading} inverted={true}>
+                            <Loader inverted={true} content="Loading" />
+                        </Dimmer>
+                        <WrappedComponent
+                            items={props.data.items!!.edges.map((p) => p.node)}
+                            loading={props.data.loading}
+                            itemsCount={props.data.items.pageInfo.itemsCount}
+                            currentPage={props.data.items.pageInfo.currentPage}
+                            pagesCount={props.data.items.pageInfo.pagesCount}
+                        />
+
+                    </div>
+                    <div style={{ minHeight: 56, paddingTop: 12 }}>
+                        <XPaging totalPages={props.data.items.pageInfo.pagesCount} currentPage={props.data.items.pageInfo.currentPage} />
+                    </div>
+                </div>
             );
         } else {
-            return <WrappedComponent items={[]} loading={true} itemsCount={0} currentPage={0} pagesCount={0} />;
+            return (
+                <div>
+                    <div style={{ position: 'relative' }}>
+                        <Dimmer active={props.data.loading} inverted={true}>
+                            <Loader inverted={true} content="Loading" />
+                        </Dimmer>
+                        <WrappedComponent items={[]} loading={true} itemsCount={0} currentPage={0} pagesCount={0} />
+                    </div>
+                    <div style={{ minHeight: 56, paddingTop: 12 }}>
+                        <XPaging totalPages={0} currentPage={0} />
+                    </div>
+                </div>
+            );
         }
     };
 }
--- src/pages/db/permits/index.tsx
+++ src/pages/db/permits/index.tsx
@@ -2,50 +2,45 @@ import * as React from 'react';
 import { Permit, withPermitsPagedQuery } from '../../../api/Permits';
 import { withPage } from '../../../components/withPage';
 import { XLink } from '../../../components/X/XLink';
-import { Table, Dimmer, Loader, Segment } from 'semantic-ui-react';
+import { Table, Segment } from 'semantic-ui-react';
 import { withPagedList } from '../../../components/withPagedList';
 import { XContainer } from '../../../components/X/XContainer';
-import { XPaging } from '../../../components/X/XPaging';
 import { PermitStatus } from '../../../components/PermitStatus';
 
 const PermitsItems = withPagedList<Permit>((props) => {
     return (
-        <div style={{ position: 'relative' }}>
-            <Dimmer active={props.loading} inverted={true}>
-                <Loader inverted={true} content="Loading" />
-            </Dimmer>
-            <Table striped={true}>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell collapsing={true}>Permit Id</Table.HeaderCell>
-                        <Table.HeaderCell collapsing={true}>Status</Table.HeaderCell>
-                        <Table.HeaderCell collapsing={true}>Type</Table.HeaderCell>
-                        <Table.HeaderCell>Description</Table.HeaderCell>
+        <Table striped={true}>
+            <Table.Header>
+                <Table.Row>
+                    <Table.HeaderCell collapsing={true}>Permit Id</Table.HeaderCell>
+                    <Table.HeaderCell collapsing={true}>Created</Table.HeaderCell>
+                    <Table.HeaderCell collapsing={true}>Status</Table.HeaderCell>
+                    <Table.HeaderCell collapsing={true}>Type</Table.HeaderCell>
+                    <Table.HeaderCell>Description</Table.HeaderCell>
+                </Table.Row>
+            </Table.Header>
+            <Table.Body>
+                {props.items.map((item) => (
+                    <Table.Row key={item.id}>
+                        <Table.Cell collapsing={true}>
+                            <XLink path={'/permits/' + item.id}>{item.id}</XLink>
+                        </Table.Cell>
+                        <Table.Cell collapsing={true}>
+                            {item.createdAt}
+                        </Table.Cell>
+                        <Table.Cell collapsing={true}>
+                            {item.status && <PermitStatus status={item.status} />}
+                        </Table.Cell>
+                        <Table.Cell collapsing={true}>
+                            {item.type}
+                        </Table.Cell>
+                        <Table.Cell>
+                            {item.description}
+                        </Table.Cell>
                     </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                    {props.items.map((item) => (
-                        <Table.Row key={item.id}>
-                            <Table.Cell collapsing={true}>
-                                <XLink path={'/permits/' + item.id}>{item.id}</XLink>
-                            </Table.Cell>
-                            <Table.Cell collapsing={true}>
-                                {item.status && <PermitStatus status={item.status} />}
-                            </Table.Cell>
-                            <Table.Cell collapsing={true}>
-                                {item.type}
-                            </Table.Cell>
-                            <Table.Cell>
-                                {item.description}
-                            </Table.Cell>
-                        </Table.Row>
-                    ))}
-                </Table.Body>
-            </Table>
-            <div style={{ minHeight: 56 }}>
-                <XPaging totalPages={props.pagesCount} currentPage={props.currentPage} />
-            </div>
-        </div>
+                ))}
+            </Table.Body>
+        </Table>
     );
 });
 
