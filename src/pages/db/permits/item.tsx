df4d53bd985b6f778c1c7f604e29bb89151f6d90
Steve Kite <korshakov.stepan@gmail.com>
2017-12-16 20:09:40 +0300

    Open Permit Url in new tab

--- src/pages/db/permits/item.tsx
+++ src/pages/db/permits/item.tsx
@@ -30,7 +30,7 @@ export default withPage(withPermitQuery((props) => {
                     Pipeline Database
                 </Header>
                 <Segment attached="bottom">
-                    PermitId: <a href={props.data.permit.governmentalUrl}>{props.data.permit.id}</a>
+                    PermitId: <a href={props.data.permit.governmentalUrl} target="_blank">{props.data.permit.id}</a>
                     <div>
                         <Step.Group>
                             <Step>
