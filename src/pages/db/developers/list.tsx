cdc539c6cae400e6a9783f54090c24411446d43d
Steve Kite <korshakov.stepan@gmail.com>
2018-01-02 00:19:02 -0800

    Show on developers list

--- src/pages/db/developers/list.tsx
+++ src/pages/db/developers/list.tsx
@@ -7,14 +7,15 @@ import { XContainer } from '../../../components/X/XContainer';
 import { XForm, XFormField, XFormSubmit, XFormGroup } from '../../../components/X/XForm';
 import { XLink } from '../../../components/X/XLink';
 import { XWriteAcces } from '../../../components/X/XWriteAccess';
+import { XCloudImage } from '../../../components/X/XCloudImage';
 
 const AddForm = withDeveloperAddMutation((props) => {
     return (
         <XForm mutate={props.add}>
             <XFormGroup>
-                <XFormField name="slug" hint="Short Name" />
-                <XFormField name="title" hint="Name of developer" />
-                <XFormSubmit title="Add Developer" />
+                <XFormField name="slug" hint="Short Name"/>
+                <XFormField name="title" hint="Name of developer"/>
+                <XFormSubmit title="Add Developer"/>
             </XFormGroup>
         </XForm>
     );
@@ -22,21 +23,28 @@ const AddForm = withDeveloperAddMutation((props) => {
 
 export default withPage(withDevelopersQuery(withLoader((props) => {
     return (
-        <div style={{ paddingTop: 32, paddingBottom: 32 }}>
+        <div style={{paddingTop: 32, paddingBottom: 32}}>
             <XContainer wide={true}>
                 <Segment>
                     <XWriteAcces>
-                        <AddForm />
+                        <AddForm/>
                     </XWriteAcces>
-                    <Table celled={true} striped={true} >
-                        {props.data.organizations.map(p => {
-                            return (
-                                <Table.Row key={p.id}>
-                                    <Table.Cell collapsing={true}><XLink path={'/organizations/' + p.slug}>{p.slug}</XLink></Table.Cell>
-                                    <Table.Cell>{p.title}</Table.Cell>
-                                </Table.Row>
-                            );
-                        })}
+                    <Table celled={true} striped={true}>
+                        <Table.Body>
+                            {props.data.organizations.map(p => {
+                                return (
+                                    <Table.Row key={p.id}>
+                                        <Table.Cell collapsing={true}>
+                                            {p.logo &&
+                                            <XCloudImage src={p.logo} maxWidth={32} maxHeight={32}/>}
+                                        </Table.Cell>
+                                        <Table.Cell collapsing={true}><XLink
+                                            path={'/organizations/' + p.slug}>{p.slug}</XLink></Table.Cell>
+                                        <Table.Cell>{p.title}</Table.Cell>
+                                    </Table.Row>
+                                );
+                            })}
+                        </Table.Body>
                     </Table>
                 </Segment>
             </XContainer>
