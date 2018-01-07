d639404c9aaba8db788da7a03afab7d5718678aa
Steve Kite <korshakov.stepan@gmail.com>
2018-01-07 00:01:32 -0800

    Added Approved In to Permit page

--- src/pages/db/permits/item.tsx
+++ src/pages/db/permits/item.tsx
@@ -7,15 +7,16 @@ import { XCounter } from '../../../components/X/XCounter';
 import { XDiff } from '../../../components/X/XDiff';
 import { XDate } from '../../../components/X/XDate';
 import { ListPermits } from '../../../components/ListPermits';
+import { formatDuration } from '../../../utils/date';
 
 function ChangeRender(props: { change: FieldChanged }) {
     if (props.change.oldValue === null) {
-        return <span><Icon name="plus" color="green" /> {props.change.newValue}</span>;
+        return <span><Icon name="plus" color="green"/> {props.change.newValue}</span>;
     } else if (props.change.newValue === null) {
-        return <span><Icon name="minus" color="red" /> {props.change.newValue}</span>;
+        return <span><Icon name="minus" color="red"/> {props.change.newValue}</span>;
     } else {
         if (props.change.fieldName === 'description') {
-            return <XDiff oldValue={props.change.oldValue} newValue={props.change.newValue} />;
+            return <XDiff oldValue={props.change.oldValue} newValue={props.change.newValue}/>;
         } else {
             return <span>{props.change.oldValue} -> {props.change.newValue}</span>;
         }
@@ -34,21 +35,22 @@ export default withPage(withPermitQuery((props) => {
                     <div>
                         <Step.Group>
                             <Step>
-                                <Icon name="puzzle" />
+                                <Icon name="puzzle"/>
                                 <Step.Content>
                                     <Step.Title>Approval</Step.Title>
                                     <Step.Description>Waiting to approval</Step.Description>
                                 </Step.Content>
                             </Step>
-                            <Step disabled={props.data.permit.status !== 'COMPLETED' && props.data.permit.status !== 'ISSUED'}>
-                                <Icon name="configure" />
+                            <Step
+                                disabled={props.data.permit.status !== 'COMPLETED' && props.data.permit.status !== 'ISSUED'}>
+                                <Icon name="configure"/>
                                 <Step.Content>
                                     <Step.Title>Construction</Step.Title>
                                     <Step.Description>Construction is in progress</Step.Description>
                                 </Step.Content>
                             </Step>
                             <Step disabled={props.data.permit.status !== 'COMPLETED'}>
-                                <Icon name="gift" />
+                                <Icon name="gift"/>
                                 <Step.Content>
                                     <Step.Title>Completed</Step.Title>
                                     <Step.Description>Construction completed</Step.Description>
@@ -57,32 +59,46 @@ export default withPage(withPermitQuery((props) => {
                         </Step.Group>
                     </div>
                     <div>Status: {props.data.permit.status} {props.data.permit.statusUpdatedAt}</div>
-                    {props.data.permit.filedAt && (<div>Filed <XDate date={props.data.permit.filedAt} large={true} /></div>)}
-                    {props.data.permit.createdAt && (<div>Created <XDate date={props.data.permit.createdAt} large={true} /></div>)}
-                    {props.data.permit.issuedAt && (<div>Issued <XDate date={props.data.permit.issuedAt} large={true} /></div>)}
-                    {props.data.permit.startedAt && (<div>Started <XDate date={props.data.permit.startedAt} large={true} /></div>)}
-                    {props.data.permit.expiresAt && (<div>Expires <XDate date={props.data.permit.expiresAt} large={true} /></div>)}
-                    {props.data.permit.expiredAt && (<div>Expired <XDate date={props.data.permit.expiredAt} large={true} /></div>)}
-                    {props.data.permit.completedAt && (<div>Completed <XDate date={props.data.permit.completedAt} large={true} /></div>)}
-                    {props.data.permit.fasterThan !== undefined && (props.data.permit.fasterThan >= 50) && (<div>Faster Than {props.data.permit.fasterThan}% of projects of the same type</div>)}
-                    {props.data.permit.fasterThan !== undefined && (props.data.permit.fasterThan < 50) && (<div>Slower Than {100 - props.data.permit.fasterThan}% of projects of the same type</div>)}
+                    {props.data.permit.approvalTime && (
+                        <div>Approved in {formatDuration(props.data.permit.approvalTime)}</div>)}
+                    {props.data.permit.filedAt && (
+                        <div>Filed <XDate date={props.data.permit.filedAt} large={true}/></div>)}
+                    {props.data.permit.createdAt && (
+                        <div>Created <XDate date={props.data.permit.createdAt} large={true}/></div>)}
+                    {props.data.permit.issuedAt && (
+                        <div>Issued <XDate date={props.data.permit.issuedAt} large={true}/></div>)}
+                    {props.data.permit.startedAt && (
+                        <div>Started <XDate date={props.data.permit.startedAt} large={true}/></div>)}
+                    {props.data.permit.expiresAt && (
+                        <div>Expires <XDate date={props.data.permit.expiresAt} large={true}/></div>)}
+                    {props.data.permit.expiredAt && (
+                        <div>Expired <XDate date={props.data.permit.expiredAt} large={true}/></div>)}
+                    {props.data.permit.completedAt && (
+                        <div>Completed <XDate date={props.data.permit.completedAt} large={true}/></div>)}
+                    {props.data.permit.fasterThan !== undefined && (props.data.permit.fasterThan >= 50) && (
+                        <div>Faster Than {props.data.permit.fasterThan}% of projects of the same type</div>)}
+                    {props.data.permit.fasterThan !== undefined && (props.data.permit.fasterThan < 50) && (
+                        <div>Slower Than {100 - props.data.permit.fasterThan}% of projects of the same type</div>)}
 
                     <div>Type: {props.data.permit.type} {props.data.permit.typeWood}</div>
                     {props.data.permit.streetNumbers!!.map((s) =>
-                        (<div key={s.streetId}>Address: {s.streetNumber} {s.steetNumberSuffix} {s.streetName} {s.streetNameSuffix}</div>))
+                        (<div
+                            key={s.streetId}>Address: {s.streetNumber} {s.steetNumberSuffix} {s.streetName} {s.streetNameSuffix}</div>))
                     }
                     {props.data.permit.proposedStories && (
-                        <div>Stories: <XCounter value={props.data.permit.proposedStories} oldValue={props.data.permit.existingStories} /></div>
+                        <div>Stories: <XCounter value={props.data.permit.proposedStories}
+                                                oldValue={props.data.permit.existingStories}/></div>
                     )}
                     {props.data.permit.proposedUnits &&
-                        (<div>Units: <XCounter value={props.data.permit.proposedUnits} oldValue={props.data.permit.existingUnits} /></div>)
+                    (<div>Units: <XCounter value={props.data.permit.proposedUnits}
+                                           oldValue={props.data.permit.existingUnits}/></div>)
                     }
                     <div> Description: {props.data.permit.description}</div>
                     <div> Proposed Use: {props.data.permit.proposedUse}</div>
                 </Segment>
 
                 <Segment>
-                    <ListPermits permits={props.data.permit.relatedPermits} />
+                    <ListPermits permits={props.data.permit.relatedPermits}/>
                 </Segment>
 
                 <Header attached="top">
@@ -125,7 +141,7 @@ export default withPage(withPermitQuery((props) => {
                                                 {s.fieldName}
                                             </Table.Cell>
                                             <Table.Cell>
-                                                <ChangeRender change={s} />
+                                                <ChangeRender change={s}/>
                                             </Table.Cell>
                                         </Table.Row>
                                     );
@@ -136,8 +152,8 @@ export default withPage(withPermitQuery((props) => {
                         </Table.Body>
                     </Table>
                     <Form>
-                        <Form.TextArea />
-                        <Button content="Add Comment" icon="edit" primary={true} />
+                        <Form.TextArea/>
+                        <Button content="Add Comment" icon="edit" primary={true}/>
                     </Form>
                 </Segment>
             </XContainer>
