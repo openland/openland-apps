87acedeb51a7c2e10ddc965df9753812c0718de4
Fred Morozov (iMac) <morfedor@hotmail.com>
2018-01-17 00:28:10 +0300

    Actual logotype

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,11 +18,7 @@ export class Header extends React.Component<{hero?: boolean}, { isShown: boolean
             <div className={'x-header' + (this.props.hero ? '' : ' is-top') + (this.state.isShown ? ' is-shown' : '')}>
                 <XContainer wide={true} clearfix={true}>
                     <div className="x-header--wrap">
-                        {this.props.hero && (<a className="x-header--logo" href="https://statecraft.one/">
-                                <img src="/static/img/logotype.svg" alt=""/>
-                            </a>)}
-
-                        {!this.props.hero && (<XLink className="x-header--label" path="/">San Francisco</XLink>)}
+                        <XLink className="x-header--label" path="/">San Francisco</XLink>
 
                         <a className="x-header--open visible-xs" href="#" onClick={(e) => {
                             e.preventDefault();
