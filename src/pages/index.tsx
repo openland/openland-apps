78364fd046fbe15df6f02c7cbf67eee680739d0c
Steve Kite <korshakov.stepan@gmail.com>
2018-01-17 14:24:48 -0800

    Improve wording

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -100,7 +100,7 @@ export default withLandingPage(withBuildingProjectsStats(withLoader((props) => {
                     <TrendsChart>
                         <XBarChart data={props.data.permitsUnitsFiledStats} maxY={9000} defaultColor="#FAA61F" stacked={true} />
                     </TrendsChart>
-                    <TrendsText title="2016 was a record year for new permit applications">
+                    <TrendsText title="New permit applications set record in 2016">
                         <p>However, year-to-year variation is high and the 2017 year saw significantly reduced volume of applications. To meet mayor’s goal in housing production, permit filings volume needs to stay high consistently for several years.</p>
                     </TrendsText>
                 </TrendsItem>
