2d776c625634dffad11acf3e59aed0138b545904
Steve Kite <korshakov.stepan@gmail.com>
2018-01-19 09:53:39 -0800

    Fixing header of parcels

--- src/pages/db/parcels/map.tsx
+++ src/pages/db/parcels/map.tsx
@@ -3,11 +3,13 @@ import { withPage } from '../../../components/withPage';
 import { XMap } from '../../../components/X/XMap';
 import { XMapOverlay } from '../../../components/X/XMapOverlay';
 import { withParcelsQuery } from '../../../api/Parcels';
+import { XHead } from '../../../components/X/XHead';
 
 export default withPage(withParcelsQuery((props) => {
     console.warn(props.data.parcels)
     return (
         <>
+        <XHead title={['Statecraft', 'San Francisco', 'Parcels']} />
         <div className="x-in">
             <XMap style={{ width: '100%', height: 400 }}>
                 <XMapOverlay records={props.data.parcels} />
