4a20b060ea381d1eebc1b1e8b3289eddadef6e7b
Steve Kite <korshakov.stepan@gmail.com>
2018-01-23 16:49:00 -0800

    Fixing compilation error

--- src/pages/db/parcels/map.tsx
+++ src/pages/db/parcels/map.tsx
@@ -1,26 +1,26 @@
 import * as React from 'react';
+import { XHead } from '../../../components/X/XHead';
 import { withPage } from '../../../components/withPage';
 import { XFullScreenPage } from '../../../components/X/XFullScreenPage';
 import { XMap } from '../../../components/X/XMap';
-import { XMapOverlay } from '../../../components/X/XMapOverlay';
-import { XHead } from '../../../components/X/XHead';
-import { withBlocksQuery, withParcelsQuery } from '../../../api/Parcels';
+// import { XMapOverlay } from '../../../components/X/XMapOverlay';
+// import { withBlocksQuery, withParcelsQuery } from '../../../api/Parcels';
 
-let BlocksOverlay = withBlocksQuery((props) => {
-    if (props.data.points) {
-        return (<XMapOverlay id="blocks" records={props.data.points} maxZoom={17} minZoom={14} />)
-    } else {
-        return null;
-    }
-})
+// let BlocksOverlay = withBlocksQuery((props) => {
+//     if (props.data.points) {
+//         return (<XMapOverlay id="blocks" records={props.data.points} maxZoom={17} minZoom={14} />)
+//     } else {
+//         return null;
+//     }
+// })
 
-let ParcelsOverlay = withParcelsQuery((props) => {
-    if (props.data.points) {
-        return (<XMapOverlay id="parcels" records={props.data.points} minZoom={16} />)
-    } else {
-        return null;
-    }
-})
+// let ParcelsOverlay = withParcelsQuery((props) => {
+//     if (props.data.points) {
+//         return (<XMapOverlay id="parcels" records={props.data.points} minZoom={16} />)
+//     } else {
+//         return null;
+//     }
+// })
 
 export default withPage((props) => {
     return (
