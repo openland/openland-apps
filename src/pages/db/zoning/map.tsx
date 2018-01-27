9499cfc15c6182efe2b3762271d41cec1455af4a
new_max <qwertynestor40@gmail.com>
2018-01-27 03:16:11 +0300

    container for map-legend and fix css

--- src/pages/db/zoning/map.tsx
+++ src/pages/db/zoning/map.tsx
@@ -5,36 +5,38 @@ import { XFullScreenPage } from '../../../components/X/XFullScreenPage';
 import { XMap2 } from '../../../components/X/XMap2';
 import { XCard } from '../../../components/X/XCard';
 
+export function XMapLegendContainer (props: {children: any}) {
+    return (
+        <div className="x-map--legend">
+            <XCard>
+                {props.children}
+            </XCard>
+        </div>
+    )
+}
+
+export function XMapLegendCell (props: { title: string; color: string }) {
+    return (
+        <div className={'legend-cell'}>
+            <div className={'legend-indicator'} style={{ backgroundColor: `${props.color}` }} />
+            <span className={'legend-description'}>{props.title}</span>
+        </div>
+    )
+}
+
 export default withPageFullScreen((props) => {
     return (
         <>
         <XHead title={['Statecraft', 'San Francisco', 'Zoning']} />
         <XFullScreenPage behindHeader={true}>
             <XMap2 mapStyle="mapbox://styles/steve-kite/cjcsbw6zq00dg2squfjuum14i" />
-            <div className={'x-map--legend'}>
-                <XCard>
-                    <div className={'legend-cell'}>
-                        <div className={'legend-indicator'} style={{ backgroundColor: 'hsl(336, 82%, 51%)' }} />
-                        <span className={'legend-description'}>Resident</span>
-                    </div>
-                    <div className={'legend-cell'}>
-                        <div className={'legend-indicator'} style={{ backgroundColor: 'rgb(241, 121, 215)' }} />
-                        <span className={'legend-description'}>Mixed Use</span>
-                    </div>
-                    <div className={'legend-cell'}>
-                        <div className={'legend-indicator'} style={{ backgroundColor: 'hsl(36, 79%, 52%)' }} />
-                        <span className={'legend-description'}>Industrial</span>
-                    </div>
-                    <div className={'legend-cell'}>
-                        <div className={'legend-indicator'} style={{ backgroundColor: 'hsl(220, 79%, 52%)' }} />
-                        <span className={'legend-description'}>Commercial</span>
-                    </div>
-                    <div className={'legend-cell'}>
-                        <div className={'legend-indicator'} style={{ backgroundColor: 'hsl(112, 59%, 68%)' }} />
-                        <span className={'legend-description'}>Public</span>
-                    </div>
-                </XCard>
-            </div>
+            <XMapLegendContainer>
+                <XMapLegendCell title={'Resident'} color={'hsl(336, 82%, 51%)'} />
+                <XMapLegendCell title={'Mixed Use'} color={'rgb(241, 121, 215)'} />
+                <XMapLegendCell title={'Industrial'} color={'hsl(36, 79%, 52%)'} />
+                <XMapLegendCell title={'Commercial'} color={'hsl(220, 79%, 52%)'} />
+                <XMapLegendCell title={'Public'} color={'hsl(112, 59%, 68%)'} />
+            </XMapLegendContainer>
         </XFullScreenPage>
         </>
     )
