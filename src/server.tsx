c6ed5c955ad7f77a7882cc1c86a480167424993f
Steve Kite <steve@statecraft.one>
2018-01-29 16:18:52 -0800

    Improve production host binding

--- src/server.tsx
+++ src/server.tsx
@@ -12,6 +12,7 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev: dev, dir: __dirname });
 const handle = Routes.getRequestHandler(app);
 const endpoint = process.env.API_ENDPOINT || 'http://localhost:9000';
+const bindHost = dev ? 'localhost' : '0.0.0.0';
 
 async function start() {
     await app.prepare();
@@ -29,12 +30,17 @@ async function start() {
 
     server.get('*', handle);
 
-    server.listen(port, (err: any) => {
+    server.listen(port, bindHost, (err: any) => {
         if (err) { throw err; }
         // tslint:disable
-        console.log(`> Ready on http://localhost:${port}`);
+        console.log(`> Ready on http://${bindHost}:${port}`);
         // tslint:enable
     });
 }
+
+// Hack for handling Ctrl-C
+process.on('SIGINT', function() {
+    process.exit();
+});
 
 start();
