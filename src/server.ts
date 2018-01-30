6a395ac6156cb01248ec8bd14fde21d9b0e5aa84
Steve Kite <steve@statecraft.one>
2018-01-29 16:49:12 -0800

    Added request logging

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import { redirectToHTTPS } from 'express-http-to-https';
 import { RequestHandler } from 'express';
 import * as Routes from './routes';
 import { graphiqlExpress } from 'apollo-server-express';
+import * as Morgan from 'morgan';
 
 const port = process.env.PORT ? parseInt(process.env.PORT as string, 10) : 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -17,19 +18,45 @@ const bindHost = dev ? 'localhost' : '0.0.0.0';
 async function start() {
     await app.prepare();
     const server = express();
+
+    //
+    // Enable Loggin
+    //
+    server.use(Morgan('tiny'));
+
+    //
+    // Compression
+    //
     server.use(compression() as RequestHandler);
+
+    //
+    // Redirect
+    //
     if (process.env.APP_REDIRECT_HTTPS === 'true') {
         server.use(redirectToHTTPS());
     }
 
+    //
+    // Configuration for WebApp
+    // TODO: Remove this endpoint
+    //
     server.get('/config.js', (req, res) => {
         res.send('window.server = { "endpoint": "' + endpoint + '" }');
     });
 
+    //
+    // GraphiQL Sandbox
+    //
     server.use('/sandbox', graphiqlExpress({ endpointURL: endpoint + '/api' }));
 
+    //
+    // Main Handler
+    //
     server.get('*', handle);
 
+    //
+    // Starting Server 
+    //
     server.listen(port, bindHost, (err: any) => {
         if (err) { throw err; }
         // tslint:disable
