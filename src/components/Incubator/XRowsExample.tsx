a60b64bf9aab9b3a7cff14033d52c04efb289615
new_max <qwertynestor40@gmail.com>
2018-02-09 06:52:10 +0300

    XRows example

--- src/components/Incubator/XRowsExample.tsx
+++ src/components/Incubator/XRowsExample.tsx
@@ -2,80 +2,74 @@ import * as React from 'react'
 import Glamorous from 'glamorous'
 import { XButton } from '../X/XButton';
 
-// interface XRowProps {
-//     children: any,
-//     type?: 'header' | 'footer',
-//     style?: 'inline' | 'column',
-//     justifyContent?: 'space-between' | ''
-// }
-
 interface XCardExampleProps {
     children?: any,
     headerTitle: string,
     headerDescription: string
 }
 
-export const XCardWrapper = Glamorous.div<{children: any}>({
-    display: 'flex',
-    flexDirection: 'column'
-})
-
 export const XRow = Glamorous.div<{children: any}>({
     display: 'flex',
     flexDirection: 'column',
 })
 
-export const XRowTitle = Glamorous.div<{children?: any}>({
-    display: 'flex',
-    justifyContent: 'space-between',
+export const XRowContent = Glamorous.div<{children: any; column?: boolean; spaceBetween?: boolean}>((props) => {
+    return {
+        display: 'flex',
+        alignItems: props.column ? undefined : 'center',
+        justifyContent: props.spaceBetween ? 'space-between' : undefined,
+        flexDirection: props.column ? 'column' : undefined
+    }
+})
+
+export const Title = Glamorous.span({
     fontSize: '24px',
     lineHeight: '34px',
     fontWeight: 500,
 })
 
-export const XRowText = Glamorous.div<{column?: boolean}>((props) => {
-    return {
-        display: props.column ? undefined : 'flex',
-        flexDirection: props.column ? 'column' : undefined,
-        color: '#5D677A',
-        fontSize: '17px',
-        lineHeight: '26px'
-    }
-})
-
-export const XRowContent = Glamorous.div<{children: any}>({
+export const Text = Glamorous.span({
     color: '#5D677A',
     fontSize: '17px',
-    lineHeight: '26px',
+    lineHeight: '26px'
+})
+
+// ------------
+export const XCardWrapper = Glamorous.div<{children: any}>({
     display: 'flex',
-    justifyContent: 'space-between'
+    flexDirection: 'column'
 })
 
 export function XCardExample(props: XCardExampleProps) {
     return (
         <XCardWrapper>
             <XRow>
-                <XRowTitle>
+                <Title>
                     {props.headerTitle}
-                </XRowTitle>
-                <XRowText>
-                    {props.headerDescription}
-                    <a>description inline child components</a>
-                </XRowText>
-            </XRow>
-            <XRow>
-                <XRowTitle>
-                    Header title example with component
-                    <XButton alignSelf="flex-end" style="dark">Bordered</XButton>
-                </XRowTitle>
-                <XRowText column={true}>
-                    {props.headerDescription}
-                    <a style={{display: 'block'}}>description column child components</a>
-                </XRowText>
-            </XRow>
-            <XRow>
+                </Title>
                 <XRowContent>
-                    {props.headerDescription}
+                    <Text>
+                        {props.headerDescription}
+                    </Text>
+                    <a>description inline child components</a>
+                </XRowContent>
+            </XRow>
+            <XRow>
+                <XRowContent spaceBetween={true}>
+                    <Title>Header title example with component</Title>
+                    <XButton alignSelf="flex-end" style="dark">Bordered</XButton>
+                </XRowContent>
+                <XRowContent column={true}>
+                    <Text>{props.headerDescription}</Text>
+                    <a>description column child components</a>
+                </XRowContent>
+            </XRow>
+            <XRow>
+                <XRowContent spaceBetween={true}>
+                    <XRowContent column={true}>
+                        <Text>{props.headerDescription}</Text>
+                        <a>description column child components</a>
+                    </XRowContent>
                     <div>
                         <XButton alignSelf="flex-end" style="dark">Bordered</XButton>
                         <XButton alignSelf="flex-end" style="dark">Bordered</XButton>
