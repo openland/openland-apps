ef66cd34b8a7df3790f240e1627bce605073e754
new_max <qwertynestor40@gmail.com>
2018-02-10 18:31:42 +0300

    Header child fix

--- src/components/X/XCardHeader.tsx
+++ src/components/X/XCardHeader.tsx
@@ -1,20 +1,16 @@
 import * as React from 'react';
 import Glamorous from 'glamorous';
-import { XHorizontal } from './XHorizontal'
-
-export const XHorizontalDiv = Glamorous(XHorizontal)({
-    alignItems: 'center'
-})
 
 export const XCardTitle = Glamorous.div({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
     fontSize: '24px',
     lineHeight: '34px',
     fontWeight: 500,
 })
 
 export const XCardDescription = Glamorous.div({
-    display: 'flex',
-    alignItems: 'center',
     color: '#5D677A',
     fontSize: '17px',
     lineHeight: '26px',
@@ -32,13 +28,13 @@ let XCardHeaderDiv = Glamorous.div({
 export function XCardHeader(props: { children?: any, title?: string | null, description?: string | null }) {
     return (
         <XCardHeaderDiv>
-            <XCardTitle>{props.title}</XCardTitle>
-            <XHorizontalDiv>
-                <XCardDescription>
-                    {props.description}
-                </XCardDescription>
+            <XCardTitle>
+                {props.title}
                 {props.children}
-            </XHorizontalDiv>
+            </XCardTitle>
+            <XCardDescription>
+                {props.description}
+            </XCardDescription>
         </XCardHeaderDiv>
     )
 }
