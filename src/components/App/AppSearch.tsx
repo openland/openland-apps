51a75cd69489bf7b417e0d0103a3d925000b2cb2
Steve Kite <steve@statecraft.one>
2018-02-13 17:57:18 -0800

    Added react-select

---
+++ src/components/App/AppSearch.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+import { XSelect } from '../X/XSelect';
+// import 'react-select/dist/react-select.css';
+
+export class AppSearch extends React.Component {
+    render() {
+        return (
+            <div style={{width: 400}}><XSelect /></div>
+        )
+    }
+}
