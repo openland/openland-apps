1f22178c36273440e777b53321852fa49c0e664e
new_max <qwertynestor40@gmail.com>
2018-02-27 02:27:40 +0300

    fix

--- src/components/X/XSelect.tsx
+++ src/components/X/XSelect.tsx
@@ -1,16 +1,6 @@
 import * as React from 'react';
 import Glamorous from 'glamorous';
-import * as glamor from 'glamor';
 import Select, { ReactSelectProps } from 'react-select';
-
-const SelectAnimationFadeIn = glamor.keyframes({
-    '0%': { opacity: 0 },
-    'to': { opacity: 1 }
-})
-
-const SelectAnimationSpin = glamor.keyframes({
-    'to': { transform: 'rotate(1turn)' }
-})
 
 export const XSelectDiv = Glamorous(Select)({
     '& .Select-menu > .Select-option.is-focused': {
