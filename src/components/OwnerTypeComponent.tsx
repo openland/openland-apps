23909ae35917788b4949d992884c2b630e80fc65
Steve Kite <steve@statecraft.one>
2018-03-27 05:47:02 -0700

    Fix compilation

--- src/components/OwnerTypeComponent.tsx
+++ src/components/OwnerTypeComponent.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react';
 import * as Types from '../api/Types';
 
-export function OwnerTypeComponent(props: { type: Types.OwnerTyoe }) {
+export function OwnerTypeComponent(props: { type: Types.OwnerType }) {
     return (
         <>
             {props.type === 'PRIVATE' && 'Private Owner'}
             {props.type === 'CITY' && 'City'}
             {props.type === 'MIXED' && 'Mixed City and Private ownership'}
             {props.type === 'OTHER' && 'Public Authority, State of Federal ownership'}
-            {props.type === 'EXCLUDED' && 'Mixed ownership that is exempted from taxing'}
-
+            {props.type === 'EXCLUDED' && 'Mixed ownership (Tax exempt)'}
         </>
     );
 }
