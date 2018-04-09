1d2a59fc58af87b3269f1261d137bf8c370387c3
Steve Kite <steve@statecraft.one>
2018-04-09 06:12:28 -0700

    Fixing imports

--- packages/openland-web/pages/main/prospecting/zoning.tsx
+++ packages/openland-web/pages/main/prospecting/zoning.tsx
@@ -1,18 +1,12 @@
 import '../../../globals';
 import * as React from 'react';
-import Glamorous from 'glamorous';
 import { XHead } from '../../../components/X/XHead';
 import { withApp } from '../../../components/withApp';
 import { XCard } from '../../../components/X/XCard';
 import { XButton } from '../../../components/X/XButton';
 import { AppContent } from '../../../components/App/AppContent';
-import { XLink } from '../../../components/X/XLink';
 import { OpportunitiesTable } from '../../../components/OpportunitiesTable';
 import { ProspectingNavigation } from '../../../components/ProspectingNavigation';
-
-let Link = Glamorous(XLink)({
-    color: '#3297d3',
-});
 
 export default withApp('Zoning Review', 'viewer', () => {
     return (
