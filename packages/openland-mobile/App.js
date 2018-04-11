75e499c8e043ae53b44efc575aefcb12d4beafb6
Steve Kite <steve@statecraft.one>
2018-04-11 04:26:13 -0700

    Reenabled openland-mobile

--- packages/openland-mobile/App.js
+++ packages/openland-mobile/App.js
@@ -13,19 +13,18 @@ import {
 } from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
+  ios: 'Press Cmd+R to reload6,\n' +
+    'Cmd+D or shake for dev menu222',
   android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native!2
         </Text>
         <Text style={styles.instructions}>
           To get started, edit App.js
