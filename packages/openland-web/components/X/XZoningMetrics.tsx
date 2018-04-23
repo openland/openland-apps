d0c9cebd5d5212fcf9945ac7bde2cd140cc8b719
kor_ka <gleb.putintsev@openland.com>
2018-04-24 00:05:43 +0300

    details zoning add groups

--- packages/openland-web/components/X/XZoningMetrics.tsx
+++ packages/openland-web/components/X/XZoningMetrics.tsx
@@ -9,6 +9,12 @@ export interface ZoneMetric {
     subtype: string | undefined;
     units: string | undefined;
 }
+
+class ZoneMetricValueGroup {
+    title: string;
+    names: string[];
+    metrics?: ZoneMetricValue[];
+}
 class ZoneMetricValue {
     value: string | number;
     meta: ZoneMetric;
@@ -22,15 +28,23 @@ export class ZoneData {
         this.name = zone;
     }
 
-    pick(metricsToPick: string[], showEmpty?: boolean): ZoneMetricValue[] {
-        let res: ZoneMetricValue[] = [];
+    pick(metricsToPick: ZoneMetricValueGroup[], showEmpty?: boolean): ZoneMetricValueGroup[] {
+        let res: ZoneMetricValueGroup[] = [];
 
-        for (let nameToPick of metricsToPick) {
-            for (let metric of this.data) {
-                if (metric.meta.name === nameToPick && (showEmpty === undefined || showEmpty || (metric.value != null && metric.value !== '-'))) {
-                    res.push(metric);
+        for (let group of metricsToPick) {
+            let addGroup = false;
+            for (let nameToPick of group.names) {
+                for (let metric of this.data) {
+                    if (metric.meta.name === nameToPick && (showEmpty === undefined || showEmpty || (metric.value != null && metric.value !== '-'))) {
+                        if (addGroup === false) {
+                            addGroup = true;
+                            group.metrics = [];
+                        }
+                        group.metrics!!.push(metric);
+                    }
                 }
             }
+            res.push(group);
         }
         return res;
     }
@@ -63,31 +77,46 @@ export function XZoningMetrics(props: { codes: string[] }) {
     for (let itm of items) {
         let zone = zoneData(itm);
 
-        let toPick = ['Minimum Lot Width',
-            'Minimum Lot Area',
-            'Density Factor',
-            'Side Yard: Combined',
-            'Side Yard: Each',
-            'Minimum Rear Yard',
-            'Minimum Setback',
-            'Maximum Units',
-            'Minimum Unit Size',
-            'Maximum FAR',
-            'Maximum Lot Coverage',
-            'Minimum Parking',
-            'Height: Perimiter Wall',
-            'Height: Building',
-            'Height: Base (min-max)',
-            'Minimum Open Space'];
+        let toPick = [{
+            title: '', names: ['Minimum Lot Width',
+                'Minimum Lot Area',
+                'Density Factor',
+                'Side Yard: Combined',
+                'Side Yard: Each',
+                'Minimum Rear Yard',
+                'Minimum Setback',
+                'Maximum Units',
+                'Minimum Unit Size',
+                'Maximum FAR',
+                'Maximum Lot Coverage',
+                'Minimum Parking',
+                'Minimum Open Space']
+        },
+        {
+            title: 'Height',
+            names: [
+                'Height: Perimiter Wall',
+                'Height: Building',
+                'Height: Base (min-max)']
+        }
+        ];
 
-        components.push(
-            zone && (
-                <XCard.PropertyList title={zone.name}>{
-                    zone.pick(toPick, false).map((v, i) => (
-                        <XCard.Property key={v.meta.name + v.meta.subtype + zone!!.name} title={v.meta.name + (v.meta.subtype ? ' (' + v.meta.subtype + ')' : '')}>{v.value + (v.meta.units && v.value !== '-' ? ' ' + v.meta.units : '')}</XCard.Property>))}
-                </XCard.PropertyList>
-            )
-        );
+        if (zone) {
+            let metricsComponents = [];
+
+            for (let group of zone.pick(toPick, false)) {
+                if (group.title && group.metrics!!.length > 1) {
+                    metricsComponents.push(
+                        <XCard.Property key={group.title} title={group.title}>{''}</XCard.Property>);
+                }
+                for (let v of group.metrics!!) {
+                    metricsComponents.push(
+                        <XCard.Property key={v.meta.name + v.meta.subtype + zone!!.name} title={v.meta.name + (v.meta.subtype ? ' (' + v.meta.subtype + ')' : '')}>{v.value + (v.meta.units && v.value !== '-' ? ' ' + v.meta.units : '')}</XCard.Property>);
+                }
+            }
+            components.push(<XCard.PropertyList key={zone.name} title={'Zoning: ' + zone.name}>{metricsComponents}</XCard.PropertyList>);
+
+        }
+        return components.length === 1 ? components[0] : <React.Fragment>{components}</React.Fragment>;
     }
-    return components.length === 1 ? components[0] : <React.Fragment>{components}</React.Fragment>;
 }
