ca6c565b93812476178d94f1363ac8e6ab974b70
Steve Kite <steve@statecraft.one>
2018-05-05 22:52:22 +0300

    Working on new XButton

--- packages/openland-web/pages/dev/ui/buttons.tsx
+++ packages/openland-web/pages/dev/ui/buttons.tsx
@@ -1,26 +1,47 @@
 import * as React from 'react';
 import { withApp } from '../../../components/withApp';
 import { DevDocsScaffold } from '../../../components/DevDocsScaffold';
-import { XButton } from '../../../components/X/XButton';
+import { XButton as XButton2 } from '../../../components/X/XButton';
 import { XVertical } from '../../../components/X/XVertical';
 import { XContent } from '../../../components/X/XContent';
+import { XButton } from 'openland-x/XButton';
+import { XHorizontal } from '../../../components/X/XHorizontal';
+import { XTitle } from '../../../components/X/XTitle';
 
 export default withApp('UI Framework - Buttons', 'viewer', (props) => {
     return (
         <DevDocsScaffold title="Buttons">
             <XContent>
                 <XVertical>
-                    <XButton alignSelf="flex-start">Default</XButton>
-                    <XButton alignSelf="flex-start" style="dark" disabled={true}>Bordered</XButton>
+                    <XTitle>Sizes</XTitle>
+                    <XHorizontal>
+                        <XButton size="x-large">x-large</XButton>
+                        <XButton size="large">large</XButton>
+                        <XButton>default</XButton>
+                        <XButton size="small">small</XButton>
+                        <XButton size="x-small">x-small</XButton>
+                    </XHorizontal>
+                    <XTitle>Styles</XTitle>
+                    <XHorizontal>
+                        <XButton style="primary">primary</XButton>
+                        <XButton style="danger">danger</XButton>
+                        <XButton>default</XButton>
+                        <XButton style="ghost">ghost</XButton>
+                        <XButton style="flat">flat</XButton>
+                    </XHorizontal>
 
-                    <XButton alignSelf="flex-start" style="dark" icon="launch">icon</XButton>
-                    <XButton alignSelf="flex-start" icon="close" />
+                    <XTitle>Classic</XTitle>
+                    <XButton2 alignSelf="flex-start">Default</XButton2>
+                    <XButton2 alignSelf="flex-start" style="dark" disabled={true}>Bordered</XButton2>
 
-                    <XButton alignSelf="flex-start" style="dark" size="large">Large</XButton>
-                    <XButton alignSelf="flex-start" style="dark" size="large" bounce={true} disabled={true}>Bounce</XButton>
-                    <XButton alignSelf="flex-start" style="important">Important</XButton>
-                    <XButton alignSelf="flex-start" loading={true} />
-                    <XButton alignSelf="flex-start" loading={true} disabled={true}>Loading</XButton>
+                    <XButton2 alignSelf="flex-start" style="dark" icon="launch">icon</XButton2>
+                    <XButton2 alignSelf="flex-start" icon="close" />
+
+                    <XButton2 alignSelf="flex-start" style="dark" size="large">Large</XButton2>
+                    <XButton2 alignSelf="flex-start" style="dark" size="large" bounce={true} disabled={true}>Bounce</XButton2>
+                    <XButton2 alignSelf="flex-start" style="important">Important</XButton2>
+                    <XButton2 alignSelf="flex-start" loading={true} />
+                    <XButton2 alignSelf="flex-start" loading={true} disabled={true}>Loading</XButton2>
                 </XVertical>
             </XContent>
         </DevDocsScaffold>
---
+++ packages/openland-x-utils/styleResolver.ts
@@ -0,0 +1,16 @@
+import { CSSProperties } from 'glamorous';
+
+export function styleResolver(styles: { [key: string]: CSSProperties }) {
+    return new StyleMap(styles).resolve;
+}
+
+export class StyleMap {
+    private styles: { [key: string]: CSSProperties };
+    constructor(styles: { [key: string]: CSSProperties }) {
+        this.styles = styles;
+    }
+    resolve = (key?: string) => {
+        let k = key || 'default';
+        return (this.styles[k] || this.styles.default || {}) as CSSProperties;
+    }
+}
