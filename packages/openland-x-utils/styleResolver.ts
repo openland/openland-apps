6623a3dcaea6cced399bb76f7cfc1da9c34f2eaf
Steve Kite <steve@statecraft.one>
2018-05-05 22:56:19 +0300

    Fixing build

--- packages/openland-x-utils/styleResolver.ts
+++ packages/openland-x-utils/styleResolver.ts
@@ -1,9 +1,5 @@
 import { CSSProperties } from 'glamorous';
 
-export function styleResolver(styles: { [key: string]: CSSProperties }) {
-    return new StyleMap(styles).resolve;
-}
-
 export class StyleMap {
     private styles: { [key: string]: CSSProperties };
     constructor(styles: { [key: string]: CSSProperties }) {
@@ -13,4 +9,8 @@ export class StyleMap {
         let k = key || 'default';
         return (this.styles[k] || this.styles.default || {}) as CSSProperties;
     }
+}
+
+export function styleResolver(styles: { [key: string]: CSSProperties }) {
+    return new StyleMap(styles).resolve;
 }
