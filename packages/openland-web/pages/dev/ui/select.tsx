24feca5234a86896ea0db11c3e09c3c014170228
Steve Kite <steve@statecraft.one>
2018-05-09 02:12:28 +0300

    Fixing compilation

--- packages/openland-web/pages/dev/ui/select.tsx
+++ packages/openland-web/pages/dev/ui/select.tsx
@@ -5,7 +5,6 @@ import { DevDocsScaffold } from '../../../components/DevDocsScaffold';
 import { XVertical } from 'openland-x-layout/XVertical';
 import { XContent } from 'openland-x-layout/XContent';
 import { XTitle } from 'openland-x/XTitle';
-import { XSwitcher } from 'openland-x/XSwitcher';
 
 export default withApp('UI Framework - Select', 'viewer', (props) => {
     return (
