565dbdcfa3348d2758109d07723428c36b03e414
Максим Волков <max.volkov@openland.com>
2018-05-22 20:38:10 +0300

    style fix

--- packages/openland-web/components/Incubator/MapComponents/MapCitySelect.tsx
+++ packages/openland-web/components/Incubator/MapComponents/MapCitySelect.tsx
@@ -7,7 +7,7 @@ import { XLink, XLinkProps } from 'openland-x/XLink';
 import XStyles from 'openland-x/XStyles';
 
 const MapFilterWrapper = Glamorous(XCard)<{ active: boolean }>((props) => ({
-    width: 270,
+    width: 325,
     position: 'absolute',
     top: 18,
     left: 18,
