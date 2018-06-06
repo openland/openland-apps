fb039caf33a7c879d11c07937f8a0551bb71a485
Максим Волков <max.volkov@openland.com>
2018-06-06 14:27:53 +0300

    Add Log Out button on Need_Info page

--- packages/openland-web/pages/init/need_info.page.tsx
+++ packages/openland-web/pages/init/need_info.page.tsx
@@ -1,11 +1,18 @@
 import '../../globals';
 import * as React from 'react';
+import Glamorous from 'glamorous';
 import { MessagePage } from '../../components/MessagePage';
 import { MessagePageContent } from '../../components/MessagePageContent';
 import { withAppBase } from '../../components/withAppBase';
 import { XDocumentHead } from 'openland-x-routing/XDocumentHead';
 import { XTrack } from 'openland-x-analytics/XTrack';
 import { AuthRouter } from '../../components/AuthRouter';
+import { TextGlobal } from 'openland-text/TextGlobal';
+import { XButton } from 'openland-x/XButton';
+
+const InfoText = Glamorous.div({
+    marginBottom: 15
+});
 
 export default withAppBase((props) => {
     return (
@@ -14,7 +21,8 @@ export default withAppBase((props) => {
             <XTrack event="View Need Info">
                 <MessagePage>
                     <MessagePageContent title="We need more info">
-                        To continue working with system we need more information from you
+                        <InfoText>To continue working with system we need more information from you</InfoText>
+                        <XButton path="/auth/logout" text={TextGlobal.signOut} style="primary" alignSelf="center"/>
                     </MessagePageContent>
                 </MessagePage>
             </XTrack>
