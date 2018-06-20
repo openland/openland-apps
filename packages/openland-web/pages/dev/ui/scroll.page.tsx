46eaa0a15db7bc50b650275b4015c90991ccbc23
Steve Kite <steve@statecraft.one>
2018-06-21 01:44:14 +0300

    Added scroll bar dev page

--- packages/openland-web/pages/dev/ui/scroll.page.tsx
+++ packages/openland-web/pages/dev/ui/scroll.page.tsx
@@ -1,16 +1,55 @@
 import '../../../globals';
 import * as React from 'react';
+import Glamorous from 'glamorous';
 import { withApp } from '../../../components/withApp';
 import { DevDocsScaffold } from './components/DevDocsScaffold';
 import { XContent } from 'openland-x-layout/XContent';
 import { XView } from 'openland-x-layout/XView';
+import { XScrollView } from 'openland-x/XScrollView';
+import { XVertical } from 'openland-x-layout/XVertical';
+
+const Box = Glamorous.div({
+    width: '100px',
+    height: '100px',
+    backgroundColor: '#0000ff',
+    margin: 8,
+    flexShrink: 0
+});
 
 export default withApp('UI Framework - Scroll', 'viewer', (props) => {
     return (
         <DevDocsScaffold title="Scroll">
             <XContent>
-                <XView width={200} height={500} css={{ backgroundColor: '#ff0000' }}>
-                    {}
+                <XView css={{ backgroundColor: '#ff0000', width: 200, height: 500 }}>
+                    <XScrollView width={200} height={500}>
+                        <XVertical>
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                            <Box />
+                        </XVertical>
+                    </XScrollView>
                 </XView>
             </XContent>
         </DevDocsScaffold>
--- packages/openland-x/XScrollView.tsx
+++ packages/openland-x/XScrollView.tsx
@@ -1,21 +1,22 @@
 import * as React from 'react';
 import Glamorous from 'glamorous';
+import { XFlexStyles, extractFlexProps, applyFlex } from './basics/Flex';
 // import Simplebar from 'simplebar';
 
-export interface XScrollViewProps {
+export interface XScrollViewProps extends XFlexStyles {
     className?: string;
     scroll?: 'vertical' | 'horizontal' | 'both';
 }
 
-const ScrollDiv = Glamorous.div<{ scroll: 'vertical' | 'horizontal' | 'both' }>((props) => ({
+const ScrollDiv = Glamorous.div<{ scroll: 'vertical' | 'horizontal' | 'both' } & XFlexStyles>([(props) => ({
     overflowX: props.scroll === 'horizontal' || props.scroll === 'both' ? 'scroll' : 'hidden',
     overflowY: props.scroll === 'vertical' || props.scroll === 'both' ? 'scroll' : 'hidden'
-}));
+}), applyFlex]);
 
 export class XScrollView extends React.Component<XScrollViewProps> {
     render() {
         return (
-            <ScrollDiv className={this.props.className} scroll={this.props.scroll || 'vertical'}>
+            <ScrollDiv className={this.props.className} scroll={this.props.scroll || 'vertical'} {...extractFlexProps(this.props)}>
                 {this.props.children}
             </ScrollDiv>
         );
