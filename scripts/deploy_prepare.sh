40f2f12bc48f19edf0ef1db60375a9fbb2cfb0f9
Steve Kite <steve@statecraft.one>
2018-06-27 20:27:46 +0300

    Silencing prepare

--- scripts/deploy_prepare.sh
+++ scripts/deploy_prepare.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 echo "Installing G Cloud Tools"
-curl -o /tmp/google-cloud-sdk.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-200.0.0-linux-x86_64.tar.gz
-tar -xvf /tmp/google-cloud-sdk.tar.gz -C /tmp/
-/tmp/google-cloud-sdk/install.sh -q
-source /tmp/google-cloud-sdk/path.bash.inc
+curl -o /tmp/google-cloud-sdk.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-200.0.0-linux-x86_64.tar.gz &>/dev/null
+tar -xvf /tmp/google-cloud-sdk.tar.gz -C /tmp/ &>/dev/null
+/tmp/google-cloud-sdk/install.sh -q &>/dev/null
+source /tmp/google-cloud-sdk/path.bash.inc &>/dev/null
 echo "Installing Kubernetes Tools"
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-echo $GCLOUD_API_KEYFILE | base64 --decode --ignore-garbage > ./gcloud-api-key.json
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl &>/dev/null
+chmod +x ./kubectl &>/dev/null
+echo $GCLOUD_API_KEYFILE | base64 --decode --ignore-garbage > ./gcloud-api-key.json &>/dev/null
 echo "Activate Service Account"
-gcloud auth activate-service-account --key-file gcloud-api-key.json
-gcloud --quiet config set project statecraft-188615
-gcloud --quiet config set compute/zone us-west1-a
-gcloud --quiet container clusters get-credentials statekube
+gcloud auth activate-service-account --key-file gcloud-api-key.json &>/dev/null
+gcloud --quiet config set project statecraft-188615 &>/dev/null
+gcloud --quiet config set compute/zone us-west1-a &>/dev/null
+gcloud --quiet container clusters get-credentials statekube &>/dev/null
