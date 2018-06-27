9b0c021e707b0a382f650f74ccdf0c7402b4283a
Steve Kite <steve@statecraft.one>
2018-06-27 20:36:08 +0300

    Better deployment scripts

--- scripts/deploy_staging_ci.sh
+++ scripts/deploy_staging_ci.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+source /tmp/google-cloud-sdk/path.bash.inc &>/dev/null
 ./kubectl set image deployment/openland-web-dev openland-web-dev=index.docker.io/openland/web:git-${CIRCLE_SHA1}
 ./kubectl set image deployment/openland-web-next openland-web-next=index.docker.io/openland/web:git-${CIRCLE_SHA1}
 ./kubectl rollout status deployments openland-web-next
