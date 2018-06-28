d10c254f23e35931b9fcd67754d0914b1bc3a2aa
kor_ka <gleb.putintsev@openland.com>
2018-06-28 13:11:30 +0300

    XAvatarUpload disable hover if no pic

--- packages/openland-x/basics/XAvatarUploadBasic.tsx
+++ packages/openland-x/basics/XAvatarUploadBasic.tsx
@@ -67,7 +67,7 @@ const Placeholder = Glamorous.div<{ hasImage: boolean }>((props) => ({
     zIndex: 1,
     '&:hover': {
         color: props.hasImage ? '#fff' : '#dcdee4',
-        backgroundColor: 'rgba(0, 0, 0, 0.47)'
+        backgroundColor: props.hasImage ? 'rgba(0, 0, 0, 0.47)' : '#fff' 
     }
 }));
 
