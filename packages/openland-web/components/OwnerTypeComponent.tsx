9c7005fda4dcd05a6894c21c8644fefa93443d89
Максим Волков <max.volkov@openland.com>
2018-06-28 15:59:00 +0300

    bug fix

--- packages/openland-web/components/OwnerTypeComponent.tsx
+++ packages/openland-web/components/OwnerTypeComponent.tsx
@@ -3,12 +3,12 @@ import * as Types from 'openland-api/Types';
 
 export function OwnerTypeComponent(props: { type: Types.OwnerType }) {
     return (
-        <>
+        <React.Fragment>
             {props.type === 'PRIVATE' && 'Private'}
             {props.type === 'CITY' && 'City'}
             {props.type === 'MIXED' && 'Mixed City and Private ownership'}
             {props.type === 'OTHER' && 'Public Authority, State of Federal ownership'}
             {props.type === 'EXCLUDED' && 'Mixed ownership (Tax exempt)'}
-        </>
+        </React.Fragment>
     );
-}
+} 
