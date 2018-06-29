3c1acab27c395e6989ae8d4fb83354fe40e3bd36
kor_ka <gleb.putintsev@openland.com>
2018-06-30 00:00:45 +0300

    clear x-openland-user-photo on logout

--- packages/openland-web/pages/auth/logout.page.tsx
+++ packages/openland-web/pages/auth/logout.page.tsx
@@ -3,6 +3,7 @@ import '../../globals';
 import * as React from 'react';
 import * as Cookie from 'js-cookie';
 import { withData } from '../../components/withData';
+import { canUseDOM } from 'openland-x-utils/canUseDOM';
 
 class LogoutHandler extends React.Component<{}, {}> {
 
@@ -13,7 +14,16 @@ class LogoutHandler extends React.Component<{}, {}> {
     componentDidMount() {
         Cookie.remove('x-openland-org');
         Cookie.remove('x-openland-token');
-        Cookie.remove('x-openland-user-photo');
+        if (canUseDOM) {
+            let keepDomain = Cookie.defaults.domain;
+            let keepPath = Cookie.defaults.path;
+            let host = window.location.hostname.split('.').reverse();
+            Cookie.defaults.domain = (host[1] ? host[1] + '.' : '') + host[0];
+            Cookie.defaults.path = '/';
+            Cookie.remove('x-openland-user-photo');
+            Cookie.defaults.domain = keepDomain;
+            Cookie.defaults.path = keepPath;
+        }
         window.location.href = '/';
     }
 
