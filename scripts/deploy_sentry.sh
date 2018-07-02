d4dd5f98cc58cd3755320ccbea96469f4da040c2
Steve Kite <steve@statecraft.one>
2018-07-03 01:11:38 +0300

    Trying to fix sentry commit

--- scripts/deploy_sentry.sh
+++ scripts/deploy_sentry.sh
@@ -3,5 +3,5 @@ set -e
 RELEASE_ID=${1}
 sentry-cli releases -o openland -p openland-app new $RELEASE_ID
 sentry-cli releases -o openland -p openland-app-next new $RELEASE_ID
-sentry-cli releases -o openland -p openland-app set-commits $RELEASE_ID --commit "openland/openland-apps:${REVISION}"
-sentry-cli releases -o openland -p openland-app-next set-commits $RELEASE_ID --auto "openland/openland-apps:${REVISION}"
+sentry-cli releases -o openland -p openland-app set-commits $RELEASE_ID --commit "openland/openland-apps@${REVISION}"
+sentry-cli releases -o openland -p openland-app-next set-commits $RELEASE_ID --commit "openland/openland-apps@${REVISION}"
