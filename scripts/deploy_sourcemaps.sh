f99566c93b84b21a5e8c9ee2388db61befccdf32
Steve Kite <steve@statecraft.one>
2018-07-03 00:54:09 +0300

    Disable sourcemap upload

--- scripts/deploy_sourcemaps.sh
+++ scripts/deploy_sourcemaps.sh
@@ -2,11 +2,11 @@
 set -e
 RELEASE_ID=${1}
 export SENTRY_ORG=openland
-sentry-cli releases -o openland -p openland-app new $RELEASE_ID
-sentry-cli releases -o openland -p openland-app-next new $RELEASE_ID
+# sentry-cli releases -o openland -p openland-app new $RELEASE_ID
+# sentry-cli releases -o openland -p openland-app-next new $RELEASE_ID
 
-sentry-cli releases -o openland -p openland-app files $RELEASE_ID upload-sourcemaps --url-prefix=~/ ./dist/
-sentry-cli releases -o openland -p openland-app-next files $RELEASE_ID upload-sourcemaps --url-prefix=~/ ./dist/
+# sentry-cli releases -o openland -p openland-app files $RELEASE_ID upload-sourcemaps --url-prefix=~/ ./dist/
+# sentry-cli releases -o openland -p openland-app-next files $RELEASE_ID upload-sourcemaps --url-prefix=~/ ./dist/
 
-sentry-cli releases -p openland-app files $RELEASE_ID upload-sourcemaps --url-prefix=~/.next ./dist/.next
-sentry-cli releases -o openland -p openland-app-next files $RELEASE_ID upload-sourcemaps --url-prefix=~/.next ./dist/.next
+# sentry-cli releases -p openland-app files $RELEASE_ID upload-sourcemaps --url-prefix=~/.next ./dist/.next
+# sentry-cli releases -o openland -p openland-app-next files $RELEASE_ID upload-sourcemaps --url-prefix=~/.next ./dist/.next
