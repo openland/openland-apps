41563869b687680da73e9733c02814af1a41db8c
Steve Kite <steve@statecraft.one>
2018-07-03 06:16:58 +0300

    Fix query loader

--- packages/openland-web/components/withQueryLoader.tsx
+++ packages/openland-web/components/withQueryLoader.tsx
@@ -4,11 +4,11 @@ import { XLoader } from 'openland-x/XLoader';
 export function withQueryLoader<P>(WrappedComponent: React.ComponentType<P>): React.ComponentType<P> {
     return (props: P) => {
         if ((props as any).data as any) {
-            if ((props as any).data.loading) {
+            if ((props as any).loading) {
                 return <XLoader loading={true} />;
             }
-            if ((props as any).data.error) {
-                throw (props as any).data.error;
+            if ((props as any).error) {
+                throw (props as any).error;
             }
         }
         return <WrappedComponent {...props} />;
