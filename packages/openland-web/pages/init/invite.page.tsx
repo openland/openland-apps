d79f3693118b56cc6e14f095db1dd12f35fae83f
kor_ka <gleb.putintsev@openland.com>
2018-07-09 20:01:54 +0300

    recover join redirect + invite page fire mutation

--- packages/openland-web/pages/init/invite.page.tsx
+++ packages/openland-web/pages/init/invite.page.tsx
@@ -5,16 +5,35 @@ import { withAppBase } from '../../components/withAppBase';
 import { XDocumentHead } from 'openland-x-routing/XDocumentHead';
 import { XTrack } from 'openland-x-analytics/XTrack';
 import { AuthRouter } from '../../components/AuthRouter';
-import { withInviteInfo } from '../../api/withInviteInfo';
 import { InitTexts } from './_text';
 import { XPageRedirect } from 'openland-x-routing/XPageRedirect';
+import { withInviteActivation } from '../../api/withInviteActivation';
 
-export default withAppBase('Invite', withInviteInfo((props) => {
+class ActivateInvite extends React.Component<{ mutation: any }, { complete: boolean }> {
+    constructor(props: any) {
+        super(props);
+        this.state = { complete: false };
+        this.doJoin(props.mutation);
+    }
+
+    doJoin = async (mutation: any) => {
+        await mutation({});
+        this.setState({ complete: true });
+    }
+
+    render() {
+        return (
+            this.state.complete ? < XPageRedirect path="/" /> : null
+        );
+    }
+}
+
+export default withAppBase('Invite', withInviteActivation((props) => {
     return (
         <AuthRouter>
             <XDocumentHead title={InitTexts.join.pageTitle} titleSocial={InitTexts.socialPageTitle} />
             <XTrack event="Invite">
-                <XPageRedirect path="/" />
+                <ActivateInvite mutation={props.activate} />
             </XTrack>
         </AuthRouter>
     );
