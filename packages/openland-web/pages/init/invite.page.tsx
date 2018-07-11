49f45869a5156c94b0eb898be6af608b877a3b29
kor_ka <gleb.putintsev@openland.com>
2018-07-11 18:11:54 +0300

    Global invite displu no invite correctly

--- packages/openland-web/pages/init/invite.page.tsx
+++ packages/openland-web/pages/init/invite.page.tsx
@@ -1,6 +1,7 @@
 import '../init';
 import '../../globals';
 import * as React from 'react';
+import Glamorous from 'glamorous';
 import { MessagePage } from '../../components/MessagePage';
 import { MessagePageContent } from '../../components/MessagePageContent';
 import { withAppBase } from '../../components/withAppBase';
@@ -8,29 +9,37 @@ import { XDocumentHead } from 'openland-x-routing/XDocumentHead';
 import { XTrack } from 'openland-x-analytics/XTrack';
 import { AuthRouter } from '../../components/AuthRouter';
 import { XButton } from 'openland-x/XButton';
+import { switchOrganization } from '../../utils/switchOrganization';
 import { InitTexts } from './_text';
 import { withInviteActivation } from '../../api/withInviteActivation';
-import createHistory from 'history/createBrowserHistory';
+
+const InfoText = Glamorous.div({
+    marginBottom: 15
+});
 
 export default withAppBase('Invite', withInviteActivation((props) => {
     return (
         <AuthRouter>
             <XDocumentHead title={InitTexts.invite.pageTitle} titleSocial={InitTexts.socialPageTitle} />
-            <XTrack event="Join">
+            <XTrack event="Invite">
                 <MessagePage>
-                    <MessagePageContent title={InitTexts.invite.title}>
-                        <XButton
-                            text={InitTexts.invite.joinButton}
-                            action={async () => {
-                                await props.activate({});
-                                createHistory({
-                                    forceRefresh: true
-                                }).replace('/');
-                            }}
-                            style="primary"
-                        />
-                    </MessagePageContent>
-
+                    {props.data.invite && (
+                        <MessagePageContent title={props.data.invite.title}>
+                            <XButton
+                                text={InitTexts.invite.joinButton}
+                                action={async () => {
+                                    await props.activate({});
+                                    switchOrganization(props.data.invite!!.orgId);
+                                }}
+                                style="primary"
+                            />
+                        </MessagePageContent>
+                    )}
+                    {!props.data.invite && (
+                        <MessagePageContent title="Invite">
+                            <InfoText>{InitTexts.invite.unableToFindInvite}</InfoText>
+                        </MessagePageContent>
+                    )}
                 </MessagePage>
             </XTrack>
         </AuthRouter>
