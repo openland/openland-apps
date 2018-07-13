808dcb38505dea6b3a3b9834a27a574b4ebc3b3f
Steve Kite <steve@statecraft.one>
2018-07-13 14:41:00 +0300

    Track errors

--- packages/openland-web/components/messenger/model/PushEngine.ts
+++ packages/openland-web/components/messenger/model/PushEngine.ts
@@ -1,3 +1,5 @@
+import { trackError } from 'openland-x-analytics';
+
 function urlBase64ToUint8Array(base64String: string) {
     const padding = '='.repeat((4 - base64String.length % 4) % 4);
     const base64 = (base64String + padding)
@@ -32,6 +34,7 @@ export class PushEngine {
                     console.log('Service Worker is registered');
                     console.log(registration);
                 } catch (e) {
+                    trackError(e);
                     console.warn(e);
                     return;
                 }
@@ -53,58 +56,62 @@ export class PushEngine {
     private wireSubscriptions = () => {
         if (this.serviceWorkerSupported && this.notificationsSupported && this.pushSupported) {
             (async () => {
-                let subsctiption = await this.registration!!.pushManager.getSubscription();
-                if (this.pushSettings!!.applicationKey) {
-                    // Create new subscription or reuse existing
-                    let key = urlBase64ToUint8Array(this.pushSettings!!.applicationKey!!);
-                    if (subsctiption) {
-                        if (!subsctiption.options.applicationServerKey) {
-                            // WTF?
-                            this.handleUnsubscription(subsctiption);
-                            await subsctiption.unsubscribe();
-                        } else {
-                            let existing = new Uint8Array(subsctiption.options.applicationServerKey);
-                            let isSame = true;
-                            if (existing.length !== key.length) {
-                                isSame = false;
-                            } else {
-                                for (let i = 0; i < key.length; i++) {
-                                    if (key[i] !== existing[i]) {
-                                        isSame = false;
-                                        break;
-                                    }
-                                    //
-                                }
-                            }
-                            if (isSame) {
-                                console.log('Reusing existing');
-                                this.handleSubscription(subsctiption);
-                            } else {
-                                // Resubscribe
-                                console.log('Resubscribing with new key');
+                try {
+                    let subsctiption = await this.registration!!.pushManager.getSubscription();
+                    if (this.pushSettings!!.applicationKey) {
+                        // Create new subscription or reuse existing
+                        let key = urlBase64ToUint8Array(this.pushSettings!!.applicationKey!!);
+                        if (subsctiption) {
+                            if (!subsctiption.options.applicationServerKey) {
+                                // WTF?
                                 this.handleUnsubscription(subsctiption);
                                 await subsctiption.unsubscribe();
-                                let res = await this.registration!!.pushManager.subscribe({
-                                    userVisibleOnly: true,
-                                    applicationServerKey: key
-                                });
-                                this.handleSubscription(res);
+                            } else {
+                                let existing = new Uint8Array(subsctiption.options.applicationServerKey);
+                                let isSame = true;
+                                if (existing.length !== key.length) {
+                                    isSame = false;
+                                } else {
+                                    for (let i = 0; i < key.length; i++) {
+                                        if (key[i] !== existing[i]) {
+                                            isSame = false;
+                                            break;
+                                        }
+                                        //
+                                    }
+                                }
+                                if (isSame) {
+                                    console.log('Reusing existing');
+                                    this.handleSubscription(subsctiption);
+                                } else {
+                                    // Resubscribe
+                                    console.log('Resubscribing with new key');
+                                    this.handleUnsubscription(subsctiption);
+                                    await subsctiption.unsubscribe();
+                                    let res = await this.registration!!.pushManager.subscribe({
+                                        userVisibleOnly: true,
+                                        applicationServerKey: key
+                                    });
+                                    this.handleSubscription(res);
+                                }
                             }
+                        } else {
+                            console.log('New subscription');
+                            let res = await this.registration!!.pushManager.subscribe({
+                                userVisibleOnly: true,
+                                applicationServerKey: key
+                            });
+                            this.handleSubscription(res);
                         }
                     } else {
-                        console.log('New subscription');
-                        let res = await this.registration!!.pushManager.subscribe({
-                            userVisibleOnly: true,
-                            applicationServerKey: key
-                        });
-                        this.handleSubscription(res);
+                        // Do unsubsctibe
+                        if (subsctiption) {
+                            this.handleUnsubscription(subsctiption);
+                            await subsctiption.unsubscribe();
+                        }
                     }
-                } else {
-                    // Do unsubsctibe
-                    if (subsctiption) {
-                        this.handleUnsubscription(subsctiption);
-                        await subsctiption.unsubscribe();
-                    }
+                } catch (e) {
+                    trackError(e);
                 }
             })();
         }
--- packages/openland-web/components/messenger/model/utils/Notifications.ts
+++ packages/openland-web/components/messenger/model/utils/Notifications.ts
@@ -1,4 +1,5 @@
 import { Router } from '../../../../routes';
+import { trackError } from 'openland-x-analytics';
 
 export class Notifications {
     private readonly notificationsSupported = 'Notification' in window;
@@ -11,31 +12,39 @@ export class Notifications {
 
     constructor(onGranted: () => void) {
         this.onGranted = onGranted;
-        if (this.isSupported) {
-            if (!this.isDecided) {
-                Notification.requestPermission((permission) => {
-                    if (permission === 'granted') {
-                        this.onGranted();
-                    }
-                });
-            } else if (this.isGranted) {
-                this.onGranted();
+        try {
+            if (this.isSupported) {
+                if (!this.isDecided) {
+                    Notification.requestPermission((permission) => {
+                        if (permission === 'granted') {
+                            this.onGranted();
+                        }
+                    });
+                } else if (this.isGranted) {
+                    this.onGranted();
+                }
             }
+        } catch (e) {
+            trackError(e);
         }
     }
 
     displayNotification = (title: string, body: string, path: string, image?: string) => {
         console.warn('notification');
-        if (this.isGranted) {
-            let notification = new Notification(title, {
-                body: body,
-                icon: image
-            });
-            notification.onclick = function () {
-                Router.replaceRoute(path);
-                window.focus();
-                this.close();
-            };
+        try {
+            if (this.isGranted) {
+                let notification = new Notification(title, {
+                    body: body,
+                    icon: image
+                });
+                notification.onclick = function () {
+                    Router.replaceRoute(path);
+                    window.focus();
+                    this.close();
+                };
+            }
+        } catch (e) {
+            trackError(e);
         }
     }
 }
