6ccad6a82a3892c2351e6d4f1594fb07c6138537
kor_ka <gleb.putintsev@openland.com>
2018-07-17 12:19:51 +0300

    recover intercom

--- packages/openland-web/pages/_document.page.tsx
+++ packages/openland-web/pages/_document.page.tsx
@@ -82,8 +82,8 @@ export default class StateDocument extends Document {
                     {config.sentryEndpoint && config.release && <script dangerouslySetInnerHTML={{ __html: `Raven.config('${config.sentryEndpoint}', { release: '${config.release}' }).install();` }}>{}</script>}
 
                     {/* Intercom */}
-                    {/* {config.intercomKey && <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `window.intercomSettings = { app_id: "${config.intercomKey}" };` }}>{}</script>}
-                    {config.intercomKey && <script dangerouslySetInnerHTML={{ __html: `(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/n7hi8wya';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()` }}>{}</script>} */}
+                    {config.intercomKey && <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `window.intercomSettings = { app_id: "${config.intercomKey}" };` }}>{}</script>}
+                    {config.intercomKey && <script dangerouslySetInnerHTML={{ __html: `(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/n7hi8wya';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()` }}>{}</script>}
                 </Head>
                 <body>
                     <Main />
