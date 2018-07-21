dfe506418f287479be5b741b75a24785487c4371
Steve Kite <steve@statecraft.one>
2018-07-21 09:26:23 +0300

    Fixing linking

--- packages/openland-mobile/packages/openland-mobile/pages/auth/Login.tsx
+++ packages/openland-mobile/packages/openland-mobile/pages/auth/Login.tsx
@@ -21,58 +21,64 @@ export class Login extends React.Component<NavigationInjectedProps, { loading: b
     };
 
     handlePress = async () => {
-        this.setState({ loading: true });
-        let nonce = 'asdasdasdasd'; // TODO: Better nonce
-        let res = await AuthSession.startAsync({
-            authUrl: 'https://auth.openland.com/authorize?' + qs.stringify({
-                connection: 'google-oauth2',
-                client_id: 'v3R2Rr6D4LzzcWKHf91jwKJyDnEm4L96',
-                response_type: 'token id_token',
-                scope: 'openid profile email',
-                nonce: nonce,
-                redirect_uri: AuthSession.getRedirectUrl()
-            })
-        });
-
-        if (res.type === 'success') {
-            // Yahoo
-            // console.log(JSON.stringify(res.params));
-            let idToken = res.params.id_token;
-            let accessToken = res.params.access_token;
-            var uploaded = await fetch('https://api.openland.com/v2/auth', {
-                method: 'POST',
-                headers: {
-                    'authorization': 'Bearer ' + idToken,
-                    'x-openland-access-token': accessToken
-                }
+        try {
+            this.setState({ loading: true });
+            let nonce = 'asdasdasdasd'; // TODO: Better nonce
+            let res = await AuthSession.startAsync({
+                authUrl: 'https://auth.openland.com/authorize?' + qs.stringify({
+                    connection: 'google-oauth2',
+                    client_id: 'v3R2Rr6D4LzzcWKHf91jwKJyDnEm4L96',
+                    response_type: 'token id_token',
+                    scope: 'openid profile email',
+                    nonce: nonce,
+                    redirect_uri: AuthSession.getRedirectUrl()
+                })
             });
 
-            if (uploaded.ok) {
-                let body = await uploaded.json();
-                if (body.ok) {
-                    await AsyncStorage.setItem('openland-token', body.token);
-                    let client = buildClient(body.token);
-                    saveClient(client);
-                    this.props.navigation.navigate('App');
-                    return;
+            if (res.type === 'success') {
+                // Yahoo
+                // console.log(JSON.stringify(res.params));
+                let idToken = res.params.id_token;
+                let accessToken = res.params.access_token;
+                var uploaded = await fetch('https://api.openland.com/v2/auth', {
+                    method: 'POST',
+                    headers: {
+                        'authorization': 'Bearer ' + idToken,
+                        'x-openland-access-token': accessToken
+                    }
+                });
 
+                if (uploaded.ok) {
+                    let body = await uploaded.json();
+                    if (body.ok) {
+                        await AsyncStorage.setItem('openland-token', body.token);
+                        let client = buildClient(body.token);
+                        saveClient(client);
+                        this.props.navigation.navigate('App');
+                        return;
+
+                    }
                 }
             }
+
+            // TODO: Better error
+            Alert.alert('Unable to authenticate');
+
+            this.setState({ loading: false });
+        } catch (e) {
+            Alert.alert(e.message + '\n' + JSON.stringify(e));
         }
-
-        // TODO: Better error
-        Alert.alert('Unable to authenticate');
-
-        this.setState({ loading: false });
     }
 
     render() {
+        console.log('render');
         return (
             <View style={styles.container}>
                 {this.state.loading && <ProgressViewIOS style={{ position: 'absolute' }} />}
-                <Text>Welcome to Openland!</Text>
+                <Text>Welcome to Openland4</Text>
                 <View flexDirection="column">
                     <Button title="Login with Google" onPress={this.handlePress} />
+                    <Button title="Test button" onPress={() => console.warn('Hello??!!')} />
                 </View>
             </View>
         );
