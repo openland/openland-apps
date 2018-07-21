29f98def6228945aebcca90b721fa94524ac47ab
Steve Kite <steve@statecraft.one>
2018-07-21 06:34:09 +0300

    Prepare building

--- packages/openland-mobile/packages/openland-mobile/pages/main/Messages.tsx
+++ packages/openland-mobile/packages/openland-mobile/pages/main/Messages.tsx
@@ -4,8 +4,9 @@ import { withApp } from '../../components/withApp';
 import { NavigationInjectedProps } from 'react-navigation';
 import { Query } from '../../../../node_modules/react-apollo';
 import { ChatListQuery } from 'openland-api/ChatListQuery';
+import { ZAvatar } from '../../components/ZAvatar';
 
-class DialogComponent extends React.PureComponent<{ item: any }> {
+class DialogComponent extends React.PureComponent<{ item: { id: string, title: string, photos: string[] } }> {
     handlePress = () => {
         //
     }
@@ -13,7 +14,8 @@ class DialogComponent extends React.PureComponent<{ item: any }> {
     render() {
         return (
             <TouchableHighlight onPress={this.handlePress} underlayColor="#f8f8fb">
-                <View height={44}>
+                <View height={44} flexDirection="row">
+                    <ZAvatar src={this.props.item.photos.length > 0 ? this.props.item.photos[0] : undefined} size={34} />
                     <Text>{(this.props.item as any).title}</Text>
                 </View>
             </TouchableHighlight>
@@ -40,7 +42,7 @@ class MessagesComponent extends React.Component<NavigationInjectedProps> {
                         }
                         return (
                             <FlatList
-                                data={(res.data.chats.conversations as any[]).map((v) => ({ key: v.id, title: v.title }))}
+                                data={(res.data.chats.conversations as any[]).map((v) => ({ key: v.id, title: v.title, photos: v.photos }))}
                                 renderItem={(item) => (<DialogComponent item={item.item as any} />)}
                             />
                         );
