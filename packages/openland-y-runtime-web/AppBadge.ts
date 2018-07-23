d6ef4da4ebf008181b71217481e5adcaede4c462
Steve Kite <steve@statecraft.one>
2018-07-23 11:30:01 +0300

    Move badge control to runtime

---
+++ packages/openland-y-runtime-web/AppBadge.ts
@@ -0,0 +1,48 @@
+import { AppBadgeAPi } from 'openland-y-runtime-api/AppBadgeApi';
+import { canUseDOM } from 'openland-x-utils/canUseDOM';
+import { backoff } from 'openland-y-utils/timer';
+import { AppVisibility } from './AppVisibility';
+
+class BadgeStub implements AppBadgeAPi {
+    items: Promise<any[]> | null = null;
+    private isVisible = false;
+
+    constructor() {
+        if (canUseDOM) {
+            this.isVisible = AppVisibility.isVisible;
+            AppVisibility.watch((isVisible: boolean) => {
+                this.isVisible = isVisible;
+                if (isVisible) {
+                    this.setBadge(0);
+                }
+            });
+
+            this.items = backoff(() => import('favico.js')).then((v) => {
+                let els = document.querySelectorAll('link[rel=icon]');
+                let res = [];
+                for (let i = 0; i < els.length; i++) {
+                    res.push(new (v as any)({
+                        position: 'topright',
+                        animation: 'none',
+                        element: els.item(i)
+                    }));
+                }
+                return res;
+            });
+        }
+    }
+
+    setBadge(count: number) {
+        if (this.isVisible) {
+            if (this.items) {
+                this.items!!.then((v) => {
+                    for (let i of v) {
+                        i.badge(count);
+                    }
+                });
+            }
+        }
+    }
+}
+
+export const AppBadge = new BadgeStub();
--- packages/openland-y-runtime/Badge.ts
+++
@@ -1,9 +0,0 @@
-import { BadgeAPi } from 'openland-y-runtime-api/BadgeApi';
-
-class BadgeStub implements BadgeAPi {
-    setBadge(count: number) {
-        //
-    }
-}
-
-export const Badge = new BadgeStub();
