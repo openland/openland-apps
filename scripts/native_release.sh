8f49889f9d4c3a768109543c44a34b5591b210bf
Steve Kite <steve@statecraft.one>
2018-07-25 17:30:55 +0300

    Set release description

--- scripts/native_release.sh
+++ scripts/native_release.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 IOS_VERSION=`cat ./ios/VERSION`
-./node_modules/.bin/appcenter codepush release -a ex3ndr/Openland-iOS -d Staging --update-contents-path ./build/bundle-ios -t $IOS_VERSION
+./node_modules/.bin/appcenter codepush release -a ex3ndr/Openland-iOS -d Staging --update-contents-path ./build/bundle-ios -t $IOS_VERSION -description "CI Build: $1"
