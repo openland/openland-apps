e82be91acabba2a3b1318868c98e247cfcf1ff7e
Steve Kite <steve@statecraft.one>
2018-07-27 15:16:09 +0300

    Fix android codepush releases

--- scripts/native_release.sh
+++ scripts/native_release.sh
@@ -3,4 +3,4 @@ set -e
 IOS_VERSION=`cat ./ios/VERSION`
 ./node_modules/.bin/appcenter codepush release -a ex3ndr/Openland-iOS -d Staging --update-contents-path ./build/bundle-ios -t $IOS_VERSION --description "CI Build: $1"
 ANDROID_VERSION=`cat ./android/VERSION`
-./node_modules/.bin/appcenter codepush release -a ex3ndr/Openland-Android -d Staging --update-contents-path ./build/bundle-android -t $IOS_VERSION --description "CI Build: $1"
+./node_modules/.bin/appcenter codepush release -a ex3ndr/Openland-Android -d Staging --update-contents-path ./build/bundle-android -t $ANDROID_VERSION --description "CI Build: $1"
