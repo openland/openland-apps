d78bf8a37e6ca5259dbb351426a92557bdb3fd9e
Steve Kite <steve@statecraft.one>
2018-07-31 19:11:12 +0300

    ZTouchable for old androids

--- packages/openland-mobile/components/ZTouchable.tsx
+++ packages/openland-mobile/components/ZTouchable.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { isAndroid } from '../utils/isAndroid';
-import { TouchableNativeFeedback, TouchableOpacity, View } from 'react-native';
+import { TouchableNativeFeedback, TouchableOpacity, View, Platform } from 'react-native';
 import ViewOverflow from 'react-native-view-overflow';
 
 export class ZTouchable extends React.PureComponent<{ onPress: () => void }> {
     render() {
-        if (isAndroid) {
+        if (isAndroid && Platform.Version >= 21) {
             return (
                 <TouchableNativeFeedback onPress={this.props.onPress} background={TouchableNativeFeedback.Ripple('#fff', true)} useForeground={true}>
                     <View>
