0df8b37e0257a9aa530da9cd3c5327e5fcdd9b55
Fred Morozov <morozov@fred.md>
2018-08-10 19:16:10 +0300

    Dark tooltip for Channels icons

--- packages/openland-text/TextAppBar.ts
+++ packages/openland-text/TextAppBar.ts
@@ -1,7 +1,7 @@
 export const TextAppBar = {
     items: {
         home: 'Home',
-        channel: 'Channel',
+        channel: 'Channels',
         mail: 'Messenger',
         explore: 'Explore',
         directory: 'Directory',
