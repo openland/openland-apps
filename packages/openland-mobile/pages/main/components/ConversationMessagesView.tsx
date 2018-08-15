bf66d39d60f845076330e293c5307fa4dfaab78e
Steve Kite <steve@statecraft.one>
2018-08-15 04:22:54 +0300

    Pure-ify message list rendering

---
+++ packages/openland-mobile/pages/main/components/ConversationMessagesView.tsx
@@ -0,0 +1,167 @@
+import * as React from 'react';
+import { Day, MessageGroup } from 'openland-engines/messenger/ConversationState';
+import { MessageFullFragment } from 'openland-api/Types';
+import { View, Text, ListRenderItemInfo, FlatList, Platform } from 'react-native';
+import { ZLoader } from '../../../components/ZLoader';
+import { MessageView } from 'openland-shared/MessageView';
+import { ConversationEngine } from 'openland-engines/messenger/ConversationEngine';
+import { ZSafeAreaContext } from '../../../components/layout/ZSafeAreaContext';
+
+let months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+];
+
+function dateFormat(day: number, month: number, year: number) {
+    let now = new Date();
+    let prefix = '';
+    if (now.getFullYear() !== year) {
+        prefix = year.toString() + ', ';
+    }
+    if (now.getFullYear() === year && now.getMonth() === month && now.getDate() === day) {
+        return 'Today';
+    }
+    return (prefix + months[month] + ' ' + day + 'th');
+}
+class DateSeparator extends React.PureComponent<{ day: number, month: number, year: number }> {
+    render() {
+        return (
+            <View flexDirection={'row'}>
+                <View height={1} flexGrow={1} backgroundColor="#b9c1cd" opacity={0.6} marginRight={8} marginTop={10} />
+                <Text style={{ fontSize: 12, height: 19, lineHeight: 19, textAlignVertical: 'center', color: '#99a2b0' }}>{dateFormat(this.props.day, this.props.month, this.props.year)}</Text>
+                <View height={1} flexGrow={1} backgroundColor="#b9c1cd" opacity={0.6} marginLeft={8} marginTop={10} />
+            </View>
+        );
+    }
+}
+
+type MessageListItem = {
+    type: 'footer'
+} | {
+    type: 'header'
+} | {
+    type: 'date',
+    day: number;
+    month: number;
+    year: number;
+} | {
+    type: 'message',
+    message: MessageGroup
+};
+
+function convertMessages(days: Day[]) {
+    let res: MessageListItem[] = [];
+    res.push({ type: 'header' });
+    for (let d of days) {
+        let msgs = [...d.messages];
+        msgs.reverse();
+        res.push({ 'type': 'date', day: d.day, month: d.month, year: d.year });
+        for (let g of d.messages) {
+            res.push({ 'type': 'message', message: g });
+        }
+    }
+    res.push({ type: 'footer' });
+    res.reverse();
+    return res;
+}
+
+export interface ConversationMessagesViewProps {
+    onEndReached: () => void;
+    loaded: boolean;
+    messages: Day[];
+    engine: ConversationEngine;
+    onAvatarPress?: (userId: string) => void;
+    onPhotoPress?: (message: MessageFullFragment, view?: View) => void;
+}
+
+class ConversationMessagesViewComponent extends React.PureComponent<ConversationMessagesViewProps & { topInset: number, bottomInset: number }> {
+
+    private renderItem = (src: ListRenderItemInfo<MessageListItem>) => {
+        const itm = src.item;
+        if (itm.type === 'header') {
+            return (
+                <View height={this.props.topInset + 48}>
+                    <ZLoader appearance="small" transparent={true} enabled={!this.props.loaded && this.props.messages.length !== 0} />
+                </View>
+            );
+        } else if (itm.type === 'footer') {
+            return (<View height={this.props.bottomInset} />);
+        } else if (itm.type === 'date') {
+            return (<DateSeparator day={itm.day} month={itm.month} year={itm.year} />);
+        } else if (itm.type === 'message') {
+            return (
+                <MessageView
+                    key={itm.message.key}
+                    onPhotoPress={this.props.onPhotoPress}
+                    onAvatarPress={this.props.onAvatarPress}
+                    message={itm.message}
+                    engine={this.props.engine}
+                />
+            );
+        } else {
+            throw Error('Unexpected item');
+        }
+    }
+
+    private extractKey = (itm: MessageListItem) => {
+        if (itm.type === 'header') {
+            return 'header';
+        } else if (itm.type === 'footer') {
+            return 'footer';
+        } else if (itm.type === 'date') {
+            return 'date-' + itm.day + '-' + itm.month + '-' + itm.year;
+        } else if (itm.type === 'message') {
+            return itm.message.key;
+        } else {
+            throw Error('Unexpected item');
+        }
+    }
+
+    private handleEndReached = () => {
+        this.props.onEndReached();
+    }
+
+    render() {
+        const messages = convertMessages(this.props.messages);
+        return (
+            <FlatList
+                data={messages}
+                renderItem={this.renderItem}
+                inverted={true}
+                flexBasis={0}
+                flexGrow={1}
+                onEndReachedThreshold={1}
+                onEndReached={this.handleEndReached}
+                // ref={this.listRef}
+                initialNumToRender={Platform.OS === 'android' ? 0 : undefined}
+                scrollIndicatorInsets={{
+                    bottom: this.props.topInset,
+                    top: this.props.bottomInset
+                }}
+                keyboardDismissMode="interactive"
+                removeClippedSubviews={true}
+                keyExtractor={this.extractKey}
+                extraData={this.props.bottomInset * 10000 + this.props.topInset}
+                maxToRenderPerBatch={Platform.OS === 'android' ? 3 : undefined}
+            />
+        );
+    }
+}
+
+export const ConversationMessagesView = (props: ConversationMessagesViewProps) => {
+    return (
+        <ZSafeAreaContext.Consumer>
+            {area => (<ConversationMessagesViewComponent {...props} bottomInset={area.bottom} topInset={area.top} />)}
+        </ZSafeAreaContext.Consumer>
+    );
+};
--- packages/openland-mobile/pages/main/components/ConversationView.tsx
+++ packages/openland-mobile/pages/main/components/ConversationView.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
 import { ConversationEngine, ConversationStateHandler } from 'openland-engines/messenger/ConversationEngine';
-import { ConversationState, Day, MessageGroup } from 'openland-engines/messenger/ConversationState';
-import { View, Image, Text, Dimensions, Platform, FlatList, ListRenderItemInfo } from 'react-native';
+import { ConversationState } from 'openland-engines/messenger/ConversationState';
+import { View, Image, Text, Dimensions, Platform, FlatList } from 'react-native';
 import { ZLoader } from '../../../components/ZLoader';
-import { ZAppConfig } from '../../../components/ZAppConfig';
-import { MessageView } from 'openland-shared/MessageView';
 import { MessageFullFragment } from 'openland-api/Types';
 import { ZSafeAreaContext } from '../../../components/layout/ZSafeAreaContext';
 import { ZSafeAreaView } from '../../../components/layout/ZSafeAreaView';
+import { ConversationMessagesView } from './ConversationMessagesView';
 
 export interface MessagesListProps {
     onAvatarPress: (userId: string) => void;
@@ -15,81 +14,7 @@ export interface MessagesListProps {
     engine: ConversationEngine;
 }
 
-interface MessagesSection {
-    day?: Day;
-    key: string;
-    data: MessageGroup[];
-}
-
-type MessageListItem = {
-    type: 'footer'
-} | {
-    type: 'header'
-} | {
-    type: 'date',
-    day: number;
-    month: number;
-    year: number;
-} | {
-    type: 'message',
-    message: MessageGroup
-};
-
-function convertMessages(days: Day[]) {
-    let res: MessageListItem[] = [];
-    res.push({ type: 'header' });
-    for (let d of days) {
-        let msgs = [...d.messages];
-        msgs.reverse();
-        res.push({ 'type': 'date', day: d.day, month: d.month, year: d.year });
-        for (let g of d.messages) {
-            res.push({ 'type': 'message', message: g });
-        }
-    }
-    res.push({ type: 'footer' });
-    res.reverse();
-    return res;
-}
-
-let months = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec'
-];
-
-function dateFormat(day: number, month: number, year: number) {
-    let now = new Date();
-    let prefix = '';
-    if (now.getFullYear() !== year) {
-        prefix = year.toString() + ', ';
-    }
-    if (now.getFullYear() === year && now.getMonth() === month && now.getDate() === day) {
-        return 'Today';
-    }
-    return (prefix + months[month] + ' ' + day + 'th');
-}
-class DateSeparator extends React.PureComponent<{ day: number, month: number, year: number }> {
-    render() {
-        return (
-            <View flexDirection={'row'}>
-                <View height={1} flexGrow={1} backgroundColor="#b9c1cd" opacity={0.6} marginRight={8} marginTop={10} />
-                <Text style={{ fontSize: 12, height: 19, lineHeight: 19, textAlignVertical: 'center', color: '#99a2b0' }}>{dateFormat(this.props.day, this.props.month, this.props.year)}</Text>
-                <View height={1} flexGrow={1} backgroundColor="#b9c1cd" opacity={0.6} marginLeft={8} marginTop={10} />
-            </View>
-        );
-    }
-}
-
-class ConversationViewComponent extends React.PureComponent<MessagesListProps & { bottomInset: number, topInset: number }, { loading: boolean, empty: boolean, messages: MessageListItem[], loadingHistoty?: boolean, historyFullyLoaded?: boolean }> implements ConversationStateHandler {
+class ConversationViewComponent extends React.PureComponent<MessagesListProps & { bottomInset: number, topInset: number }, { conversation: ConversationState }> implements ConversationStateHandler {
     private unmount: (() => void) | null = null;
     private unmount2: (() => void) | null = null;
     private listRef = React.createRef<FlatList<any>>();
@@ -98,25 +23,17 @@ class ConversationViewComponent extends React.PureComponent<MessagesListProps & 
         super(props);
         let initialState = props.engine.getState();
         this.state = {
-            loading: initialState.loading,
-            messages: convertMessages(initialState.messagesPrepprocessed),
-            empty: initialState.messagesPrepprocessed.length === 0
+            conversation: initialState
         };
     }
 
-    componentDidMount() {
+    componentWillMount() {
         this.unmount2 = this.props.engine.engine.mountConversation(this.props.engine.conversationId);
         this.unmount = this.props.engine.subscribe(this);
     }
 
     onConversationUpdated(state: ConversationState) {
-        this.setState({
-            loading: state.loading,
-            messages: convertMessages(state.messagesPrepprocessed),
-            empty: state.messagesPrepprocessed.length === 0,
-            loadingHistoty: state.loadingHistory,
-            historyFullyLoaded: state.historyFullyLoaded
-        });
+        this.setState({ conversation: state });
     }
 
     onMessageSend() {
@@ -137,82 +54,23 @@ class ConversationViewComponent extends React.PureComponent<MessagesListProps & 
         }
     }
 
-    renderItem = (src: ListRenderItemInfo<MessageListItem>) => {
-        const itm = src.item;
-        if (itm.type === 'header') {
-            return (
-                this.state.loadingHistoty && !this.state.historyFullyLoaded ?
-                    (
-                        <View height={this.props.topInset + 48}>
-                            <ZLoader appearance="small" transparent={true} />
-                        </View>
-                    )
-                    : <View height={this.props.topInset + 48} />
-            );
-        } else if (itm.type === 'footer') {
-            return (<View height={this.props.bottomInset} />);
-        } else if (itm.type === 'date') {
-            return (<DateSeparator day={itm.day} month={itm.month} year={itm.year} />);
-        } else if (itm.type === 'message') {
-            return (
-                <MessageView
-                    key={itm.message.key}
-                    onPhotoPress={this.props.onPhotoPress}
-                    onAvatarPress={this.props.onAvatarPress}
-                    message={itm.message}
-                    engine={this.props.engine}
-                />
-            );
-        } else {
-            throw Error('Unexpected item');
-        }
-    }
-
-    extractKey = (itm: MessageListItem) => {
-        if (itm.type === 'header') {
-            return 'header';
-        } else if (itm.type === 'footer') {
-            return 'footer';
-        } else if (itm.type === 'date') {
-            return 'date-' + itm.day + '-' + itm.month + '-' + itm.year;
-        } else if (itm.type === 'message') {
-            return itm.message.key;
-        } else {
-            throw Error('Unexpected item');
-        }
-    }
-
-    onEndReached = (info: { distanceFromEnd: number }) => {
+    onEndReached = () => {
         this.props.engine.loadBefore();
     }
 
     render() {
-        console.log(this.props.bottomInset);
         return (
             <View flexBasis={0} flexGrow={1}>
                 <Image source={require('assets/img_chat.png')} style={{ position: 'absolute', left: 0, top: 0, width: Dimensions.get('window').width, height: Dimensions.get('window').height }} resizeMode="repeat" />
-
-                <FlatList
-                    data={this.state.messages}
-                    renderItem={this.renderItem}
-                    inverted={true}
-                    flexBasis={0}
-                    flexGrow={1}
-                    onEndReachedThreshold={1}
+                <ConversationMessagesView
+                    messages={this.state.conversation.messagesPrepprocessed}
+                    loaded={this.state.conversation.historyFullyLoaded}
+                    onAvatarPress={this.props.onAvatarPress}
+                    onPhotoPress={this.props.onPhotoPress}
                     onEndReached={this.onEndReached}
-                    ref={this.listRef}
-                    initialNumToRender={Platform.OS === 'android' ? 0 : undefined}
-                    scrollIndicatorInsets={{
-                        bottom: this.props.topInset,
-                        top: this.props.bottomInset
-                    }}
-                    keyboardDismissMode="interactive"
-                    removeClippedSubviews={true}
-                    keyExtractor={this.extractKey}
-                    extraData={this.props.bottomInset * 10000 + this.props.topInset}
-                    maxToRenderPerBatch={Platform.OS === 'android' ? 3 : undefined}
+                    engine={this.props.engine}
                 />
-                {!this.state.loading && this.state.messages.length <= 2 && (
+                {!this.state.conversation.loading && this.state.conversation.messages.length === 0 && (
                     <ZSafeAreaView style={{ position: 'absolute', top: 0, right: 0, left: 0, bottom: 0, alignItems: 'center', justifyContent: 'center' }}>
                         <View style={{ width: 375, height: 375, flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
                             <Image source={require('assets/back.png')} resizeMode="stretch" style={{ position: 'absolute', top: 0, right: 0, left: 0, bottom: 0 }} />
@@ -222,7 +80,7 @@ class ConversationViewComponent extends React.PureComponent<MessagesListProps & 
                     </ZSafeAreaView>
                 )}
                 <ZSafeAreaView style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0 }} pointerEvents="none">
-                    <ZLoader transparent={true} enabled={this.state.loading} />
+                    <ZLoader transparent={true} enabled={this.state.conversation.loading} />
                 </ZSafeAreaView>
             </View>
         );
