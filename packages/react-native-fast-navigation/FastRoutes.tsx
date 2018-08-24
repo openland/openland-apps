3523dace99ba78d7cab710b4ae48aa782ba1ebcb
Steve Kite <steve@statecraft.one>
2018-08-24 16:02:52 +0300

    Fix compilation

--- packages/react-native-fast-navigation/FastRoutes.tsx
+++ packages/react-native-fast-navigation/FastRoutes.tsx
@@ -1,5 +1,20 @@
 import * as React from 'react';
 
+export class FastRoutes {
+    readonly defaultRoute: string;
+    private routes: Map<string, React.ComponentType<{}>>;
+    constructor(defaultRoute: string, map: Map<string, React.ComponentType<{}>>) {
+        this.defaultRoute = defaultRoute;
+        this.routes = map;
+    }
+    resolvePath(name: string) {
+        if (!this.routes.has(name)) {
+            throw Error('Unable to resolve path ' + name);
+        }
+        return this.routes.get(name)!!;
+    }
+}
+
 export class FastRoutesBuilder {
     private defaultRoute: string | null = null;
     private routes = new Map<string, React.ComponentType<{}>>();
@@ -14,20 +29,5 @@ export class FastRoutesBuilder {
 
     build() {
         return new FastRoutes(this.defaultRoute!, this.routes);
-    }
-}
-
-export class FastRoutes {
-    readonly defaultRoute: string;
-    private routes: Map<string, React.ComponentType<{}>>;
-    constructor(defaultRoute: string, map: Map<string, React.ComponentType<{}>>) {
-        this.defaultRoute = defaultRoute;
-        this.routes = map;
-    }
-    resolvePath(name: string) {
-        if (!this.routes.has(name)) {
-            throw Error('Unable to resolve path ' + name);
-        }
-        return this.routes.get(name)!!;
     }
 }
