4eae94d087e96613802513aa626e097b27c84397
Steve Kite <steve@statecraft.one>
2018-08-26 20:42:51 +0300

    Better messaging design

--- packages/openland-mobile/pages/main/components/async/AsyncMessageView.tsx
+++ packages/openland-mobile/pages/main/components/async/AsyncMessageView.tsx
@@ -11,17 +11,21 @@ export class AsyncMessageView extends React.PureComponent<{ message: ModelMessag
         const sender = isServerMessage(this.props.message) ? this.props.message.sender : this.props.engine.engine.user;
         const isOut = sender.id === this.props.engine.engine.user.id;
         return (
-            <ASFlex flexDirection="row">
-                <AsyncAvatar
-                    size={28}
-                    src={sender.picture}
-                    placeholderKey={sender.id}
-                    placeholderTitle={sender.name}
-                />
-                <ASFlex flexDirection="column" flexGrow={1} flexBasis={0}>
+            <ASFlex flexDirection="row" marginLeft={4} marginRight={4} alignItems="flex-end">
+                {!isOut &&
+                    <ASFlex marginBottom={7} marginRight={-4} marginLeft={4}>
+                        <AsyncAvatar
+                            size={28}
+                            src={sender.picture}
+                            placeholderKey={sender.id}
+                            placeholderTitle={sender.name}
+                        />
+                    </ASFlex>
+                }
+                <ASFlex flexDirection="column" alignItems={isOut ? 'flex-end' : 'flex-start'} flexGrow={1} flexBasis={0} marginLeft={isOut ? 50 : 0} marginRight={isOut ? 0 : 50}>
                     <AsyncBubbleView isOut={isOut} compact={true}>
                         <ASFlex marginLeft={14} marginRight={14} marginTop={7} marginBottom={8} flexDirection="column">
-                            <ASText numberOfLines={2} flexShrink={0}>{this.props.message.message}</ASText>
+                            <ASText color={isOut ? '#fff' : '#000'} lineHeight={22} fontSize={16}>{this.props.message.message}</ASText>
                         </ASFlex>
                     </AsyncBubbleView>
                 </ASFlex>
