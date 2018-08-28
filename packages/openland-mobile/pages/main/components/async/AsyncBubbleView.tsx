7070cff7b54e350eb877066e1234972daccf8a87
Steve Kite <steve@statecraft.one>
2018-08-28 19:54:25 +0300

    Improve async chat design

--- packages/openland-mobile/pages/main/components/async/AsyncBubbleView.tsx
+++ packages/openland-mobile/pages/main/components/async/AsyncBubbleView.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import { ASFlex } from 'react-native-async-view/ASFlex';
 import { Image } from 'react-native';
 
-export class AsyncBubbleView extends React.PureComponent<{ isOut: boolean, compact: boolean }> {
+export class AsyncBubbleView extends React.PureComponent<{ isOut: boolean, compact: boolean, appearance?: 'media' | 'text' }> {
     render() {
-        const isMedia = false;
+        const isMedia = this.props.appearance === 'media';
         const compact = this.props.compact;
         const image = isMedia
             ? require('assets/chat-bubble-media.png')
@@ -16,11 +16,12 @@ export class AsyncBubbleView extends React.PureComponent<{ isOut: boolean, compa
             : compact
                 ? { left: 4, right: 4, top: 2, bottom: 4 }
                 : this.props.isOut ? { left: 4, right: 8, top: 2, bottom: 4 } : { left: 8, right: 4, top: 2, bottom: 4 };
-
         return (
-            <ASFlex backgroundPatch={{ source: Image.resolveAssetSource(image).uri, top: 20, left: 29, right: 29, bottom: 20 }} flexDirection="column" alignItems="stretch">
-                <ASFlex marginTop={contentInsets.top} marginBottom={contentInsets.bottom} marginLeft={contentInsets.left} marginRight={contentInsets.right} flexDirection="column" alignItems="stretch">
-                    {this.props.children}
+            <ASFlex marginRight={isMedia ? 8 : (compact ? 4 : 0)} marginLeft={isMedia ? 8 : (compact ? 4 : 0)} flexDirection="column" alignItems="stretch">
+                <ASFlex backgroundPatch={{ source: Image.resolveAssetSource(image).uri, top: 20, left: 29, right: 29, bottom: 20 }} flexDirection="column" alignItems="stretch">
+                    <ASFlex marginTop={contentInsets.top} marginBottom={contentInsets.bottom} marginLeft={contentInsets.left} marginRight={contentInsets.right} flexDirection="column" alignItems="stretch">
+                        {this.props.children}
+                    </ASFlex>
                 </ASFlex>
             </ASFlex>
         );
--- packages/openland-mobile/pages/main/components/async/AsyncMessageView.tsx
+++ packages/openland-mobile/pages/main/components/async/AsyncMessageView.tsx
@@ -6,17 +6,72 @@ import { AsyncAvatar } from './AsyncAvatar';
 import { ConversationEngine, DataSourceMessageItem } from 'openland-engines/messenger/ConversationEngine';
 import { ASImage } from 'react-native-async-view/ASImage';
 import { formatTime } from '../../../../utils/formatTime';
+import { Platform, Dimensions } from 'react-native';
+import { layoutMedia } from 'openland-shared/utils/layoutMedia';
 
-export class AsyncMessageView extends React.PureComponent<{ message: DataSourceMessageItem, engine: ConversationEngine }> {
-
+export class AsyncMessageTextView extends React.PureComponent<{ message: DataSourceMessageItem, engine: ConversationEngine }> {
     paddedText = ' ' + '\u00A0'.repeat(13);
     paddedTextOut = ' ' + '\u00A0'.repeat(16);
+    render() {
+        return (
+            <AsyncBubbleView isOut={this.props.message.isOut} compact={false}>
+                <ASFlex marginLeft={14} marginRight={14} marginTop={6} marginBottom={9} flexDirection="column">
+                    <ASText
+                        color={this.props.message.isOut ? '#fff' : '#000'}
+                        lineHeight={21.7}
+                        letterSpacing={-0.29}
+                        fontSize={18}
+                        fontWeight="400"
+                    >
+                        {this.props.message.text}
+                        {this.props.message.isOut ? this.paddedTextOut : this.paddedText}
+                    </ASText>
+                </ASFlex>
+                <ASFlex overlay={true} alignItems="flex-end" justifyContent="flex-end" marginRight={14} marginBottom={12}>
+                    <ASFlex flexDirection="row" height={15}>
+                        <ASText backgroundColor="#f00" fontSize={13} lineHeight={15} color={this.props.message.isOut ? '#fff' : '#8a8a8f'}>{formatTime(this.props.message.date)}</ASText>
+                        {this.props.message.isOut && (
+                            <ASFlex width={18} height={13} marginLeft={2} justifyContent="center" alignItems="flex-start">
+                                {this.props.message.isSending && <ASImage source={require('assets/ic-sending.png')} width={13} height={13} />}
+                                {!this.props.message.isSending && <ASImage source={require('assets/ic-sent.png')} width={9} height={8} marginTop={2} />}
+                            </ASFlex>
+                        )}
+                    </ASFlex>
+                </ASFlex>
+            </AsyncBubbleView>
+        );
+    }
+}
+
+export class AsyncMessageImageView extends React.PureComponent<{ message: DataSourceMessageItem, engine: ConversationEngine }> {
+    render() {
+        let maxSize = Platform.select({
+            default: 400,
+            ios: Math.min(Dimensions.get('window').width - 120, 400),
+            android: Math.min(Dimensions.get('window').width - 120, 400)
+        });
+        let layout = layoutMedia(this.props.message.file!!.imageSize!!.width, this.props.message.file!!.imageSize!!.height, maxSize, maxSize);
+        let optimalSize = layoutMedia(this.props.message.file!!.imageSize!!.width, this.props.message.file!!.imageSize!!.height, 1024, 1024);
+
+        return (
+            <AsyncBubbleView isOut={this.props.message.isOut} compact={false} appearance="media">
+                <ASImage
+                    source={{ uri: 'https://ucarecdn.com/' + this.props.message.file!!.fileId + '/' }}
+                    width={layout.width}
+                    height={layout.height}
+                    borderRadius={10}
+                />
+            </AsyncBubbleView>
+        );
+    }
+}
+export class AsyncMessageView extends React.PureComponent<{ message: DataSourceMessageItem, engine: ConversationEngine }> {
 
     render() {
         return (
             <ASFlex flexDirection="row" marginLeft={4} marginRight={4} alignItems="flex-end">
                 {!this.props.message.isOut &&
-                    <ASFlex marginBottom={7} marginRight={-4} marginLeft={4}>
+                    <ASFlex marginBottom={4} marginRight={-4} marginLeft={4}>
                         <AsyncAvatar
                             size={28}
                             src={this.props.message.senderPhoto}
@@ -26,31 +81,12 @@ export class AsyncMessageView extends React.PureComponent<{ message: DataSourceM
                     </ASFlex>
                 }
                 <ASFlex flexDirection="column" alignItems={this.props.message.isOut ? 'flex-end' : 'flex-start'} flexGrow={1} flexBasis={0} marginLeft={this.props.message.isOut ? 50 : 0} marginRight={this.props.message.isOut ? 0 : 50}>
-                    <AsyncBubbleView isOut={this.props.message.isOut} compact={true}>
-                        <ASFlex marginLeft={14} marginRight={14} marginTop={6} marginBottom={9} flexDirection="column">
-                            <ASText
-                                color={this.props.message.isOut ? '#fff' : '#000'}
-                                lineHeight={21.7}
-                                letterSpacing={-0.29}
-                                fontSize={18}
-                                fontWeight="400"
-                            >
-                                {this.props.message.text}
-                                {this.props.message.isOut ? this.paddedTextOut : this.paddedText}
-                            </ASText>
-                        </ASFlex>
-                        <ASFlex overlay={true} alignItems="flex-end" justifyContent="flex-end" marginRight={14} marginBottom={12}>
-                            <ASFlex flexDirection="row" height={15}>
-                                <ASText backgroundColor="#f00" fontSize={13} lineHeight={15} color={this.props.message.isOut ? '#fff' : '#8a8a8f'}>{formatTime(this.props.message.date)}</ASText>
-                                {this.props.message.isOut && (
-                                    <ASFlex width={18} height={13} marginLeft={2} justifyContent="center" alignItems="flex-start">
-                                        {this.props.message.isSending && <ASImage source={require('assets/ic-sending.png')} width={13} height={13} />}
-                                        {!this.props.message.isSending && <ASImage source={require('assets/ic-sent.png')} width={9} height={8} marginTop={2} />}
-                                    </ASFlex>
-                                )}
-                            </ASFlex>
-                        </ASFlex>
-                    </AsyncBubbleView>
+                    {this.props.message.text && !this.props.message.file && (
+                        <AsyncMessageTextView message={this.props.message} engine={this.props.engine} />
+                    )}
+                    {this.props.message.file && this.props.message.file.isImage && (
+                        <AsyncMessageImageView message={this.props.message} engine={this.props.engine} />
+                    )}
                 </ASFlex>
             </ASFlex>
         );
