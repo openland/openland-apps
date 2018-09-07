d49d5ad7a48032128bcf841352629500fe4a1515
kor_ka <gleb.putintsev@openland.com>
2018-09-07 19:36:29 +0300

    big single emoji

--- packages/openland-web/components/messenger/components/view/content/MessageTextComponent.tsx
+++ packages/openland-web/components/messenger/components/view/content/MessageTextComponent.tsx
@@ -9,11 +9,11 @@ export interface MessageTextComponentProps {
     isService: boolean;
 }
 
-const TextWrapper = Glamorous.span<{ isService: boolean }>((props) => ({
+const TextWrapper = Glamorous.span<{ isService: boolean, big: boolean }>((props) => ({
     display: 'inline',
     whiteSpace: 'pre-wrap',
     opacity: props.isService ? 0.3 : 1,
-    fontSize: 14,
+    fontSize: props.big ? 44 : 14,
     lineHeight: 1.71,
     letterSpacing: -0.2,
     color: '#1f3449',
@@ -28,23 +28,30 @@ const TextWrapper = Glamorous.span<{ isService: boolean }>((props) => ({
 
 export class MessageTextComponent extends React.PureComponent<MessageTextComponentProps> {
     private preprocessed: Span[];
+    big = false;
     constructor(props: MessageTextComponentProps) {
         super(props);
         this.preprocessed = preprocessText(props.message);
+
+        this.big = this.props.message.length <= 3 && this.props.message.search(/(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g) !== -1;
+        this.big = this.big || (this.props.message.length < 10 && this.props.message.startsWith(':') && this.props.message.endsWith(':'));
+        console.warn(this.props.message, this.props.message.length, this.props.message.search(/(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g));
     }
     componentWillUpdate(nextProps: MessageTextComponentProps) {
         this.preprocessed = preprocessText(nextProps.message);
     }
+
     render() {
+
         let parts = this.preprocessed.map((v, i) => {
             if (v.type === 'new_line') {
                 return <br key={'br-' + i} />;
             } else if (v.type === 'link') {
                 return <XLinkExternal className="link" key={'link-' + i} href={v.link!!} content={v.text!!} showIcon={false} />;
             } else {
-                return <span key={'text-' + i}>{emojify(v.text!!, { style: { height: 18 } })}</span>;
+                return <span key={'text-' + i}>{emojify(v.text!!, { style: { height: this.big ? 44 : 18 } })}</span>;
             }
         });
-        return <TextWrapper isService={this.props.isService}>{parts}</TextWrapper>;
+        return <TextWrapper big={this.big} isService={this.props.isService}>{parts}</TextWrapper>;
     }
 }
