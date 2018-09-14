848daf48c72805d8aa96c0720fba8e59a08271be
Steve Kite <steve@statecraft.one>
2018-09-14 18:04:23 +0300

    Improve safe area calculation for XS Max

--- packages/react-native-s/SDevice.ts
+++ packages/react-native-s/SDevice.ts
@@ -4,14 +4,17 @@ import { Platform, Dimensions } from 'react-native';
 const { height: D_HEIGHT, width: D_WIDTH } = Dimensions.get('window');
 const X_WIDTH = 375;
 const X_HEIGHT = 812;
+const XSMAX_WIDTH = 414;
+const XSMAX_HEIGHT = 896;
 const isIphoneX = Platform.OS === 'ios' && D_WIDTH === X_WIDTH && D_HEIGHT === X_HEIGHT;
+const isIphoneXSMAX = Platform.OS === 'ios' && D_WIDTH === XSMAX_WIDTH && D_HEIGHT === XSMAX_HEIGHT;
 
 //
 // Library supports safe area, status bar height only for iOS since there are no reliable way to detect them on Android.
 // Eventually we will add support for safe area api for androids.
 //
-const safeAreaTop = Platform.OS === 'ios' ? (isIphoneX ? 22 : 0) : 0;
-const safeAreaBottom = Platform.OS === 'ios' ? (isIphoneX ? 34 : 0) : 0;
+const safeAreaTop = Platform.OS === 'ios' ? ((isIphoneX || isIphoneXSMAX) ? 22 : 0) : 0;
+const safeAreaBottom = Platform.OS === 'ios' ? ((isIphoneX || isIphoneXSMAX) ? 34 : 0) : 0;
 
 //
 // Sizes of UINavigationController/AppBar
