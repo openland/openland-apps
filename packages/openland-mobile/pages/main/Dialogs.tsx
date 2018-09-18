fd5bda0209a0fda6cae74dd832a8fe940f750a7a
kor_ka <gleb.putintsev@openland.com>
2018-09-18 20:18:55 +0300

    wip(mobile): chatlist search + avatar placeholder  emoji fix

--- packages/openland-mobile/pages/main/Dialogs.tsx
+++ packages/openland-mobile/pages/main/Dialogs.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { withApp } from '../../components/withApp';
 import { DialogListComponent } from './components/DialogListComponent';
 import { PageProps } from '../../components/PageProps';
-import { MobileMessengerContext } from '../../messenger/MobileMessenger';
+import { MobileMessengerContext, DialogItemViewAsync } from '../../messenger/MobileMessenger';
 import { SHeader } from 'react-native-s/SHeader';
 import { SHeaderButton } from 'react-native-s/SHeaderButton';
 import { SSearchControler } from 'react-native-s/SSearchController';
@@ -10,34 +10,38 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import { SScrollView } from 'react-native-s/SScrollView';
 import { ZQuery } from '../../components/ZQuery';
 import { ChatSearchTextQuery } from 'openland-api';
-import { SRouter } from 'react-native-s/SRouter';
 import { SRouterContext } from 'react-native-s/SRouterContext';
+import { ASView } from 'react-native-async-view/ASView';
 
 class DialogsSearch extends React.Component<{ query: string }> {
     render() {
-        if (this.props.query.trim().length > 0) {
-            return (
-                <SRouterContext.Consumer>
-                    {r => (
-                        <ZQuery query={ChatSearchTextQuery} variables={{ query: this.props.query }}>
-                            {resp => {
-                                return (
-                                    <SScrollView keyboardDismissMode="on-drag">
-                                        {resp.data.items.map((v) => (
-                                            <TouchableOpacity onPress={() => r!!.push('Conversation', { id: v.id })}>
-                                                <View marginTop={0} height={56}><Text>{v.title}</Text></View>
-                                            </TouchableOpacity>
+        return this.props.query.trim().length > 0 ? (
+            <SRouterContext.Consumer>
+                {r => (
+                    <ZQuery query={ChatSearchTextQuery} variables={{ query: this.props.query }}>
+                        {resp => {
+                            let data = resp.data.items.map(d => ({ key: d.flexibleId, title: d.title, type: d.__typename, unread: d.unreadCount }));
+                            console.warn(resp.data.items);
+
+                            return (
+                                <SScrollView keyboardDismissMode="on-drag">
+                                    <View style={{ flexDirection: 'column', width: '100%'}}>
+                                        {data.map((item) => (
+                                            <ASView style={{ height: 100 }}>
+                                                <DialogItemViewAsync item={item} onPress={() => r!!.push('Conversation', { id: item.key })} />
+                                            </ASView>
+                                            // <View><Text>{item.key}</Text></View>
                                         ))}
-                                    </SScrollView>
-                                );
-                            }}
-                        </ZQuery>
-                    )}
-                </SRouterContext.Consumer>
-            );
-        } else {
-            return null;
-        }
+                                    </View>
+
+                                </SScrollView>
+
+                            );
+                        }}
+                    </ZQuery>
+                )}
+            </SRouterContext.Consumer>
+        ) : null;
     }
 }
 
@@ -54,7 +58,7 @@ class DialogsComponent extends React.Component<PageProps> {
                 <SHeaderButton title="New" icon={require('assets/ic-new.png')} onPress={() => this.props.router.push('ComposeModal')} />
                 <SSearchControler searchRender={DialogsSearch}>
                     <MobileMessengerContext.Consumer>
-                        {engine => (<DialogListComponent engine={engine} />)}
+                        {engine => (<DialogListComponent dialogs={engine.dialogs} />)}
                     </MobileMessengerContext.Consumer>
                 </SSearchControler>
             </>
