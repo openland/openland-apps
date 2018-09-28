35c8ec33925afbe0a6d147c60ad8ce6601c79c46
Steve Kite <steve@statecraft.one>
2018-09-27 23:06:05 -0700

    Update notifications wording

--- packages/openland-mobile/pages/main/SettingsNotifications.tsx
+++ packages/openland-mobile/pages/main/SettingsNotifications.tsx
@@ -46,23 +46,25 @@ class SettingsNotifciationsComponent extends React.Component<PageProps> {
                                             }
                                         }}
                                     >
-                                        <ZListItemGroup header="On mobile">
+                                        {/* <ZListItemGroup header="Push notifications">
+                                            <ZListItem text="Alert" toggle={true} />
+                                            <ZListItem text="Include preview of messages" toggle={true} />
+                                        </ZListItemGroup> */}
+                                        <ZListItemGroup header="Send notification about">
                                             <ZListItem text="All new messages" checkmarkField={{ key: 'input.mobileNotifications', value: 'ALL' }} />
                                             <ZListItem text="Direct messages" checkmarkField={{ key: 'input.mobileNotifications', value: 'DIRECT' }} />
                                             <ZListItem text="Never notify me" checkmarkField={{ key: 'input.mobileNotifications', value: 'NONE' }} />
                                         </ZListItemGroup>
-                                        <ZListItemGroup header={null}>
-                                            <ZListItem text="Alert" toggle={true} />
-                                            <ZListItem text="Include preview of messages" toggle={true} />
-                                        </ZListItemGroup>
-                                        <ZListItemGroup header="On desktop">
+                                        <ZListItemGroup header="Desktop & Email">
                                             <ZListItem text="All new messages" checkmarkField={{ key: 'input.desktopNotifications', value: 'ALL' }} />
                                             <ZListItem text="Direct messages" checkmarkField={{ key: 'input.desktopNotifications', value: 'DIRECT' }} />
                                             <ZListItem text="Never notify me" checkmarkField={{ key: 'input.desktopNotifications', value: 'NONE' }} />
                                         </ZListItemGroup>
-                                        <ZListItemGroup header="By email" footer="When you’re busy or not online, Openland can send you email notifications about new messages. We will use jstatham@gmail.com for notifications">
+                                        <ZListItemGroup header="Email frequency" footer={'When you’re busy or not online, Openland can send you email notifications about new messages. We will use ' + resp.data.settings.primaryEmail + ' for notifications'}>
                                             <ZListItem text="Notify every 15 minutes" checkmarkField={{ key: 'input.emailFrequency', value: 'MIN_15' }} />
                                             <ZListItem text="Notify maximum once per 1 hour" checkmarkField={{ key: 'input.emailFrequency', value: 'HOUR_1' }} />
+                                            <ZListItem text="Once per 24 hours" checkmarkField={{ key: 'input.emailFrequency', value: 'HOUR_24' }} />
+                                            <ZListItem text="Once a week" checkmarkField={{ key: 'input.emailFrequency', value: 'WEEK_1' }} />
                                             <ZListItem text="Never notify me" checkmarkField={{ key: 'input.emailFrequency', value: 'NEVER' }} />
                                         </ZListItemGroup>
                                     </ZForm>
