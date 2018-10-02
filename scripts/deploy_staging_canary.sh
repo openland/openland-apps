44ddf417249e79d1eba7dd53f391419a9f9c09ac
Steve Kite <steve@statecraft.one>
2018-10-02 22:21:06 +0300

    Fix canary deployment

--- scripts/deploy_staging_canary.sh
+++ scripts/deploy_staging_canary.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-./kubectl set image deployment/openland-web-dev openland-web-next=index.docker.io/openland/web:${1}
+./kubectl set image deployment/openland-web-dev openland-web-dev=index.docker.io/openland/web:${1}
 ./kubectl rollout status deployments openland-web-dev
