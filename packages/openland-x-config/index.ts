ab53829714a2504752bdaa3e0096e2f6f12b7334
Steve Kite <steve@statecraft.one>
2018-10-03 19:09:29 +0300

    Disable uploadcare scanning

--- packages/openland-x-config/index.ts
+++ packages/openland-x-config/index.ts
@@ -3,7 +3,8 @@ import { Config } from './Config';
 export function saveConfig(config: Config) {
     let res = `window.__APP_CONFIG=${JSON.stringify(config)};`;
     if (config.uploadcareKey) {
-        res += 'UPLOADCARE_PUBLIC_KEY = \'' + config.uploadcareKey + '\';';
+        res += 'window.UPLOADCARE_PUBLIC_KEY = \'' + config.uploadcareKey + '\';';
+        res += 'window.UPLOADCARE_LIVE = false;';
     }
     return res;
 }
