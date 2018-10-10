ff36d8ecd81506e40a25cd27f69ac0e86c21cd5d
kor_ka <gleb.putintsev@openland.com>
2018-10-10 16:54:53 +0300

    mobile org male primary

--- packages/openland-mobile/pages/main/ProfileOrganization.tsx
+++ packages/openland-mobile/pages/main/ProfileOrganization.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import { withApp } from '../../components/withApp';
 import { ZQuery } from '../../components/ZQuery';
-import { OrganizationQuery } from 'openland-api';
+import { OrganizationQuery, ProfileUpdateMutation, ProfileQuery, AccountSettingsQuery } from 'openland-api';
 import { ZListItemHeader } from '../../components/ZListItemHeader';
 import { ZListItemGroup } from '../../components/ZListItemGroup';
 import { ZListItem } from '../../components/ZListItem';
 import { PageProps } from '../../components/PageProps';
 import { SScrollView } from 'react-native-s/SScrollView';
 import { SHeader } from 'react-native-s/SHeader';
+import { YQuery } from 'openland-y-graphql/YQuery';
+import { YMutation } from 'openland-y-graphql/YMutation';
+import { startLoader, stopLoader } from '../../components/ZGlobalLoader';
 
 class ProfileOrganizationComponent extends React.Component<PageProps> {
 
@@ -19,58 +22,77 @@ class ProfileOrganizationComponent extends React.Component<PageProps> {
         return (
             <>
                 <SHeader title="Info" />
-                <ZQuery query={OrganizationQuery} variables={{ organizationId: this.props.router.params.id }}>
-                    {(resp) => {
-                        return (
-                            <>
-                                <SScrollView backgroundColor={'#fff'}>
-                                    <ZListItemHeader
-                                        photo={resp.data.organization.photo}
-                                        id={resp.data.organization.id}
-                                        title={resp.data.organization.name}
-                                        subtitle="Organization"
-                                        action="Send message"
-                                        onPress={this.handleSend}
-                                    />
-                                    {(resp.data.organization.isMine || resp.data.organization.isOwner) && (
-                                        <ZListItemGroup header={null}>
-                                            <ZListItem text="Edit info" appearance="action" onPress={() => this.props.router.push('EditOrganization', { id: this.props.router.params.id })} />
-                                            <ZListItem text="Make primary" appearance="action" />
-                                            <ZListItem text="Manage members" appearance="action" />
-                                        </ZListItemGroup>
-                                    )}
-                                    <ZListItemGroup header="Information">
-                                        {resp.data.organization.about && <ZListItem multiline={true} title="about" text={resp.data.organization.about} />}
-                                        {resp.data.organization.website && <ZListItem title="website" text={resp.data.organization.website} />}
-                                        {resp.data.organization.facebook && <ZListItem title="facebook" text={resp.data.organization.facebook} />}
-                                        {resp.data.organization.twitter && <ZListItem title="twitter" text={resp.data.organization.twitter} />}
-                                    </ZListItemGroup>
-
-                                    <ZListItemGroup header="Public channels">
-                                        {resp.data.organization.channels.map((v) => (
-                                            <ZListItem
-                                                key={v!!.id}
-                                                leftAvatar={{ photo: v!!.photo, title: v!!.title, key: v!!.id }}
-                                                text={v!!.title}
-                                                onPress={() => this.props.router.push('Conversation', { flexibleId: v!!.id })}
+                <ZQuery query={AccountSettingsQuery}>
+                    {settings => (
+                        <ZQuery query={OrganizationQuery} variables={{ organizationId: this.props.router.params.id }}>
+                            {(resp) => {
+                                return (
+                                    <>
+                                        <SScrollView backgroundColor={'#fff'}>
+                                            <ZListItemHeader
+                                                photo={resp.data.organization.photo}
+                                                id={resp.data.organization.id}
+                                                title={resp.data.organization.name}
+                                                subtitle="Organization"
+                                                action="Send message"
+                                                onPress={this.handleSend}
                                             />
-                                        ))}
-                                    </ZListItemGroup>
+                                            {(resp.data.organization.isMine || resp.data.organization.isOwner) && (
+                                                <ZListItemGroup header={null}>
+                                                    <ZListItem text="Edit info" appearance="action" onPress={() => this.props.router.push('EditOrganization', { id: this.props.router.params.id })} />
+                                                    {resp.data.organization.id !== (settings.data && settings.data.primaryOrganization && settings.data.primaryOrganization.id) && <YMutation mutation={ProfileUpdateMutation} refetchQueries={[AccountSettingsQuery]}>
+                                                        {updateProfile => (
+                                                            <ZListItem
+                                                                text="Make primary"
 
-                                    <ZListItemGroup header="Members">
-                                        {resp.data.organization.members.map((v) => (
-                                            <ZListItem
-                                                key={v.user.id}
-                                                text={v.user.name}
-                                                leftAvatar={{ photo: v.user.picture, title: v.user.name, key: v.user.id }}
-                                                onPress={() => this.props.router.push('ProfileUser', { id: v.user.id })}
-                                            />
-                                        ))}
-                                    </ZListItemGroup>
-                                </SScrollView>
-                            </>
-                        );
-                    }}
+                                                                appearance="action"
+                                                                onPress={async () => {
+                                                                    startLoader();
+                                                                    await updateProfile({ variables: { input: { alphaPrimaryOrganizationId: resp.data.organization.id } } });
+                                                                    stopLoader();
+
+                                                                }}
+                                                            />
+                                                        )}
+                                                    </YMutation>}
+                                                    <ZListItem text="Manage members" appearance="action" />
+                                                </ZListItemGroup>
+                                            )}
+                                            <ZListItemGroup header="Information">
+                                                {resp.data.organization.about && <ZListItem multiline={true} title="about" text={resp.data.organization.about} />}
+                                                {resp.data.organization.website && <ZListItem title="website" text={resp.data.organization.website} />}
+                                                {resp.data.organization.facebook && <ZListItem title="facebook" text={resp.data.organization.facebook} />}
+                                                {resp.data.organization.twitter && <ZListItem title="twitter" text={resp.data.organization.twitter} />}
+                                            </ZListItemGroup>
+
+                                            <ZListItemGroup header="Public channels">
+                                                {resp.data.organization.channels.map((v) => (
+                                                    <ZListItem
+                                                        key={v!!.id}
+                                                        leftAvatar={{ photo: v!!.photo, title: v!!.title, key: v!!.id }}
+                                                        text={v!!.title}
+                                                        onPress={() => this.props.router.push('Conversation', { flexibleId: v!!.id })}
+                                                    />
+                                                ))}
+                                            </ZListItemGroup>
+
+                                            <ZListItemGroup header="Members">
+                                                {resp.data.organization.members.map((v) => (
+                                                    <ZListItem
+                                                        key={v.user.id}
+                                                        text={v.user.name}
+                                                        leftAvatar={{ photo: v.user.picture, title: v.user.name, key: v.user.id }}
+                                                        onPress={() => this.props.router.push('ProfileUser', { id: v.user.id })}
+                                                    />
+                                                ))}
+                                            </ZListItemGroup>
+                                        </SScrollView>
+                                    </>
+                                );
+                            }}
+                        </ZQuery>
+                    )}
+
                 </ZQuery>
             </>
         );
