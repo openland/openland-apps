e9413c3fbcb9294e7baba7f30aa1ca335573589c
Fred Morozov <morozov@fred.md>
2018-10-15 16:54:07 +0300

    fix position of AddMenu

--- packages/openland-web/components/Scaffold.tsx
+++ packages/openland-web/components/Scaffold.tsx
@@ -689,7 +689,7 @@ class AddMenu extends React.Component<{}, { show?: boolean }> {
                 placement="right-end"
                 show={this.state.show}
                 marginTop={5}
-                marginLeft={16}
+                marginLeft={11}
                 content={<AddListingContent />}
                 onClickOutside={this.closer}
             >
