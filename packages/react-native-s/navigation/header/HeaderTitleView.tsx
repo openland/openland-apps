10fbe2d8d306daca429f00af91d74b13cf43e31f
kor_ka <gleb.putintsev@openland.com>
2018-10-24 14:14:10 +0300

    adnroid back button

--- packages/react-native-s/navigation/header/HeaderTitleView.tsx
+++ packages/react-native-s/navigation/header/HeaderTitleView.tsx
@@ -5,13 +5,15 @@ import { SNavigationViewStyle } from '../../SNavigationView';
 import { SDevice } from '../../SDevice';
 import { StyleSheet, ViewStyle, TextStyle, View, Text } from 'react-native';
 import { SAnimated } from '../../SAnimated';
+import { SCloseButton } from 'react-native-s/SCloseButton';
+import { SBackButton } from 'react-native-s/SBackButton';
 
 const styles = StyleSheet.create({
     root: {
         position: 'absolute',
         top: SDevice.statusBarHeight + SDevice.safeArea.top,
         right: 0,
-        left: 56
+        left: 0
     } as ViewStyle,
     rootFirst: {
         paddingLeft: 16
@@ -43,6 +45,7 @@ const styles = StyleSheet.create({
 export class HeaderTitleView extends React.PureComponent<{ manager: NavigationManager, page: HeaderPage, current: boolean, style: SNavigationViewStyle }> {
     render() {
         let v = this.props.page;
+        console.warn('fooo', this.props.manager.parent, this.props.page.page.startIndex);
         return (
             <SAnimated.View name={'header--' + this.props.page.page.key} style={[styles.root, this.props.page.page.startIndex === 0 && styles.rootFirst]} pointerEvents={this.props.current ? 'box-none' : 'none'}>
                 <View style={styles.titleContainer} pointerEvents="box-none">
@@ -54,10 +57,17 @@ export class HeaderTitleView extends React.PureComponent<{ manager: NavigationMa
                             right: 0,
                         }}
                         flexDirection="row"
-                        justifyContent="space-between"
+                        flexGrow={1}
                     >
-                        <Text style={[styles.title, { color: this.props.style.accentColor }]}>{this.props.page.config.title}</Text>
-                        <View flexDirection="row" alignItems="center">
+                        <View
+                            flexDirection="row"
+                            flexGrow={1}
+                        >
+                            {(!!this.props.manager.parent && this.props.page.page.startIndex === 0) && <SCloseButton onPress={this.props.manager.pop} tintColor={this.props.style.accentColor} />}
+                            {(this.props.manager.parent || this.props.page.page.startIndex !== 0) && <SBackButton onPress={this.props.manager.pop} tintColor={this.props.style.accentColor} />}
+                            <Text style={[styles.title, { color: this.props.style.textColor }]}>{this.props.page.config.title}</Text>
+                        </View>
+                        <View flexDirection="row" alignItems="center" alignSelf="flex-end">
                             {v.config.buttons && v.config.buttons.map((b) => (<View key={'btn-' + b.id}>{b.render(this.props.style)}</View>))}
                         </View>
 
