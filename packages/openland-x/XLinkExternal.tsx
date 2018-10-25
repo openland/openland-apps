c0a64f0ac0c2bd505a913145392e61b805839561
Fred Morozov <morozov@fred.md>
2018-10-25 12:55:40 +0300

    decodeURI for links in message

--- packages/openland-x/XLinkExternal.tsx
+++ packages/openland-x/XLinkExternal.tsx
@@ -43,7 +43,7 @@ export function XLinkExternal(props: { href: string, content?: string, showIcon?
 
     return (
         <ExternalLinkDiv href={props.href} target="_blank" className={props.className}>
-            {content}
+            {decodeURI(content)}
             {props.showIcon !== false && <ExternalLinkIcon icon="launch" />}
         </ExternalLinkDiv>
     );
