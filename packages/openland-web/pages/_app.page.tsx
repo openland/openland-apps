a63d18b92e031373de5ee3002ad3170fdbf0ba47
Steve Kite <steve@statecraft.one>
2018-11-19 10:47:50 +0300

    Move Apollo logic to app.js

--- packages/openland-web/pages/_app.page.tsx
+++ packages/openland-web/pages/_app.page.tsx
@@ -6,17 +6,30 @@ import * as Sentry from '@sentry/browser';
 import { loadConfig } from 'openland-x-config';
 import { canUseDOM } from 'openland-x-utils/canUseDOM';
 import { buildConfig } from '../config';
+import { withData } from '../components/withData2';
+import { OpenApolloClient } from 'openland-y-graphql/apolloClient';
+import { YApolloProvider } from 'openland-y-graphql/YApolloProvider';
+import { RootErrorBoundary } from '../components/RootErrorBoundary';
+import moment from 'moment-timezone';
+import { getClientStorage } from 'openland-x-utils/SharedStorage';
+import { trackPage } from 'openland-x-analytics';
 
-export default class MyApp extends App {
+export default withData(class MyApp extends App<{ apollo: OpenApolloClient }> {
 
     private isSentryEnabled = false;
 
-    constructor(props: AppProps) {
+    constructor(props: { apollo: OpenApolloClient } & AppProps) {
         super(props);
         let cfg = canUseDOM ? loadConfig() : buildConfig();
         if (cfg.sentryEndpoint && cfg.release) {
             this.isSentryEnabled = true;
             Sentry.init({ dsn: cfg.sentryEndpoint, release: cfg.release });
+        }
+
+        if (canUseDOM) {
+            let tz = moment.tz.guess();
+            let storage = getClientStorage();
+            storage.writeValue('timezone', tz);
         }
     }
 
@@ -34,13 +47,22 @@ export default class MyApp extends App {
         super.componentDidCatch!!(error, errorInfo);
     }
 
+    componentDidMount() {
+        // Hack to track initial page view
+        trackPage();
+    }
+
     render() {
         const { Component, pageProps } = this.props;
 
         return (
             <Container>
-                <Component {...pageProps} />
+                <RootErrorBoundary>
+                    <YApolloProvider client={this.props.apollo}>
+                        <Component {...pageProps} />
+                    </YApolloProvider>
+                </RootErrorBoundary>
             </Container>
         );
     }
-}
+});
