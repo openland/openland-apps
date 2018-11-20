90634e80aab44c186078109afadf5d4151b6bf3a
Steve Kite <steve@statecraft.one>
2018-11-20 17:48:02 +0300

    Wat?!

--- packages/openland-web/pages/dev/ui/slider.page.tsx
+++ packages/openland-web/pages/dev/ui/slider.page.tsx
@@ -5,17 +5,13 @@ import { DevDocsScaffold } from './components/DevDocsScaffold';
 import { XVertical } from 'openland-x-layout/XVertical';
 import { XContent } from 'openland-x-layout/XContent';
 import { XTitle } from 'openland-x/XTitle';
-import { XSlider, XRange } from 'openland-x/XSlider';
 
 export default withApp('UI Framework - Sliders', 'viewer', (props) => {
     return (
         <DevDocsScaffold title="Sliders">
             <XContent>
                 <XVertical>
-                    <XTitle>Sliders</XTitle>
-                    <XSlider min={0} max={100} />
-                    <XTitle>Range</XTitle>
-                    <XRange min={0} max={100} />
+                    {}
                 </XVertical>
             </XContent>
         </DevDocsScaffold>
--- packages/openland-x/XCode.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export function XCode(props: { src: string, language: string }) {
-    return (
-        <pre>
-            {props.src}
-        </pre>
-    );
-}
