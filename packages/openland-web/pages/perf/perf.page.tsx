28a4b7193cbdf1e7f72185dcf01cc38e82138994
Steve Kite <steve@statecraft.one>
2018-11-20 16:23:53 +0300

    Linara?

--- packages/openland-web/pages/perf/perf.page.tsx
+++ packages/openland-web/pages/perf/perf.page.tsx
@@ -3,6 +3,11 @@ import { XVertical } from 'openland-x-layout/XVertical';
 import { XHorizontal } from 'openland-x-layout/XHorizontal';
 import { XButton } from 'openland-x/XButton';
 import { XRouterContext } from 'openland-x-routing/XRouterContext';
+import { css } from 'linaria';
+
+const FastButton = css`
+    width: 100px
+`;
 
 class FirstText extends React.Component {
     render() {
@@ -20,7 +25,15 @@ class FirstText extends React.Component {
 
 class SecondText extends React.Component {
     render() {
-        return null;
+        let ch: any[] = [];
+        for (let a = 0; a < 1000; a++) {
+            ch.push(<div className={FastButton} key={'b-' + a}>button</div>);
+        }
+        return (
+            <XVertical>
+                {ch}
+            </XVertical>
+        );
     }
 }
 
@@ -31,11 +44,14 @@ export default class Perf extends React.Component {
                 <XHorizontal>
                     <XButton text="First" query={{ field: 'tab' }} />
                     <XButton text="Second" query={{ field: 'tab', value: '2' }} />
+                    <XButton text="Third" query={{ field: 'tab', value: '3' }} />
                 </XHorizontal>
                 <XRouterContext.Consumer>
                     {ctx => {
                         if (ctx!.query.tab === '2') {
                             return <SecondText />;
+                        } else if (ctx!.query.tab === '3') {
+                            return null;
                         } else {
                             return <FirstText />;
                         }
