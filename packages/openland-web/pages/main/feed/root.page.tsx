031bd829ab53fda63bc85d1855b87afe325717c9
Steve Kite <steve@statecraft.one>
2018-11-25 12:38:04 +0300

    More fun with feed

--- packages/openland-web/pages/main/feed/root.page.tsx
+++ packages/openland-web/pages/main/feed/root.page.tsx
@@ -6,12 +6,13 @@ import { Scaffold } from 'openland-web/components/Scaffold';
 import { XTitle } from 'openland-x/XTitle';
 import { XButton } from 'openland-x/XButton';
 import { XLink } from 'openland-x/XLink';
+import { XSelect } from 'openland-x/XSelect';
 
 const Post = (props: { source: string, title: string, date: number, kind: 'public' | 'anonymous' | 'introduction' }) => {
     return (
         <XView borderRadius={8} padding={16} marginTop={8} marginBottom={8} level="1" alignItems="stretch" backgroundColor={props.kind === 'anonymous' ? '#abe3c5' : undefined}>
             <XTitle marginTop={8} marginBottom={24}>{props.source} {props.kind} {props.title}</XTitle>
-            <XView flexDirection="row" justifyContent="space-between"> <XButton text="Direct Message" /> <XButton text="Discuss" /> </XView>
+            <XView flexDirection="row" justifyContent="space-between"> <XButton text="Direct Message" /> <XButton text="Discuss" /> <XButton style="danger" text="Not interested" /> </XView>
         </XView>
     );
 };
@@ -23,10 +24,11 @@ export default withApp('Home', 'viewer', (props) => {
             <Scaffold>
                 <Scaffold.Content padding={false} bottomOffset={false}>
                     <XView flexDirection="row" alignItems="stretch" justifyContent="center">
-                        <XView flexDirection="column" width={200}>
-                            {/* <XLink>#hiring</XLink>
-                            <XLink>#advice</XLink>
-                            <XLink>#intro</XLink> */}
+                        <XView flexDirection="column" width={200} alignItems="center" marginTop={32}>
+                            <XTitle>Channels</XTitle>
+                            <XLink>Founders</XLink>
+                            <XLink>Developers</XLink>
+                            <XLink>Openland</XLink>
                         </XView>
                         <XView
                             flexDirection="column"
@@ -37,6 +39,11 @@ export default withApp('Home', 'viewer', (props) => {
                             margin={16}
                             maxWidth={860}
                         >
+                            <XView flexDirection="row">
+                                <XTitle marginTop={8}>All updates</XTitle>
+                                <XView flexGrow={1} />
+                                <XSelect value="all" multi={false} options={[{ value: 'all', label: 'All' }, { value: 'rejected', label: 'Rejected' }]} />
+                            </XView>
                             <Post
                                 source="founders"
                                 title="Need to hire a new CEO"
@@ -56,7 +63,7 @@ export default withApp('Home', 'viewer', (props) => {
                                 kind="introduction"
                             />
                         </XView>
-                        <XView flexDirection="column" width={200} alignItems="center" marginTop={16}>
+                        <XView flexDirection="column" width={200} alignItems="center" marginTop={32}>
                             <XTitle>Hot topics</XTitle>
                             <XLink>#hiring</XLink>
                             <XLink>#advice</XLink>
