0411badc8d7612617b012a98c9a4c6e68c0c6c25
Steve Kite <steve@statecraft.one>
2018-12-02 17:30:31 +0300

    Fix eu deployment

--- scripts/deploy_staging_eu.sh
+++ scripts/deploy_staging_eu.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-./kubectl set image deployment/openland-web-eu-next web=index.docker.io/openland/web:v${BITBUCKET_BUILD_NUMBER}
+./kubectl set image deployment/openland-web-eu-next web=index.docker.io/openland/web:${1}
 ./kubectl rollout status deployments openland-web-eu-next
