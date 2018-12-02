a48bb4dcd9feb9d4a10019a721bbee59c8e10e3b
Steve Kite <steve@statecraft.one>
2018-12-02 21:32:07 +0300

    Deploy CDN to EU also

--- scripts/deploy_cdn_app_ci.sh
+++ scripts/deploy_cdn_app_ci.sh
@@ -2,3 +2,4 @@
 set -e
 BUILD_ID=$(cat "./dist/.next/BUILD_ID")
 gsutil -m -h "Cache-Control:public, max-age=31536000" cp -r -n ./dist/.next/* "gs://cdn.openland.com/app/_next/"
+gsutil -m -h "Cache-Control:public, max-age=31536000" cp -r -n ./dist/.next/* "gs://cdn-eu.openland.com/app/_next/"
