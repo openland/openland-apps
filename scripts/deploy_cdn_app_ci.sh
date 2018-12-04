f667ca4713379166cba1f1f1c351cc582d4dd1ff
Steve Kite <steve@statecraft.one>
2018-12-04 06:37:31 +0300

    Fix CDN deployment

--- scripts/deploy_cdn_app_ci.sh
+++ scripts/deploy_cdn_app_ci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 BUILD_ID=$(cat "./dist/.next/BUILD_ID")
-gsutil -m -h "Cache-Control:public, max-age=31536000" cp -r -n ./dist/.next/* "gs://cdn.openland.com/app/_next/"
-gsutil -m -h "Cache-Control:public, max-age=31536000" cp -r -n ./dist/.next/* "gs://cdn-eu.openland.com/app/_next/"
+gsutil -m -h "Cache-Control:public, max-age=31536000" cp -r ./dist/.next/* "gs://cdn.openland.com/app/_next/"
+gsutil -m -h "Cache-Control:public, max-age=31536000" cp -r ./dist/.next/* "gs://cdn-eu.openland.com/app/_next/"
