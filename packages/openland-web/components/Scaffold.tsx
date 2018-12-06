dbec372ad2f40f79af877310c5db47869e38ee0e
kor_ka <gleb.putintsev@openland.com>
2018-12-06 14:24:20 +0300

    scaffold add stats

--- packages/openland-web/components/Scaffold.tsx
+++ packages/openland-web/components/Scaffold.tsx
@@ -354,60 +354,53 @@ class UserPopper extends React.Component<UserPopperProps, { show: boolean }> {
                                         </XVertical>
                                     </TitleContainer>
 
-                                    {organizations &&
-                                        organizations.length > 1 && (
-                                            <XPopper
-                                                placement="right"
-                                                contentContainer={
-                                                    <XMenuVertical />
-                                                }
-                                                showOnHover={true}
-                                                padding={25}
-                                                marginLeft={8}
-                                                marginBottom={5}
-                                                arrow={null}
-                                                content={
-                                                    <OutherOrgWrapepr
-                                                        separator="none"
-                                                        ref={this.onInner}
-                                                        maxHeight="90vh"
-                                                    >
-                                                        {organizations
-                                                            .sort((a, b) =>
-                                                                a.name.localeCompare(
-                                                                    b.name,
-                                                                ),
-                                                            )
-                                                            .map(
-                                                                (org, index) =>
-                                                                    index >=
-                                                                    0 ? (
-                                                                        <XMenuItem
-                                                                            path={
-                                                                                (org.isCommunity
-                                                                                    ? '/directory/c/'
-                                                                                    : '/directory/o/') +
-                                                                                org.id
-                                                                            }
-                                                                            key={
-                                                                                'other-' +
-                                                                                org.id
-                                                                            }
-                                                                        >
-                                                                            {
-                                                                                org.name
-                                                                            }
-                                                                        </XMenuItem>
-                                                                    ) : null,
-                                                            )}
-                                                    </OutherOrgWrapepr>
-                                                }
-                                            >
-                                                <XMenuItem iconRight="x-right">
-                                                    Other organizations
-                                                </XMenuItem>
-                                            </XPopper>
-                                        )}
+                                    {organizations && organizations.length > 1 && (
+                                        <XPopper
+                                            placement="right"
+                                            contentContainer={<XMenuVertical />}
+                                            showOnHover={true}
+                                            padding={25}
+                                            marginLeft={8}
+                                            marginBottom={5}
+                                            arrow={null}
+                                            content={
+                                                <OutherOrgWrapepr
+                                                    separator="none"
+                                                    ref={this.onInner}
+                                                    maxHeight="90vh"
+                                                >
+                                                    {organizations
+                                                        .sort((a, b) =>
+                                                            a.name.localeCompare(
+                                                                b.name,
+                                                            ),
+                                                        )
+                                                        .map((org, index) =>
+                                                            index >= 0 ? (
+                                                                <XMenuItem
+                                                                    path={
+                                                                        (org.isCommunity
+                                                                            ? '/directory/c/'
+                                                                            : '/directory/o/') +
+                                                                        org.id
+                                                                    }
+                                                                    key={
+                                                                        'other-' +
+                                                                        org.id
+                                                                    }
+                                                                >
+                                                                    {org.name}
+                                                                </XMenuItem>
+                                                            ) : null,
+                                                        )}
+                                                </OutherOrgWrapepr>
+                                            }
+                                        >
+                                            <XMenuItem iconRight="x-right">
+                                                Other organizations
+                                            </XMenuItem>
+                                        </XPopper>
+                                    )}
                                 </>
                             )}
                         </XVertical>
@@ -614,6 +607,9 @@ class AdminMenu extends React.Component<{}, { show?: boolean }> {
                     <XMenuItem path="/ui">
                         {TextAppBar.items.xFramework}
                     </XMenuItem>
+                    <XMenuItem href="https://logs.openland.io/app/kibana#/dashboard/3ca91120-f946-11e8-aa74-4b89079261c0?_g=(filters%3A!(('%24state'%3A(store%3AglobalState)%2Cmeta%3A(alias%3A'test%20acccs%20(hl)'%2Cdisabled%3A!f%2Cindex%3Ab3bf80a0-db87-11e8-9410-7338f67f9eca%2Ckey%3Abody.uid%2Cnegate%3A!t%2Cparams%3A!('4'%2C'101'%2C'21'%2C'1002'%2C'31'%2C'40')%2Ctype%3Aphrases%2Cvalue%3A'4%2C%20101%2C%2021%2C%201%2C002%2C%2031%2C%2040')%2Cquery%3A(bool%3A(minimum_should_match%3A1%2Cshould%3A!((match_phrase%3A(body.uid%3A'4'))%2C(match_phrase%3A(body.uid%3A'101'))%2C(match_phrase%3A(body.uid%3A'21'))%2C(match_phrase%3A(body.uid%3A'1002'))%2C(match_phrase%3A(body.uid%3A'31'))%2C(match_phrase%3A(body.uid%3A'40'))))))%2C('%24state'%3A(store%3AglobalState)%2Cmeta%3A(alias%3A'test%20accs%20(msgs)'%2Cdisabled%3A!f%2Cindex%3Ac91953d0-f6f9-11e8-aa74-4b89079261c0%2Ckey%3Auid%2Cnegate%3A!t%2Cparams%3A!('5'%2C'101'%2C'21'%2C'1002'%2C'31'%2C'40')%2Ctype%3Aphrases%2Cvalue%3A'5%2C%20101%2C%2021%2C%201%2C002%2C%2031%2C%2040')%2Cquery%3A(bool%3A(minimum_should_match%3A1%2Cshould%3A!((match_phrase%3A(uid%3A'5'))%2C(match_phrase%3A(uid%3A'101'))%2C(match_phrase%3A(uid%3A'21'))%2C(match_phrase%3A(uid%3A'1002'))%2C(match_phrase%3A(uid%3A'31'))%2C(match_phrase%3A(uid%3A'40')))))))%2CrefreshInterval%3A(display%3AOff%2Cpause%3A!f%2Cvalue%3A0)%2Ctime%3A(from%3Anow-30d%2Cmode%3Aquick%2Cto%3Anow))">
+                        {TextAppBar.items.stats}
+                    </XMenuItem>
                 </>
             );
         });
@@ -650,13 +646,12 @@ export const RoomButton = withNotificationCounter(props => {
         >
             <NavigatorItem path="/channel" activateForSubpaths={true}>
                 <RoomIcon />
-                {props.data.counter &&
-                    props.data.counter.unreadCount > 0 && (
-                        <XCounter
-                            borderColor="#f5f7f9"
-                            count={props.data.counter.unreadCount}
-                        />
-                    )}
+                {props.data.counter && props.data.counter.unreadCount > 0 && (
+                    <XCounter
+                        borderColor="#f5f7f9"
+                        count={props.data.counter.unreadCount}
+                    />
+                )}
             </NavigatorItem>
         </XPopper>
     );
@@ -675,13 +670,12 @@ export const MessengerButton = withNotificationCounter(props => {
         >
             <NavigatorItem path="/mail" activateForSubpaths={true}>
                 <MessagesIcon />
-                {props.data.counter &&
-                    props.data.counter.unreadCount > 0 && (
-                        <XCounter
-                            borderColor="#ececec"
-                            count={props.data.counter.unreadCount}
-                        />
-                    )}
+                {props.data.counter && props.data.counter.unreadCount > 0 && (
+                    <XCounter
+                        borderColor="#ececec"
+                        count={props.data.counter.unreadCount}
+                    />
+                )}
             </NavigatorItem>
         </XPopper>
     );
