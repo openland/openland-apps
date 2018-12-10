5dfe8f1658c7fe42db22ae5ca265626d2383442c
Sergey Lapin <lapanoid@gmail.com>
2018-12-11 01:24:01 +0300

    propagate form submited state thought context and show error whe user submit form

--- packages/openland-x-forms/XFormContext.ts
+++ packages/openland-x-forms/XFormContext.ts
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { XStoreState } from 'openland-y-store/XStoreState';
 
 export interface XFormContextValue {
+    submited?: boolean;
     store: XStoreState;
     submit: (action?: (data: any) => any) => any;
     validated: any;
