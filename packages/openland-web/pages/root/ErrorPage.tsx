bcb80660c6b8df312e06ea3c3c0b2818d2cd4fb7
Fred Morozov <morozov@fred.md>
2018-12-18 20:10:36 +0200

    refactoring(web): Refactoring packages/openland-web/pages/root/ErrorPage.tsx

--- packages/openland-web/pages/root/ErrorPage.tsx
+++ packages/openland-web/pages/root/ErrorPage.tsx
@@ -1,143 +1,87 @@
 import * as React from 'react';
-import Glamorous from 'glamorous';
 import { XDocumentHead } from 'openland-x-routing/XDocumentHead';
 import { XTrack } from 'openland-x-analytics/XTrack';
 import { XButton } from 'openland-x/XButton';
-import { XHorizontal } from 'openland-x-layout/XHorizontal';
+import { XView } from 'react-mental';
+import { css } from 'linaria';
 
-const Root = Glamorous.div({
-    display: 'flex',
-    height: '100vh',
-    width: '100%',
-    background: '#ffffff',
-    position: 'relative',
-    flexDirection: 'column',
-});
+const Description = css`
+    text-align: center;
+    font-size: 14px;
+    line-height: 22px;
+    color: rgba(0, 0, 0, 0.6);
+    margin-bottom: 28px;
 
-const HeaderWrapper = Glamorous.div({
-    padding: '19px 32px',
-});
+    a {
+        color: #1790ff;
 
-const HeaderLogo = Glamorous.div({
-    width: 145,
-    height: 42,
-    background: 'url(/static/X/signup/logo-2.svg) no-repeat',
-    backgroundSize: '100% 100%',
-});
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+`;
 
-const Box = Glamorous.div({
-    flex: 1,
-    display: 'flex',
-    alignItems: 'center',
-});
-
-const Inner = Glamorous.div({
-    position: 'relative',
-    flex: 1,
-});
-
-const Image = Glamorous.div({
-    margin: '-9px auto 53px',
-    width: 346,
-    height: 222,
-    background: 'url(/static/X/illustration-error.png) no-repeat',
-    backgroundImage:
-        '-webkit-image-set(url(/static/X/illustration-error.png) 1x, url(/static/X/illustration-error@2x.png) 2x)',
-    backgroundSize: '100% auto',
-    backgroundPosition: 'center bottom',
-});
-
-const Title = Glamorous.div({
-    textAlign: 'center',
-    fontSize: 24,
-    fontWeight: 400,
-    lineHeight: '29px',
-    letterSpacing: 0,
-    color: '#000000',
-    marginBottom: 10,
-});
-
-const Description = Glamorous.div({
-    textAlign: 'center',
-    fontSize: 14,
-    fontWeight: 400,
-    lineHeight: '22px',
-    letterSpacing: 0,
-    color: 'rgba(0, 0, 0, 0.6)',
-    marginBottom: 28,
-
-    '& a': {
-        color: '#1790ff',
-
-        '&:hover': {
-            textDecoration: 'underline',
-        },
-    },
-});
-
-const ButtonsWrapper = Glamorous(XHorizontal)({
-    width: 150,
-    margin: '0 auto',
-});
-
-const Button = Glamorous(XButton)({
-    flex: 1,
-});
-
-const Copyrights = Glamorous.div({
-    padding: '14px 0',
-    textAlign: 'center',
-    fontSize: 14,
-    fontWeight: 400,
-    lineHeight: '24px',
-    letterSpacing: 0,
-    color: 'rgba(0, 0, 0, 0.4)',
-});
-
-export class ErrorPage extends React.Component<{
+interface ErrorPageProps {
     statusCode: number | null | undefined;
     message?: string;
-}> {
-    render() {
-        return (
-            <>
-                <XDocumentHead
-                    title={this.props.statusCode === 404 ? ['Not found'] : ['Something went wrong']}
-                />
-                <XTrack event="View 404">
-                    <Root>
-                        <HeaderWrapper>
-                            <HeaderLogo />
-                        </HeaderWrapper>
-                        <Box>
-                            <Inner>
-                                <Image />
-                                <Title>
-                                    {this.props.statusCode === 404
-                                        ? 'Not found'
-                                        : this.props.message || 'Something went wrong'}
-                                </Title>
-                                <Description>
-                                    Return home or contact our team at{' '}
-                                    <a href="mailto:hello@openland.com">hello@openland.com</a>
-                                </Description>
-                                <ButtonsWrapper separator={12}>
-                                    <Button
-                                        style="primary"
-                                        onClick={() => {
-                                            document.location!.replace('/');
-                                        }}
-                                        text="Return home"
-                                        size="large"
-                                    />
-                                </ButtonsWrapper>
-                            </Inner>
-                        </Box>
-                        <Copyrights>© Openland {new Date().getFullYear()}</Copyrights>
-                    </Root>
-                </XTrack>
-            </>
-        );
-        // return 'Error: ' + this.props.statusCode;
-    }
 }
+
+export const ErrorPage = (props: ErrorPageProps) => (
+    <>
+        <XDocumentHead title={props.statusCode === 404 ? 'Not found' : 'Something went wrong'} />
+        <XTrack event="View 404">
+            <XView minHeight="100vh" width="100%" backgroundColor="#ffffff" alignItems="center">
+                <XView
+                    paddingTop={19}
+                    paddingLeft={32}
+                    paddingBottom={19}
+                    paddingRight={32}
+                    alignSelf="flex-start"
+                >
+                    <XView as="img" width={145} height={42} src="/static/X/signup/logo-2.svg" />
+                </XView>
+                <XView flexGrow={1} alignItems="center">
+                    <XView flexGrow={1} justifyContent="center" alignItems="center">
+                        <XView
+                            as="img"
+                            marginTop={-9}
+                            marginBottom={53}
+                            width={346}
+                            height={222}
+                            src="/static/X/illustration-error.png"
+                            srcSet="/static/X/illustration-error@2x.png 2x"
+                        />
+                        <XView fontSize={24} lineHeight="29px" color="#000000" marginBottom={10}>
+                            {props.statusCode === 404
+                                ? 'Not found'
+                                : props.message || 'Something went wrong'}
+                        </XView>
+                        <div className={Description}>
+                            Return home or contact our team at{' '}
+                            <a href="mailto:hello@openland.com">hello@openland.com</a>
+                        </div>
+                        <XView width={150}>
+                            <XButton
+                                style="primary"
+                                onClick={() => {
+                                    document.location!.replace('/');
+                                }}
+                                text="Return home"
+                                size="large"
+                            />
+                        </XView>
+                    </XView>
+                </XView>
+                <XView
+                    paddingTop={14}
+                    paddingBottom={14}
+                    fontSize={14}
+                    lineHeight="24px"
+                    color="rgba(0, 0, 0, 0.4)"
+                >
+                    © Openland {new Date().getFullYear()}
+                </XView>
+            </XView>
+        </XTrack>
+    </>
+);
