d48dd92e2ba42e7d97406a87f2a721deb938de67
Fred Morozov <morozov@fred.md>
2018-12-18 19:15:00 +0200

    refactoring(web): Refactor pages/main/directory/components/EmptySearchBlock.tsx

--- packages/openland-web/pages/main/directory/components/EmptySearchBlock.tsx
+++ packages/openland-web/pages/main/directory/components/EmptySearchBlock.tsx
@@ -1,43 +1,22 @@
 import * as React from 'react';
-import Glamorous from 'glamorous';
+import { XView } from 'react-mental';
 
-const EmptySearchWrapper = Glamorous.div({
-    height: 'calc(100vh - 118px)',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-});
-
-const EmptySearchBox = Glamorous.div({
-    width: '100%',
-    height: 600,
-    display: 'flex',
-    alignItems: 'center',
-    flexDirection: 'column',
-    justifyContent: 'center',
-});
-
-const EmptySearchBlockTitle = Glamorous.div({
-    marginTop: 54,
-    fontSize: 16,
-    fontWeight: 600,
-    lineHeight: '24px',
-    letterSpacing: -0.5,
-    color: '#99a2b0',
-});
-
-export class EmptySearchBlock extends React.Component<{ text: string }> {
-    render() {
-        return (
-            <EmptySearchWrapper>
-                <EmptySearchBox>
-                    <img
-                        src=""
-                        srcSet="/static/X/directory/empty-state.png, /static/X/directory/empty-state@2x.png 2x, /static/X/directory/empty-state@3x.png 3x"
-                    />
-                    <EmptySearchBlockTitle>{this.props.text}</EmptySearchBlockTitle>
-                </EmptySearchBox>
-            </EmptySearchWrapper>
-        );
-    }
-}
+export const EmptySearchBlock = (props: { text: string }) => (
+    <XView height="calc(100vh - 118px)" alignItems="center" justifyContent="center">
+        <XView
+            width="100%"
+            height={600}
+            alignItems="center"
+            flexDirection="column"
+            justifyContent="center"
+        >
+            <img
+                src="/static/X/directory/empty-state.pngs"
+                srcSet="/static/X/directory/empty-state.png, /static/X/directory/empty-state@2x.png 2x, /static/X/directory/empty-state@3x.png 3x"
+            />
+            <XView marginTop={54} fontSize={16} fontWeight="600" lineHeight="24px" color="#99a2b0">
+                {props.text}
+            </XView>
+        </XView>
+    </XView>
+);
