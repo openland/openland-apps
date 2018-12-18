ef4c92be49e8087ea7f22139b5df59790a9b42a2
Fred Morozov <morozov@fred.md>
2018-12-19 01:02:15 +0200

    refactoring(web): Refactoring components/Sidebar.tsx

--- packages/openland-web/components/Sidebar.tsx
+++ packages/openland-web/components/Sidebar.tsx
@@ -1,106 +1,74 @@
 import * as React from 'react';
-import Glamorous from 'glamorous';
-import { XLink, XLinkProps } from 'openland-x/XLink';
 import RightIcon from './icons/ic-arrow-rignt.svg';
+import { XView } from 'react-mental';
+import { css } from 'linaria';
 
-interface SidebarContainerProps {
-    width?: number;
-    paddingLeft?: number;
-    paddingRight?: number;
-    paddingTop?: number;
-}
+const ArrowIconClass = css`
+    path:last-child {
+        opacity: 0.2;
+        fill: #000000;
+    }
+`;
 
-const SidebarContainer = Glamorous.div<SidebarContainerProps>(props => ({
-    display: 'flex',
-    flexDirection: 'column',
-    width: props.width ? props.width : '100%',
-    paddingRight: props.paddingRight ? props.paddingRight : 0,
-    paddingLeft: props.paddingLeft ? props.paddingLeft : 0,
-    paddingTop: props.paddingTop ? props.paddingTop : undefined,
-}));
-
-const SidebarHeader = Glamorous.div({
-    display: 'flex',
-    flexDirection: 'row',
-    flexShrink: 0,
-    paddingTop: 16,
-    paddingLeft: 16,
-    paddingBottom: 21,
-    paddingRight: 16,
-
-    fontSize: 22,
-    fontWeight: 400,
-    lineHeight: '24px',
-    color: 'rgba(0, 0, 0, 0.9)',
-});
-
-interface SidebarItemProps extends XLinkProps {
+interface SidebarItemProps {
+    path?: string;
     arrow?: boolean;
     children?: any;
 }
 
-const SidebarItemWrapper = Glamorous(XLink)<SidebarItemProps>(props => ({
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    height: 44,
-    fontSize: 14,
-    lineHeight: '20px',
-    color: '#000000',
-    paddingLeft: 16,
-    paddingRight: props.arrow ? 15 : 16,
-    '&:hover': {
-        color: '#000000',
-        backgroundColor: '#f9f9f9',
-    },
-    '&.is-active': {
-        color: '#000000',
-        backgroundColor: '#f9f9f9',
-        cursor: 'default',
-    },
-    '& .arrow path:last-child': {
-        opacity: 0.2,
-        fill: '#000000',
-    },
-}));
+export const SidebarItem = (props: SidebarItemProps) => (
+    <XView
+        as="a"
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center"
+        height={44}
+        fontSize={14}
+        lineHeight="20px"
+        color="#000000"
+        paddingLeft={16}
+        paddingRight={15}
+        hoverColor="#000000"
+        hoverBackgroundColor="#f9f9f9"
+        selectedColor="#000000"
+        selectedBackgroundColor="#f9f9f9"
+        path={props.path}
+        linkSelectable={true}
+    >
+        <XView flexGrow={1}>{props.children}</XView>
+        {props.arrow && <RightIcon className={ArrowIconClass} />}
+    </XView>
+);
 
-export const SidebarItem = (props: SidebarItemProps) => {
-    let { children, ...others } = props;
+interface SidebarSubItemProps {
+    path?: string;
+    children?: any;
+}
 
-    return (
-        <SidebarItemWrapper {...others}>
-            {children}
-            {props.arrow && <RightIcon className="arrow" />}
-        </SidebarItemWrapper>
-    );
-};
-
-export const SidebarSubItem = Glamorous(XLink)({
-    display: 'flex',
-    alignItems: 'center',
-    height: 40,
-    fontSize: 14,
-    lineHeight: '20px',
-    color: '#000000',
-    paddingLeft: 36,
-    paddingRight: 24,
-    '&:hover': {
-        color: '#000000',
-        backgroundColor: '#f9f9f9',
-    },
-    '&.is-active': {
-        color: '#000000',
-        backgroundColor: '#f9f9f9',
-        cursor: 'default',
-    },
-});
+export const SidebarSubItem = (props: SidebarSubItemProps) => (
+    <XView
+        as="a"
+        alignItems="flex-start"
+        justifyContent="center"
+        height={40}
+        fontSize={14}
+        lineHeight="20px"
+        color="#000000"
+        paddingLeft={36}
+        paddingRight={24}
+        hoverColor="#000000"
+        hoverBackgroundColor="#f9f9f9"
+        selectedColor="#000000"
+        selectedBackgroundColor="#f9f9f9"
+        path={props.path}
+        linkSelectable={true}
+    >
+        {props.children}
+    </XView>
+);
 
 interface SidebarProps {
     title: string;
-    width?: number;
-    paddingLeft?: number;
-    paddingRight?: number;
-    paddingTop?: number;
 }
 
 export class Sidebar extends React.Component<SidebarProps> {
@@ -108,13 +76,23 @@ export class Sidebar extends React.Component<SidebarProps> {
     static Subitem = SidebarSubItem;
 
     render() {
-        const { title, children, ...other } = this.props;
-
         return (
-            <SidebarContainer {...other}>
-                <SidebarHeader>{title}</SidebarHeader>
-                {children}
-            </SidebarContainer>
+            <XView flexDirection="column" width="100%">
+                <XView
+                    flexDirection="row"
+                    flexShrink={0}
+                    paddingTop={16}
+                    paddingLeft={16}
+                    paddingBottom={21}
+                    paddingRight={16}
+                    fontSize={22}
+                    lineHeight="24px"
+                    color="rgba(0, 0, 0, 0.9)"
+                >
+                    {this.props.title}
+                </XView>
+                {this.props.children}
+            </XView>
         );
     }
 }
