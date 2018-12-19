60b2b2810bc7d1164aabac7683398784bf836b33
Steve Kite <steve@statecraft.one>
2018-12-19 07:52:04 +0300

    refactoring(web): Refactoring of DialogListFragment

--- packages/openland-web/fragments/dialogs/DialogListFragment.tsx
+++ packages/openland-web/fragments/dialogs/DialogListFragment.tsx
@@ -1,13 +1,72 @@
 import * as React from 'react';
+import Glamorous from 'glamorous';
 import { XView } from 'react-mental';
 import { DialogListView } from './components/DialogListView';
 import { DialogsInviteButton } from './components/DialogsInviteButton';
+import { XButton } from 'openland-x/XButton';
+import { XThemeDefault } from 'openland-x/XTheme';
+import { XFont } from 'openland-x/XFont';
+import { XHorizontal } from 'openland-x-layout/XHorizontal';
+import PlusIcon from '../../components/icons/ic-add-medium-2.svg';
+
+const ChatListContainer = Glamorous.div({
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+    width: 344,
+    flexShrink: 0,
+    borderRightWidth: '1px',
+    borderRightStyle: 'solid',
+    borderRightColor: XThemeDefault.separatorColor,
+    backgroundColor: XThemeDefault.backgroundColor,
+    '@media (max-width: 1100px)': {
+        width: 300,
+    },
+    '@media (max-width: 950px)': {
+        width: 230,
+    },
+});
+
+const AddButton = Glamorous(XButton)({
+    '& svg > g > path': {
+        transition: 'all .2s',
+    },
+    '& svg > g > path:last-child': {
+        fill: '#1790ff',
+        opacity: 0.5,
+    },
+});
+
+const Header = Glamorous(XHorizontal)({
+    height: 48,
+    paddingLeft: 16,
+    paddingRight: 16,
+    marginTop: 4,
+    marginBottom: 3,
+    flexShrink: 0,
+});
+
+const Title = Glamorous.div({
+    ...XFont.h600,
+});
 
 export const DialogListFragment = React.memo(() => {
     return (
-        <XView flexGrow={1} flexBasis={0}>
-            <DialogListView />
-            <DialogsInviteButton />
-        </XView>
+        <ChatListContainer>
+            <XView flexGrow={1} flexBasis={0}>
+                <Header alignItems="center" justifyContent="space-between">
+                    <Title data-test-id="messages-title">Messages</Title>
+                    <AddButton
+                        style="light"
+                        path="/mail/new"
+                        text="New"
+                        icon={<PlusIcon />}
+                        size="small"
+                    />
+                </Header>
+                <DialogListView />
+                <DialogsInviteButton />
+            </XView>
+        </ChatListContainer>
     )
 });
--- packages/openland-web/pages/main/mail/root.page.tsx
+++ packages/openland-web/pages/main/mail/root.page.tsx
@@ -7,8 +7,6 @@ import { Scaffold } from '../../../components/Scaffold';
 import { MessengerComponent } from '../../../components/messenger/MessengerComponent';
 import { DialogListFragment } from '../../../fragments/dialogs/DialogListFragment';
 import { ComposeComponent } from '../../../components/messenger/ComposeComponent';
-import { XButton } from 'openland-x/XButton';
-import { XHorizontal } from 'openland-x-layout/XHorizontal';
 import { RoomsExploreComponent } from '../../../fragments/RoomsExploreComponent';
 import { MessengerEmptyComponent } from '../../../fragments/MessengerEmptyComponent';
 import { RoomsInviteComponent } from '../../../fragments/RoomsInviteComponent';
@@ -18,8 +16,6 @@ import { UserProfile } from '../profile/UserProfileComponent';
 import { withChannelInviteInfo } from '../../../api/withChannelInviteInfo';
 import { XLoader } from 'openland-x/XLoader';
 import { XPageRedirect } from 'openland-x-routing/XPageRedirect';
-import PlusIcon from '../../../components/icons/ic-add-medium-2.svg';
-import { XFont } from 'openland-x/XFont';
 import { canUseDOM } from 'openland-x-utils/canUseDOM';
 import { XThemeDefault } from 'openland-x/XTheme';
 import { withRouter } from 'openland-x-routing/withRouter';
@@ -41,24 +37,6 @@ export const ChatContainer = Glamorous.div({
     overflow: 'hidden',
 });
 
-export const ChatListContainer = Glamorous.div({
-    display: 'flex',
-    flexDirection: 'column',
-    height: '100%',
-    width: 344,
-    flexShrink: 0,
-    borderRightWidth: '1px',
-    borderRightStyle: 'solid',
-    borderRightColor: XThemeDefault.separatorColor,
-    backgroundColor: XThemeDefault.backgroundColor,
-    '@media (max-width: 1100px)': {
-        width: 300,
-    },
-    '@media (max-width: 950px)': {
-        width: 230,
-    },
-});
-
 export const ConversationContainer = Glamorous.div({
     display: 'flex',
     flexDirection: 'column',
@@ -76,19 +54,6 @@ export const ConversationContainer = Glamorous.div({
     '@media (max-width: 950px)': {
         width: 'calc(100% - 230px)',
     },
-});
-
-export const Header = Glamorous(XHorizontal)({
-    height: 48,
-    paddingLeft: 16,
-    paddingRight: 16,
-    marginTop: 4,
-    marginBottom: 3,
-    flexShrink: 0,
-});
-
-export const Title = Glamorous.div({
-    ...XFont.h600,
 });
 
 export const OrganizationProfileContainer = Glamorous.div({
@@ -115,34 +80,6 @@ export const RoomInviteFromLink = withChannelInviteInfo(
                 <XLoader loading={true} />
             ),
 );
-
-export const AddButton = Glamorous(XButton)({
-    '& svg > g > path': {
-        transition: 'all .2s',
-    },
-    '& svg > g > path:last-child': {
-        fill: '#1790ff',
-        opacity: 0.5,
-    },
-});
-
-const ChatList = React.memo(() => {
-    return (
-        <ChatListContainer>
-            <Header alignItems="center" justifyContent="space-between">
-                <Title data-test-id="messages-title">Messages</Title>
-                <AddButton
-                    style="light"
-                    path="/mail/new"
-                    text="New"
-                    icon={<PlusIcon />}
-                    size="small"
-                />
-            </Header>
-            <DialogListFragment />
-        </ChatListContainer>
-    );
-});
 
 interface MessagePageInnerState extends MessagesStateContextProps { }
 
@@ -336,7 +273,7 @@ class MessagePageInner extends React.Component<{ router: XRouter }, MessagePageI
                     <Scaffold.Content padding={false} bottomOffset={false}>
                         <MessagesStateContext.Provider value={this.state}>
                             <ChatContainer>
-                                <ChatList />
+                                <DialogListFragment />
 
                                 <ConversationContainer>
                                     {tab === 'compose' && (
