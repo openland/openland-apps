3d41ec91007f6697db1cbbc37ced84ae6f8dc622
Steve Kite <steve@statecraft.one>
2018-12-19 14:15:05 +0300

    refactoring(web): Use hooks in TalkBarComponent

--- packages/openland-web/pages/main/mail/components/conference/TalkBarComponent.tsx
+++ packages/openland-web/pages/main/mail/components/conference/TalkBarComponent.tsx
@@ -24,107 +24,102 @@ import { XView } from 'react-mental';
 // }
 
 export const TalkBarComponent = (props: { conversationId: string }) => {
+    const apollo = React.useContext(YApolloContext)!;
+    let ctx = React.useContext(TalkContext);
     return (
         <XView height={0} alignSelf="stretch">
-            <YApolloContext.Consumer>
-                {apollo => (
-                    <YQuery query={ConferenceQuery} variables={{ id: props.conversationId }}>
-                        {data => {
-                            if (data.loading) {
-                                return null;
-                            }
+            <YQuery query={ConferenceQuery} variables={{ id: props.conversationId }}>
+                {data => {
+                    if (data.loading) {
+                        return null;
+                    }
 
-                            if (data.data!.conference.peers.length === 0) {
-                                return (
+                    if (data.data!.conference.peers.length === 0) {
+                        return (
+                            <>
+                                <TalkWatchComponent
+                                    apollo={apollo!}
+                                    id={data.data!.conference.id}
+                                />
+                            </>
+                        );
+                    }
+                    return (
+                        <>
+                            <XView
+                                position="absolute"
+                                top={0}
+                                left={0}
+                                right={0}
+                                zIndex={1}
+                                flexShrink={0}
+                                paddingTop={8}
+                                paddingBottom={8}
+                                alignItems="center"
+                                justifyContent="center"
+                                backgroundColor="#32bb78"
+                                flexDirection="row"
+                            >
+                                {data.data!.conference.peers.map(v => (
+                                    <React.Fragment key={v.id}>
+                                        <XView flexDirection="row">
+                                            <XAvatar
+                                                size="m-small"
+                                                style="user"
+                                                objectId={v.user.id}
+                                                online={false}
+                                                cloudImageUuid={
+                                                    v.user.photo || undefined
+                                                }
+                                            />
+                                            {/* <span>{v.connection && v.connection.state}</span> */}
+                                        </XView>
+                                        <XView width={8} />
+                                    </React.Fragment>
+                                ))}
+                                {ctx.cid === props.conversationId && (
                                     <>
-                                        <TalkWatchComponent
-                                            apollo={apollo!}
-                                            id={data.data!.conference.id}
+                                        <XButton
+                                            style="success"
+                                            text={ctx.muted ? 'Unmute' : 'Mute'}
+                                            onClick={() => ctx.toggleMute()}
+                                        />
+                                        <XView width={8} />
+                                        <XButton
+                                            style="success"
+                                            text={
+                                                ctx.state === 'connecting'
+                                                    ? 'Connecting'
+                                                    : 'Leave'
+                                            }
+                                            onClick={() => ctx.leaveCall()}
                                         />
                                     </>
-                                );
-                            }
-                            return (
-                                <>
-                                    <TalkContext.Consumer>
-                                        {ctx => (
-                                            <XView
-                                                position="absolute"
-                                                top={0}
-                                                left={0}
-                                                right={0}
-                                                zIndex={1}
-                                                flexShrink={0}
-                                                paddingTop={8}
-                                                paddingBottom={8}
-                                                alignItems="center"
-                                                justifyContent="center"
-                                                backgroundColor="#32bb78"
-                                                flexDirection="row"
-                                            >
-                                                {data.data!.conference.peers.map(v => (
-                                                    <React.Fragment key={v.id}>
-                                                        <XView flexDirection="row">
-                                                            <XAvatar
-                                                                size="m-small"
-                                                                style="user"
-                                                                objectId={v.user.id}
-                                                                online={false}
-                                                                cloudImageUuid={
-                                                                    v.user.photo || undefined
-                                                                }
-                                                            />
-                                                            {/* <span>{v.connection && v.connection.state}</span> */}
-                                                        </XView>
-                                                        <XView width={8} />
-                                                    </React.Fragment>
-                                                ))}
-                                                {ctx.cid === props.conversationId && (
-                                                    <>
-                                                        <XButton
-                                                            style="success"
-                                                            text={ctx.muted ? 'Unmute' : 'Mute'}
-                                                            onClick={() => ctx.toggleMute()}
-                                                        />
-                                                        <XView width={8} />
-                                                        <XButton
-                                                            style="success"
-                                                            text={
-                                                                ctx.state === 'connecting'
-                                                                    ? 'Connecting'
-                                                                    : 'Leave'
-                                                            }
-                                                            onClick={() => ctx.leaveCall()}
-                                                        />
-                                                    </>
-                                                )}
-                                                {ctx.cid !== props.conversationId && (
-                                                    <XButton
-                                                        style="success"
-                                                        text={ctx.cid ? 'Leave' : 'Join'}
-                                                        onClick={
-                                                            ctx.cid
-                                                                ? () => ctx.leaveCall()
-                                                                : () =>
-                                                                      ctx.joinCall(
-                                                                          props.conversationId,
-                                                                      )
-                                                        }
-                                                    />
-                                                )}
-                                            </XView>
-                                        )}
-                                    </TalkContext.Consumer>
-                                    <TalkWatchComponent
-                                        apollo={apollo!}
-                                        id={data.data!.conference.id}
+                                )}
+                                {ctx.cid !== props.conversationId && (
+                                    <XButton
+                                        style="success"
+                                        text={ctx.cid ? 'Leave' : 'Join'}
+                                        onClick={
+                                            ctx.cid
+                                                ? () => ctx.leaveCall()
+                                                : () =>
+                                                    ctx.joinCall(
+                                                        props.conversationId,
+                                                    )
+                                        }
                                     />
-                                </>
-                            );
-                        }}
-                    </YQuery>
-                )}
-            </YApolloContext.Consumer>
+                                )}
+                            </XView>
+                            )}
+                            <TalkWatchComponent
+                                apollo={apollo!}
+                                id={data.data!.conference.id}
+                            />
+                        </>
+                    );
+                }}
+            </YQuery>
         </XView>
     );
 };
