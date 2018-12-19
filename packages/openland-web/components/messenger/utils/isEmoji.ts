f8a374ca399e023e8a8c7563df3465ab79931ad9
Steve Kite <steve@statecraft.one>
2018-12-19 05:24:15 +0300

    Fix emoji

--- packages/openland-web/components/messenger/utils/isEmoji.ts
+++ packages/openland-web/components/messenger/utils/isEmoji.ts
@@ -1,8 +1,11 @@
 import emojiData from './data/emoji-data';
 
-const asciiToUnicodeCache = new Map();
+const asciiToUnicodeCache = new Map<string, string>();
 
 function isUnicodeEmoji(src: string) {
+    if (asciiToUnicodeCache.has(src)) {
+        return true;
+    }
     for (const [regExp, unicode] of emojiData.asciiToUnicode.entries()) {
         if (src.replace(regExp, unicode) === unicode) {
             asciiToUnicodeCache.set(src, unicode);
@@ -17,6 +20,10 @@ function isTextEmoji(src: string) {
 }
 
 export function isEmoji(src: string) {
+    // What the heck?
+    if (emojiData.unicodeToShort.has(src)) {
+        return true;
+    }
     if (isTextEmoji(src)) {
         return true;
     }
