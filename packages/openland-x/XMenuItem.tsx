371b5c31b75607c0f3d3c9e1a46355610a7f5a5f
Fred Morozov <morozov@fred.md>
2018-12-19 10:44:08 +0200

    refactoring(web): Refactoring icons in XMenuItem

--- packages/openland-x/XMenuItem.tsx
+++ packages/openland-x/XMenuItem.tsx
@@ -4,11 +4,11 @@ import { styleResolver } from 'openland-x-utils/styleResolver';
 import { XLink, XLinkProps } from 'openland-x/XLink';
 import { XIcon } from 'openland-x/XIcon';
 import { XPopperContent } from 'openland-x/popper/XPopperContent';
-import RightIcon from './icons/ic-right-1.svg';
-import DropdownChatIcon from './icons/ic-dropdown-message.svg';
-import DropdownRoomIcon from './icons/ic-dropdown-channel.svg';
-import DropdownCommunityIcon from './icons/ic-dropdown-community.svg';
-import DropdownOrganizationIcon from './icons/ic-dropdown-organization.svg';
+import RightIcon from 'openland-icons/ic-right-1.svg';
+import DropdownChatIcon from 'openland-icons/ic-dropdown-message.svg';
+import DropdownRoomIcon from 'openland-icons/ic-dropdown-channel.svg';
+import DropdownCommunityIcon from 'openland-icons/ic-dropdown-community.svg';
+import DropdownOrganizationIcon from 'openland-icons/ic-dropdown-organization.svg';
 
 type XMenuItemStyle = 'default' | 'danger' | 'gray';
 
@@ -19,51 +19,51 @@ interface XMenuItemProps extends XLinkProps {
 }
 
 let XMenuItemColorStyles = styleResolver({
-    'default': {
+    default: {
         color: '#000000',
         '& i': {
-            color: 'rgba(0, 0, 0, 0.25)'
+            color: 'rgba(0, 0, 0, 0.25)',
         },
         '& svg *': {
-            fill: 'rgba(0, 0, 0, 0.25)'
+            fill: 'rgba(0, 0, 0, 0.25)',
         },
         ':hover': {
             color: '#1790ff',
             backgroundColor: 'rgba(23, 144, 255, 0.05)',
             '& i': {
-                color: 'rgba(23, 144, 255, 0.5)'
+                color: 'rgba(23, 144, 255, 0.5)',
             },
             '& svg *': {
-                fill: 'rgba(23, 144, 255, 0.5)'
-            }
-        }
+                fill: 'rgba(23, 144, 255, 0.5)',
+            },
+        },
     },
-    'danger': {
+    danger: {
         color: '#d75454',
         '& i': {
-            color: '#d75454'
+            color: '#d75454',
         },
         '& svg *': {
-            fill: '#d75454'
+            fill: '#d75454',
         },
         ':hover': {
             color: '#d75454',
             backgroundColor: '#fdf6f6',
             '& i': {
-                color: '#d75454'
+                color: '#d75454',
             },
             '& svg *': {
-                fill: '#d75454'
-            }
-        }
+                fill: '#d75454',
+            },
+        },
     },
-    'gray': {
+    gray: {
         color: 'rgba(0, 0, 0, 0.8)',
         '&:hover': {
             color: 'rgba(0, 0, 0, 0.8)',
-            backgroundColor: '#f6f6f6'
-        }
-    }
+            backgroundColor: '#f6f6f6',
+        },
+    },
 });
 
 const XMenuItemStyled = Glamorous(XLink)<{ colorTheme?: XMenuItemStyle }>([
@@ -75,10 +75,10 @@ const XMenuItemStyled = Glamorous(XLink)<{ colorTheme?: XMenuItemStyle }>([
         alignItems: 'center',
 
         '& .svg-icon-left': {
-            marginRight: 10
-        }
+            marginRight: 10,
+        },
     },
-    (props) => XMenuItemColorStyles(props.colorTheme)
+    props => XMenuItemColorStyles(props.colorTheme),
 ]);
 
 const XMenuItemIcon = Glamorous(XIcon)({
@@ -86,12 +86,12 @@ const XMenuItemIcon = Glamorous(XIcon)({
     lineHeight: '40px',
     '&.icon-left': {
         marginLeft: -3,
-        marginRight: 10
+        marginRight: 10,
     },
     '&.icon-right': {
         marginRight: -8,
-        marginLeft: 10
-    }
+        marginLeft: 10,
+    },
 });
 
 const XMenuItemText = Glamorous.div({
@@ -103,43 +103,66 @@ const XMenuItemText = Glamorous.div({
     letterSpacing: 0,
     overflow: 'hidden',
     whiteSpace: 'nowrap',
-    textOverflow: 'ellipsis'
+    textOverflow: 'ellipsis',
 });
 
 export class XMenuItem extends React.Component<XMenuItemProps> {
-    CustomIcons = ['x-right', 'x-dropdown-chat', 'x-dropdown-room', 'x-dropdown-community', 'x-dropdown-organization'];
+    CustomIcons = [
+        'x-right',
+        'x-dropdown-chat',
+        'x-dropdown-room',
+        'x-dropdown-community',
+        'x-dropdown-organization',
+    ];
 
-    getCustomIcon (i: string, position?: string) {
+    getCustomIcon(i: string, position?: string) {
         switch (i) {
-            case this.CustomIcons[0]: return <RightIcon className={(position) ? 'svg-icon-' + position : undefined} />;
-            case this.CustomIcons[1]: return <DropdownChatIcon className={(position) ? 'svg-icon-' + position : undefined} />;
-            case this.CustomIcons[2]: return <DropdownRoomIcon className={(position) ? 'svg-icon-' + position : undefined} />;
-            case this.CustomIcons[3]: return <DropdownCommunityIcon className={(position) ? 'svg-icon-' + position : undefined} />;
-            case this.CustomIcons[4]: return <DropdownOrganizationIcon className={(position) ? 'svg-icon-' + position : undefined} />;
-            default: return undefined;
+            case this.CustomIcons[0]:
+                return <RightIcon className={position ? 'svg-icon-' + position : undefined} />;
+            case this.CustomIcons[1]:
+                return (
+                    <DropdownChatIcon className={position ? 'svg-icon-' + position : undefined} />
+                );
+            case this.CustomIcons[2]:
+                return (
+                    <DropdownRoomIcon className={position ? 'svg-icon-' + position : undefined} />
+                );
+            case this.CustomIcons[3]:
+                return (
+                    <DropdownCommunityIcon
+                        className={position ? 'svg-icon-' + position : undefined}
+                    />
+                );
+            case this.CustomIcons[4]:
+                return (
+                    <DropdownOrganizationIcon
+                        className={position ? 'svg-icon-' + position : undefined}
+                    />
+                );
+            default:
+                return undefined;
         }
     }
 
-    isCustomIcon (i: string) {
-        return (this.CustomIcons.indexOf(i) > -1);
+    isCustomIcon(i: string) {
+        return this.CustomIcons.indexOf(i) > -1;
     }
 
     render() {
         return (
-            <XMenuItemStyled
-                {...this.props}
-                colorTheme={this.props.style}
-            >
-                {this.props.icon && this.isCustomIcon(this.props.icon) && this.getCustomIcon(this.props.icon, 'left')}
+            <XMenuItemStyled {...this.props} colorTheme={this.props.style}>
+                {this.props.icon &&
+                    this.isCustomIcon(this.props.icon) &&
+                    this.getCustomIcon(this.props.icon, 'left')}
                 {this.props.icon && !this.isCustomIcon(this.props.icon) && (
                     <XMenuItemIcon icon={this.props.icon} className="icon icon-left" />
                 )}
 
-                <XMenuItemText>
-                    {this.props.children}
-                </XMenuItemText>
+                <XMenuItemText>{this.props.children}</XMenuItemText>
 
-                {this.props.iconRight && this.isCustomIcon(this.props.iconRight) && this.getCustomIcon(this.props.iconRight)}
+                {this.props.iconRight &&
+                    this.isCustomIcon(this.props.iconRight) &&
+                    this.getCustomIcon(this.props.iconRight)}
                 {this.props.iconRight && !this.isCustomIcon(this.props.iconRight) && (
                     <XMenuItemIcon icon={this.props.iconRight} className="icon icon-right" />
                 )}
@@ -159,7 +182,7 @@ export const XMenuItemWrapper = Glamorous.div({
 
     '& > *': {
         width: '100%',
-        minHeight: '100%'
+        minHeight: '100%',
     },
 
     '& label': {
@@ -170,8 +193,8 @@ export const XMenuItemWrapper = Glamorous.div({
         backgroundColor: '#f3f9ff',
         '& span': {
             color: '#1790ff!important',
-        }
-    }
+        },
+    },
 });
 
 export const XMenuTitle = Glamorous.div({
@@ -181,19 +204,23 @@ export const XMenuTitle = Glamorous.div({
     fontSize: 14,
     lineHeight: '20px',
     letterSpacing: 0,
-    fontWeight: 700
+    fontWeight: 700,
 });
 
-export const XMenuItemSeparator = Glamorous.div<{ marginTop?: number; marginBottom?: number; }>((props) => ({
-    height: 1,
-    background: '#ececec',
-    marginTop: props.marginTop || 4,
-    marginBottom: props.marginBottom || 4,
-    marginLeft: 0,
-    marginRight: 0,
-}));
+export const XMenuItemSeparator = Glamorous.div<{ marginTop?: number; marginBottom?: number }>(
+    props => ({
+        height: 1,
+        background: '#ececec',
+        marginTop: props.marginTop || 4,
+        marginBottom: props.marginBottom || 4,
+        marginLeft: 0,
+        marginRight: 0,
+    }),
+);
 
-export const XMenuVertical = Glamorous<{ paddingTop?: number; paddingBottom?: number; }>(XPopperContent)((props) => ({
+export const XMenuVertical = Glamorous<{ paddingTop?: number; paddingBottom?: number }>(
+    XPopperContent,
+)(props => ({
     paddingTop: props.paddingTop || 8,
     paddingBottom: props.paddingBottom || 8,
     paddingLeft: 0,
