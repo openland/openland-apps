c7831215e5d0e62674ae6c6a961df8a0aed44849
Sergey Lapin <lapanoid@gmail.com>
2018-12-21 16:06:20 +0300

    fix(web): typings style from https://app.zeplin.io/project/5adeed705408617dbfd15cc4/screen/5beeabeb9c1d023f1728af56

--- packages/openland-web/components/messenger/typings/TypingsView.tsx
+++ packages/openland-web/components/messenger/typings/TypingsView.tsx
@@ -5,10 +5,12 @@ import { css } from 'linaria';
 
 const typingClassName = css`
     opacity: 0.5;
-    font-size: 12px;
-    font-weight: 500;
-    letter-spacing: -0.2px;
-    color: rgb(51, 69, 98);
+    font-family: SFProText-Regular;
+    font-size: 13px;
+    font-weight: normal;
+    font-style: normal;
+    line-height: normal;
+    color: #000000;
 `;
 
 export interface TypingsViewProps {
@@ -34,17 +36,8 @@ export const TypingsView = React.memo<TypingsViewProps>(props => {
 
     if (typing) {
         return (
-            <XView
-                marginLeft={55}
-                alignItems="flex-start"
-                opacity={0.5}
-                fontSize={12}
-                fontWeight="600"
-                color="#334562"
-                marginTop={8}
-                marginBottom={8}
-            >
-                <span className={typingClassName}>{typing}</span>
+            <XView marginLeft={55} alignItems="flex-start" marginTop={8} marginBottom={8}>
+                <div className={typingClassName}>{typing}</div>
             </XView>
         );
     } else {
