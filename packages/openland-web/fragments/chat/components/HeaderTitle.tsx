ee519a1c2f7ee6628540a946aebff9358463d598
Steve Kite <steve@statecraft.one>
2018-12-27 18:52:15 +0300

    fix(web): Emojify chat header

--- packages/openland-web/fragments/chat/components/HeaderTitle.tsx
+++ packages/openland-web/fragments/chat/components/HeaderTitle.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { XView } from 'react-mental';
 import { Room_room_PrivateRoom_user_primaryOrganization } from 'openland-api/Types';
 import { css } from 'linaria';
+import { emoji } from 'openland-web/utils/emoji';
 
 const titleInnerClass = css`
     height: 18px;
@@ -15,33 +16,33 @@ export const HeaderTitle = (props: {
     path?: string;
     organization?: Room_room_PrivateRoom_user_primaryOrganization | null;
 }) => (
-    <XView marginTop={-2} minWidth={0} flexShrink={1} flexDirection="row">
-        <XView
-            as="a"
-            fontSize={14}
-            fontWeight="600"
-            lineHeight="18px"
-            color="#000000"
-            path={props.path}
-            minWidth={0}
-            flexShrink={1}
-            hoverTextDecoration="none"
-        >
-            <div className={titleInnerClass}>{props.value}</div>
-        </XView>
-        {props.organization && (
+        <XView marginTop={-2} minWidth={0} flexShrink={1} flexDirection="row">
             <XView
                 as="a"
-                marginLeft={6}
-                fontSize={13}
+                fontSize={14}
                 fontWeight="600"
-                color="rgba(0, 0, 0, 0.4)"
                 lineHeight="18px"
-                path={'/mail/o/' + props.organization.id}
+                color="#000000"
+                path={props.path}
+                minWidth={0}
+                flexShrink={1}
                 hoverTextDecoration="none"
             >
-                {props.organization.name}
+                <div className={titleInnerClass}>{emoji(props.value, 14)}</div>
             </XView>
-        )}
-    </XView>
-);
+            {props.organization && (
+                <XView
+                    as="a"
+                    marginLeft={6}
+                    fontSize={13}
+                    fontWeight="600"
+                    color="rgba(0, 0, 0, 0.4)"
+                    lineHeight="18px"
+                    path={'/mail/o/' + props.organization.id}
+                    hoverTextDecoration="none"
+                >
+                    {emoji(props.organization.name, 14)}
+                </XView>
+            )}
+        </XView>
+    );
