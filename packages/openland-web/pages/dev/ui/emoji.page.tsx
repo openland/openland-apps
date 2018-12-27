96fcf755718d1aef05169ec21016b17fa2fd1e61
Steve Kite <steve@statecraft.one>
2018-12-27 18:05:38 +0300

    feat(emoji): Working on new emoji

---
+++ packages/openland-web/pages/dev/ui/emoji.page.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { withApp } from '../../../components/withApp';
+import { DevDocsScaffold } from './components/DevDocsScaffold';
+import { XContent } from 'openland-x-layout/XContent';
+import { XVertical } from 'openland-x-layout/XVertical';
+import { XTitle } from 'openland-x/XTitle';
+import { emoji } from 'openland-web/utils/emoji';
+
+export default withApp('UI Framework - Files', 'viewer', props => {
+    return (
+        <DevDocsScaffold title="Files">
+            <XContent>
+                <XVertical>
+                    <XTitle>Simple</XTitle>
+                    <span>{emoji('🦄🌈👷🏽‍♂️')}</span>
+                </XVertical>
+            </XContent>
+        </DevDocsScaffold>
+    );
+});
