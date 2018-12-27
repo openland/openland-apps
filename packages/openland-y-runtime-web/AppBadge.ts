4889a0d6f7eb4f62c37d0bf6ac236b332cf6f580
Steve Kite <steve@statecraft.one>
2018-12-27 11:52:37 +0300

    feat(desktop): Add counter support

--- packages/openland-y-runtime-web/AppBadge.ts
+++ packages/openland-y-runtime-web/AppBadge.ts
@@ -1,46 +1,17 @@
 import { AppBadgeAPi } from 'openland-y-runtime-api/AppBadgeApi';
 import { canUseDOM } from 'openland-x-utils/canUseDOM';
-import { backoff } from 'openland-y-utils/timer';
-import { AppVisibility } from './AppVisibility';
 
 class BadgeStub implements AppBadgeAPi {
-    items: Promise<any[]> | null = null;
-    private isVisible = false;
-
-    constructor() {
-        if (canUseDOM) {
-            this.isVisible = AppVisibility.isVisible;
-            AppVisibility.watch((isVisible: boolean) => {
-                this.isVisible = isVisible;
-                if (isVisible) {
-                    this.setBadge(0);
-                }
-            });
-
-            this.items = backoff(() => import('favico.js')).then((v) => {
-                let els = document.querySelectorAll('link[rel=icon]');
-                let res = [];
-                for (let i = 0; i < els.length; i++) {
-                    res.push(new (v.default as any)({
-                        position: 'topright',
-                        animation: 'none',
-                        element: els.item(i)
-                    }));
-                }
-                return res;
-            });
-        }
-    }
+    private readonly isDesktop = canUseDOM && !!(global as any).require;
 
     setBadge(count: number) {
-        if (this.isVisible) {
-            // if (this.items) {
-            //     this.items!!.then((v) => {
-            //         for (let i of v) {
-            //             i.badge(count);
-            //         }
-            //     });
-            // }
+        if (this.isDesktop) {
+            let app = (global as any).require('electron').remote.app;
+            if (count <= 0) {
+                app.setBadgeCount(0);
+            } else {
+                app.setBadgeCount(count);
+            }
         }
     }
 }
