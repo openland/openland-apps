1f512ce4701918d22e3af8dd28d40544c112cc80
Fred Morozov <morozov@fred.md>
2018-12-28 10:59:43 +0200

    feat(web): Working on about page

--- packages/openland-landing/about.page.tsx
+++ packages/openland-landing/about.page.tsx
@@ -3,6 +3,8 @@ import { Page } from './components/Page';
 import { XDocumentHead } from 'openland-x-routing/XDocumentHead';
 import { css } from 'linaria';
 import { Container } from './components/Container';
+import { XView } from 'react-mental';
+import { LandingLinks } from './components/_links';
 
 let aboutRootClass = css`
     padding: 42px 0 13px;
@@ -41,6 +43,62 @@ let aboutLabelClass = css`
     }
 `;
 
+let aboutInfoClass = css`
+    margin: 13px 0 0;
+    font-size: 16px;
+    line-height: 22px;
+    font-weight: 400;
+    width: 180px;
+
+    a {
+        color: #1790ff;
+        text-decoration: none;
+    }
+    a:hover {
+        text-decoration: underline;
+    }
+
+    @media (max-width: 999px) {
+        width: auto;
+    }
+`;
+
+let aboutBoxClass = css`
+    width: calc(100% - 253px);
+
+    @media (max-width: 999px) {
+        width: auto;
+    }
+    @media (min-width: 1000px) and (max-width: 1179px) {
+        width: calc(100% - 215px);
+    }
+`;
+
+let aboutAdditionalClass = css`
+    font-style: italic;
+    color: #536086;
+    margin: 3px 0 0;
+    font-size: 15px;
+    line-height: 24px;
+
+    a {
+        color: inherit;
+        text-decoration: none;
+        font-weight: 600;
+        display: inline-block;
+        vertical-align: top;
+    }
+    a:hover {
+        text-decoration: underline;
+    }
+
+    @media (max-width: 767px) {
+        br {
+            display: none;
+        }
+    }
+`;
+
 export default class AboutPage extends React.Component {
     render() {
         return (
@@ -50,12 +108,40 @@ export default class AboutPage extends React.Component {
                     <div className={aboutRootClass}>
                         <div className={aboutRowClass}>
                             <div className={aboutLabelClass}>About Openland</div>
+                            <div className={aboutBoxClass}>
+                                <XView fontSize={16} lineHeight="24px">
+                                    Openland is&nbsp;a&nbsp;professional messenger designed
+                                    to&nbsp;support all communication needs of&nbsp;a&nbsp;modern
+                                    business. It&nbsp;can be&nbsp;used for fundraising, sales,
+                                    recruiting, customer service, and business operations. The
+                                    company was founded in&nbsp;2017 and is&nbsp;based in&nbsp;San
+                                    Francisco. Openland is&nbsp;backed by&nbsp;30&nbsp;prominent
+                                    venture investors, including Y&nbsp;Combinator, Gagarin Capital,
+                                    Sinai Ventures, Soma Capital, Liquid 2&nbsp;Ventures, and
+                                    Rainfall Ventures.
+                                </XView>
+                            </div>
                         </div>
                         <div className={aboutRowClass}>
                             <div className={aboutLabelClass}>Founders</div>
+                            <div className={aboutBoxClass}>Box</div>
                         </div>
                         <div className={aboutRowClass}>
-                            <div className={aboutLabelClass}>Open positions</div>
+                            <div className={aboutLabelClass}>
+                                Open positions
+                                <div className={aboutInfoClass}>
+                                    Up-to-date opportunities are posted at&nbsp;
+                                    <a href={LandingLinks.careers}>angel.co</a>
+                                </div>
+                            </div>
+                            <div className={aboutBoxClass}>
+                                <div className={aboutAdditionalClass}>
+                                    If&nbsp;you don&rsquo;t see a&nbsp;matching opening for you
+                                    or&nbsp;just want to&nbsp;say "Hi"
+                                    <br /> please reach&nbsp;us at&nbsp;
+                                    <a href="mailto:hello@openland.com">hello@openland.com</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </Container>
