75b574d8daa2bd1f3489aceefba986b52cbd217f
Fred Morozov <morozov@fred.md>
2019-01-08 11:44:00 +0200

    fix(landing): mobile view for signup buttons

--- packages/openland-landing/components/home/HomeForm.tsx
+++ packages/openland-landing/components/home/HomeForm.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { css } from 'linaria';
 import { LandingLinks } from '../_links';
 import { GoogleButton, EmailButton } from 'openland-web/pages/init/components/SignComponents';
-import { XView } from 'react-mental';
 
 let homeFormRootClass = css`
     margin: 0 0 30px;
@@ -11,8 +10,9 @@ let homeFormRootClass = css`
     flex-direction: row;
 
     @media (max-width: 767px) {
-        margin: 0 0 20px;
-        width: auto;
+        margin: 0 auto 20px;
+        width: 234px;
+        flex-direction: column;
     }
     @media (min-width: 768px) and (max-width: 999px) {
         margin-left: auto;
@@ -20,21 +20,42 @@ let homeFormRootClass = css`
     }
 `;
 
+let homeFormBtnClass = css`
+    flex-grow: 1;
+
+    &:first-child {
+        margin-right: 6px;
+
+        @media (max-width: 767px) {
+            margin-right: 0;
+            margin-bottom: 12px;
+        }
+    }
+
+    &:last-child {
+        margin-left: 6px;
+
+        @media (max-width: 767px) {
+            margin-left: 0;
+        }
+    }
+`;
+
 export const HomeForm = () => (
     <div className={homeFormRootClass}>
-        <XView flexGrow={1} marginRight={6}>
+        <div className={homeFormBtnClass}>
             <GoogleButton
                 path={LandingLinks.signup + '?google=true'}
                 text="Sign up with Google"
                 rounded={true}
             />
-        </XView>
-        <XView flexGrow={1} marginLeft={6}>
+        </div>
+        <div className={homeFormBtnClass}>
             <EmailButton
                 path={LandingLinks.signup + '?email=true'}
                 text="Sign up with Email"
                 rounded={true}
             />
-        </XView>
+        </div>
     </div>
 );
