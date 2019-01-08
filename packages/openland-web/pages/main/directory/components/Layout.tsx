41145371f5158125049a433f9ab18bd27b1e8fbc
Max Volkov <wolkow_1996@mail.ru>
2019-01-08 19:52:11 +0300

    feat(web): Directory adaptive markup

--- packages/openland-web/pages/main/directory/components/Layout.tsx
+++ packages/openland-web/pages/main/directory/components/Layout.tsx
@@ -9,17 +9,24 @@ import OrganizationsIcon from 'openland-icons/dir-organizations.svg';
 import CommunityIcon from 'openland-icons/dir-communities.svg';
 import RightIcon from 'openland-icons/ic-arrow-rignt.svg';
 import PlusIcon from 'openland-icons/dir-new.svg';
-import { XVertical } from 'openland-x-layout/XVertical';
 import { XButton } from 'openland-x/XButton';
 import { XPopper } from 'openland-x/XPopper';
 import { XMenuVertical, XMenuItem } from 'openland-x/XMenuItem';
 import { TextDirectory } from 'openland-text/TextDirectory';
 
-export const RootWrapper = Glamorous.div({
-    height: '100vh',
-    width: '100%',
-    display: 'flex',
-});
+const RootWrapperStyle = css`
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    @media (max-width: 700px) {
+        flex-direction: column;
+    }
+`;
+
+export const RootWrapper = (props: { children: any }) => (
+    <div className={RootWrapperStyle}>{props.children}</div>
+);
 
 const ContainerStyle = css`
     width: calc(100% - 344px);
@@ -31,6 +38,10 @@ const ContainerStyle = css`
     @media (max-width: 950px) {
         width: calc(100% - 230px);
     }
+    @media (max-width: 700px) {
+        width: 100%;
+        height: calc(100% - 53px);
+    }
 `;
 
 export const Container = (props: { children: any }) => (
@@ -41,33 +52,47 @@ export const Results = Glamorous(XScrollView2)({
     height: 'calc(100vh - 61px)',
 });
 
-const SidebarWrapper = Glamorous.div({
-    width: 344,
-    height: '100%',
-    borderRight: '1px solid #ececec',
-    backgroundColor: '#ffffff',
-    '@media (max-width: 1100px)': {
-        width: 300,
-    },
-    '@media (max-width: 950px)': {
-        width: 230,
-    },
-});
+const SidebarWrapper = css`
+    width: 344px;
+    height: 100%;
+    border-right: 1px solid #ececec;
+    background-color: #ffffff;
+    flex-shrink: 0;
+    position: relative;
+    @media (max-width: 1100px) {
+        width: 300px;
+    }
+    @media (max-width: 950px) {
+        width: 230px;
+    }
+    @media (max-width: 700px) {
+        width: 100%;
+        display: flex;
+        height: 53px;
+        border-right: none;
+        border-bottom: 1px solid #ececec;
+    }
+`;
 
-const SidebarHeader = Glamorous.div({
-    display: 'flex',
-    padding: '14px 15px 19px 17px',
+const SidebarHeader = css`
+    display: flex;
+    padding: 14px 15px 19px 17px;
 
-    '& > span': {
-        flex: 1,
-        fontSize: 22,
-        lineHeight: '28px',
-        letterSpacing: 0,
-        fontWeight: 400,
-        color: '#000000',
-        opacity: 0.9,
-    },
-});
+    & > span {
+        flex: 1;
+        font-size: 22px;
+        line-height: 28px;
+        letter-spacing: 0;
+        font-weight: 400;
+        color: #000;
+        opacity: 0.9;
+    }
+    @media (max-width: 700px) {
+        width: 100%;
+        justify-content: space-between;
+        padding: 10px 15px 19px 17px;
+    }
+`;
 
 const SidebarItemWrapper = Glamorous(XLink)({
     display: 'flex',
@@ -123,10 +148,11 @@ interface SidebarItemProps {
     path: string;
     icon: 'communities' | 'organizations' | 'rooms' | 'people';
     active?: boolean;
+    onClick: () => void;
 }
 
 const SidebarItem = (props: SidebarItemProps) => (
-    <SidebarItemWrapper path={props.path} className={props.active ? 'is-active' : ''}>
+    <SidebarItemWrapper path={props.path} className={props.active ? 'is-active' : ''} onClick={props.onClick}>
         <div className="icon-wrapper">
             {props.icon === 'rooms' && <RoomIcon/>}
             {props.icon === 'people' && <PeopleIcon/>}
@@ -145,6 +171,14 @@ const NewButtonWrapper = Glamorous(XButton)({
     '& svg > g > path:last-child': {
         fill: '#1790ff',
         opacity: 0.5,
+    },
+    '@media (max-width: 700px)': {
+        '& span': {
+            display: 'none',
+        },
+        '& svg': {
+            margin: '0 !important',
+        },
     },
 });
 
@@ -219,29 +253,93 @@ class NewButton extends React.Component<{}, { show?: boolean }> {
     }
 }
 
-export const Sidebar = (props: { active?: string }) => (
-    <SidebarWrapper>
-        <SidebarHeader>
-            <span>Directory</span>
-            <NewButton/>
-        </SidebarHeader>
-        <XVertical separator={0}>
-            <SidebarItem path="/directory" icon="rooms" active={props.active === 'rooms'}/>
-            <SidebarItem
-                path="/directory/people"
-                icon="people"
-                active={props.active === 'people'}
-            />
-            <SidebarItem
-                path="/directory/organizations"
-                icon="organizations"
-                active={props.active === 'organizations'}
-            />
-            <SidebarItem
-                path="/directory/communities"
-                icon="communities"
-                active={props.active === 'communities'}
-            />
-        </XVertical>
-    </SidebarWrapper>
-);
+const MobileMenuButton = css`
+    display: none;
+    width: 140px;
+    height: 20px;
+    cursor: pointer;
+    align-items: center;
+    justify-content: center;
+    opacity: 0.9;
+    font-size: 18px;
+    font-weight: 600;
+    color: #000000;
+    position: absolute;
+    left: calc(50% - 70px);
+    top: calc(50% - 10px);
+    & > .select-icon {
+        transform: rotate(90deg);
+        margin-left: 6px;
+    }
+    @media (max-width: 700px) {
+        display: flex;
+    }
+`;
+
+const LinksWrapper = css`
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    background-color: #fff;
+    @media (max-width: 700px) {
+        position: absolute;
+        z-index: 1;
+        overflow: hidden;
+        height: 0;
+        top: 53px;
+        width: 100%;
+        
+        &.show {
+            height: auto;
+        }
+    }
+`;
+
+export const Sidebar = React.memo<{ active?: string }>((props) => {
+    let [showMenu, handler] = React.useState(false);
+
+    const menuHandler = () => {
+        handler(!showMenu);
+    };
+
+    return (
+        <div className={SidebarWrapper}>
+            <div className={SidebarHeader}>
+                <span>Directory</span>
+                <NewButton/>
+            </div>
+            <div className={MobileMenuButton} onClick={menuHandler}>
+                {props.active && (
+                    <span>{props.active.charAt(0).toUpperCase() + props.active.slice(1)}</span>
+                )}
+                <RightIcon className="select-icon"/>
+            </div>
+            <div className={`${LinksWrapper} ${showMenu ? 'show' : undefined}`}>
+                <SidebarItem
+                    path="/directory"
+                    icon="rooms"
+                    active={props.active === 'rooms'}
+                    onClick={menuHandler}
+                />
+                <SidebarItem
+                    path="/directory/people"
+                    icon="people"
+                    active={props.active === 'people'}
+                    onClick={menuHandler}
+                />
+                <SidebarItem
+                    path="/directory/organizations"
+                    icon="organizations"
+                    active={props.active === 'organizations'}
+                    onClick={menuHandler}
+                />
+                <SidebarItem
+                    path="/directory/communities"
+                    icon="communities"
+                    active={props.active === 'communities'}
+                    onClick={menuHandler}
+                />
+            </div>
+        </div>
+    );
+});
