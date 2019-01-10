fe462e396a326cbdf39cace30f5ea70c38860d98
Steve Kite <steve@statecraft.one>
2019-01-10 21:53:31 +0300

    feat(web): Trying to implement mention callback

--- packages/openland-web/pages/dev/ui/rich.page.tsx
+++ packages/openland-web/pages/dev/ui/rich.page.tsx
@@ -6,9 +6,11 @@ import { XVertical } from 'openland-x-layout/XVertical';
 import { XTitle } from 'openland-x/XTitle';
 import { XRichTextInput2 } from 'openland-x/XRichTextInput2';
 import { XView } from 'react-mental';
+import { XButton } from 'openland-x/XButton';
 
 export default withApp('UI Framework - Rich Input', 'viewer', props => {
     let [currentWord, setCurrentWord] = React.useState<string | undefined>(undefined);
+    let ref = React.useRef<XRichTextInput2>(null);
     return (
         <DevDocsScaffold title="Rich Input">
             <XContent>
@@ -17,8 +19,14 @@ export default withApp('UI Framework - Rich Input', 'viewer', props => {
                     <XView height={24}>
                         {currentWord}
                     </XView>
+                    <XButton
+                        text="set mention"
+                        onClick={() => {
+                            ref.current!.applyMention({ id: 'someid', name: 'Fabulous developer' })
+                        }}
+                    />
                     <XView backgroundColor="rgba(0,0,0,0.1)">
-                        <XRichTextInput2 onCurrentWordChanged={setCurrentWord} />
+                        <XRichTextInput2 ref={ref} onCurrentWordChanged={setCurrentWord} />
                     </XView>
                 </XVertical>
             </XContent>
--- packages/openland-x/XRichTextInput2.tsx
+++ packages/openland-x/XRichTextInput2.tsx
@@ -1,15 +1,12 @@
 import * as React from 'react';
-import { Editor, EditorState } from 'draft-js';
+import { Editor, EditorState, Modifier } from 'draft-js';
 import { canUseDOM } from 'openland-x-utils/canUseDOM';
 
-function findActiveWord(state: EditorState): string | undefined {
+function findActiveWordStart(state: EditorState): number {
     let content = state.getCurrentContent();
     let selection = state.getSelection();
-    if (!selection.getHasFocus()) {
-        return undefined;
-    }
     if (selection.getStartKey() !== selection.getEndKey()) {
-        return undefined;
+        return -1;
     }
     let text = content.getBlockForKey(selection.getStartKey()).getText();
 
@@ -21,8 +18,18 @@ function findActiveWord(state: EditorState): string | undefined {
             break;
         }
     }
+    return startIndex + 1;
+}
 
-    let res = text.substring(startIndex + 1, selection.getStartOffset());
+function findActiveWord(state: EditorState): string | undefined {
+    let content = state.getCurrentContent();
+    let selection = state.getSelection();
+    if (!selection.getHasFocus()) {
+        return undefined;
+    }
+    let startIndex = findActiveWordStart(state);
+    let text = content.getBlockForKey(selection.getStartKey()).getText();
+    let res = text.substring(startIndex, selection.getStartOffset());
     if (res.length === 0) {
         return undefined;
     } else {
@@ -30,21 +37,53 @@ function findActiveWord(state: EditorState): string | undefined {
     }
 }
 
-export interface XRichTextInput2 {
+export interface XRichTextInput2Props {
     onCurrentWordChanged?: (word: string | undefined) => void;
 }
 
-export const XRichTextInput2 = React.memo<XRichTextInput2>((props) => {
-    if (!canUseDOM) {
-        return null;
-    }
-    let [editorState, setEditorState] = React.useState(() => EditorState.createEmpty());
-    let editorStateHandler = React.useCallback((value: EditorState) => {
-        if (props.onCurrentWordChanged) {
-            props.onCurrentWordChanged(findActiveWord(value));
-        }
-        return setEditorState(value);
-    }, [props.onCurrentWordChanged]);
+export class XRichTextInput2 extends React.PureComponent<XRichTextInput2Props, { editorState: EditorState }> {
 
-    return <Editor editorState={editorState} onChange={editorStateHandler} />;
-});
+    private ref = React.createRef<Editor>();
+
+    constructor(props: XRichTextInput2Props) {
+        super(props);
+        this.state = { editorState: EditorState.createEmpty() };
+    }
+
+    applyMention = (src: { name: string, id: string }) => {
+        this.setState((s) => {
+            let selection = s.editorState.getSelection()
+            let start = findActiveWordStart(s.editorState);
+            if (start < 0) {
+                return s;
+            }
+            let s2 = selection.merge({
+                anchorOffset: start,
+                focusOffset: selection.getFocusOffset()
+            }) as any;
+            let replace = Modifier.replaceText(s.editorState.getCurrentContent(), s2, src.name)
+
+            let s3 = EditorState.push(
+                s.editorState,
+                replace,
+                'insert-mention' as any,
+            );
+            return { editorState: s3 };
+        }, () => this.ref.current!.focus());
+    }
+
+    private handleEditorChange = (value: EditorState) => {
+        if (this.props.onCurrentWordChanged) {
+            this.props.onCurrentWordChanged(findActiveWord(value));
+        }
+        this.setState({ editorState: value });
+    }
+
+    render() {
+        if (!canUseDOM) {
+            return null;
+        }
+
+        return <Editor ref={this.ref} stripPastedStyles={true} editorState={this.state.editorState} onChange={this.handleEditorChange} />;
+    }
+}
