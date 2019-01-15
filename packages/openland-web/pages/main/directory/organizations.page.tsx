f322888d6a7dcda841d1c1bcf0e381f785d03592
Fred Morozov <morozov@fred.md>
2019-01-15 20:02:08 +0200

    fix(web): fix organization directory pagination

--- packages/openland-web/pages/main/directory/organizations.page.tsx
+++ packages/openland-web/pages/main/directory/organizations.page.tsx
@@ -44,8 +44,8 @@ const OrganizationCards = withExploreOrganizations(props => {
                         <XOrganizationCard key={'_org_card_' + i.node.id} organization={i.node} />
                     ))}
                     <PagePagination
+                        currentRoute="/directory/organizations"
                         pageInfo={props.data.items.pageInfo}
-                        onPageChange={(props as any).onPageChange}
                     />
                 </XContentWrapper>
             )}
@@ -166,20 +166,19 @@ class RootComponent extends React.Component<XWithRouter, RootComponentState> {
                                                 paddingBottom={12}
                                             />
                                         )}
-                                        {query.length > 0 &&
-                                            orgCount > 0 && (
-                                                <XSubHeader
-                                                    title="Organizations"
-                                                    counter={orgCount}
-                                                    right={
-                                                        <SortPicker
-                                                            sort={this.state.sort}
-                                                            onPick={this.changeSort}
-                                                        />
-                                                    }
-                                                    paddingBottom={12}
-                                                />
-                                            )}
+                                        {query.length > 0 && orgCount > 0 && (
+                                            <XSubHeader
+                                                title="Organizations"
+                                                counter={orgCount}
+                                                right={
+                                                    <SortPicker
+                                                        sort={this.state.sort}
+                                                        onPick={this.changeSort}
+                                                    />
+                                                }
+                                                paddingBottom={12}
+                                            />
+                                        )}
                                         <Organizations
                                             featuredFirst={this.state.sort.featured}
                                             orderBy={this.state.sort.orderBy}
