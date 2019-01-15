ccf4d4f2be454e484a47c43d393547d6425e3afb
Sergey Lapin <lapanoid@gmail.com>
2019-01-15 16:03:13 +0300

    wip(web): add all states for navigation

--- packages/openland-web/pages/dev/ui/mobileNavigation.page.tsx
+++ packages/openland-web/pages/dev/ui/mobileNavigation.page.tsx
@@ -1,15 +1,34 @@
 import * as React from 'react';
 import { withApp } from '../../../components/withApp';
-import { XVertical } from 'openland-x-layout/XVertical';
 import { XContent } from 'openland-x-layout/XContent';
-import { XTitle } from 'openland-x/XTitle';
 import { DevDocsScaffold } from './components/DevDocsScaffold';
+import { CreateWrapIntoState } from './signin/utils';
+
+const WrapIntoState = CreateWrapIntoState({
+    root: {
+        mode: {
+            type: 'select',
+            value: [
+                'sidebar',
+                'headerWithSearch',
+                'dialogHeader',
+                'roomsWithSearch',
+                'roomsSelectionMode',
+            ],
+            default: 'sidebar',
+        },
+    },
+});
 
 export default withApp('UI Framework - Mobile Navigation', 'viewer', props => {
     return (
         <DevDocsScaffold title="Mobile Navigation">
             <XContent>
-                <XVertical>TODO</XVertical>
+                <WrapIntoState>
+                    {({ branch, ...branchProps }: any) => {
+                        return <div>{JSON.stringify(branchProps, null, 2)}</div>;
+                    }}
+                </WrapIntoState>
             </XContent>
         </DevDocsScaffold>
     );
