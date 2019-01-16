c274bfbaaa2e5ab6e7a92c471be19c85a31f80a0
Steve Kite <steve@statecraft.one>
2019-01-16 19:24:57 +0300

    feat(web): Enable event persisting

--- packages/openland-engines/Tracking.ts
+++ packages/openland-engines/Tracking.ts
@@ -1,16 +1,50 @@
 import { AppStorage } from 'openland-y-runtime-native/AppStorage';
 import uuid from 'uuid/v4';
+import ApolloClient from 'apollo-client';
+import gql from 'graphql-tag';
+import { backoff } from 'openland-y-utils/timer';
+
+const Persist = gql`
+    mutation PersistEvents($did: String!, $events: [Event!]!) {
+        track(did: $did, events: $events)
+    }
+`;
 
 class TrackingEngine {
 
+    private client!: ApolloClient<{}>;
     private initPromise: Promise<void> | undefined;
     private deviceId!: string;
+    private pending: { id: string, event: string, params?: string }[] = [];
+    private isSending = false;
 
-    track(event: string, params?: { [key: string]: any }) {
-        this.doTrack(event, params);
+    setClient(client: ApolloClient<{}>) {
+        if (!this.client) {
+            this.client = client;
+            this.flush();
+        }
     }
 
-    private async doTrack(event: string, params?: { [key: string]: any }) {
+    track(event: string, params?: { [key: string]: any }) {
+        console.log('Event: ' + event, params);
+
+        this.pending.push({ event: event, params: params ? JSON.stringify(params) : undefined, id: uuid() });
+
+        this.flush();
+    }
+
+    private async flush() {
+        if (!this.client) {
+            return;
+        }
+        if (this.isSending) {
+            return;
+        }
+        if (this.pending.length === 0) {
+            return;
+        }
+
+        this.isSending = true;
         if (!this.initPromise) {
             this.initPromise = (async () => {
                 let did = await AppStorage.readKey<string>('device-id');
@@ -26,9 +60,21 @@ class TrackingEngine {
 
         await this.initPromise;
 
-        console.log('Event: ' + event, params);
+        await backoff(async () => {
+            let events = [...this.pending];
+            let res = await this.client.mutate({
+                mutation: Persist, variables: {
+                    did: this.deviceId,
+                    events: events
+                }
+            });
+            if (res.errors && res.errors.length > 0) {
+                throw Error('Error during persisting event');
+            }
+        })
 
-        // TODO: Implement reporting
+        this.isSending = false;
+        this.flush();
     }
 }
 
