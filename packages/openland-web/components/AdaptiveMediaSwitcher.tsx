8eef90c9a360e92ec0fc9abb8e17010cb86b8677
Sergey Lapin <lapanoid@gmail.com>
2019-01-18 11:44:23 +0300

    fix(web): remove color red, blue from AdaptiveMediaSwitcher, this was for debug

--- packages/openland-web/components/AdaptiveMediaSwitcher.tsx
+++ packages/openland-web/components/AdaptiveMediaSwitcher.tsx
@@ -4,22 +4,18 @@ import { css } from 'linaria';
 const showMobileClassName = css`
     width: 100%;
     @media (min-width: 700px) {
-        color: red;
         display: none;
     }
     @media (max-width: 700px) {
-        color: blue;
         display: block;
     }
 `;
 const showDesktopClassName = css`
     width: 100%;
     @media (min-width: 700px) {
-        color: red;
         display: block;
     }
     @media (max-width: 700px) {
-        color: blue;
         display: none;
     }
 `;
