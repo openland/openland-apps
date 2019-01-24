507b014af825ee0325347984aaf879939bc70ce9
Steve Kite <steve@statecraft.one>
2019-01-24 17:20:46 +0300

    fix(android): Fix truncated text

--- packages/openland-mobile/pages/main/components/UserView.tsx
+++ packages/openland-mobile/pages/main/components/UserView.tsx
@@ -13,7 +13,7 @@ export const UserView = (props: { user: UserShort, isAdmin?: boolean, role?: str
         <View alignSelf="center" flexGrow={1} flexBasis={0} alignItems="flex-start" justifyContent="center" flexDirection="column" opacity={props.enabled === false ? 0.5 : 1}>
             <View flexDirection="row">
                 {props.isAdmin && <Image source={require('assets/ic-star-16.png')} style={{ tintColor: '#fbc139', alignSelf: 'center', marginRight: 2, marginBottom: 4 }} />}
-                <Text numberOfLines={1} style={{ fontSize: 16, fontWeight: '500', color: '#181818', height: 19, marginBottom: 4 }}>{props.user.name}{props.user.primaryOrganization && <Text style={{ fontSize: 15, color: '#99a2b0' }}>  {props.user.primaryOrganization.name}</Text>}</Text>
+                <Text numberOfLines={1} style={{ fontSize: 16, fontWeight: '500', color: '#181818', height: 22, marginBottom: 0 }}>{props.user.name}{props.user.primaryOrganization && <Text style={{ fontSize: 15, color: '#99a2b0' }}>  {props.user.primaryOrganization.name}</Text>}</Text>
             </View>
             <PresenceComponent uid={props.user.id} style={{ fontSize: 14, color: '#99a2b0', height: 16 }} onlineStyle={{ color: '#0084fe' }} />
         </View>
