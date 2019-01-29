f00536bba526e95b2b0aa375a27c5a166ee6fccd
Steve Kite <steve@statecraft.one>
2019-01-29 14:15:10 +0300

    fix(mobile): Fix thread production bundling

--- scripts/native_bundle.sh
+++ scripts/native_bundle.sh
@@ -4,7 +4,9 @@ yarn native:build
 rm -fr ./build/bundle-ios
 rm -fr ./build/bundle-android 
 mkdir ./build/bundle-ios
+mkdir ./build/bundle-ios/threads
 mkdir ./build/bundle-android
+mkdir ./build/bundle-android/threads
 ./node_modules/.bin/react-native bundle --platform ios --entry-file index.js --bundle-output ./build/bundle-ios/main.jsbundle --assets-dest ./build/bundle-ios/ --dev false
 ./node_modules/.bin/react-native bundle --platform ios --entry-file index.thread.js --bundle-output ./build/bundle-ios/threads/index.thread.jsbundle --assets-dest ./build/bundle-ios/ --dev false
 ./node_modules/.bin/react-native bundle --platform android --entry-file index.js --bundle-output ./build/bundle-android/main.jsbundle --assets-dest ./build/bundle-android/ --dev false
