95b7f257043c63d27f88d51e6c88ac3c99bd31f8
Steve Kite <steve@statecraft.one>
2019-01-29 14:02:05 +0300

    fix(mobile): Fix thread production bundling

--- scripts/native_bundle.sh
+++ scripts/native_bundle.sh
@@ -6,6 +6,6 @@ rm -fr ./build/bundle-android
 mkdir ./build/bundle-ios
 mkdir ./build/bundle-android
 ./node_modules/.bin/react-native bundle --platform ios --entry-file index.js --bundle-output ./build/bundle-ios/main.jsbundle --assets-dest ./build/bundle-ios/ --dev false
-./node_modules/.bin/react-native bundle --platform ios --entry-file index.thread.js --bundle-output ./build/bundle-ios/index.thread.jsbundle --assets-dest ./build/bundle-ios/ --dev false
+./node_modules/.bin/react-native bundle --platform ios --entry-file index.thread.js --bundle-output ./build/bundle-ios/threads/index.thread.jsbundle --assets-dest ./build/bundle-ios/ --dev false
 ./node_modules/.bin/react-native bundle --platform android --entry-file index.js --bundle-output ./build/bundle-android/main.jsbundle --assets-dest ./build/bundle-android/ --dev false
-./node_modules/.bin/react-native bundle --platform android --entry-file index.thread.js --bundle-output ./build/bundle-android/index.thread.jsbundle --assets-dest ./build/bundle-android/ --dev false
+./node_modules/.bin/react-native bundle --platform android --entry-file index.thread.js --bundle-output ./build/bundle-android/threads/index.thread.bundle --assets-dest ./build/bundle-android/ --dev false
