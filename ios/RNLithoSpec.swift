a7bb2f8b448339b9a61ea6d47b7bf82e59d43929
Steve Kite <steve@statecraft.one>
2019-02-07 00:43:33 +0300

    feat(*): Upgrade react

---
+++ ios/RNLithoSpec.swift
@@ -0,0 +1,85 @@
+//
+//  RNLithoSpec.swift
+//  openland
+//
+//  Created by Steve Kite on 2/6/19.
+//  Copyright © 2019 Openland. All rights reserved.
+//
+
+import Foundation
+
+enum RNLithoFlexDirection {
+  case row
+  case column
+  case bind(String)
+}
+
+enum RNLithoFlexAlignItems {
+  case start
+  case end
+  case center
+  case stretch
+  case bind(String)
+}
+
+enum RNLithoFlexAlignSelf {
+  case start
+  case end
+  case center
+  case stretch
+  case bind(String)
+}
+
+enum RNLithoFlexJustifyContent {
+  case start
+  case end
+  case center
+  case bind(String)
+}
+
+enum RNLithoDoubleValue {
+  case value(Double)
+  case bind(String)
+  case empty
+}
+
+class RNLithoIf {
+}
+
+class RNLithoText {
+}
+
+class RNLithoView {
+  
+}
+
+private func resolveViewSpec(_ spec: JSON) {
+  
+}
+
+private func resolveLithoSpec(_ spec: JSON) {
+  if (src.array != nil) {
+    fatalError("Spec can't be an array")
+  }
+  if (src["type"].string == nil) {
+    fatalError("Type can't be empty")
+  }
+  
+  let type = src["type"].stringValue
+  if type == "tview" {
+    resolveViewSpec(src)
+  } else if type == "ttext" {
+    
+  } else if type == "tif" {
+    
+  } else if type == "ttextbind" {
+    
+  }
+  
+  fatalError("Unknown view type:" + type)
+}
+
+func parseLithoSpec(spec: String) {
+  let src = JSON(parseJSON: spec)
+  resolveLithoSpec(src)
+}
