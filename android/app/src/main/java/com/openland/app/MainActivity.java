553e7aeabd1208a65ee1a6cc998a2d60f1f46d04
kor_ka <gleb.putintsev@openland.com>
2019-02-12 20:43:16 +0300

    fix(android): white status bar

--- android/app/src/main/java/com/openland/app/MainActivity.java
+++ android/app/src/main/java/com/openland/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.openland.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.facebook.react.ReactActivity;
 
@@ -47,6 +48,12 @@ public class MainActivity extends ReactActivity {
                 Log.d("MainActivity","onDestroy");
             }
         };
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR | View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
     }
 
     @Override
