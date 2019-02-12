0a6f7bd04ff5445ce9c1c87b2e832aad6910cc63
Fred Morozov <morozov@fred.md>
2019-02-12 14:37:54 +0300

    fix(mobile): Non-breaking space in mentions & service messages

--- packages/openland-mobile/messenger/components/service/views/RoomWrapper.tsx
+++ packages/openland-mobile/messenger/components/service/views/RoomWrapper.tsx
@@ -3,6 +3,7 @@ import { ASText } from 'react-native-async-view/ASText';
 import { RoomShort } from 'openland-api/Types';
 import { TextStyles } from '../../../../styles/AppStyles';
 import { ThemeContext } from '../../AsyncServiceMessageView';
+import { useNonBreakingSpaces } from 'openland-y-utils/TextProcessor';
 
 interface RoomWrapperProps {
     room: RoomShort;
@@ -22,7 +23,7 @@ export const RoomWrapper = (props: RoomWrapperProps) => (
                 marginRight={6}
                 onPress={() => props.onRoomPress(props.room.id)}
             >
-                {props.room.__typename === 'PrivateRoom' ? props.room.user.name : props.room.title}
+                {props.room.__typename === 'PrivateRoom' ? useNonBreakingSpaces(props.room.user.name) : props.room.title}
             </ASText>
         )}
     </ThemeContext.Consumer>
