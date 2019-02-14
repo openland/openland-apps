dca08d6e68a02eff3aeb3847e205163ad5d27c98
Steve Kite <steve@statecraft.one>
2019-02-14 18:55:33 +0300

    feat(mobile): Empty babel transformer

---
+++ packages/react-native-async-view-babel/src/createTraversal.ts
@@ -0,0 +1,9 @@
+import * as t from '@babel/types';
+import { VisitNodeObject } from "@babel/traverse";
+
+export function createTraversal() {
+    // const traverseOptions: { JSXElement: VisitNodeObject<t.JSXElement>, Program: VisitNodeObject<t.Program> } = {
+
+    // }
+    return {};
+}
