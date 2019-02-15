c6e49bddb86103eb4961e556046841d34cae412e
Steve Kite <steve@statecraft.one>
2019-02-15 13:40:07 +0300

    Revert "Revert "feat(mobile): Add babel processor""
    This reverts commit 4f4966ad70fa8e2fa3b40e3dba9c6fe5dd061d27.

---
+++ packages/react-native-async-view-babel/lib/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const createTraversal_1 = require("./src/createTraversal");
+exports.default = () => {
+    return { visitor: createTraversal_1.createTraversal() };
+};
+//# sourceMappingURL=index.js.map
