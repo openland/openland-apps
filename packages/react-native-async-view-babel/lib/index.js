4f4966ad70fa8e2fa3b40e3dba9c6fe5dd061d27
Sergey Lapin <lapanoid@gmail.com>
2019-02-15 13:31:32 +0300

    Revert "feat(mobile): Add babel processor"
    This reverts commit 6a8d170113e587c94772013bda86c95665090681.

--- packages/react-native-async-view-babel/lib/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const createTraversal_1 = require("./src/createTraversal");
-exports.default = () => {
-    return { visitor: createTraversal_1.createTraversal() };
-};
-//# sourceMappingURL=index.js.map
--- packages/react-native-async-view-babel/src/createTraversal.ts
+++ packages/react-native-async-view-babel/src/createTraversal.ts
@@ -1,55 +1,9 @@
 import * as t from '@babel/types';
-import { VisitNodeObject, NodePath } from "@babel/traverse";
+import { VisitNodeObject } from "@babel/traverse";
 
 export function createTraversal() {
-    const traverseOptions: {} = {
-        Program: {
-            enter(traversePath: NodePath<t.Program>) {
-                traversePath.traverse({
-                    JSXElement: {
-                        enter(traversePath2: NodePath<t.Node>) {
-                            // throw Error('!!!');
-                            // throw traversePath2.buildCodeFrameError('!!!');
-                            let node = traversePath2.node as t.JSXElement;
-                            if ((node.openingElement.name as t.JSXIdentifier).name !== 'ASFlex') {
-                                return;
-                            }
-                            // throw traversePath2.buildCodeFrameError(JSON.stringify(node.openingElement.name));
+    // const traverseOptions: { JSXElement: VisitNodeObject<t.JSXElement>, Program: VisitNodeObject<t.Program> } = {
 
-                            let hasBlacklisted = !!node.openingElement.attributes
-                                .filter((v) => v.type === 'JSXAttribute')
-                                .find((v) => !!['onPress', 'onLongPress', 'backgroundColor'].find((v2) => ((v as t.JSXAttribute).name as t.JSXIdentifier).name === v2))
-                            if (hasBlacklisted) {
-                                return;
-                            }
-
-                            (node.openingElement.name as t.JSXIdentifier).name = 'asyncview';
-                            node.openingElement.attributes.push(t.jsxAttribute(
-                                t.jsxIdentifier('asyncViewName'),
-                                t.jsxExpressionContainer(t.stringLiteral('flex'))
-                            ))
-
-                            if (node.closingElement) {
-                                (node.closingElement.name as t.JSXIdentifier).name = 'asyncview'
-                            }
-                        }
-                    }
-                })
-                // throw traversePath.buildCodeFrameError('???');
-                // isImported = false;
-                // pageHasStyles = false;
-                // body = traversePath.node.body;
-                // pending = [];
-            },
-            exit(traversePath: NodePath<t.Program>) {
-                // if (!isImported && pageHasStyles) {
-                //     for (let p of pending) {
-                //         body.unshift(p);
-                //     }
-                //     body.unshift(t.importDeclaration([t.importSpecifier(t.identifier('calculateStyles'), t.identifier('calculateStyles'))], t.stringLiteral('openland-x-styles/calculateStyles')));
-                // }
-            }
-        }
-    }
-    return traverseOptions;
+    // }
+    return {};
 }
