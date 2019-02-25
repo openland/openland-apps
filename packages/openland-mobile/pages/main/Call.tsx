b29e903b9a46f30ccb00e8134ce8b3e3166e5ecb
Steve Kite <steve@statecraft.one>
2019-02-25 18:27:20 +0300

    design(mobile): Better call screen design

--- packages/openland-mobile/pages/main/Call.tsx
+++ packages/openland-mobile/pages/main/Call.tsx
@@ -11,6 +11,7 @@ import { XMemo } from 'openland-y-utils/XMemo';
 import { SStatusBar } from 'react-native-s/SStatusBar';
 import { ZModalController, showModal } from 'openland-mobile/components/ZModal';
 import { ZLoader } from 'openland-mobile/components/ZLoader';
+import LinearGradient from 'react-native-linear-gradient';
 
 let Content = XMemo<{ id: string, modal: ZModalController }>((props) => {
     let room = getClient().useRoomTiny({ id: props.id }).room!!;
@@ -20,7 +21,7 @@ let Content = XMemo<{ id: string, modal: ZModalController }>((props) => {
     let title = room.__typename === 'PrivateRoom' ? room.user.name : room.title;
     let placeholderKey = room.id
 
-    React.useEffect(() => {
+    React.useLayoutEffect(() => {
         SStatusBar.setBarStyle('light-content');
         return () => {
             SStatusBar.setBarStyle('dark-content');
@@ -28,40 +29,42 @@ let Content = XMemo<{ id: string, modal: ZModalController }>((props) => {
     }, []);
 
     return (
-        <ASSafeAreaView flexDirection="column" alignItems="stretch" flexGrow={1}>
-            <View alignItems="center" justifyContent="center" paddingTop={40} paddingHorizontal={16}>
-                {/* <ZAvatar size={80} placeholderKey={placeholderKey} placeholderTitle={title} /> */}
-                {/* <View style={{ marginLeft: 16 }}>
+        <LinearGradient colors={['#0084fe', '#004280']} style={{ flexGrow: 1, flexDirection: 'column' }}>
+            <ASSafeAreaView flexDirection="column" alignItems="stretch" flexGrow={1}>
+                <View alignItems="center" justifyContent="center" paddingTop={82} paddingHorizontal={16}>
+                    {/* <ZAvatar size={80} placeholderKey={placeholderKey} placeholderTitle={title} /> */}
+                    {/* <View style={{ marginLeft: 16 }}>
                     <Text style={{ fontSize: 32, height: 36, color: 'white' }} numberOfLines={1}>{title}</Text>
                 </View> */}
-                <Text style={{ fontSize: 28, height: 34, color: 'white' }} numberOfLines={1}>{title}</Text>
-            </View>
-            <View flexGrow={1} />
-            <View justifyContent="center" alignItems="center" marginBottom={56}>
-                <TouchableOpacity
-                    onPress={() => {
-                        SStatusBar.setBarStyle('dark-content');
-                        props.modal.hide();
-                    }}
-                    style={{ width: 70, height: 70 }}
-                >
-                    <View backgroundColor="#f6564e" width={70} height={70} borderRadius={35} alignItems="center" justifyContent="center">
-                        <Image source={require('assets/ic-call-end-36.png')} style={{ tintColor: 'white' }} />
-                    </View>
-                </TouchableOpacity>
-            </View>
-            <CallController id={conference.id} conference={conference} />
-            {/* <View flexDirection="row" paddingHorizontal={16} paddingVertical={16}>
+                    <Text style={{ fontSize: 28, height: 34, fontWeight: '600', color: 'white' }} numberOfLines={1}>{title}</Text>
+                </View>
+                <View flexGrow={1} />
+                <View justifyContent="center" alignItems="center" marginBottom={56}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            SStatusBar.setBarStyle('dark-content');
+                            props.modal.hide();
+                        }}
+                        style={{ width: 70, height: 70 }}
+                    >
+                        <View backgroundColor="#f6564e" width={70} height={70} borderRadius={35} alignItems="center" justifyContent="center">
+                            <Image source={require('assets/ic-call-end-36.png')} style={{ tintColor: 'white' }} />
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <CallController id={conference.id} conference={conference} />
+                {/* <View flexDirection="row" paddingHorizontal={16} paddingVertical={16}>
                 {conference.peers.map((v) => (<View><Text>{v.user.name}</Text></View>))}
             </View> */}
-        </ASSafeAreaView>
+            </ASSafeAreaView>
+        </LinearGradient>
     )
 });
 
 export function showCallModal(id: string) {
     showModal((ctx) => {
         return (
-            <View backgroundColor="#2a4763" flexGrow={1} flexDirection="column">
+            <View flexGrow={1} flexDirection="column">
                 <React.Suspense fallback={<ZLoader />}>
                     <Content id={id} modal={ctx} />
                 </React.Suspense>
