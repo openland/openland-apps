70901a5c801fbb4c4992649d8330643cc434fe4e
Max Volkov <wolkow_1996@mail.ru>
2019-02-28 20:44:48 +0300

    fix(web): landing banner

--- packages/openland-landing/components/MobileCustomPromo.tsx
+++ packages/openland-landing/components/MobileCustomPromo.tsx
@@ -5,19 +5,19 @@ import { XView, XImage } from 'react-mental';
 import CloseIcon from 'openland-icons/ic-close-banner.svg';
 import { canUseDOM } from 'openland-y-utils/canUseDOM';
 
-const PromoWrapper = Glamorous.div({
+const PromoWrapper = Glamorous.div<{ hidden: boolean }>(props => ({
     height: 60,
     width: '100%',
     flexShrink: 0,
     backgroundColor: '#f7f7f7',
-    display: 'flex',
+    display: props.hidden ? 'none' : 'flex',
     alignItems: 'center',
     flexDirection: 'row',
     justifyContent: 'space-between',
     paddingLeft: 16,
     paddingRight: 16,
     borderBottom: '1px solid #ececec',
-});
+}));
 
 const PromoContent = Glamorous.div({
     display: 'flex',
@@ -35,81 +35,80 @@ export const MobileCustomPromo = () => {
     if (!canUseDOM) {
         return null;
     }
-    const [banner, bannerHandler] = React.useState(true);
+    const [hidden, bannerHandler] = React.useState(true);
     const handleHideBanner = () => {
-        bannerHandler(false);
+        bannerHandler(true);
     };
-    const userAgent = window.navigator.userAgent;
     let iosChrome = false;
     let iosMozila = false;
     let androidMozila = false;
-    if (userAgent.match(/iPhone|iPad|iPod/i) && userAgent.match(/CriOS/i)) {
-        iosChrome = true;
-    }
-    if (userAgent.match(/iPhone|iPad|iPod/i) && userAgent.match(/Firefox/i)) {
-        iosMozila = true;
-    }
-    if (userAgent.match(/Android/i) && userAgent.match(/Firefox/i)) {
-        androidMozila = true;
-    }
-    if (!banner) {
-        return null;
-    }
-    if (iosChrome || iosMozila || androidMozila) {
-        return (
-            <PromoWrapper>
-                <PromoContent>
-                    <XView
-                        cursor="pointer"
-                        alignItems="center"
-                        justifyContent="center"
-                        width={25}
-                        height={25}
-                        marginRight={5}
-                        onClick={handleHideBanner}
-                    >
-                        <CloseIcon className={CloseBannerIconClassName} />
-                    </XView>
-                    <XImage
-                        width={48}
-                        height={48}
-                        src="/static/X/logo-5.png"
-                        srcSet="/static/X/logo-5@2x.png 2x"
-                        marginRight={12}
-                    />
-                    <XView>
-                        <XView fontSize={16} fontWeight="400" color="#000">
-                            Openland
-                        </XView>
-                        <XView fontSize={14} opacity={0.7} color="#000">
-                            {androidMozila ? 'Use Android app' : 'Use iOS app'}
-                        </XView>
-                    </XView>
-                </PromoContent>
+    React.useEffect(() => {
+        const userAgent = window.navigator.userAgent;
+        if (userAgent.match(/iPhone|iPad|iPod/i) && userAgent.match(/CriOS/i)) {
+            iosChrome = true;
+        }
+        if (userAgent.match(/iPhone|iPad|iPod/i) && userAgent.match(/Firefox/i)) {
+            iosMozila = true;
+        }
+        if (userAgent.match(/Android/i) && userAgent.match(/Firefox/i)) {
+            androidMozila = true;
+        }
+        if (iosChrome || iosMozila || androidMozila) {
+            bannerHandler(true);
+        }
+    });
+    return (
+        <PromoWrapper hidden={hidden}>
+            <PromoContent>
                 <XView
-                    as="a"
                     cursor="pointer"
-                    flexDirection="row"
                     alignItems="center"
                     justifyContent="center"
-                    paddingHorizontal={12}
-                    paddingVertical={6}
-                    width={82}
-                    height={28}
-                    borderRadius={8}
-                    backgroundColor="#007aff"
-                    color="#fff"
-                    hoverColor="#fff"
-                    fontSize={14}
-                    fontWeight="600"
-                    hoverTextDecoration="none"
-                    whiteSpace="nowrap"
-                    href={androidMozila ? 'https://oplnd.com/android' : 'https://oplnd.com/ios'}
+                    width={25}
+                    height={25}
+                    marginRight={5}
+                    onClick={handleHideBanner}
                 >
-                    USE APP
+                    <CloseIcon className={CloseBannerIconClassName} />
                 </XView>
-            </PromoWrapper>
-        );
-    }
-    return null;
+                <XImage
+                    width={48}
+                    height={48}
+                    src="/static/X/logo-5.png"
+                    srcSet="/static/X/logo-5@2x.png 2x"
+                    marginRight={12}
+                />
+                <XView>
+                    <XView fontSize={16} fontWeight="400" color="#000">
+                        Openland
+                    </XView>
+                    <XView fontSize={14} opacity={0.7} color="#000">
+                        {androidMozila ? 'Use Android app' : 'Use iOS app'}
+                    </XView>
+                </XView>
+            </PromoContent>
+            <XView
+                as="a"
+                cursor="pointer"
+                flexDirection="row"
+                alignItems="center"
+                justifyContent="center"
+                paddingHorizontal={12}
+                paddingVertical={6}
+                width={82}
+                height={28}
+                borderRadius={8}
+                backgroundColor="#007aff"
+                color="#fff"
+                hoverColor="#fff"
+                fontSize={14}
+                fontWeight="600"
+                hoverTextDecoration="none"
+                whiteSpace="nowrap"
+                href={androidMozila ? 'https://oplnd.com/android' : 'https://oplnd.com/ios'}
+            >
+                USE APP
+            </XView>
+        </PromoWrapper>
+    );
 };
