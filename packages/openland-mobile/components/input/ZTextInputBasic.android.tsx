5b6e2b1bd70a7215f803ab45f65c422643c08ec0
Fred Morozov <morozov@fred.md>
2019-03-04 16:29:41 +0300

    wip(android): material TextInput

--- packages/openland-mobile/components/input/ZTextInputBasic.android.tsx
+++ packages/openland-mobile/components/input/ZTextInputBasic.android.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { TextInput, Text, View, TextInputProps } from 'react-native';
 import { ThemeContext } from 'openland-mobile/themes/ThemeContext';
+import { TextField } from 'react-native-material-textfield';
 
 export interface ZTextInputBasicProps extends TextInputProps {
     invalid?: boolean;
@@ -14,50 +15,30 @@ export interface ZTextInputBasicProps extends TextInputProps {
 }
 
 export const ZTextInputBasic = (props: ZTextInputBasicProps) => {
-    let { title, prefix, border, invalid, enabled, placeholder, ...others } = props;
+    let { title, invalid, enabled, placeholder, ...others } = props;
     let theme = React.useContext(ThemeContext);
 
     return (
-        <View paddingLeft={16} paddingRight={16} flexDirection="column">
-            <View flexDirection="row" alignItems="stretch" flexGrow={1}>
-                {prefix && (
-                    <Text
-                        style={{
-                            color: '#000000',
-                            fontSize: 16,
-                            lineHeight: 22,
-                            alignSelf: 'center',
-                        }}
-                    >
-                        {prefix}
-                    </Text>
-                )}
-                <TextInput
-                    {...others}
-                    placeholder={title || placeholder}
-                    placeholderTextColor="#a0a0a0"
-                    style={{
-                        color: theme.textColor,
-                        flex: 1,
-                        minHeight: props.border ? 49 : 50,
-                        fontSize: 16,
-                        padding: 0,
-                        margin: 0,
-                        borderWidth: 0,
-                        marginRight: 17,
-                    }}
-                />
-            </View>
-
-            {border && (
-                <View
-                    style={{
-                        height: 1,
-                        marginLeft: 0,
-                        backgroundColor: (invalid || !enabled) ? '#f6564e' : theme.separatorColor
-                    }}
-                />
-            )}
+        <View
+            paddingLeft={16}
+            paddingRight={16}
+            marginTop={title ? 10 : undefined}
+            height={props.multiline ? undefined : 50}
+        >
+            <TextField
+                {...others}
+                label={title}
+                placeholder={title ? undefined : placeholder}
+                labelFontSize={14}
+                tintColor="#0084fe"
+                baseColor={theme.separatorColor}
+                textColor={theme.textColor}
+                fontSize={16}
+                animationDuration={150}
+                labelPadding={0}
+                labelHeight={18}
+                lineWidth={1}
+            />
         </View>
     );
 }
