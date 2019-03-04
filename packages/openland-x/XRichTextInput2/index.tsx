4641695bd57909267f105b70e4d04751de2fdc64
Sergey Lapin <lapanoid@gmail.com>
2019-03-04 18:05:56 +0300

    perf(web): wrap xRich into memo

--- packages/openland-x/XRichTextInput2/index.tsx
+++ packages/openland-x/XRichTextInput2/index.tsx
@@ -23,117 +23,119 @@ export interface XRichTextInput2Props extends XFlexStyles {
     onCurrentWordChanged?: (word: string | undefined) => void;
 }
 
-export const XRichTextInput2 = React.forwardRef<XRichTextInput2RefMethods, XRichTextInput2Props>(
-    (props: XRichTextInput2Props, ref) => {
-        if (!canUseDOM) {
-            return null;
-        }
-
-        const { onSubmit, onChange, value, mentionsData, placeholder } = props;
-
-        const editorRef = React.useRef<Editor>(null);
-
-        const {
-            editorState,
-            setEditorState,
-            updateEditorStateFromText,
-            activeWord,
-            setActiveWord,
-            handleEditorChange,
-            addMention,
-            addEmoji,
-            onEmojiPicked,
-            getMentions,
-        } = useHandleEditorChange({
-            onChange,
-            value,
-        });
-
-        const { handlePastedText } = useHandlePastedText({ setEditorState });
-
-        const { resetAndFocus } = useInputMethods({
-            ref,
-            editorRef,
-            editorState,
-            setEditorState,
-            getMentions,
-        });
-
-        const { onPasteFiles } = usePasteFiles({
-            onPasteFile: props.onPasteFile,
-            resetAndFocus,
-        });
-
-        const emojiState = useEmojiSuggestions({
-            activeWord,
-        });
-
-        const mentionState = useMentionSuggestions({
-            mentionsData,
-            activeWord,
-        });
-
-        const applyCurrentSuggestedMention = () => {
-            const mentionEntry = mentionState.suggestions[mentionState.selectedEntryIndex];
-            if (mentionEntry) {
-                addMention(mentionEntry);
-                setActiveWord('');
+export const XRichTextInput2 = React.memo(
+    React.forwardRef<XRichTextInput2RefMethods, XRichTextInput2Props>(
+        (props: XRichTextInput2Props, ref) => {
+            if (!canUseDOM) {
+                return null;
             }
-        };
 
-        const applyCurrentSuggestedEmoji = () => {
-            const emojiData = emojiState.suggestions[emojiState.selectedEntryIndex];
-            if (emojiData) {
-                addEmoji(emojiData);
-                setActiveWord('');
-            }
-        };
+            const { onSubmit, onChange, value, mentionsData, placeholder } = props;
 
-        const { keyBinding, onHandleKey } = useDraftKeyHandling({
-            updateEditorStateFromText,
-            onSubmit,
-            mentionState,
-            emojiState,
-            applyCurrentSuggestedMention: applyCurrentSuggestedMention,
-            applyCurrentSuggestedEmoji,
-        });
+            const editorRef = React.useRef<Editor>(null);
 
-        return (
-            <EditorContainer
-                {...props}
-                editorState={editorState}
-                setEditorState={setEditorState}
-                activeWord={activeWord}
-                emojiState={emojiState}
-                onEmojiPicked={onEmojiPicked}
-                mentionState={mentionState}
-                onMentionPicked={(mentionEntry: MentionDataT) => {
-                    if (mentionEntry) {
-                        addMention(mentionEntry);
-                        setActiveWord('');
-                    }
-                }}
-            >
-                <Editor
-                    ref={editorRef}
-                    placeholder={placeholder}
-                    keyBindingFn={keyBinding}
-                    handleKeyCommand={onHandleKey}
-                    handlePastedFiles={onPasteFiles}
-                    handlePastedText={handlePastedText as any}
-                    onDownArrow={(event: any) => {
-                        mentionState.handleDown(event);
-                        emojiState.handleDown(event);
-                    }}
-                    onUpArrow={(event: any) => {
-                        mentionState.handleUp(event);
-                        emojiState.handleUp(event);
-                    }}
-                    stripPastedStyles={true}
+            const {
+                editorState,
+                setEditorState,
+                updateEditorStateFromText,
+                activeWord,
+                setActiveWord,
+                handleEditorChange,
+                addMention,
+                addEmoji,
+                onEmojiPicked,
+                getMentions,
+            } = useHandleEditorChange({
+                onChange,
+                value,
+            });
+
+            const { handlePastedText } = useHandlePastedText({ setEditorState });
+
+            const { resetAndFocus } = useInputMethods({
+                ref,
+                editorRef,
+                editorState,
+                setEditorState,
+                getMentions,
+            });
+
+            const { onPasteFiles } = usePasteFiles({
+                onPasteFile: props.onPasteFile,
+                resetAndFocus,
+            });
+
+            const emojiState = useEmojiSuggestions({
+                activeWord,
+            });
+
+            const mentionState = useMentionSuggestions({
+                mentionsData,
+                activeWord,
+            });
+
+            const applyCurrentSuggestedMention = () => {
+                const mentionEntry = mentionState.suggestions[mentionState.selectedEntryIndex];
+                if (mentionEntry) {
+                    addMention(mentionEntry);
+                    setActiveWord('');
+                }
+            };
+
+            const applyCurrentSuggestedEmoji = () => {
+                const emojiData = emojiState.suggestions[emojiState.selectedEntryIndex];
+                if (emojiData) {
+                    addEmoji(emojiData);
+                    setActiveWord('');
+                }
+            };
+
+            const { keyBinding, onHandleKey } = useDraftKeyHandling({
+                updateEditorStateFromText,
+                onSubmit,
+                mentionState,
+                emojiState,
+                applyCurrentSuggestedMention: applyCurrentSuggestedMention,
+                applyCurrentSuggestedEmoji,
+            });
+
+            return (
+                <EditorContainer
+                    {...props}
                     editorState={editorState}
-                    onChange={handleEditorChange}
-                />
-            </EditorContainer>
-        );
-    },
+                    setEditorState={setEditorState}
+                    activeWord={activeWord}
+                    emojiState={emojiState}
+                    onEmojiPicked={onEmojiPicked}
+                    mentionState={mentionState}
+                    onMentionPicked={(mentionEntry: MentionDataT) => {
+                        if (mentionEntry) {
+                            addMention(mentionEntry);
+                            setActiveWord('');
+                        }
+                    }}
+                >
+                    <Editor
+                        ref={editorRef}
+                        placeholder={placeholder}
+                        keyBindingFn={keyBinding}
+                        handleKeyCommand={onHandleKey}
+                        handlePastedFiles={onPasteFiles}
+                        handlePastedText={handlePastedText as any}
+                        onDownArrow={(event: any) => {
+                            mentionState.handleDown(event);
+                            emojiState.handleDown(event);
+                        }}
+                        onUpArrow={(event: any) => {
+                            mentionState.handleUp(event);
+                            emojiState.handleUp(event);
+                        }}
+                        stripPastedStyles={true}
+                        editorState={editorState}
+                        onChange={handleEditorChange}
+                    />
+                </EditorContainer>
+            );
+        },
+    ),
 );
