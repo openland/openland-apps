be939ff996226a88b63dea73e412498727891a91
Max Volkov <wolkow_1996@mail.ru>
2019-03-04 21:57:43 +0300

    fix(web): revert send message simple text input on mobile

--- packages/openland-web/fragments/MessageComposeComponent/SendMessage/MobileSendMessage.tsx
+++ packages/openland-web/fragments/MessageComposeComponent/SendMessage/MobileSendMessage.tsx
@@ -1,29 +1,7 @@
 import * as React from 'react';
-import Glamorous from 'glamorous';
+import { XView } from 'react-mental';
+import { ContentEditableTextInput } from '../ContentEditableTextInput';
 import { TextInputComponentInnerT } from '../DumpSendMessage';
-import { XRichTextInput2 } from 'openland-x/XRichTextInput2';
-
-export const TextInputWrapper = Glamorous.div({
-    flexGrow: 1,
-    maxHeight: '100%',
-    maxWidth: '100%',
-    '& > div': {
-        maxHeight: '100%',
-        height: '100%',
-        '& .DraftEditor-root': {
-            overflow: 'auto',
-            borderRadius: 10,
-            backgroundColor: '#ffffff',
-            border: 'solid 1px #ececec',
-            minHeight: 40,
-            maxHeight: 255,
-            paddingTop: 9,
-            paddingBottom: 9,
-            paddingLeft: 16,
-            paddingRight: 40,
-        },
-    },
-});
 
 export const MobileSendMessage = ({
     inputValue,
@@ -31,13 +9,10 @@ export const MobileSendMessage = ({
     inputRef,
     handleSend,
     handleDrop,
-    mentionsState,
 }: TextInputComponentInnerT) => {
     return (
-        <TextInputWrapper>
-            <XRichTextInput2
-                flexGrow={1}
-                mentionsData={mentionsState!!.mentionsData}
+        <XView flexGrow={1} maxHeight="100%" maxWidth="100%">
+            <ContentEditableTextInput
                 placeholder="Write a message..."
                 onChange={handleChange}
                 onSubmit={handleSend}
@@ -45,6 +20,6 @@ export const MobileSendMessage = ({
                 value={inputValue}
                 onPasteFile={handleDrop}
             />
-        </TextInputWrapper>
+        </XView>
     );
 };
