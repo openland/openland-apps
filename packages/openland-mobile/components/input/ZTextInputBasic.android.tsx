b0a568ef90ab9606ea626cd2c792ba4720c29f96
Fred Morozov <morozov@fred.md>
2019-03-05 15:22:38 +0300

    fix(android): support dark theme for ZTextInput2

--- packages/openland-mobile/components/input/ZTextInputBasic.android.tsx
+++ packages/openland-mobile/components/input/ZTextInputBasic.android.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {  View, TextInputProps } from 'react-native';
+import { View, TextInputProps, Platform } from 'react-native';
 import { ThemeContext } from 'openland-mobile/themes/ThemeContext';
 import { TextField } from 'react-native-material-textfield';
 
@@ -30,8 +30,8 @@ export const ZTextInputBasic = (props: ZTextInputBasicProps) => {
                 label={title}
                 placeholder={title ? undefined : placeholder}
                 labelFontSize={14}
-                tintColor="#0084fe"
-                baseColor="rgba(0, 0, 0, 0.5)"
+                tintColor={theme.accentColor}
+                baseColor={Platform.OS === 'android' ? '#9B9B9B' : 'rgba(138, 138, 143, 0.7)'}
                 textColor={theme.textColor}
                 fontSize={16}
                 animationDuration={150}
