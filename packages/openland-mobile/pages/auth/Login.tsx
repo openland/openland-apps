bffc4a3f2b8fc1a54f8d4cddc5d502726e9c35bc
Fred Morozov <morozov@fred.md>
2019-03-07 17:02:36 +0300

    feat(mobile): new Welcome screen

--- packages/openland-mobile/pages/auth/Login.tsx
+++ packages/openland-mobile/pages/auth/Login.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet, AsyncStorage, Image, ViewStyle, TextStyle, TouchableOpacity, ActivityIndicator, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, AsyncStorage, Image, ViewStyle, TextStyle, TouchableOpacity, ActivityIndicator, Dimensions, Platform } from 'react-native';
 import { Auth0Client } from '../../index';
 import RNRestart from 'react-native-restart';
 import { PageProps } from '../../components/PageProps';
 import { withApp } from '../../components/withApp';
-import { SDevice } from 'react-native-s/SDevice';
 import { SHeader } from 'react-native-s/SHeader';
 import { Alert } from 'openland-mobile/components/AlertBlanket';
-import { ASSafeAreaView } from 'react-native-async-view/ASSafeAreaView';
+import { SDevice } from 'react-native-s/SDevice';
 
 const styles = StyleSheet.create({
     container: {
@@ -17,19 +16,22 @@ const styles = StyleSheet.create({
         right: 0,
         bottom: 0,
         flex: 1,
-        // backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
     } as ViewStyle,
-    title: {
-        fontWeight: '500',
-        color: '#fff',
-        fontSize: 22,
-        height: 26,
-        lineHeight: 26
-    } as TextStyle,
+    logoWrapper: {
+        height: '65%',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+    } as ViewStyle,
+    buttons: {
+        height: '35%',
+        justifyContent: 'center'
+    } as ViewStyle,
     buttonWrapper: {
-        flexGrow: 1, maxWidth: 315, marginLeft: 28, marginRight: 28
+        flexGrow: 1,
+        maxWidth: 315,
+        marginLeft: 28,
+        marginRight: 28
     } as ViewStyle,
     button: {
         flexDirection: 'row',
@@ -38,7 +40,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#0084fe',
         color: '#fff',
         borderRadius: 28,
-        flexGrow: 1
+        flexGrow: 1,
     } as ViewStyle,
     buttonEmail: {
         backgroundColor: '#fff',
@@ -54,19 +56,12 @@ const styles = StyleSheet.create({
         lineHeight: 56,
         flexGrow: 1,
         textAlign: 'center',
-        textAlignVertical: 'center'
+        textAlignVertical: 'center',
     } as TextStyle,
     buttonTitleEmail: {
         color: '#000',
         height: 56,
     } as TextStyle,
-    footer: {
-        color: '#000',
-        opacity: 0.6,
-        fontSize: 12,
-        width: '100%',
-        textAlign: 'center'
-    } as TextStyle
 });
 
 class LoginComponent extends React.Component<PageProps, { initing: boolean, loading: boolean }> {
@@ -119,7 +114,6 @@ class LoginComponent extends React.Component<PageProps, { initing: boolean, load
 
             // TODO: Better error
             Alert.alert('Unable to authenticate');
-
         } catch (e) {
             if (e.error) {
                 if (e.error === 'a0.session.user_cancelled') {
@@ -135,47 +129,44 @@ class LoginComponent extends React.Component<PageProps, { initing: boolean, load
 
     handleEmailPress = () => {
         this.props.router.push('EmailStart');
-        // (async () => {
-        //     Auth0.requestEmailAuth('korshakov.stepan@gmail.com');
-        // })();
     }
 
     render() {
         return (
             <>
                 <SHeader hidden={true} />
-                <View style={{ backgroundColor: '#fff', width: '100%', height: '100%' }}>
-                    <Image source={require('assets/img-chat-3.jpg')} style={{ position: 'absolute', top: 0, left: 0, width: Dimensions.get('window').width, height: Dimensions.get('window').height }} resizeMode="cover" />
-                    <ASSafeAreaView style={styles.container}>
-                        <View style={{ width: '100%', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', flexGrow: 1 }}>
-                            <View style={{ width: '100%', marginTop: 32, alignItems: 'center', justifyContent: 'center' }}>
-                                <Image source={require('assets/logo-unicorn.png')} style={{ width: 130, height: 155 }} />
-                                <Image source={require('assets/logotype.png')} style={{ width: 186, height: 38, marginTop: 28 }} />
-                                <Text style={{ marginTop: 12, fontSize: 18, lineHeight: 21, height: 21, color: '#000', opacity: 0.8, fontWeight: '500' }}>Modern Messenger for Work</Text>
+                <View style={{ backgroundColor: '#fff', flex: 1, marginBottom: Platform.OS === 'android' ? SDevice.safeArea.bottom : undefined }}>
+                    <Image source={require('assets/img-chat-1.jpg')} style={{ position: 'absolute', top: 0, left: 0, width: Dimensions.get('window').width, height: Dimensions.get('window').height }} resizeMode="cover" />
+                    <View style={styles.container}>
+                        <View style={styles.logoWrapper}>
+                            <Image source={require('assets/logo-unicorn.png')} style={{ marginTop: Platform.OS === 'ios' ? 50 : 0, width: 130, height: 155 }} />
+                            <Image source={require('assets/logotype.png')} style={{ width: 186, height: 38, marginTop: 28 }} />
+                        </View>
+
+                        <View style={styles.buttons}>
+                            <View flexDirection="row">
+                                <TouchableOpacity onPress={this.handleGoogleAuth} disabled={this.state.loading} style={styles.buttonWrapper}>
+                                    <View style={styles.button}>
+                                        <View style={{ width: 56, height: 56, justifyContent: 'center', alignItems: 'center' }}>
+                                            {!this.state.loading && <Image source={require('assets/ic-google-signup.png')} />}
+                                        </View>
+                                        <Text style={styles.buttonTitle} allowFontScaling={false}>{!this.state.loading && 'Sign in with Google'}</Text>
+                                        <View style={{ width: 56, height: 56 }}>
+                                            {}
+                                        </View>
+                                        {this.state.loading && <View style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, alignItems: 'center', justifyContent: 'center' }}><ActivityIndicator color="#fff" /></View>}
+                                    </View>
+                                </TouchableOpacity>
+                            </View>
+                            <View flexDirection="row" style={{ marginTop: 15 }}>
+                                <TouchableOpacity onPress={this.handleEmailPress} disabled={this.state.loading} style={styles.buttonWrapper}>
+                                    <View style={[styles.button, styles.buttonEmail]}>
+                                        <Text style={[styles.buttonTitle, styles.buttonTitleEmail]} allowFontScaling={false}>{'Continue with Email'}</Text>
+                                    </View>
+                                </TouchableOpacity>
                             </View>
                         </View>
-                        <View flexDirection="row">
-                            <TouchableOpacity onPress={this.handleGoogleAuth} disabled={this.state.loading} style={styles.buttonWrapper}>
-                                <View style={styles.button}>
-                                    <View style={{ width: 56, height: 56, justifyContent: 'center', alignItems: 'center' }}>
-                                        {!this.state.loading && <Image source={require('assets/ic-google-signup.png')} />}
-                                    </View>
-                                    <Text style={styles.buttonTitle} allowFontScaling={false}>{!this.state.loading && 'Sign in with Google'}</Text>
-                                    <View style={{ width: 56, height: 56 }}>
-                                        {}
-                                    </View>
-                                    {this.state.loading && <View style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, alignItems: 'center', justifyContent: 'center' }}><ActivityIndicator color="#fff" /></View>}
-                                </View>
-                            </TouchableOpacity>
-                        </View>
-                        <View flexDirection="row" style={{ marginTop: 15, marginBottom: 46 + SDevice.safeArea.bottom }}>
-                            <TouchableOpacity onPress={this.handleEmailPress} disabled={this.state.loading} style={styles.buttonWrapper}>
-                                <View style={[styles.button, styles.buttonEmail]}>
-                                    <Text style={[styles.buttonTitle, styles.buttonTitleEmail]} allowFontScaling={false}>Continue with Email</Text>
-                                </View>
-                            </TouchableOpacity>
-                        </View>
-                    </ASSafeAreaView>
+                    </View>
                 </View>
             </>
         );
