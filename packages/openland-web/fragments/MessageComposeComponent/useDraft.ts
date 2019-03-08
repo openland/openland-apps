4d2b1d7b75cea21cfb4167c73d904d7cc1990e81
Sergey Lapin <lapanoid@gmail.com>
2019-03-08 20:10:13 +0300

    fix(web): fix build

--- packages/openland-web/fragments/MessageComposeComponent/useDraft.ts
+++ packages/openland-web/fragments/MessageComposeComponent/useDraft.ts
@@ -64,7 +64,7 @@ export function useDraft({
     };
 
     const cleanDraft = () => {
-        changeDraft('');
+        changeDraft('', []);
         DraftStore.cleanDraftMessage(conversationId);
     };
 
