fdf5600666d2345ed4ddbe3da8bf80e9508a9b6c
Fred Morozov <morozov@fred.md>
2019-03-08 14:00:27 +0300

    feat(mobile): Create community

--- packages/openland-mobile/pages/main/Compose.tsx
+++ packages/openland-mobile/pages/main/Compose.tsx
@@ -55,8 +55,9 @@ const ComposeComponent = XMemo<PageProps>((props) => {
                 <React.Suspense fallback={<ZLoader />}>
                     <SScrollView keyboardDismissMode="interactive">
                         <ZListItemGroup divider={false}>
-                            <ZListItem leftIcon={require('assets/ic-group-24.png')} text="Create group" path="CreateGroupAttrs" pathRemove={true} />
-                            {/* <ZListItem leftIcon={require('assets/ic-group-24.png')} text="Create community" path="CreateCommunity" /> */}
+                            <ZListItem leftIcon={require('assets/ic-room-24.png')} text="Create group" path="CreateGroupAttrs" pathRemove={true} />
+                            <ZListItem leftIcon={require('assets/ic-community-24.png')} text="Create community" path="NewOrganization" pathParams={{ isCommunity: true }} pathRemove={true} />
+                            <View height={15} />
                             {/* <ZListItem leftIcon={require('assets/ic-lock-24.png')} text="Create secret group" path="CreateGroupAttrs" pathRemove={true} /> */}
                         </ZListItemGroup>
                         <UserSearchComponent query="" router={props.router} />
