56cba1bfb4d38124cdb49e50d60ea3f2b5790523
Max Volkov <wolkow_1996@mail.ru>
2019-03-12 14:02:13 +0300

    fix(web): searching mentions

--- packages/openland-x/XRichTextInput2/useMentionSuggestions.ts
+++ packages/openland-x/XRichTextInput2/useMentionSuggestions.ts
@@ -36,10 +36,32 @@ export const useMentionSuggestions = ({
             const alphabetSort = activeWord.startsWith('@') && activeWord.length === 1;
             const searchText = activeWord.slice(1).toLowerCase();
             let filteredSuggestions = (mentionsData ? mentionsData : []).filter(
-                ({ name }: { name: string }) =>
-                    name.toLowerCase().startsWith(searchText) &&
-                    activeWord !== '' &&
-                    activeWord[0] === '@',
+                ({ name }: { name: string }) => {
+                    const user = name.split(' ');
+                    const firstName = user[0];
+                    const lastName = user[1];
+                    const finedFirstName = firstName.toLowerCase().startsWith(searchText);
+                    const finedLastName = lastName.toLowerCase().startsWith(searchText);
+                    if (finedFirstName) {
+                        return (
+                            name.toLowerCase().includes(searchText) &&
+                            activeWord !== '' &&
+                            activeWord[0] === '@'
+                        );
+                    } else if (finedLastName) {
+                        return (
+                            name.toLowerCase().includes(searchText) &&
+                            activeWord !== '' &&
+                            activeWord[0] === '@'
+                        );
+                    } else {
+                        return (
+                            name.toLowerCase().startsWith(searchText) &&
+                            activeWord !== '' &&
+                            activeWord[0] === '@'
+                        );
+                    }
+                },
             );
             if (alphabetSort) {
                 filteredSuggestions = (mentionsData ? mentionsData : []).sort((a, b) =>
