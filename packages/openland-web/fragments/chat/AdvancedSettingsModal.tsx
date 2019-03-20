2c81677a18a9defc87fbbfe65f22b0ac9ebe78b0
Max Volkov <wolkow_1996@mail.ru>
2019-03-20 20:56:46 +0300

    fix(web): working from advance

--- packages/openland-web/fragments/chat/AdvancedSettingsModal.tsx
+++ packages/openland-web/fragments/chat/AdvancedSettingsModal.tsx
@@ -10,9 +10,106 @@ import { XCheckbox } from 'openland-x/XCheckbox';
 import { XSelect } from 'openland-x/XSelect';
 import { XInput } from 'openland-x/XInput';
 import {
+    RoomUpdate,
+    RoomUpdateVariables,
     Room_room_SharedRoom_members_user,
     Room_room_SharedRoom_welcomeMessage,
+    UpdateWelcomeMessage,
+    UpdateWelcomeMessageVariables,
 } from 'openland-api/Types';
+import { MutationFn } from 'react-apollo';
+
+interface AdvancedSettingsInnerProps {
+    socialImage: string | null;
+    roomId: string;
+    canChangeAdvancedSettingsMembersUsers: Room_room_SharedRoom_members_user[];
+    welcomeMessage: Room_room_SharedRoom_welcomeMessage;
+    alter: MutationFn<RoomUpdate, Partial<RoomUpdateVariables>>;
+    updateWelcomeMessage: MutationFn<UpdateWelcomeMessage, Partial<UpdateWelcomeMessageVariables>>;
+}
+
+interface AdvancedSettingsInnerState {
+    showWelcomMessage: boolean;
+    welcomMessage: string;
+}
+
+class AdvancedSettingsInner extends React.Component<AdvancedSettingsInnerProps> {
+    render() {
+        const { props } = this;
+        const selectOptions = props.canChangeAdvancedSettingsMembersUsers.map(
+            (user: Room_room_SharedRoom_members_user) => {
+                return { value: user.id, label: user.name };
+            },
+        );
+        return (
+            <XModalForm
+                scrollableContent={true}
+                targetQuery="advancedSettings"
+                useTopCloser={true}
+                title="Advanced settings"
+                defaultAction={async data => {
+                    let newSocialImage = data.input.socialImageRef;
+
+                    await props.alter({
+                        variables: {
+                            roomId: props.roomId,
+                            input: {
+                                ...(newSocialImage && newSocialImage.uuid !== props.socialImage
+                                    ? {
+                                          socialImageRef: sanitizeImageRef(newSocialImage),
+                                      }
+                                    : {}),
+                            },
+                        },
+                    });
+
+                    await props.updateWelcomeMessage({
+                        variables: {
+                            roomId: props.roomId,
+                            welcomeMessageIsOn: data.input.welcomeMessageIsOn === 'true',
+                            welcomeMessageSender: data.input.welcomeMessageSender,
+                            welcomeMessageText: data.input.welcomeMessageText,
+                        },
+                    });
+                }}
+                defaultData={{
+                    input: {
+                        welcomeMessageIsOn: props.welcomeMessage.isOn ? 'true' : 'false',
+                        welcomeMessageText: props.welcomeMessage.message,
+                        welcomeMessageSender: props.welcomeMessage.sender
+                            ? props.welcomeMessage.sender.user.id
+                            : null,
+                        socialImageRef: props.socialImage ? { uuid: props.socialImage } : undefined,
+                    },
+                }}
+            >
+                <XVertical separator={12}>
+                    <XView>Welcome message</XView>
+                    <XView>
+                        Send an automatic message in 1:1 chat to every new member who joins this
+                        group
+                    </XView>
+                    <XCheckbox label="On" field="input.welcomeMessageIsOn" switcher={true} />
+                    <XView>
+                        Choose an image to display when sharing invite to this group on social
+                        networks
+                    </XView>
+                    <XSelect options={selectOptions} field="input.welcomeMessageSender" />
+                    <XInput size="large" field="input.welcomeMessageText" />
+                    <XView>Social sharing image</XView>
+                    <XAvatarUpload
+                        cropParams="1:1, free"
+                        field="input.socialImageRef"
+                        placeholder={{
+                            add: 'Add social image',
+                            change: 'Change social image',
+                        }}
+                    />
+                </XVertical>
+            </XModalForm>
+        );
+    }
+}
 
 type AdvancedSettingsModalT = {
     socialImage: string | null;
@@ -23,82 +120,15 @@ type AdvancedSettingsModalT = {
 
 export const AdvancedSettingsModal = withUpdateWelcomeMessage(withAlterChat(props => {
     const typedProps = props as typeof props & AdvancedSettingsModalT;
-    const alter = typedProps.alter;
-    const updateWelcomeMessage = (typedProps as any).updateWelcomeMessage;
-
-    let editSocialImageRef = typedProps.socialImage;
-
-    const selectOptions = typedProps.canChangeAdvancedSettingsMembersUsers.map(
-        (user: Room_room_SharedRoom_members_user) => {
-            return { value: user.id, label: user.name };
-        },
-    );
 
     return (
-        <XModalForm
-            scrollableContent={true}
-            targetQuery="advancedSettings"
-            useTopCloser={true}
-            title="Advanced settings"
-            defaultAction={async data => {
-                let newSocialImage = data.input.socialImageRef;
-
-                await alter({
-                    variables: {
-                        roomId: typedProps.roomId,
-                        input: {
-                            ...(newSocialImage && newSocialImage.uuid !== editSocialImageRef
-                                ? {
-                                      socialImageRef: sanitizeImageRef(newSocialImage),
-                                  }
-                                : {}),
-                        },
-                    },
-                });
-
-                await updateWelcomeMessage({
-                    variables: {
-                        roomId: typedProps.roomId,
-                        welcomeMessageIsOn: data.input.welcomeMessageIsOn === 'true',
-                        welcomeMessageSender: data.input.welcomeMessageSender,
-                        welcomeMessageText: data.input.welcomeMessageText,
-                    },
-                });
-            }}
-            defaultData={{
-                input: {
-                    welcomeMessageIsOn: typedProps.welcomeMessage.isOn ? 'true' : 'false',
-                    welcomeMessageText: typedProps.welcomeMessage.message,
-                    welcomeMessageSender: typedProps.welcomeMessage.sender
-                        ? typedProps.welcomeMessage.sender.user.id
-                        : null,
-                    socialImageRef: typedProps.socialImage
-                        ? { uuid: typedProps.socialImage }
-                        : undefined,
-                },
-            }}
-        >
-            <XVertical separator={12}>
-                <XView>Welcome message</XView>
-                <XView>
-                    Send an automatic message in 1:1 chat to every new member who joins this group
-                </XView>
-                <XCheckbox label="On" field="input.welcomeMessageIsOn" switcher={true} />
-                <XView>
-                    Choose an image to display when sharing invite to this group on social networks
-                </XView>
-                <XSelect options={selectOptions} field="input.welcomeMessageSender" />
-                <XInput size="large" field="input.welcomeMessageText" />
-                <XView>Social sharing image</XView>
-                <XAvatarUpload
-                    cropParams="1:1, free"
-                    field="input.socialImageRef"
-                    placeholder={{
-                        add: 'Add social image',
-                        change: 'Change social image',
-                    }}
-                />
-            </XVertical>
-        </XModalForm>
+        <AdvancedSettingsInner
+            alter={typedProps.alter}
+            roomId={typedProps.roomId}
+            canChangeAdvancedSettingsMembersUsers={typedProps.canChangeAdvancedSettingsMembersUsers}
+            welcomeMessage={typedProps.welcomeMessage}
+            socialImage={typedProps.socialImage}
+            updateWelcomeMessage={(typedProps as any).updateWelcomeMessage}
+        />
     );
 }) as any) as React.ComponentType<AdvancedSettingsModalT>;
