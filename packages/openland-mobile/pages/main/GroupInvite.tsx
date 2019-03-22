120d560bbd0748e7b51eebd3d211fba75ada18f6
Fred Morozov <morozov@fred.md>
2019-03-22 16:16:24 +0300

    fix(mobile): show "New group" label in invite screen for groups with membersCount < 10

--- packages/openland-mobile/pages/main/GroupInvite.tsx
+++ packages/openland-mobile/pages/main/GroupInvite.tsx
@@ -59,12 +59,16 @@ const styles = StyleSheet.create({
     } as TextStyle
 });
 
+const MIN_MEMBERS_COUNT_TO_SHOW = 10;
+
 const GroupInviteContent = XMemo<PageProps>((props) => {
     let invite: RoomInviteInfo_invite = props.router.params.invite;
     let inviteId = props.router.params.inviteId;
     let room = invite.room;
     let user = invite.invitedByUser;
     let screenHeight = Dimensions.get('screen').height;
+
+    let showMembersCount = room.membersCount ? room.membersCount >= MIN_MEMBERS_COUNT_TO_SHOW : false;
 
     return (
         <ASSafeAreaView flexGrow={1}>
@@ -78,12 +82,12 @@ const GroupInviteContent = XMemo<PageProps>((props) => {
                     <ZAvatar size={86} src={room.photo} placeholderKey={room.id} placeholderTitle={room.title} />
                     <Text style={styles.title}>{room.title}</Text>
 
-                    {!!room.membersCount && room.membersCount >= 100 && (
-                        <View flexDirection="row">
-                            <Image source={require('assets/ic-members-16.png')} style={styles.membersIcon} />
-                            <Text style={styles.members}>{room.membersCount} members</Text>
-                        </View>
-                    )}
+                    <View flexDirection="row">
+                        {showMembersCount && (<Image source={require('assets/ic-members-16.png')} style={styles.membersIcon} />)}
+                        <Text style={styles.members}>
+                            {showMembersCount ? (room.membersCount + ' members') : 'New group'}
+                        </Text>
+                    </View>
 
                     {typeof room.description === 'string' && (
                         <Text style={styles.description}>{room.description}</Text>
