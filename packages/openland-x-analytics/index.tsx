e1a87ca18858238a784dea37d61733cdac371d4c
Steve Kite <steve@statecraft.one>
2019-03-22 18:27:31 +0300

    fix(web): fix build

--- packages/openland-x-analytics/index.tsx
+++ packages/openland-x-analytics/index.tsx
@@ -1,9 +1,5 @@
 import { canUseDOM } from 'openland-y-utils/canUseDOM';
-import { loadConfig } from 'openland-x-config';
 import { Track } from 'openland-engines/Tracking';
-
-let sentryEndpoint = canUseDOM && loadConfig().sentryEndpoint && loadConfig().release;
-
 
 export function trackEvent(event: string, params?: { [key: string]: any }) {
     if (canUseDOM) {
@@ -12,37 +8,11 @@ export function trackEvent(event: string, params?: { [key: string]: any }) {
 }
 
 export function trackPage(page?: string) {
-    // if (shouldTrack) {
-    //     let p = page ? page : document.location.pathname;
-
-    //     // Track in other analytical platforms
-    //     // Replacing with application level event tracking
-    //     // trackEvent('Page Viewed', { page: p });
-    // }
+    // Nothing to do
 }
 
 export function trackProfile(id: string, firstName: string, lastName: string | null, email: string | null) {
-    // if (shouldTrack) {
-
-    //     // Identify mixpanel
-    //     // if (MixpanelToken) {
-    //     //     Mixpanel.identify(id);
-    //     //     Mixpanel.people.set({
-    //     //         '$email': email,
-    //     //         '$first_name': firstName,
-    //     //         '$last_name': lastName
-    //     //     });
-    //     // }
-
-    //     // Identify Centry
-    //     if (sentryEndpoint) {
-    //         // TODO: Fix Me
-    //         // (window as any).Raven.setUserContext({
-    //         //     email: email,
-    //         //     id: id
-    //         // });
-    //     }
-    // }
+    // Nothing to do
 }
 
 export function trackError(src: any) {
