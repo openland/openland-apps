4cace4c2ce4d76d76d7bea017452b53ed2c801cf
Max Volkov <wolkow_1996@mail.ru>
2019-03-25 17:20:39 +0300

    fix(web): Heart always be red while reaction picker is in active state

--- packages/openland-x-layout/XHorizontal.tsx
+++ packages/openland-x-layout/XHorizontal.tsx
@@ -2,34 +2,56 @@ import * as React from 'react';
 import Glamorous from 'glamorous';
 import { XFlexStyles, applyFlex } from 'openland-x/basics/Flex';
 
-let XHorizontalDiv = Glamorous.div<XFlexStyles & { separator?: 'large' | 'normal' | 'none' | number, alignItems?: 'stretch' | 'flex-start' | 'flex-end' | 'center' }>([
-    (props) => ({
+let XHorizontalDiv = Glamorous.div<
+    XFlexStyles & {
+        separator?: 'large' | 'normal' | 'none' | number;
+        alignItems?: 'stretch' | 'flex-start' | 'flex-end' | 'center';
+    }
+>([
+    props => ({
         display: 'flex',
         flexDirection: 'row',
         alignItems: props.alignItems || 'stretch',
         position: 'relative',
-        ...((props.separator !== 'none') ? {
-            '> *': {
-                marginLeft: (typeof props.separator === 'number') ? props.separator : props.separator === 'large' ? '16px' : '8px',
-                marginRight: (typeof props.separator === 'number') ? props.separator : props.separator === 'large' ? '16px' : '8px'
-            },
-            '>:first-child': {
-                marginLeft: '0px',
-            },
-            '>:last-child': {
-                marginRight: '0px',
-            }
-        } : {}),
+        ...(props.separator !== 'none'
+            ? {
+                  '> *': {
+                      marginLeft:
+                          typeof props.separator === 'number'
+                              ? props.separator
+                              : props.separator === 'large'
+                                  ? '16px'
+                                  : '8px',
+                      marginRight:
+                          typeof props.separator === 'number'
+                              ? props.separator
+                              : props.separator === 'large'
+                                  ? '16px'
+                                  : '8px',
+                  },
+                  '>:first-child': {
+                      marginLeft: '0px',
+                  },
+                  '>:last-child': {
+                      marginRight: '0px',
+                  },
+              }
+            : {}),
     }),
-    applyFlex
+    applyFlex,
 ]);
 
-export class XHorizontal extends React.Component<{ separator?: 'large' | 'normal' | 'none' | number, className?: string, alignItems?: 'stretch' | 'flex-start' | 'flex-end' | 'center', onClick?: () => void } & XFlexStyles> {
+interface XHorizontalProps extends XFlexStyles {
+    separator?: 'large' | 'normal' | 'none' | number;
+    className?: string;
+    alignItems?: 'stretch' | 'flex-start' | 'flex-end' | 'center';
+    onClick?: () => void;
+    onMouseLeave?: React.MouseEventHandler<any>;
+    onMouseEnter?: React.MouseEventHandler<any>;
+}
+
+export class XHorizontal extends React.Component<XHorizontalProps> {
     render() {
-        return (
-            <XHorizontalDiv {...this.props}>
-                {this.props.children}
-            </XHorizontalDiv>
-        );
+        return <XHorizontalDiv {...this.props}>{this.props.children}</XHorizontalDiv>;
     }
 }
--- packages/openland-x-layout/XVertical.tsx
+++ packages/openland-x-layout/XVertical.tsx
@@ -2,34 +2,56 @@ import * as React from 'react';
 import Glamorous from 'glamorous';
 import { XFlexStyles, applyFlex } from 'openland-x/basics/Flex';
 
-let VerticalDiv = Glamorous.div<XFlexStyles & { separator?: 'large' | 'normal' | 'none' | number, alignItems?: 'stretch' | 'flex-start' | 'flex-end' | 'center' }>([
-    (props) => ({
+let VerticalDiv = Glamorous.div<
+    XFlexStyles & {
+        separator?: 'large' | 'normal' | 'none' | number;
+        alignItems?: 'stretch' | 'flex-start' | 'flex-end' | 'center';
+    }
+>([
+    props => ({
         display: 'flex',
         flexDirection: 'column',
         alignItems: props.alignItems || 'stretch',
         position: 'relative',
-        ...((props.separator !== 'none') ? {
-            '> *': {
-                marginTop: (typeof props.separator === 'number') ? props.separator : props.separator === 'large' ? '16px' : '8px',
-                marginBottom: (typeof props.separator === 'number') ? props.separator : props.separator === 'large' ? '16px' : '8px'
-            },
-            '>:first-child': {
-                marginTop: '0px',
-            },
-            '>:last-child': {
-                marginBottom: '0px',
-            }
-        } : {}),
+        ...(props.separator !== 'none'
+            ? {
+                  '> *': {
+                      marginTop:
+                          typeof props.separator === 'number'
+                              ? props.separator
+                              : props.separator === 'large'
+                                  ? '16px'
+                                  : '8px',
+                      marginBottom:
+                          typeof props.separator === 'number'
+                              ? props.separator
+                              : props.separator === 'large'
+                                  ? '16px'
+                                  : '8px',
+                  },
+                  '>:first-child': {
+                      marginTop: '0px',
+                  },
+                  '>:last-child': {
+                      marginBottom: '0px',
+                  },
+              }
+            : {}),
     }),
-    applyFlex
+    applyFlex,
 ]);
 
-export class XVertical extends React.Component<{ separator?: 'large' | 'normal' | 'none' | number, alignItems?: 'stretch' | 'flex-start' | 'flex-end' | 'center', onClick?: () => void, className?: string } & XFlexStyles> {
+interface XVerticalProps extends XFlexStyles {
+    separator?: 'large' | 'normal' | 'none' | number;
+    alignItems?: 'stretch' | 'flex-start' | 'flex-end' | 'center';
+    onClick?: () => void;
+    className?: string;
+    onMouseLeave?: React.MouseEventHandler<any>;
+    onMouseEnter?: React.MouseEventHandler<any>;
+}
+
+export class XVertical extends React.Component<XVerticalProps> {
     render() {
-        return (
-            <VerticalDiv {...this.props}>
-                {this.props.children}
-            </VerticalDiv>
-        );
+        return <VerticalDiv {...this.props}>{this.props.children}</VerticalDiv>;
     }
 }
