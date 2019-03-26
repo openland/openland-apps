cf6d2b123701a6350a261df5b13f4dea9cd0ccc5
Steve Kite <steve@statecraft.one>
2019-03-26 15:53:34 +0300

    feat(mobile): Enable offline mode everywhere

--- packages/openland-mobile/utils/apolloClient.ts
+++ packages/openland-mobile/utils/apolloClient.ts
@@ -1,11 +1,11 @@
-import { buildClient } from 'openland-y-graphql/apolloClient';
+// import { buildClient } from 'openland-y-graphql/apolloClient';
 import { Track } from 'openland-engines/Tracking';
 import { OpenlandClient } from 'openland-api/OpenlandClient';
-import { DirectApollolClient } from 'openland-graphql/direct/DirectApolloClient';
-import { createWorkerClient } from 'openland-mobile/apollo/createWorkerClient';
-import { createDumbBridgeClient } from 'openland-graphql/proxy/DumbBridgeClient';
+// import { DirectApollolClient } from 'openland-graphql/direct/DirectApolloClient';
+// import { createWorkerClient } from 'openland-mobile/apollo/createWorkerClient';
+// import { createDumbBridgeClient } from 'openland-graphql/proxy/DumbBridgeClient';
 import { NativeApolloClient } from 'openland-mobile/apollo/NativeApolloClient';
-import { Platform } from 'react-native';
+// import { Platform } from 'react-native';
 // import { Platform } from 'react-native';
 
 let cachedClient: OpenlandClient | null;
@@ -31,23 +31,25 @@ export function getClient(): OpenlandClient {
 
 export function buildNativeClient(storage: string, token: string) {
 
-    if (Platform.OS === 'ios') {
-        return new OpenlandClient(new NativeApolloClient(storage, token));
-    }
+    return new OpenlandClient(new NativeApolloClient(storage, token));
+
+    // if (Platform.OS === 'ios') {
+    //     return new OpenlandClient(new NativeApolloClient(storage, token));
+    // }
 
     // if (Platform.OS === 'android') {
     //     return new OpenlandClient(new NativeApolloClient(token));
     // }
 
-    if (__DEV__) {
-        return new OpenlandClient(createDumbBridgeClient(new DirectApollolClient(buildClient({
-            token: token,
-            endpoint: 'https://api.openland.com/api',
-            wsEndpoint: 'wss://api.openland.com/api'
-        }))));
-    } else {
-        return new OpenlandClient(createWorkerClient(token));
-    }
+    // if (__DEV__) {
+    //     return new OpenlandClient(createDumbBridgeClient(new DirectApollolClient(buildClient({
+    //         token: token,
+    //         endpoint: 'https://api.openland.com/api',
+    //         wsEndpoint: 'wss://api.openland.com/api'
+    //     }))));
+    // } else {
+    //     return new OpenlandClient(createWorkerClient(token));
+    // }
 
     // return new OpenlandClient(createWorkerClient(token));
 
