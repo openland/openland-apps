5e515518c38cad3a55c9725fb52d95a4c64a4fad
Sergey Lapin <lapanoid@gmail.com>
2019-04-02 11:32:03 +0300

    chore(web): remove anoying logs

--- packages/openland-web/globals.ts
+++ packages/openland-web/globals.ts
@@ -2,6 +2,10 @@ import { canUseDOM } from 'openland-y-utils/canUseDOM';
 import { AppNotifications } from 'openland-y-runtime-web/AppNotifications';
 import { Router } from './routes';
 import { Howler } from 'howler';
+import { disableTag } from 'mental-log';
+
+// Disable annoying logs
+disableTag('API');
 
 // Is it a good place?
 if (canUseDOM) {
