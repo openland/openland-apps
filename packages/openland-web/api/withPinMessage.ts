0359ea4bd38f273e01464fdde98a3c2268a9c345
Sergey Lapin <lapanoid@gmail.com>
2019-04-04 00:54:07 +0300

    chore(web): kill withPinMessage

--- packages/openland-web/api/withPinMessage.ts
+++
@@ -1,5 +0,0 @@
-import { graphqlMutation } from 'openland-x-graphql/graphqlMutation';
-import { PinMessageMutation, UnpinMessageMutation } from 'openland-api';
-
-export const withPinMessage = graphqlMutation(PinMessageMutation, 'pinMessage');
-export const withUnpinMessage = graphqlMutation(UnpinMessageMutation, 'unpinMessage');
