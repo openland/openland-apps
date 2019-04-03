304ce9542ab8fde031395c5f39185aa4629c6b06
Sergey Lapin <lapanoid@gmail.com>
2019-04-04 00:43:03 +0300

    chore(web): kill withPostMessage

--- packages/openland-web/api/withPostMessage.ts
+++
@@ -1,5 +0,0 @@
-import { graphqlMutation } from 'openland-x-graphql/graphqlMutation';
-import { SendPostMessageMutation, EditPostMessageMutation } from 'openland-api';
-
-export const withSendPostMessage = graphqlMutation(SendPostMessageMutation, 'sendPost');
-export const withEditPostMessage = graphqlMutation(EditPostMessageMutation, 'editPost');
