9cad800801f954345468fe1e9d20ce9dfa73e42e
Sergey Lapin <lapanoid@gmail.com>
2019-04-03 13:20:28 +0300

    chore(web): kill withAppInvite, withUserSimple

--- packages/openland-web/api/withAppInvite.ts
+++
@@ -1,5 +0,0 @@
-import { graphqlRouted } from 'openland-x-graphql/graphqlRouted';
-import { AccountAppInviteQuery, AccountAppInviteInfoQuery } from 'openland-api';
-
-export const withAppInvite = graphqlRouted(AccountAppInviteQuery);
-export const withAppInviteInfo = graphqlRouted(AccountAppInviteInfoQuery);
