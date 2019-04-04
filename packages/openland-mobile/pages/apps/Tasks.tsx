d742725c8cdaaf05111cdfe74236d5a1e9f32b20
Steve Kite <steve@statecraft.one>
2019-04-04 14:59:43 +0300

    feat(mobile): Add task app

--- packages/openland-mobile/pages/apps/Tasks.tsx
+++ packages/openland-mobile/pages/apps/Tasks.tsx
@@ -9,6 +9,9 @@ import { PromptBuilder } from 'openland-mobile/components/Prompt';
 import { randomKey } from 'openland-graphql/utils/randomKey';
 import { SHeaderButton } from 'react-native-s/SHeaderButton';
 import { ZListItem } from 'openland-mobile/components/ZListItem';
+import { showBlanketModal } from 'openland-mobile/components/showBlanketModal';
+import { ActionSheetBuilder } from 'openland-mobile/components/ActionSheet';
+import { AlertBlanketBuilder } from 'openland-mobile/components/AlertBlanket';
 
 const Tasks = React.memo(() => {
     let userStorage = getMessenger().engine.userStorage;
@@ -37,7 +40,23 @@ const Tasks = React.memo(() => {
             <SHeaderButton title="Create" onPress={callback} />
             <SScrollView>
                 {data.map((v) => (
-                    <ZListItem key={v.id} text={v.name} description="project" />
+                    <ZListItem
+                        key={v.id}
+                        text={v.name}
+                        description="project"
+                        onLongPress={() => {
+                            let builder = new AlertBlanketBuilder();
+                            builder.button('Cancel', 'cancel');
+                            builder.action('Delete', 'destructive', () => {
+                                userStorage.setKeys('app.tasks', [{
+                                    key: 'index.projects',
+                                    value: JSON.stringify(data.filter((v2) => v.id !== v2.id))
+                                }])
+                            });
+                            builder.title('Are you sure want to delete ' + v.name + '?')
+                            builder.show();
+                        }}
+                    />
                     // <View key={v.id} width={100} height={100} backgroundColor="red">
                     //     <ZText text={v.name} />
                     // </View>
