6abcdbd3ced69a84642bca89bb24cc1c75cbd240
Steve Kite <steve@statecraft.one>
2019-04-04 18:25:50 +0300

    des(web): Create tasks

--- packages/openland-web/fragments/apps/AppContainer.tsx
+++ packages/openland-web/fragments/apps/AppContainer.tsx
@@ -1,19 +1,39 @@
 import * as React from 'react';
 import { XLoader } from 'openland-x/XLoader';
 import { XScrollView } from 'openland-x/XScrollView';
-import { XView } from 'react-mental';
 import { ClientCacheProvider } from 'openland-graphql/ClientCache';
+import { css } from 'linaria';
+import { XView } from 'react-mental';
+import { XLoadingCircular } from 'openland-x/XLoadingCircular';
+
+const bg = css`
+    display: flex;
+    position: relative;
+    flex-grow: 1;
+    flex-shrink: 1;
+    flex-direction: column;
+    background-image: url('/static/apps/tasks-bg.jpg');
+    background-size: cover;
+`;
+
+const AppLoader = () => {
+    return (
+        <XView position="absolute" top={0} left={0} right={0} bottom={0} alignItems="center" justifyContent="center">
+            <XLoadingCircular color="#fff" />
+        </XView>
+    )
+}
 
 export const AppContainer = React.memo<{ children?: any }>((props) => {
     return (
-        <ClientCacheProvider>
-            <React.Suspense fallback={<XLoader loading={true} />}>
-                <XView flexGrow={1} alignItems="stretch" alignSelf="stretch" flexDirection="column" flexShrink={1}>
+        <div className={bg} >
+            <ClientCacheProvider>
+                <React.Suspense fallback={<AppLoader />}>
                     <XScrollView flexGrow={1}>
                         {props.children}
                     </XScrollView>
-                </XView>
-            </React.Suspense>
-        </ClientCacheProvider>
+                </React.Suspense>
+            </ClientCacheProvider>
+        </div>
     );
 });
