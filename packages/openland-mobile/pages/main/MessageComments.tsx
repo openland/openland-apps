5e68a8fdb3311c5165bb59f779a98f685fa8b268
Fred Morozov <morozov@fred.md>
2019-04-10 13:38:51 +0300

    wip(mobile): comments

--- packages/openland-mobile/pages/main/MessageComments.tsx
+++ packages/openland-mobile/pages/main/MessageComments.tsx
@@ -154,14 +154,14 @@ class MessageCommentsInner extends React.Component<MessageCommentsInnerProps, Me
         );
 
         const replyView = replyTo ? (
-            <View marginLeft={52} paddingLeft={8} marginRight={81} borderLeftColor="#0084fe" borderLeftWidth={2} marginTop={10} marginBottom={4} flexDirection="row">
+            <View marginLeft={15} paddingLeft={8} marginRight={52} borderLeftColor="#0084fe" borderLeftWidth={2} marginTop={10} marginBottom={4} flexDirection="row">
                 <View flexGrow={1}>
                     <Text style={{ color: '#0084fe', fontSize: 14, lineHeight: 20, marginBottom: 1, fontWeight: TextStyles.weight.medium } as TextStyle} numberOfLines={1}>{replyTo.sender.name}</Text>
                     <Text style={{ color: '#99a2b0', fontSize: 14 }} numberOfLines={1}>{replyTo.message}</Text>
                 </View>
                 <TouchableWithoutFeedback onPress={this.handleReplyClear}>
                     <View marginLeft={11} width={18} height={38} alignItems="center" justifyContent="center">
-                        <Image source={require('assets/ic-clear-16.png')} style={{ width: 16, height: 16 }} />
+                        <Image source={require('assets/ic-cancel-gray-18.png')} style={{ tintColor: '#b9c1cd', width: 18, height: 18 }} />
                     </View>
                 </TouchableWithoutFeedback>
             </View>
