e60da437049602508ab0f77c791d62b257a6f6ab
Steve Kite <steve@statecraft.one>
2019-04-11 12:36:33 +0300

    perf(web): Use new XScrollViewReversed2

--- packages/openland-x/XScrollViewReversed2.tsx
+++ packages/openland-x/XScrollViewReversed2.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { css } from 'linaria';
 import ResizeObserver from 'resize-observer-polyfill';
+import { XView, XStyles } from 'react-mental';
+import { XScrollValues } from './XScrollView3';
 
 const NativeScrollStyle = css`
     overflow-y: overlay;
@@ -21,11 +23,28 @@ const NativeScrollContentStyle = css`
     overflow-x: hidden;
 `;
 
-export const XScrollViewReverse2 = React.memo<{ children?: any }>((props) => {
+export interface XScrollViewReverse2Props extends XStyles {
+    onScroll?: (values: XScrollValues) => void;
+    children?: any
+}
+
+export const XScrollViewReverse2 = React.memo<XScrollViewReverse2Props>((props) => {
 
     const ref = React.useRef<HTMLDivElement>(null);
     const ref2 = React.useRef<HTMLDivElement>(null);
     const ref3 = React.useRef<number>(0);
+    const lastHeight = React.useRef<number>(0);
+
+    const invokeOnScroll = React.useMemo(() => {
+        return () => {
+            let clientHeight = ref2.current!!.clientHeight;
+            let scrollTop = ref.current!!.scrollTop;
+            let scrollHeight = ref.current!!.clientHeight;
+            if (props.onScroll) {
+                props.onScroll({ scrollHeight, scrollTop, clientHeight });
+            }
+        };
+    }, []);
 
     React.useLayoutEffect(() => {
         let v = ref.current!!;
@@ -35,25 +54,28 @@ export const XScrollViewReverse2 = React.memo<{ children?: any }>((props) => {
             let scrollHeight = ref.current!!.clientHeight;
             let scrollBottom = clientHeight - scrollTop - scrollHeight;
             ref3.current = scrollBottom;
+
+            invokeOnScroll();
         }, { passive: true });
 
-        let lastHeight = v.clientHeight;
+        let lastHeight2 = v.clientHeight;
         let observer = new ResizeObserver(() => {
-            let delta = v.clientHeight - lastHeight;
-            lastHeight = v.clientHeight;
+            let delta = v.clientHeight - lastHeight2;
+            lastHeight2 = v.clientHeight;
             if (delta < 0) {
                 v.scrollTop -= delta;
             }
+            invokeOnScroll();
         });
         observer.observe(v);
     }, []);
 
     React.useLayoutEffect(() => {
         let v = ref2.current!!;
-        let lastHeight = v.clientHeight;
+        lastHeight.current = v.clientHeight;
         let observer = new ResizeObserver(() => {
-            let delta = v.clientHeight - lastHeight;
-            lastHeight = v.clientHeight;
+            let delta = v.clientHeight - lastHeight.current;
+            lastHeight.current = v.clientHeight;
             if (delta > 0) {
                 if (ref3.current < 50) {
                     ref.current!!.scrollTop = v!.clientHeight - ref.current!!.clientHeight;
@@ -61,15 +83,34 @@ export const XScrollViewReverse2 = React.memo<{ children?: any }>((props) => {
                     ref.current!!.scrollTop += delta;
                 }
             }
+            invokeOnScroll();
         });
         observer.observe(v);
+        invokeOnScroll();
     }, []);
 
+    React.useLayoutEffect(() => {
+        let delta = ref2.current!!.clientHeight - lastHeight.current;
+        lastHeight.current = ref2.current!!.clientHeight;
+        if (delta > 0) {
+            if (ref3.current < 50) {
+                ref.current!!.scrollTop = ref2.current!!.clientHeight - ref.current!!.clientHeight;
+            } else {
+                ref.current!!.scrollTop += delta;
+            }
+        }
+        invokeOnScroll();
+    }, [props.children]);
+
+    const { children, ...other } = props;
+
     return (
-        <div className={NativeScrollStyle} ref={ref}>
-            <div className={NativeScrollContentStyle} ref={ref2}>
-                {props.children}
+        <XView {...other}>
+            <div className={NativeScrollStyle} ref={ref}>
+                <div className={NativeScrollContentStyle} ref={ref2}>
+                    {props.children}
+                </div>
             </div>
-        </div>
+        </XView>
     );
 });
