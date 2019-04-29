2be964b170d849eac287adc45bc28ec5a8480740
Fred Morozov <morozov@fred.md>
2019-04-29 12:19:29 +0300

    fix(mobile): improve ZUserView

--- packages/openland-mobile/components/ZUserView.tsx
+++ packages/openland-mobile/components/ZUserView.tsx
@@ -1,54 +1,41 @@
 import * as React from 'react';
-import { UserShort, UserTiny } from 'openland-api/Types';
+import { UserTiny } from 'openland-api/Types';
 import { XMemo } from 'openland-y-utils/XMemo';
-import { Platform, TouchableNativeFeedback, TouchableHighlight, View, Text, TextStyle } from 'react-native';
+import { View, Text, TextStyle } from 'react-native';
 import { ZAvatar } from './ZAvatar';
 import { TextStyles } from 'openland-mobile/styles/AppStyles';
+import { ZListItemBase } from './ZListItemBase';
 
 export interface ZUserViewProps {
     user: UserTiny;
     onPress: (userId: string) => void;
 }
 
-const ZUserViewInner = XMemo<ZUserViewProps>((props) => (
-    <View paddingHorizontal={16} paddingVertical={6} flexDirection="row" alignItems="center">
-        <ZAvatar
-            size={28}
-            src={props.user.photo}
-            placeholderTitle={props.user.name}
-            placeholderKey={props.user.id}
-        />
-        <View flexGrow={1} flexShrink={1} paddingLeft={12}>
-            <Text
-                style={{ color: '#000000', fontWeight: TextStyles.weight.medium } as TextStyle}
-                allowFontScaling={false}
-            >
-                {props.user.name}{'   '}
-                {props.user.primaryOrganization && (
-                    <Text
-                        style={{ color: '#99a2b0', fontWeight: TextStyles.weight.regular } as TextStyle}
-                        allowFontScaling={false}
-                    >
-                        {props.user.primaryOrganization.name}
-                    </Text>
-                )}
-            </Text>
+export const ZUserView = XMemo<ZUserViewProps>((props) => (
+    <ZListItemBase height={40} onPress={() => props.onPress(props.user.id) } separator={false}>
+        <View paddingHorizontal={16} paddingVertical={6} flexDirection="row" alignItems="center">
+            <ZAvatar
+                size={28}
+                src={props.user.photo}
+                placeholderTitle={props.user.name}
+                placeholderKey={props.user.id}
+            />
+            <View flexGrow={1} flexShrink={1} paddingLeft={12}>
+                <Text
+                    style={{ color: '#000000', fontWeight: TextStyles.weight.medium } as TextStyle}
+                    allowFontScaling={false}
+                >
+                    {props.user.name}{'   '}
+                    {props.user.primaryOrganization && (
+                        <Text
+                            style={{ color: '#99a2b0', fontWeight: TextStyles.weight.regular } as TextStyle}
+                            allowFontScaling={false}
+                        >
+                            {props.user.primaryOrganization.name}
+                        </Text>
+                    )}
+                </Text>
+            </View>
         </View>
-    </View>
+    </ZListItemBase>
 ))
-
-export const ZUserView = React.memo<ZUserViewProps>((props) => {
-    if (Platform.OS === 'android') {
-        return (
-            <TouchableNativeFeedback onPress={() => props.onPress(props.user.id)}>
-                <ZUserViewInner {...props} />
-            </TouchableNativeFeedback>
-        );
-    } else {
-        return (
-            <TouchableHighlight underlayColor="#eee" onPress={() => props.onPress(props.user.id)}>
-                <ZUserViewInner {...props} />
-            </TouchableHighlight>
-        )
-    }
-});
