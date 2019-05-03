576d66b7834d26bd1adbf58186592db371d3ecfb
Fred Morozov <morozov@fred.md>
2019-05-03 13:08:04 +0300

    fix(mobile): fix text clipping in ZListItemHeader

--- packages/openland-mobile/components/ZListItemHeader.tsx
+++ packages/openland-mobile/components/ZListItemHeader.tsx
@@ -15,12 +15,14 @@ const styles = StyleSheet.create({
     body: {
         flexDirection: 'column',
         flexGrow: 1,
+        flexShrink: 1,
         minWidth: 0,
         paddingLeft: 16,
         paddingRight: 16,
     } as ViewStyle,
     header: {
         flexGrow: 1,
+        flexShrink: 1,
         flexDirection: 'column',
         minWidth: 0,
     } as ViewStyle,
