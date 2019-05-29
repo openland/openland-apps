43137067875ad10569ccfc90891aefe297b4db28
Fred Morozov <morozov@fred.md>
2019-05-29 12:31:53 +0300

    ref(mobile): refactor GlobalSearchItems

--- packages/openland-mobile/pages/main/components/globalSearch/GlobalSearchItems.tsx
+++ packages/openland-mobile/pages/main/components/globalSearch/GlobalSearchItems.tsx
@@ -1,109 +1,123 @@
 import * as React from 'react';
 import { Platform } from 'react-native';
 import { ASFlex } from 'react-native-async-view/ASFlex';
-import { ZStyles } from 'openland-mobile/components/ZStyles';
 import { ASText } from 'react-native-async-view/ASText';
 import { TextStyles } from 'openland-mobile/styles/AppStyles';
 import { GlobalSearch_items_User, GlobalSearch_items_Organization, GlobalSearch_items_SharedRoom } from 'openland-api/Types';
 import { ASAvatar } from 'openland-mobile/messenger/components/ASAvatar';
 import { UserAvatar } from 'openland-mobile/messenger/components/UserAvatar';
 import { XMemo } from 'openland-y-utils/XMemo';
-import { ThemeContext, useThemeGlobal } from 'openland-mobile/themes/ThemeContext';
+import { useThemeGlobal } from 'openland-mobile/themes/ThemeContext';
+import { AppTheme } from 'openland-mobile/themes/themes';
 
-export const GlobalSearchItemSharedRoom = XMemo<{
-    item: GlobalSearch_items_SharedRoom;
-    onPress: (id: string, title: string) => void;
-}>((props) => {
+interface ItemBaseProps {
+    avatar: JSX.Element;
+    name: string;
+    theme: AppTheme;
 
-    const theme = useThemeGlobal();
-    const handlePress = React.useCallback(() => {
-        props.onPress(props.item.id, props.item.title);
-    }, [props.item]);
+    onPress: () => void;
+}
 
-    let item = props.item;
-    let height = 48;
-    let avatarSize = 30;
+const ItemBase = XMemo<ItemBaseProps>((props) => {
+    const { onPress, avatar, name, theme } = props;
+    const height = 48;
 
     return (
-        <ASFlex height={height} flexDirection="row" highlightColor={theme.selectorColor} onPress={handlePress} alignItems={'center'}>
+        <ASFlex height={height} flexDirection="row" highlightColor={theme.selectorColor} onPress={onPress} alignItems={'center'}>
             <ASFlex marginLeft={7} width={height} height={height} alignItems="center" justifyContent="center">
+                {avatar}
+            </ASFlex>
+            <ASFlex marginRight={10} marginTop={12} marginBottom={12} flexDirection="column" flexGrow={1} flexBasis={0} alignItems="stretch">
+                <ASFlex alignItems="center">
+                    <ASText fontSize={15} lineHeight={Platform.OS === 'android' ? 22 : 18} fontWeight={TextStyles.weight.medium} color={theme.textColor} flexGrow={1} flexBasis={0} marginRight={10}>
+                        {name}
+                    </ASText>
+                </ASFlex>
+            </ASFlex>
+        </ASFlex>
+    );
+});
+
+interface ItemProps {
+    onPress: (id: string, title: string) => void;
+}
+
+interface ItemRoomProps extends ItemProps {
+    item: GlobalSearch_items_SharedRoom;
+}
+
+export const GlobalSearchItemSharedRoom = XMemo<ItemRoomProps>((props) => {
+    const theme = useThemeGlobal();
+    const { item, onPress } = props;
+    const handlePress = React.useCallback(() => onPress(item.id, item.title), [item]);
+
+    return (
+        <ItemBase
+            name={item.title}
+            onPress={handlePress}
+            theme={theme}
+            avatar={
                 <ASAvatar
                     src={item.roomPhoto}
-                    size={avatarSize}
+                    size={30}
                     placeholderKey={item.id}
                     placeholderTitle={item.title}
                 />
-            </ASFlex>
-            <ASFlex marginRight={10} marginTop={12} marginBottom={12} flexDirection="column" flexGrow={1} flexBasis={0} alignItems="stretch">
-                <ASFlex height={Platform.OS === 'android' ? 22 : 18} marginTop={Platform.OS === 'android' ? -4 : 0}>
-                    <ASText fontSize={15} height={22} fontWeight={TextStyles.weight.medium} color={theme.textColor} flexGrow={1} flexBasis={0} marginRight={10}>{item.title}</ASText>
-                </ASFlex>
-            </ASFlex>
-        </ASFlex>
+            }
+        />
     );
 })
 
-export const GlobalSearchItemOrganization = XMemo<{
+interface ItemOrganizationProps extends ItemProps {
     item: GlobalSearch_items_Organization;
-    onPress: (id: string, title: string) => void;
-}>((props) => {
-    const theme = useThemeGlobal();
-    const handlePress = React.useCallback(() => {
-        props.onPress(props.item.id, props.item.name);
-    }, [props.item]);
+}
 
-    let item = props.item;
-    let height = 48;
-    let avatarSize = 30;
+export const GlobalSearchItemOrganization = XMemo<ItemOrganizationProps>((props) => {
+    const theme = useThemeGlobal();
+    const { item, onPress } = props;
+    const handlePress = React.useCallback(() => onPress(item.id, item.name), [item]);
 
     return (
-        <ASFlex height={height} flexDirection="row" highlightColor={theme.selectorColor} onPress={handlePress} alignItems={'center'}>
-            <ASFlex marginLeft={7} width={height} height={height} alignItems="center" justifyContent="center">
+        <ItemBase
+            name={item.name}
+            onPress={handlePress}
+            theme={theme}
+            avatar={
                 <ASAvatar
                     src={item.photo}
-                    size={avatarSize}
+                    size={30}
                     placeholderKey={item.id}
                     placeholderTitle={item.name}
                 />
-            </ASFlex>
-            <ASFlex marginRight={10} marginTop={12} marginBottom={12} flexDirection="column" flexGrow={1} flexBasis={0} alignItems="stretch">
-                <ASFlex height={Platform.OS === 'android' ? 22 : 18} marginTop={Platform.OS === 'android' ? -4 : 0}>
-                    <ASText fontSize={15} height={22} fontWeight={TextStyles.weight.medium} color={theme.textColor} flexGrow={1} flexBasis={0} marginRight={10}>{item.name}</ASText>
-                </ASFlex>
-            </ASFlex>
-        </ASFlex>
+            }
+        />
     );
 })
 
-export const GlobalSearchItemUser = XMemo<{
+interface ItemUserProps extends ItemProps {
     item: GlobalSearch_items_User;
-    onPress: (id: string, title: string) => void;
-}>((props) => {
+}
+
+export const GlobalSearchItemUser = XMemo<ItemUserProps>((props) => {
     const theme = useThemeGlobal();
-    const handlePress = React.useCallback(() => {
-        props.onPress(props.item.id, props.item.name);
-    }, [props.item])
-    let item = props.item;
-    let height = 48;
-    let avatarSize = 30;
+    const { item, onPress } = props;
+    const handlePress = React.useCallback(() => onPress(item.id, item.name), [item]);
 
     return (
-        <ASFlex height={height} flexDirection="row" highlightColor={theme.selectorColor} onPress={handlePress} alignItems={'center'}>
-            <ASFlex marginLeft={7} width={height} height={height} alignItems="center" justifyContent="center">
+        <ItemBase
+            name={item.name}
+            onPress={handlePress}
+            theme={theme}
+            avatar={
                 <UserAvatar
                     src={item.photo}
-                    size={avatarSize}
+                    size={30}
                     placeholderKey={item.id}
                     placeholderTitle={item.name}
                     online={item.online}
                     theme={theme}
                 />
-            </ASFlex>
-            <ASFlex marginRight={10} marginTop={12} marginBottom={12} flexDirection="column" flexGrow={1} flexBasis={0} alignItems="stretch">
-                <ASFlex height={Platform.OS === 'android' ? 22 : 18} marginTop={Platform.OS === 'android' ? -4 : 0}>
-                    <ASText fontSize={15} height={22} fontWeight={TextStyles.weight.medium} color={theme.textColor} flexGrow={1} flexBasis={0} marginRight={10}>{item.name}</ASText>
-                </ASFlex>
-            </ASFlex>
-        </ASFlex>
+            }
+        />
     );
 })
