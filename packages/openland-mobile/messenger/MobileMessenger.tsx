6c0f2bf15f24aec23bed100a4de41441409d9ac1
kor_ka <gleb.putintsev@openland.com>
2019-05-29 23:31:57 +0300

    fix(android): fix pdf viewer

--- packages/openland-mobile/messenger/MobileMessenger.tsx
+++ packages/openland-mobile/messenger/MobileMessenger.tsx
@@ -109,7 +109,7 @@ export class MobileMessenger {
         let attach = document.attachments!.filter(a => a.__typename === 'MessageAttachmentFile')[0] as FullMessage_GeneralMessage_attachments_MessageAttachmentFile;
         // { config: { uuid, name, size }
         // TODO: link android
-        if (Platform.OS === 'ios' && attach.fileMetadata.name.toLowerCase().includes('.pdf')) {
+        if (attach.fileMetadata.name.toLowerCase().includes('.pdf')) {
             this.history.navigationManager.push('PdfPreview', { config: { uuid: attach.fileId, name: attach.fileMetadata.name, size: attach.fileMetadata.size } });
         } else {
             this.history.navigationManager.push('FilePreview', { config: { uuid: attach.fileId, name: attach.fileMetadata.name, size: attach.fileMetadata.size } });
