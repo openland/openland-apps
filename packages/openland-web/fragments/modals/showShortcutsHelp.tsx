970bd9a456094ab25ebad4d88228d25ae127be20
Sergey Lapin <lapanoid@gmail.com>
2019-05-29 18:36:14 +0300

    wip(web): update shortcuts modal

--- packages/openland-web/fragments/modals/showShortcutsHelp.tsx
+++ packages/openland-web/fragments/modals/showShortcutsHelp.tsx
@@ -2,6 +2,17 @@ import * as React from 'react';
 import Glamorous from 'glamorous';
 import { showModalBox } from 'openland-x/showModalBox';
 import { XScrollView3 } from 'openland-x/XScrollView3';
+import { XView } from 'react-mental';
+import { SpanView } from 'openland-web/components/messenger/message/content/SpannedView';
+import {
+    SpanType,
+    SpanBold,
+    SpanItalic,
+    SpanCodeBlock,
+    SpanCodeInline,
+    SpanIrony,
+    SpanLoud,
+} from 'openland-y-utils/spans/Span';
 
 const KeyboardShortcuts = Glamorous.div({
     padding: '7px 40px 30px',
@@ -32,51 +43,115 @@ const KeyboardShortcut = Glamorous.div({
     },
 });
 
-export function showShortcutsHelp() {
-    showModalBox({ title: 'Keyboard shortcuts' }, () => (
+const Shortcut = ({ shortcuts, description }: { shortcuts: any[]; description?: string }) => {
+    return (
+        <KeyboardShortcut>
+            {shortcuts.map((shortcut, key) => {
+                return <span key={key}>{shortcut}</span>;
+            })}
+
+            {description}
+        </KeyboardShortcut>
+    );
+};
+
+const SpanPreview = ({
+    spanType,
+    text,
+}: {
+    spanType:
+        | SpanType.code_block
+        | SpanType.code_inline
+        | SpanType.italic
+        | SpanType.bold
+        | SpanType.irony
+        | SpanType.loud;
+
+    text: string;
+}) => {
+    return (
+        <SpanView
+            span={
+                {
+                    type: spanType,
+                    offset: 0,
+                    length: text.length,
+                } as (SpanCodeBlock | SpanCodeInline | SpanItalic | SpanBold | SpanIrony | SpanLoud)
+            }
+        >
+            {text}
+        </SpanView>
+    );
+};
+
+export const ShortcutsBody = () => {
+    return (
         <XScrollView3 flexShrink={1} useDefaultScroll={true}>
             <KeyboardShortcuts>
-                <KeyboardShortcut>
-                    <span>Ctrl + S</span> Search chats
-                </KeyboardShortcut>
-                <KeyboardShortcut>
-                    <span>Esc</span> Close chat
-                </KeyboardShortcut>
-                <KeyboardShortcut>
-                    <span>
-                        <strong>↑</strong>
-                    </span>{' '}
-                    Edit last message (works when the message box is in focus)
-                </KeyboardShortcut>
-                <KeyboardShortcut>
-                    <span>Ctrl + E</span> Edit last message
-                </KeyboardShortcut>
-                <KeyboardShortcut>
-                    <span>Option + ↑ (Mac)</span>
-                    <span>Alt + ↑ (Windows)</span> Previous chat
-                </KeyboardShortcut>
-                <KeyboardShortcut>
-                    <span>Option + ↓ (Mac)</span>
-                    <span>Alt + ↓ (Windows)</span> Next chat
-                </KeyboardShortcut>
-                <KeyboardShortcut>
-                    <span>Enter</span> Send message
-                </KeyboardShortcut>
-                <KeyboardShortcut>
-                    <span>Shift + Enter</span> New line
-                </KeyboardShortcut>
-                <KeyboardShortcut>
-                    <span>Cmd + Enter (Mac)</span>
-                    <span>Ctrl + Enter (Windows)</span> Submit form
-                </KeyboardShortcut>
-                <KeyboardShortcut>
-                    <span>Ctrl + Cmd + Space (Mac)</span> Emojis (standard Mac shortcut)
-                </KeyboardShortcut>
-                <KeyboardShortcut>
-                    <span>Ctrl + Option + N (Mac)</span>
-                    <span>Ctrl + Alt + N (Windows)</span> New chat
-                </KeyboardShortcut>
+                <XView marginBottom={16} fontSize={18} fontWeight={'600'}>
+                    Keyboard shortcuts
+                </XView>
+                <Shortcut shortcuts={['Enter']} description="Send message" />
+                <Shortcut shortcuts={['Shift + Enter']} description="New line" />
+                <Shortcut shortcuts={['Ctrl + E']} description="Edit last message" />
+                <Shortcut
+                    shortcuts={[<strong key="edit">↑</strong>]}
+                    description="Edit last message (works when the message box is in focus)"
+                />
+                <Shortcut
+                    shortcuts={['Cmd + S (Mac)', 'Ctrl + S (Windows)']}
+                    description="Search chats"
+                />
+                <Shortcut
+                    shortcuts={['Option + ↑ (Mac)', 'Alt + ↑ (Windows)']}
+                    description="Previous chat"
+                />
+                <Shortcut
+                    shortcuts={['Option + ↓ (Mac)', 'Alt + ↓ (Windows)']}
+                    description="Next chat"
+                />
+                <Shortcut shortcuts={['Esc']} description="Close chat" />
+                <Shortcut
+                    shortcuts={['Ctrl + Option + N (Mac)', 'Ctrl + Alt + N (Windows)']}
+                    description="New chat"
+                />
+                <Shortcut
+                    shortcuts={['Ctrl + Cmd + Space (Mac)']}
+                    description="Emojis (standard Mac shortcut)"
+                />
+                <Shortcut
+                    shortcuts={['Cmd + Enter (Mac)', 'Ctrl + Enter (Windows)']}
+                    description="Submit form"
+                />
+                <XView marginBottom={16} fontSize={18} fontWeight={'600'}>
+                    Text formatting
+                </XView>
+                <SpanPreview spanType={SpanType.loud} text={`# Heading 1`} />
+                <XView flexDirection="row">
+                    <SpanPreview spanType={SpanType.bold} text={`*bold*`} /> or
+                    <Shortcut shortcuts={['Cmd + B (Mac)', 'Ctrl + B (Windows)']} />
+                </XView>
+                <XView flexDirection="row">
+                    <SpanPreview spanType={SpanType.italic} text={`_italic_`} /> or
+                    <Shortcut shortcuts={['Cmd + I (Mac)', 'Ctrl + I (Windows)']} />
+                </XView>
+                <XView>
+                    <SpanPreview spanType={SpanType.irony} text={`~irony~`} />
+                </XView>
+                <XView>
+                    <SpanPreview spanType={SpanType.code_inline} text={`'one line code block'`} />
+                </XView>
+                <XView>
+                    <SpanPreview
+                        spanType={SpanType.code_block}
+                        text={`'''multiline code blocks'''`}
+                    />
+                </XView>
             </KeyboardShortcuts>
         </XScrollView3>
-    ));
+    );
+};
+
+export function showShortcutsHelp() {
+    showModalBox({ title: 'Shortcuts' }, () => <ShortcutsBody />);
 }
