2f454a5c2972e1f25bb3d9b2d8955b972dd2a286
Sergey Lapin <lapanoid@gmail.com>
2019-05-31 13:57:18 +0300

    fix(web): adjust styles on shortcuts modal

--- packages/openland-web/fragments/modals/showShortcutsHelp.tsx
+++ packages/openland-web/fragments/modals/showShortcutsHelp.tsx
@@ -13,9 +13,10 @@ import {
     SpanIrony,
     SpanLoud,
 } from 'openland-y-utils/spans/Span';
+import { css } from 'linaria';
 
 const KeyboardShortcuts = Glamorous.div({
-    padding: '7px 40px 30px',
+    padding: '2px 40px 30px',
 });
 
 const KeyboardShortcut = Glamorous.div({
@@ -24,33 +25,46 @@ const KeyboardShortcut = Glamorous.div({
     lineHeight: '20px',
     letterSpacing: 0,
     color: '#000000',
-    marginBottom: 15,
-
-    '& span': {
-        margin: '-1px 8px -2px 0',
-        padding: '1px 8px 2px',
-        display: 'inline-block',
-        fontSize: 13,
-        fontWeight: 400,
-        lineHeight: '20px',
-        color: 'rgba(0, 0, 0, 0.5)',
-        borderRadius: 8,
-        backgroundColor: 'rgba(0, 0, 0, 0.06)',
-    },
-
-    '& strong': {
-        fontWeight: 600,
-    },
+    marginBottom: 12,
 });
+
+const shortcutSpanClassName = css`
+    padding: 1px 8px 2px;
+    display: inline-block;
+    font-size: 13px
+    font-weight: 400;
+    line-height: 20px;
+    color: rgba(0, 0, 0, 0.5);
+    border-radius: 8px;
+    background-color: rgba(0, 0, 0, 0.06);
+`;
+
+const descriptionClassName = css`
+    margin-left: 12px;
+    font-size: 14px;
+    line-height: 1.43;
+    color: #000000;
+`;
 
 const Shortcut = ({ shortcuts, description }: { shortcuts: any[]; description?: string }) => {
     return (
         <KeyboardShortcut>
             {shortcuts.map((shortcut, key) => {
-                return <span key={key}>{shortcut}</span>;
+                if (!(typeof shortcut === 'string')) {
+                    return shortcut;
+                }
+                return (
+                    <span
+                        className={shortcutSpanClassName}
+                        style={{ marginLeft: key === 0 ? 0 : 8 }}
+                        key={key}
+                    >
+                        {shortcut}
+                    </span>
+                );
             })}
 
-            {description}
+            <span className={descriptionClassName}>{description}</span>
         </KeyboardShortcut>
     );
 };
@@ -84,6 +98,16 @@ const SpanPreview = ({
     );
 };
 
+const arrowUpShortcutClassName = css`
+    padding: 3px 6px 4px 6px;
+    font-size: 13px;
+    line-height: 1.54;
+    font-weight: 600;
+    border-radius: 8px;
+    color: rgba(0, 0, 0, 0.5);
+    background-color: rgba(0, 0, 0, 0.06);
+`;
+
 export const ShortcutsBody = () => {
     return (
         <XScrollView3 flexShrink={1} useDefaultScroll={true}>
@@ -95,9 +119,14 @@ export const ShortcutsBody = () => {
                 <Shortcut shortcuts={['Shift + Enter']} description="New line" />
                 <Shortcut shortcuts={['Ctrl + E']} description="Edit last message" />
                 <Shortcut
-                    shortcuts={[<strong key="edit">↑</strong>]}
+                    shortcuts={[
+                        <strong className={arrowUpShortcutClassName} key="edit">
+                            ↑
+                        </strong>,
+                    ]}
                     description="Edit last message (works when the message box is in focus)"
                 />
+                <XView height={22} />
                 {/* overlapse with Cmd + S */}
                 <Shortcut shortcuts={['Ctrl + S']} description="Search chats" />
                 {/* overlapse with option + ↑ behaves weird with scroll */}
@@ -110,11 +139,11 @@ export const ShortcutsBody = () => {
                     description="Next chat"
                 />
                 <Shortcut shortcuts={['Esc']} description="Close chat" />
-                {/* we don't have it now */}
-                {/* <Shortcut
+                <Shortcut
                     shortcuts={['Ctrl + Option + N (Mac)', 'Ctrl + Alt + N (Windows)']}
                     description="New chat"
-                /> */}
+                />
+                <XView height={20} />
                 <Shortcut
                     shortcuts={['Ctrl + Cmd + Space (Mac)']}
                     description="Emojis (standard Mac shortcut)"
@@ -123,25 +152,38 @@ export const ShortcutsBody = () => {
                     shortcuts={['Cmd + Enter (Mac)', 'Ctrl + Enter (Windows)']}
                     description="Submit form"
                 />
-                <XView marginBottom={16} fontSize={18} fontWeight={'600'}>
+                <XView marginTop={33} marginBottom={11} fontSize={18} fontWeight={'600'}>
                     Text formatting
                 </XView>
-                <SpanPreview spanType={SpanType.loud} text={`# Heading 1`} />
+                <XView marginTop={5} marginBottom={17}>
+                    <SpanPreview spanType={SpanType.loud} text={`# Heading 1`} />
+                </XView>
                 <XView flexDirection="row">
-                    <SpanPreview spanType={SpanType.bold} text={`*bold*`} /> or
+                    <SpanPreview spanType={SpanType.bold} text={`*bold*`} />
+                    <XView paddingLeft={6} paddingRight={8}>
+                        or
+                    </XView>
                     <Shortcut shortcuts={['Cmd + B (Mac)', 'Ctrl + B (Windows)']} />
                 </XView>
                 <XView flexDirection="row">
-                    <SpanPreview spanType={SpanType.italic} text={`_italic_`} /> or
+                    <SpanPreview spanType={SpanType.italic} text={`_italic_`} />{' '}
+                    <XView paddingLeft={6} paddingRight={8}>
+                        or
+                    </XView>
                     <Shortcut shortcuts={['Cmd + I (Mac)', 'Ctrl + I (Windows)']} />
                 </XView>
                 <XView>
                     <SpanPreview spanType={SpanType.irony} text={`~irony~`} />
                 </XView>
-                <XView>
-                    <SpanPreview spanType={SpanType.code_inline} text={`'one line code block'`} />
+                <XView marginTop={12}>
+                    <div>
+                        <SpanPreview
+                            spanType={SpanType.code_inline}
+                            text={`'one line code block'`}
+                        />
+                    </div>
                 </XView>
-                <XView>
+                <XView marginTop={12}>
                     <SpanPreview
                         spanType={SpanType.code_block}
                         text={`'''multiline code blocks'''`}
