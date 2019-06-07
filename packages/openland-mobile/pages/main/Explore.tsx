6d2e1dedfd2fdd83a35d28508c964683646bbc71
Fred Morozov <morozov@fred.md>
2019-06-07 19:55:17 +0300

    fix(ios): remove hairline from Discover-screen

--- packages/openland-mobile/pages/main/Explore.tsx
+++ packages/openland-mobile/pages/main/Explore.tsx
@@ -17,6 +17,7 @@ import { ASSafeAreaContext } from 'react-native-async-view/ASSafeAreaContext';
 import { ZRoundedButton } from 'openland-mobile/components/ZRoundedButton';
 import { ThemeContext } from 'openland-mobile/themes/ThemeContext';
 import { TextStyles } from 'openland-mobile/styles/AppStyles';
+import { HeaderConfigRegistrator } from 'react-native-s/navigation/HeaderConfigRegistrator';
 
 const RoomsList = (props: { router: SRouter }) => {
     let resp = getClient().useAccountSettings({ fetchPolicy: 'network-only' });
@@ -152,21 +153,23 @@ const ExplorePage = (props: PageProps) => {
     let theme = React.useContext(ThemeContext);
     if (!discoverDone.betaIsDiscoverDone) {
         return (
-            <ASSafeAreaContext.Consumer>
-                {sa => (
-                    <View width="100%" height="100%" justifyContent="space-between" alignItems="center" paddingTop={sa.top} paddingBottom={sa.bottom}>
-                        <Image marginTop={theme.blurType === 'light' ? -30 : 0} marginBottom={-25} source={theme.blurType === 'dark' ? require('assets/img-unsupported_dark.png') : require('assets/img-unsupported.png')} />
-                        <View alignItems="center" justifyContent="center">
-                            <Text style={{ fontSize: 30, color: theme.textColor, marginBottom: 10, fontWeight: TextStyles.weight.bold } as TextStyle}>Discover chats</Text>
-                            <Text style={{ fontSize: 18, color: theme.textColor }}>Find the right chats for you</Text>
+            <>
+                <HeaderConfigRegistrator config={{ hairline: 'hidden' }} />
+                <ASSafeAreaContext.Consumer>
+                    {sa => (
+                        <View width="100%" height="100%" justifyContent="space-between" alignItems="center" paddingTop={sa.top} paddingBottom={sa.bottom}>
+                            <Image marginTop={theme.blurType === 'light' ? -30 : 0} marginBottom={-25} source={theme.blurType === 'dark' ? require('assets/img-unsupported_dark.png') : require('assets/img-unsupported.png')} />
+                            <View alignItems="center" justifyContent="center">
+                                <Text style={{ fontSize: 30, color: theme.textColor, marginBottom: 10, fontWeight: TextStyles.weight.bold } as TextStyle}>Discover chats</Text>
+                                <Text style={{ fontSize: 18, color: theme.textColor }}>Find the right chats for you</Text>
+                            </View>
+
+                            <ZRoundedButton size="large" title="Start" onPress={() => props.router.push("Discover")} />
+                            <View />
                         </View>
-
-                        <ZRoundedButton size="large" title="Start" onPress={() => props.router.push("Discover")} />
-                        <View />
-                    </View>
-                )}
-
-            </ASSafeAreaContext.Consumer>
+                    )}
+                </ASSafeAreaContext.Consumer>
+            </>
         );
     }
     return (
