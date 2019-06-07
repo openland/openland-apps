c7a88368483f431b42810194da3fb0899c0ba85a
Sergey Lapin <lapanoid@gmail.com>
2019-06-07 12:07:27 +0300

    fix(web): append fix

--- packages/openland-web/pages/main/directory/discover.page.tsx
+++ packages/openland-web/pages/main/directory/discover.page.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 import { DirectoryNavigation } from './components/DirectoryNavigation';
 import { withApp } from 'openland-web/components/withApp';
 
-export default withApp('Communities', 'viewer', () => {
+export default withApp('Discover', 'viewer', () => {
     return <DirectoryNavigation>stub</DirectoryNavigation>;
 });
