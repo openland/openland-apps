f1d9ccada9f38ef155c07c0e02a26fbfed32a7df
Sergey Lapin <lapanoid@gmail.com>
2019-06-14 09:11:14 +0300

    fix(engine): fix build

--- packages/openland-engines/EnginesOptions.ts
+++ packages/openland-engines/EnginesOptions.ts
@@ -3,5 +3,5 @@ import { KeyValueStore } from 'openland-y-utils/KeyValueStore';
 export interface EngineOptions {
     conversationBatchSize: number;
     store: KeyValueStore;
-    mocked: boolean;
+    mocked?: boolean;
 }
