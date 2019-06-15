92ec633204d2d0f1932ae26a61ac42a7151a65e0
Fred Morozov <morozov@fred.md>
2019-06-15 17:48:54 +0300

    feat(mobile): handle long press for notifications

--- packages/openland-mobile/pages/main/components/notificationCenter/NotificationCenterHandlers.tsx
+++ packages/openland-mobile/pages/main/components/notificationCenter/NotificationCenterHandlers.tsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import { Alert } from 'openland-mobile/components/AlertBlanket';
 import { ActionSheetBuilder } from 'openland-mobile/components/ActionSheet';
 import { NotificationsDataSourceItem } from 'openland-engines/NotificationCenterEngine';
+import { getClient } from 'openland-mobile/utils/graphqlClient';
+import { startLoader, stopLoader } from 'openland-mobile/components/ZGlobalLoader';
+import { CommentSubscriptionType } from 'openland-api/Types';
 
 class NotificationCenterHandlersClass {
     handlePress = (id: string, item: NotificationsDataSourceItem) => {
@@ -9,14 +12,37 @@ class NotificationCenterHandlersClass {
     }
 
     handleLongPress = (id: string, item: NotificationsDataSourceItem) => {
+        const client = getClient();
         const builder = new ActionSheetBuilder();
 
-        builder.action('Turn off notifications', () => {
-            // ignore
+        builder.action(item.isSubscribedMessageComments ? 'Turn off notifications' : 'Turn on notifications', async () => {
+            startLoader();
+
+            try {
+                if (item.peerRootId) {
+                    if (item.isSubscribedMessageComments) {
+                        await client.mutateUnSubscribeMessageComments({ messageId: item.peerRootId });
+                    } else {
+                        await client.mutateSubscribeMessageComments({ messageId: item.peerRootId, type: CommentSubscriptionType.ALL });
+                    }
+                }
+            } catch (e) {
+                console.warn(e);
+            } finally {
+                stopLoader();
+            }
         });
 
-        builder.action('Remove this notification', () => {
-            // ignore
+        builder.action('Remove this notification', async () => {
+            startLoader();
+
+            try {
+                await client.mutateDeleteNotification({ notificationId: id });
+            } catch (e) {
+                console.warn(e);
+            } finally {
+                stopLoader();
+            }
         });
 
         builder.show();
