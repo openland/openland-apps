d76470d900fc3909dd231db6fe21adf6e23b5d87
Fred Morozov <morozov@fred.md>
2019-06-15 20:04:09 +0300

    feat(mobile): icon in NotificationCenterHeader

--- packages/openland-mobile/pages/main/components/notificationCenter/NotificationCenterHeader.tsx
+++ packages/openland-mobile/pages/main/components/notificationCenter/NotificationCenterHeader.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { XMemo } from 'openland-y-utils/XMemo';
-import { Text, View, Platform } from 'react-native';
+import { Text, View, Platform, Image } from 'react-native';
 import { SHeaderView } from 'react-native-s/SHeaderView';
 import { TextStyles } from 'openland-mobile/styles/AppStyles';
 import { AppTheme } from 'openland-mobile/themes/themes';
@@ -15,9 +15,10 @@ export const NotificationCenterHeader = XMemo<{ theme: AppTheme }>((props) => {
 
     return (
         <SHeaderView>
-            <View flexDirection="row" flexGrow={1}>
-                <View width={36} justifyContent="center">
-                    <View style={{ height: 36, width: 36, backgroundColor: theme.accentColor, opacity: 0.1, borderRadius: 18 }} />
+            <View flexDirection="row" flexGrow={1} alignItems="center">
+                <View width={36} height={36} justifyContent="center" alignItems="center">
+                    <View style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, height: 36, width: 36, backgroundColor: theme.accentColor, opacity: 0.1, borderRadius: 18 }} />
+                    <Image source={require('assets/ic-comment-channel-22.png')} style={{ width: 22, height: 22, tintColor: theme.accentColor }} />
                 </View>
                 <View paddingLeft={12} flexGrow={1} justifyContent="center">
                     <Text style={{ color: props.theme.textColor, fontSize: 16, fontWeight: TextStyles.weight.medium }}>
