e634003de23cd09f47a9ef1ca28fcb67161333e9
Sergey Lapin <lapanoid@gmail.com>
2019-06-17 14:50:55 +0300

    fix(web): selected comment input field should be active

--- packages/openland-web/components/messenger/message/content/comments/CommentsModalInner.tsx
+++ packages/openland-web/components/messenger/message/content/comments/CommentsModalInner.tsx
@@ -276,6 +276,7 @@ export const CommentsBlockView = ({
             scrollToComment({
                 commentId: selectedCommentMessageId,
             });
+            setShowInputId(selectedCommentMessageId);
         }
     });
 
@@ -429,21 +430,18 @@ export const CommentsModalInnerNoRouter = ({
         };
     });
 
-    React.useEffect(
-        () => {
-            if (currentCommentsInputRef.current && scrollRef.current) {
-                const targetElem = currentCommentsInputRef.current.getElement()!!
-                    .parentNode as HTMLElement;
-                if (targetElem) {
-                    scrollRef.current.scrollToBottomOfElement({
-                        targetElem,
-                        offset: 10,
-                    });
-                }
+    React.useEffect(() => {
+        if (currentCommentsInputRef.current && scrollRef.current) {
+            const targetElem = currentCommentsInputRef.current.getElement()!!
+                .parentNode as HTMLElement;
+            if (targetElem) {
+                scrollRef.current.scrollToBottomOfElement({
+                    targetElem,
+                    offset: 10,
+                });
             }
-        },
-        [showInputId],
-    );
+        }
+    }, [showInputId]);
 
     return (
         <UploadContextProvider>
