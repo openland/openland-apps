54abb1ecbb05ed352fad625fa41bbbe707b2169c
Fred Morozov <morozov@fred.md>
2019-06-19 23:58:31 +0300

    fix(engines): remove unnecessary console.warns

--- packages/openland-y-runtime-web/AppStorage.ts
+++ packages/openland-y-runtime-web/AppStorage.ts
@@ -8,8 +8,6 @@ class AppStorageImpl implements AppStorageApi {
         }
         let res = localStorage.getItem(key);
 
-        console.warn('Engine-Tracking', res);
-
         if (res) {
             return JSON.parse(res);
         } else {
