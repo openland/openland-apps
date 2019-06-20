83958a69426c636ba64797bf49cbd7065e215f97
Sergey Lapin <lapanoid@gmail.com>
2019-06-20 11:26:35 +0300

    fix(engine): remove extra spread

--- packages/openland-engines/NotificationCenterEngine.ts
+++ packages/openland-engines/NotificationCenterEngine.ts
@@ -121,7 +121,7 @@ export class NotificationCenterEngine {
                     await this.handleStateProcessed(st);
                 });
 
-                this.notifications = [...items];
+                this.notifications = items;
                 this.state = new NotificationCenterState(false, this.notifications);
 
                 this.onNotificationsUpdated();
