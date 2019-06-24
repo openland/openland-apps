aeb2184d234e820fbb940e71c17511bd57c97c91
Sergey Lapin <lapanoid@gmail.com>
2019-06-24 15:00:26 +0300

    wip(web): add chose role page with tags

--- packages/openland-web/pages/onboarding/choose-role.page.tsx
+++ packages/openland-web/pages/onboarding/choose-role.page.tsx
@@ -7,6 +7,8 @@ import { XButton } from 'openland-x/XButton';
 import { css } from 'linaria';
 import { BackSkipLogo } from './components/BackSkipLogo';
 import { getPercentageOfOnboarding } from './utils';
+import { XScrollView3 } from 'openland-x/XScrollView3';
+import { useClient } from 'openland-web/utils/useClient';
 
 const backgroundClassName = css`
     background: white;
@@ -17,47 +19,17 @@ export type Tag = { id: string; title: string };
 export type TagGroup = { id: string; title?: string | null; subtitle?: string | null; tags: Tag[] };
 
 const TagButton = (props: { tag: Tag; selected: boolean; onPress: (tag: Tag) => void }) => {
-    let style: 'fill' | 'border' = 'fill' as any;
-
     let callback = React.useCallback(() => {
         props.onPress(props.tag);
     }, [props.tag]);
+
     return (
-        <XView
-            onClick={callback}
-            marginRight={10}
-            marginBottom={12}
-            paddingHorizontal={16}
-            paddingVertical={10}
-            borderRadius={12}
-            borderWidth={2}
-            // style={
-            //     {
-            //         // backgroundColor:
-            //         //     props.tag.id === 'button_more'
-            //         //         ? undefined
-            //         //         : props.selected
-            //         //         ? style === 'fill'
-            //         //             ? theme.accentColor
-            //         //             : theme.accentBackgroundColor
-            //         //         : theme.accentBackgroundColor,
-            //         // borderColor: props.selected ? theme.accentColor : theme.accentBackgroundColor,
-            //     }
-            // }
-        >
-            <span
-                style={{
-                    fontSize: 16,
-                    // fontWeight: TextStyles.weight.medium,
-                    // color: props.selected
-                    //     ? style === 'fill'
-                    //         ? theme.textInverseColor
-                    //         : theme.accentColor
-                    //     : theme.accentColor,
-                }}
-            >
-                {props.tag.title}
-            </span>
+        <XView marginRight={12} marginBottom={12}>
+            <XButton
+                onClick={callback}
+                text={props.tag.title}
+                style={props.selected ? 'primary' : 'light'}
+            />
         </XView>
     );
 };
@@ -86,9 +58,7 @@ const TagsCloud = (props: {
 
     return (
         <XView flexDirection="column">
-            <XView height={15} />
-            {/* {props.tagsGroup.title && <Text style={{ fontSize: 16, marginBottom: 20, fontWeight: TextStyles.weight.medium, color: theme.textColor }}>{props.tagsGroup.title}</Text>} */}
-            <XView marginBottom={18} flexDirection="row" flexWrap="wrap">
+            <XView marginBottom={18} flexDirection="row" flexWrap="wrap" width={350}>
                 {props.tagsGroup.tags
                     .filter((t, i) => showAll || i < 17)
                     .map(tag => (
@@ -106,12 +76,96 @@ const TagsCloud = (props: {
                     />
                 )}
             </XView>
-            {/* {sub} */}
         </XView>
     );
 };
 
+const TagsGroupPage = (props: {
+    group?: TagGroup | null;
+    selected: Set<string>;
+    exclude: Set<string>;
+}) => {
+    let [selected, setCurretnSelected] = React.useState(props.selected);
+    let onSelectedChange = React.useCallback((s: Set<string>) => {
+        setCurretnSelected(new Set(s));
+    }, []);
+
+    // let disabled = !selected.size && props.exclude.size <= 1;
+    // let next = React.useCallback(() => {
+    //     if (disabled) {
+    //         return;
+    //     }
+    //     (async () => {
+    //         let nextPath = 'Discover';
+
+    //         let params: any = { selected, exclude: new Set(props.exclude) };
+
+    //         // props.router.push(nextPath, params);
+    //     })();
+    // }, [selected, disabled]);
+
+    if (!props.group) {
+        return null;
+    }
+
+    let { title, subtitle } = props.group;
+
+    return (
+        <>
+            <XScrollView3>
+                <XView paddingHorizontal={18}>
+                    <TagsCloud
+                        tagsGroup={props.group}
+                        selected={selected}
+                        onSelectedChange={onSelectedChange}
+                    />
+                </XView>
+                <XView height={120} />
+            </XScrollView3>
+
+            {/* <LinearGradient
+                colors={[theme.transparent, theme.backgroundColor, theme.backgroundColor]}
+                height={160}
+                position="absolute"
+                bottom={0}
+                width="100%"
+                justifyContent="center"
+                alignItems="center"
+                pointerEvents="none"
+            /> */}
+
+            {/* <ASSafeAreaContext.Consumer>
+                {sa => (
+                    <View
+                        alignContent="center"
+                        justifyContent="center"
+                        alignSelf="center"
+                        bottom={sa.bottom + 48}
+                    >
+                        <ZRoundedButton
+                            size="large"
+                            title="  Next  "
+                            style="default"
+                            enabled={!disabled}
+                            onPress={next}
+                        />
+                    </View>
+                )}
+            </ASSafeAreaContext.Consumer> */}
+        </>
+    );
+};
+
 export default withApp('Home', 'viewer', () => {
+    const client = useClient();
+    const selected: any[] = [];
+    const exclude: any[] = [];
+
+    let currentPage = client.useDiscoverNextPage(
+        { selectedTagsIds: [...selected], excudedGroupsIds: [...exclude] },
+        { fetchPolicy: 'network-only' },
+    );
+
     return (
         <div className={backgroundClassName}>
             <XDocumentHead title="Choose role" />
@@ -128,6 +182,12 @@ export default withApp('Home', 'viewer', () => {
                     <XView fontSize={16} marginBottom={40}>
                         What roles have you played?
                     </XView>
+
+                    <TagsGroupPage
+                        group={currentPage.betaNextDiscoverPage!!.tagGroup}
+                        exclude={new Set()}
+                        selected={new Set()}
+                    />
 
                     <XButton text="Continue" style="primary" size="default" />
                 </XView>
