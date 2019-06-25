6a19f25fd1d39b1f1ead1bd0e942506e2301620b
Sergey Lapin <lapanoid@gmail.com>
2019-06-25 18:21:38 +0300

    wip(web): finish chats for you

--- packages/openland-web/pages/onboarding/chats-for-you.page.tsx
+++ packages/openland-web/pages/onboarding/chats-for-you.page.tsx
@@ -8,16 +8,138 @@ import { css } from 'linaria';
 import { BackSkipLogo } from './components/BackSkipLogo';
 import { getPercentageOfOnboarding } from './utils';
 import { useClient } from 'openland-web/utils/useClient';
-import { XRoomCard } from 'openland-x/cards/XRoomCard';
+import CheckIcon from 'openland-icons/checked.svg';
+import { SuggestedRooms_suggestedRooms_SharedRoom } from 'openland-api/Types';
+import { XAvatar2 } from 'openland-x/XAvatar2';
 
 const backgroundClassName = css`
     background: white;
     width: 100%;
 `;
 
+const CheckIconClassName = css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 18px;
+    height: 18px;
+    border-radius: 18px;
+    background-color: #1790ff;
+`;
+
+const ChatsItem = ({
+    room,
+    isSelected,
+    onSelect,
+}: {
+    room: SuggestedRooms_suggestedRooms_SharedRoom;
+    isSelected: boolean;
+    onSelect: (a: string) => void;
+}) => {
+    return (
+        <XView
+            cursor="pointer"
+            flexShrink={0}
+            flexDirection="row"
+            alignItems="center"
+            paddingLeft={16}
+            paddingRight={20}
+            paddingVertical={8}
+            borderRadius={8}
+            backgroundColor={'#fff'}
+            hoverBackgroundColor="#F5F5F6"
+            onClick={() => onSelect(room.id)}
+        >
+            <XAvatar2 src={room.photo || undefined} title={room.title} id={room.id} size={40} />
+            <XView flexDirection="column" flexGrow={1} marginLeft={16}>
+                <XView fontSize={15} fontWeight="600">
+                    {room.title}
+                </XView>
+                <XView fontSize={14} color="rgba(0, 0, 0, 0.6)">
+                    {`${room.membersCount} members`}
+                </XView>
+            </XView>
+            {isSelected && (
+                <div className={CheckIconClassName}>
+                    <CheckIcon />
+                </div>
+            )}
+        </XView>
+    );
+};
+
+const ChatsItemList = ({ rooms }: { rooms: SuggestedRooms_suggestedRooms_SharedRoom[] }) => {
+    const [selectedIds, setSelectedIds] = React.useState<string[]>([]);
+    const allRoomsIds = rooms.map(({ id }) => id);
+
+    return (
+        <>
+            <XView flexDirection="row" justifyContent="space-between" marginBottom={18}>
+                <XView fontWeight={'600'} fontSize={17} color={'rgba(0, 0, 0, 0.5)'}>{`${
+                    rooms.length
+                } chats`}</XView>
+                {allRoomsIds.length === selectedIds.length ? (
+                    <XButton
+                        text={`Clear`}
+                        style="light"
+                        size="default"
+                        onClick={() => {
+                            setSelectedIds([]);
+                        }}
+                    />
+                ) : (
+                    <XButton
+                        text={`Select all`}
+                        style="light"
+                        size="default"
+                        onClick={() => {
+                            setSelectedIds(allRoomsIds);
+                        }}
+                    />
+                )}
+            </XView>
+
+            {rooms.map((room, key) => {
+                return (
+                    <ChatsItem
+                        key={key}
+                        room={room}
+                        isSelected={selectedIds.indexOf(room.id) !== -1}
+                        onSelect={id => {
+                            if (selectedIds.indexOf(id) === -1) {
+                                setSelectedIds([...selectedIds, id]);
+                            } else {
+                                setSelectedIds(selectedIds.filter(item => item !== id));
+                            }
+                        }}
+                    />
+                );
+            })}
+
+            <XView marginTop={40} flexShrink={1} alignSelf="center">
+                <XButton
+                    text={selectedIds.length ? `Join ${selectedIds.length} chats` : `Join`}
+                    style="primary"
+                    size="default"
+                    enabled={!!selectedIds.length}
+                />
+            </XView>
+        </>
+    );
+};
+
 export const ChatsForYou = () => {
     const client = useClient();
     const data = client.useSuggestedRooms();
+
+    const rooms: SuggestedRooms_suggestedRooms_SharedRoom[] = [];
+
+    for (let i = 0; i < data.suggestedRooms.length; i++) {
+        const room = data.suggestedRooms[i];
+        if (room.__typename === 'SharedRoom') {
+            rooms.push(room);
+        }
+    }
 
     return (
         <div className={backgroundClassName}>
@@ -38,34 +160,7 @@ export const ChatsForYou = () => {
                         </XView>
                     </XView>
 
-                    <XView flexDirection="row" justifyContent="space-between" marginBottom={18}>
-                        <XView fontWeight={'600'} fontSize={17} color={'rgba(0, 0, 0, 0.5)'}>{`${
-                            data.suggestedRooms.length
-                        } chats`}</XView>
-                        <XButton text={`Select all`} style="light" size="default" />
-                    </XView>
-
-                    {data.suggestedRooms.map((room, key) => {
-                        if (room.__typename === 'SharedRoom') {
-                            return (
-                                <XRoomCard
-                                    key={key}
-                                    room={room as any}
-                                    path={'/directory/p/' + room.id}
-                                    customMenu={null}
-                                />
-                            );
-                        }
-                        return null;
-                    })}
-
-                    <XView marginTop={40} flexShrink={1} alignSelf="center">
-                        <XButton
-                            text={`Join ${data.suggestedRooms.length} chats`}
-                            style="primary"
-                            size="default"
-                        />
-                    </XView>
+                    <ChatsItemList rooms={rooms} />
                 </XView>
             </XView>
         </div>
