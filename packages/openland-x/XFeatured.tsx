4491a05a7676c25984d8bb50bfb2fb8073ee0098
Fred Morozov <morozov@fred.md>
2019-07-03 22:20:48 +0300

    feat(web): featured tooltip without XPopper in message header

--- packages/openland-x/XFeatured.tsx
+++ packages/openland-x/XFeatured.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { XMemo } from 'openland-y-utils/XMemo';
 import { XView } from 'react-mental';
-import { XPopper } from './XPopper';
 import FeaturedIcon from 'openland-icons/ic-featured-star.svg';
+import { css } from 'linaria';
 
 export const XFeaturedIcon = XMemo(props => (
     <XView width={18} height={18}>
@@ -19,16 +19,50 @@ export const XFeatured = XMemo<{ text: string | JSX.Element }>(props => (
     </XView>
 ));
 
+const tooltipWrapperClass = css`
+    position: relative;
+
+    & > span {
+        display: none;
+        background: #6E7588;
+        color: #ffffff;
+        border-radius: 10px;
+        padding: 6px 12px 8px;
+        line-height: 16p;
+        box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+        position: absolute;
+        text-align: center;
+        bottom: 100%;
+        left: 50%;
+        transform: translate(-50%, 0);
+        margin-bottom: 12px;
+        font-size: 14px;
+        white-space: nowrap;
+
+        &:before {
+            content: "";
+            display: block;
+            width: 0;
+            height: 0;
+            border-left: 5px solid transparent;
+            border-right: 5px solid transparent;
+            border-top: 5px solid #6E7588;
+            position: absolute;
+            top: 100%;
+            left: 50%;
+            transform: translate(-50%, 0);
+        }
+    }
+
+    &:hover > span {
+        display: block;
+    }
+`;
+
 export const XFeaturedTooltip = XMemo<{ text: string | JSX.Element }>(props => (
-    // <XPopper
-    //     style="dark"
-    //     placement="top"
-    //     content={<span>{props.text}</span>}
-    //     showOnHoverContent={false}
-    //     showOnHover={true}
-    // >
-    //     <div>
-            <XFeaturedIcon />
-    //     </div>
-    // </XPopper>
+    <div className={tooltipWrapperClass}>
+        <XFeaturedIcon />
+
+        <span>{props.text}</span>
+    </div>
 ));
