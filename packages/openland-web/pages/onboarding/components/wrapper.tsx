f7105a8adb6b405f7de48919bf190bcdddb4323c
Max Volkov <wolkow_1996@mail.ru>
2019-07-04 19:24:06 +0300

    fix(web): discover / auth wrapper

--- packages/openland-web/pages/onboarding/components/wrapper.tsx
+++ packages/openland-web/pages/onboarding/components/wrapper.tsx
@@ -12,19 +12,47 @@ const heightClassName = css`
     height: 100%;
 `;
 
-export const Wrapper = (props: { children: any; fullHeight?: boolean }) => (
-    <div className={cx(wrapperClassName, props.fullHeight && heightClassName)}>
-        <XView
-            backgroundColor="white"
-            flexGrow={1}
-            flexShrink={0}
-            flexBasis={0}
-            width="100%"
-            height="100%"
+const iosClassName = css`
+    overflow: hidden;
+`;
+
+const androidClassName = css`
+    height: 100vh;
+`;
+
+export const Wrapper = (props: { children: any; fullHeight?: boolean }) => {
+    let userAgent = window.navigator.userAgent,
+        platform = window.navigator.platform,
+        iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+        isIos = false,
+        isAndroid = false;
+
+    if (iosPlatforms.indexOf(platform) !== -1) {
+        isIos = true;
+    } else if (/Android/.test(userAgent)) {
+        isAndroid = true;
+    }
+    return (
+        <div
+            className={cx(
+                wrapperClassName,
+                props.fullHeight && heightClassName,
+                props.fullHeight && isIos && iosClassName,
+                isAndroid && androidClassName,
+            )}
         >
-            <XView flexShrink={1} flexGrow={1} flexBasis={0}>
-                {props.children}
+            <XView
+                backgroundColor="white"
+                flexGrow={1}
+                flexShrink={0}
+                flexBasis={0}
+                width="100%"
+                height="100%"
+            >
+                <XView flexShrink={1} flexGrow={1} flexBasis={0}>
+                    {props.children}
+                </XView>
             </XView>
-        </XView>
-    </div>
-);
+        </div>
+    );
+};
