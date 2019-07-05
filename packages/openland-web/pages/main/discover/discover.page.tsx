4f07ae04bddf9310a76944554319e73928f116b7
Sergey Lapin <lapanoid@gmail.com>
2019-07-05 16:25:37 +0300

    fix(web): return chats for you

--- packages/openland-web/pages/main/discover/discover.page.tsx
+++ packages/openland-web/pages/main/discover/discover.page.tsx
@@ -2,8 +2,12 @@ import * as React from 'react';
 import { DiscoverNavigation } from './components/DiscoverNavigation';
 import { withApp } from 'openland-web/components/withApp';
 import { DiscoverStart } from '../../onboarding/start.page';
-import { DiscoverOnLocalState } from '../../onboarding/discover.page';
 import { useClient } from 'openland-web/utils/useClient';
+import { XView } from 'react-mental';
+import { XContentWrapper } from 'openland-x/XContentWrapper';
+import { XRoomCard } from 'openland-x/cards/XRoomCard';
+import { showModalBox } from 'openland-x/showModalBox';
+import { DiscoverOnLocalState } from 'openland-web/pages/onboarding/discover.page';
 
 export type tabsT = 'start' | 'discover';
 
@@ -16,14 +20,20 @@ const DiscoverContent = () => {
     const client = useClient();
 
     const discoverDone = client.useDiscoverIsDone({ fetchPolicy: 'network-only' });
-
+    const data = client.useSuggestedRooms();
     const [tab, setTab] = React.useState(
         discoverDone.betaIsDiscoverDone ? tabs.discover : tabs.start,
     );
 
+    React.useEffect(() => {
+        setTab(discoverDone.betaIsDiscoverDone ? tabs.discover : tabs.start);
+    }, [discoverDone.betaIsDiscoverDone]);
+
     const onStartClick = React.useCallback(() => {
-        setTab(tabs.discover);
-    }, [tabs.discover]);
+        showModalBox({ fullScreen: true }, () => (
+            <DiscoverOnLocalState fullHeight={true} noTopBar />
+        ));
+    }, []);
 
     return (
         <React.Suspense fallback={null}>
@@ -31,13 +41,24 @@ const DiscoverContent = () => {
                 <DiscoverStart onSkip={null} onStartClick={onStartClick} noTopBar noBackSkipLogo />
             )}
             {tab === 'discover' && (
-                <DiscoverOnLocalState
-                    noTopBar
-                    noLogo
-                    noSkipOnFirstScreen
-                    noSkipOnChatsForYou
-                    noBackOnFirstScreen
-                />
+                <XContentWrapper withPaddingBottom={true}>
+                    <XView fontSize={16} marginTop={32} marginBottom={27}>
+                        Chats for you
+                    </XView>
+                    {data.suggestedRooms.map((room, key) => {
+                        if (room.__typename === 'SharedRoom') {
+                            return (
+                                <XRoomCard
+                                    key={key}
+                                    room={room as any}
+                                    path={'/directory/p/' + room.id}
+                                    customMenu={null}
+                                />
+                            );
+                        }
+                        return null;
+                    })}
+                </XContentWrapper>
             )}
         </React.Suspense>
     );
