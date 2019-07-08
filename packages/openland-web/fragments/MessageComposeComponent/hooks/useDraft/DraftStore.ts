7f6c39ada9d0a0beaf3efe571579af2cdd0cdc5a
Fred Morozov <morozov@fred.md>
2019-07-08 21:05:12 +0300

    fix(web): remove console.warns

--- packages/openland-web/fragments/MessageComposeComponent/hooks/useDraft/DraftStore.ts
+++ packages/openland-web/fragments/MessageComposeComponent/hooks/useDraft/DraftStore.ts
@@ -24,7 +24,6 @@ export const getDraftMessage = (
     );
 
     mentions = convertSpansToUserWithOffset({ spans: mentions });
-    console.warn(mentions);
 
     let draftKey = getDraftKey(conversationId);
 
