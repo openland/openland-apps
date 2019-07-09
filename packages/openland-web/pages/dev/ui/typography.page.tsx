cd559d9eadcd70d39e45bafbbda627b27db670a2
Sergey Lapin <lapanoid@gmail.com>
2019-07-09 20:02:30 +0300

    fix(web): fix build

--- packages/openland-web/pages/dev/ui/typography.page.tsx
+++ packages/openland-web/pages/dev/ui/typography.page.tsx
@@ -4,7 +4,7 @@ import { DevDocsScaffold } from './components/DevDocsScaffold';
 import { XContent } from 'openland-x-layout/XContent';
 import { XVertical2 } from 'openland-x/XVertical2';
 import { XView } from 'react-mental';
-import { XText } from 'openland-web/components/XText';
+import { XText, Mode } from 'openland-web/components/XText';
 
 export default withApp('UI Framework - Typograpthy', 'viewer', props => {
     return (
@@ -12,22 +12,22 @@ export default withApp('UI Framework - Typograpthy', 'viewer', props => {
             <XContent>
                 <XVertical2>
                     <XView marginTop={24}>
-                        <XText mode="TitleOne">Title 1 — Bold 24/32</XText>
+                        <XText mode={Mode.TitleOne}>Title 1 — Bold 24/32</XText>
                     </XView>
                     <XView marginTop={24}>
-                        <XText mode="TitleTwo">Title 2 — Bold 17/24</XText>
+                        <XText mode={Mode.TitleTwo}>Title 2 — Bold 17/24</XText>
                     </XView>
                     <XView marginTop={24}>
-                        <XText mode="LabelSemicolumn">Label — Semibold 15/24</XText>
+                        <XText mode={Mode.LabelSemicolumn}>Label — Semibold 15/24</XText>
                     </XView>
                     <XView marginTop={24}>
-                        <XText mode="BodyRegular">Body — Regular 15/24</XText>
+                        <XText mode={Mode.BodyRegular}>Body — Regular 15/24</XText>
                     </XView>
                     <XView marginTop={24}>
-                        <XText mode="DensedRegular">Densed — Regular 14/20</XText>
+                        <XText mode={Mode.DensedRegular}>Densed — Regular 14/20</XText>
                     </XView>
                     <XView marginTop={24}>
-                        <XText mode="CaptionRegular">Caption — Regular 13/18</XText>
+                        <XText mode={Mode.CaptionRegular}>Caption — Regular 13/18</XText>
                     </XView>
                 </XVertical2>
             </XContent>
