d9f2d3a4430bedd3025e2e0d1333202c38a9c65a
Fred Morozov <morozov@fred.md>
2019-07-09 19:07:12 +0300

    wip(mobile): header, header buttons, TypeStyles

--- packages/react-native-s/SBackButton.tsx
+++ packages/react-native-s/SBackButton.tsx
@@ -23,8 +23,8 @@ let styles = StyleSheet.create({
 export class SBackButton extends React.PureComponent<{ tintColor?: string, onPress?: () => void, hideText?: boolean }> {
     render() {
         return (
-            <STouchable onPress={this.props.onPress} style={{ margin: Platform.OS === 'android' ? 13 : 0, backgroundColor: 'transparent' }} hitSlop={Platform.OS === 'android' ? { top: 13, left: 13, bottom: 13, right: 13 } : { top: 8, left: 8, bottom: 8, right: 8 }}>
-                <View style={{ backgroundColor: 'transparent', flexDirection: 'row', marginRight: (Platform.OS === 'ios' && this.props.hideText === true) ? 12 : 0 }}>
+            <STouchable onPress={this.props.onPress} style={{ backgroundColor: 'transparent' }} hitSlop={Platform.OS === 'android' ? { top: 13, left: 13, bottom: 13, right: 13 } : { top: 8, left: 8, bottom: 8, right: 8 }}>
+                <View style={{ width: 36, height: 44, backgroundColor: 'transparent', flexDirection: 'row' }}>
                     <Image source={require('assets-s/ic-back.png')} style={[styles.image, { tintColor: this.props.tintColor }]} />
                 </View>
             </STouchable>
--- packages/react-native-s/navigation/buttons/ActionButtonIOS.tsx
+++ packages/react-native-s/navigation/buttons/ActionButtonIOS.tsx
@@ -1,15 +1,26 @@
 import * as React from 'react';
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { XMemo } from 'openland-y-utils/XMemo';
-import { SNavigationViewStyle } from 'react-native-s/SNavigationView';
+import { TypeStyles } from 'openland-mobile/styles/AppStyles';
 
-export class ActionButtonIOS extends React.PureComponent<{ title: string, tintColor?: string, icon?: any, onPress?: () => void, marginLeft?: number }> {
+export class ActionButtonIOS extends React.PureComponent<{ title: string, tintColor?: string, icon?: any, onPress?: () => void }> {
     render() {
+        const { title, tintColor, icon, onPress } = this.props;
+
+        if (icon) {
+            return (
+                <TouchableOpacity onPress={onPress} hitSlop={{ bottom: 8, top: 8, left: 8, right: 8 }}>
+                    <View style={{ width: 44, height: 44, alignItems: 'center', justifyContent: 'center' }}>
+                        <Image source={icon} style={{ width: 26, height: 26, tintColor: tintColor || '#78808F' }} resizeMode="contain" />
+                    </View>
+                </TouchableOpacity>
+            );
+        }
+
         return (
-            <TouchableOpacity onPress={this.props.onPress} hitSlop={{ bottom: 8, top: 8, left: 8, right: 8 }}>
-                <View style={{ height: 44, marginLeft: this.props.marginLeft !== undefined ? this.props.marginLeft : 10, alignItems: 'center', justifyContent: 'center' }}>
-                    {!this.props.icon && <Text style={{ color: this.props.tintColor || '#4747ec', fontSize: 16, height: 22, lineHeight: 22, fontWeight: '600' }} allowFontScaling={false}>{this.props.title}</Text>}
-                    {this.props.icon && <Image source={this.props.icon} style={{ width: 26, height: 26, tintColor: this.props.tintColor || '#4747ec' }} resizeMode="contain" />}
+            <TouchableOpacity onPress={onPress} hitSlop={{ bottom: 8, top: 8, left: 8, right: 8 }}>
+                <View style={{ height: 44, alignItems: 'center', justifyContent: 'center' }}>
+                    <Text style={[TypeStyles.label1, { marginLeft: 12, marginRight: 12, color: tintColor || '#78808F' }]} allowFontScaling={false}>{title}</Text>
                 </View>
             </TouchableOpacity>
         );
@@ -18,7 +29,7 @@ export class ActionButtonIOS extends React.PureComponent<{ title: string, tintCo
 
 export const ActionButtonIOSView = XMemo<{ onPress?: () => void; children?: any }>((props) => (
     <TouchableOpacity onPress={props.onPress} hitSlop={{ bottom: 8, top: 8, left: 8, right: 8 }}>
-        <View style={{ height: 44, marginLeft: 10, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={{ height: 44, alignItems: 'center', justifyContent: 'center' }}>
             {props.children}
         </View>
     </TouchableOpacity>
