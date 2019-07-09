54596ab199e80ea7fb8c45362deccc897128f425
Fred Morozov <morozov@fred.md>
2019-07-09 18:06:48 +0300

    feat(mobile): new design for message input bar

--- packages/openland-mobile/pages/main/components/MessageInputBar.tsx
+++ packages/openland-mobile/pages/main/components/MessageInputBar.tsx
@@ -1,30 +1,10 @@
 import * as React from 'react';
-import { View, TouchableOpacity, Image, TextInput, NativeSyntheticEvent, TextInputSelectionChangeEventData, ActivityIndicator } from 'react-native';
+import { View } from 'react-native';
 import { ZKeyboardAwareBar } from '../../../components/layout/ZKeyboardAwareBar';
 import { SDevice } from 'react-native-s/SDevice';
 import { ZBlurredView } from 'openland-mobile/components/ZBlurredView';
 import { ThemeContext } from 'openland-mobile/themes/ThemeContext';
-
-const iconAttach = require('assets/ic-attachment-24.png');
-const icon = require('assets/ic-send.png');
-
-export interface MessageInputBarProps {
-    onAttachPress?: () => void;
-    onSubmitPress: () => void;
-    onChangeText: (value: string) => void;
-    onSelectionChange?: (e: NativeSyntheticEvent<TextInputSelectionChangeEventData>) => void;
-    onBlur?: () => void;
-    onFocus?: () => void;
-    enabled?: boolean;
-    attachesEnabled?: boolean;
-    text: string;
-    placeholder: string;
-    canSubmit: boolean;
-
-    suggestions?: any;
-    topView?: any;
-    showLoader?: boolean;
-}
+import { MessageInputBarProps, MessageInputInner } from './MessageInputInner';
 
 export const MessageInputBar = React.forwardRef((props: MessageInputBarProps, ref: any) => {
     let theme = React.useContext(ThemeContext);
@@ -40,62 +20,7 @@ export const MessageInputBar = React.forwardRef((props: MessageInputBarProps, re
             <View style={{ flexDirection: 'column', alignItems: 'stretch' }}>
                 {props.topView}
 
-                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                    {props.attachesEnabled !== false && (
-                        <TouchableOpacity onPress={props.onAttachPress}>
-                            <View width={48} height={50} alignItems="center" justifyContent="center">
-                                <View width={30} height={30} borderRadius={24} alignItems="center" justifyContent="center">
-                                    <Image source={iconAttach} style={{ width: 24, height: 24, tintColor: theme.foregroundSecondary }} />
-                                </View>
-                            </View>
-                        </TouchableOpacity>
-                    )}
-                    {props.attachesEnabled === false && (
-                        <View width={15} />
-                    )}
-                    <TextInput
-                        ref={ref}
-                        flexGrow={1}
-                        flexBasis={0}
-                        placeholder={props.placeholder}
-                        placeholderTextColor={theme.foregroundTertiary}
-                        onChangeText={props.onChangeText}
-                        onSelectionChange={props.onSelectionChange}
-                        onFocus={props.onFocus}
-                        onBlur={props.onBlur}
-                        value={props.text}
-                        style={{
-                            color: theme.foregroundPrimary,
-                            backgroundColor: theme.backgroundTertiary,
-                            borderRadius: 17.5,
-                            paddingLeft: 12,
-                            paddingRight: 12,
-                            paddingTop: 7,
-                            paddingBottom: 7,
-                            minHeight: 34,
-                            maxHeight: 100,
-                            fontSize: 16,
-                            marginTop: 8,
-                            marginBottom: 8
-                        }}
-                        editable={props.enabled !== false}
-                        multiline={true}
-                        allowFontScaling={false}
-                        keyboardAppearance={theme.keyboardAppearance}
-                    />
-                    {!props.showLoader && (
-                        <TouchableOpacity disabled={!props.canSubmit} onPress={props.onSubmitPress}>
-                            <View alignItems="center" justifyContent="center" width={52} height={50} paddingLeft={2}>
-                                <Image source={icon} style={{ width: 26, height: 26, tintColor: props.canSubmit && props.enabled !== false ? theme.foregroundSecondary : theme.foregroundQuaternary }} />
-                            </View>
-                        </TouchableOpacity>
-                    )}
-                    {props.showLoader && (
-                        <View width={52} height={50} alignItems="center" justifyContent="center">
-                            <ActivityIndicator height="100%" color="#0084fe" />
-                        </View>
-                    )}
-                </View>
+                <MessageInputInner {...props} theme={theme} ref={ref} />
             </View>
         </ZKeyboardAwareBar>
     );
---
+++ packages/openland-mobile/pages/main/components/MessageInputInner.tsx
@@ -0,0 +1,116 @@
+import * as React from 'react';
+import { View, TouchableOpacity, Image, TextInput, NativeSyntheticEvent, TextInputSelectionChangeEventData, ActivityIndicator, Platform } from 'react-native';
+import { ThemeGlobal } from 'openland-y-utils/themes/types';
+
+export interface MessageInputBarProps {
+    onAttachPress?: () => void;
+    onSubmitPress: () => void;
+    onChangeText: (value: string) => void;
+    onSelectionChange?: (e: NativeSyntheticEvent<TextInputSelectionChangeEventData>) => void;
+    onBlur?: () => void;
+    onFocus?: () => void;
+    enabled?: boolean;
+    attachesEnabled?: boolean;
+    text: string;
+    placeholder: string;
+    canSubmit: boolean;
+
+    suggestions?: any;
+    topView?: any;
+    showLoader?: boolean;
+}
+
+const iconAttach = require('assets/ic-input-attach-24.png');
+const icon = require('assets/ic-input-send-24.png');
+
+export const MessageInputInner = React.forwardRef((props: MessageInputBarProps & { theme: ThemeGlobal }, ref: any) => {
+    const { theme } = props;
+    const inputProps = {
+        flexGrow: 1,
+        flexBasis: 0,
+        placeholder: props.placeholder,
+        placeholderTextColor: theme.foregroundTertiary,
+        onChangeText: props.onChangeText,
+        onSelectionChange: props.onSelectionChange,
+        onFocus: props.onFocus,
+        onBlur: props.onBlur,
+        value: props.text,
+        editable: props.enabled !== false,
+        multiline: true,
+        allowFontScaling: false,
+        keyboardAppearance: theme.keyboardAppearance,
+    }
+
+    return (
+        <View style={{ flexDirection: 'row', alignItems: 'flex-end', backgroundColor: Platform.OS === 'android' ? theme.backgroundPrimary : undefined }}>
+            {props.attachesEnabled !== false && (
+                <View width={56} height={52} alignItems="center" justifyContent="center">
+                    <TouchableOpacity onPress={props.onAttachPress}>
+                        <View width={44} height={44} alignItems="center" justifyContent="center">
+                            <Image source={iconAttach} style={{ width: 24, height: 24, tintColor: theme.foregroundSecondary }} />
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            )}
+            {props.attachesEnabled === false && (
+                <View width={16} />
+            )}
+            {Platform.OS === 'ios' && (
+                <View
+                    flexGrow={1}
+                    flexBasis={0}
+                    style={{
+                        backgroundColor: theme.backgroundTertiary,
+                        borderRadius: 18,
+                        paddingHorizontal: 12,
+                        paddingVertical: 6,
+                        marginVertical: 8,
+                    }}
+                >
+                    <TextInput
+                        ref={ref}
+                        style={{
+                            paddingTop: 0,
+                            color: theme.foregroundPrimary,
+                            minHeight: 22,
+                            maxHeight: 100,
+                            fontSize: 17,
+                        }}
+                        {...inputProps}
+                    />
+                </View>
+            )}
+            {Platform.OS === 'android' && (
+                <TextInput
+                    ref={ref}
+                    style={{
+                        color: theme.foregroundPrimary,
+                        maxHeight: 100,
+                        fontSize: 17,
+                        backgroundColor: theme.backgroundTertiary,
+                        borderRadius: 18,
+                        paddingHorizontal: 12,
+                        paddingTop: 3,
+                        paddingBottom: 5,
+                        marginVertical: 8,
+                    }}
+                    {...inputProps}
+                />
+            )}
+            {!props.showLoader && (
+                <View width={52} height={52} alignItems="center" justifyContent="center">
+                    <TouchableOpacity disabled={!props.canSubmit} onPress={props.onSubmitPress}>
+                        <View width={44} height={44} alignItems="center" justifyContent="center">
+                            <Image source={icon} style={{ width: 26, height: 26, tintColor: props.canSubmit && props.enabled !== false ? theme.accentPrimary : theme.foregroundSecondary }} />
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            )}
+            {props.showLoader && (
+                <View width={52} height={52} alignItems="center" justifyContent="center">
+                    <ActivityIndicator height="100%" color={theme.foregroundSecondary} />
+                </View>
+            )}
+        </View>
+    );
+});
