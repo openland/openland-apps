85c5528fa89053f6420d4eefac9b4d4dde41ac27
Fred Morozov <morozov@fred.md>
2019-07-11 01:41:49 +0300

    ref(mobile): new DevTypography screen

--- packages/openland-mobile/pages/dev/Typography.tsx
+++ packages/openland-mobile/pages/dev/Typography.tsx
@@ -1,110 +1,28 @@
 import * as React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { SAnimated } from 'react-native-s/SAnimated';
+import { View, Text } from 'react-native';
 import { SScrollView } from 'react-native-s/SScrollView';
+import { withApp } from 'openland-mobile/components/withApp';
+import { PageProps } from 'openland-mobile/components/PageProps';
+import { TypeStyles } from 'openland-mobile/styles/AppStyles';
 
-const styles = StyleSheet.create({
-    container: {
-        width: 100,
-        height: 100,
-        backgroundColor: 'red',
-    },
-    child: {
-        position: 'absolute',
-        width: 100,
-        height: 100,
-        top: 50,
-        left: 50,
-        backgroundColor: 'green',
-    },
-});
+export const TypographyComponent = (props: PageProps) => {
+    const variants: JSX.Element[] = [];
 
-export class Typography extends React.PureComponent {
+    Object.keys(TypeStyles).forEach((style) => {
+        const variant = TypeStyles[style];
 
-    hidden = false;
-
-    render() {
-        return (
-            <>
-                {/* <FastHeader title="Testing" />
-                <FastHeaderButton
-                    title="Animate!"
-                    onPress={() => {
-                        if (this.hidden) {
-                            SAnimated.timing('typography_container', {
-                                property: 'opacity',
-                                from: 0,
-                                to: 1
-                            });
-                        } else {
-                            SAnimated.timing('typography_container', {
-                                property: 'opacity',
-                                from: 1,
-                                to: 0
-                            });
-                        }
-                        this.hidden = !this.hidden;
-                    }}
-                /> */}
-                <SScrollView>
-                    <SAnimated.View name="typography_container">
-                        <View width="100%" height="100%" backgroundColor="#fff">
-                            {/* <ScrollView width="100%" height="100%" backgroundColor="#fff"> */}
-                            <View width="100%" paddingTop={8} paddingLeft={8} flexDirection="column">
-                                <View flexDirection="row">
-                                    <View>
-                                        <Text style={[{ fontWeight: '300', width: 90, height: 20, lineHeight: 20, fontSize: 15, backgroundColor: '#fff' }]} numberOfLines={1}>Thin 15</Text>
-                                        <Text style={{ fontWeight: '100', width: 90, height: 20, lineHeight: 20, fontSize: 14, backgroundColor: '#eee' }} numberOfLines={1}>Thin 14</Text>
-                                        <Text style={{ fontWeight: '100', width: 90, height: 20, lineHeight: 20, fontSize: 13, backgroundColor: '#fff' }} numberOfLines={1}>Thin 13</Text>
-                                        <Text style={{ fontWeight: '100', width: 90, height: 20, lineHeight: 20, fontSize: 12, backgroundColor: '#eee' }} numberOfLines={1}>Thin 12</Text>
-                                        <Text style={{ fontWeight: '100', width: 90, height: 20, lineHeight: 20, fontSize: 11, backgroundColor: '#fff' }} numberOfLines={1}>Thin 11</Text>
-                                    </View>
-                                    <View>
-                                        <Text style={{ fontWeight: '300', width: 90, height: 20, lineHeight: 20, fontSize: 15, backgroundColor: '#eee' }} numberOfLines={1}>Light 15</Text>
-                                        <Text style={{ fontWeight: '300', width: 90, height: 20, lineHeight: 20, fontSize: 14, backgroundColor: '#fff' }} numberOfLines={1}>Light 14</Text>
-                                        <Text style={{ fontWeight: '300', width: 90, height: 20, lineHeight: 20, fontSize: 13, backgroundColor: '#eee' }} numberOfLines={1}>Light 13</Text>
-                                        <Text style={{ fontWeight: '300', width: 90, height: 20, lineHeight: 20, fontSize: 12, backgroundColor: '#fff' }} numberOfLines={1}>Light 12</Text>
-                                        <Text style={{ fontWeight: '300', width: 90, height: 20, lineHeight: 20, fontSize: 11, backgroundColor: '#eee' }} numberOfLines={1}>Light 11</Text>
-                                    </View>
-                                    <View>
-                                        <Text style={{ fontWeight: '400', width: 90, height: 20, lineHeight: 20, fontSize: 15, backgroundColor: '#fff' }} numberOfLines={1}>Regular 15</Text>
-                                        <Text style={{ fontWeight: '400', width: 90, height: 20, lineHeight: 20, fontSize: 14, backgroundColor: '#eee' }} numberOfLines={1}>Regular 14</Text>
-                                        <Text style={{ fontWeight: '400', width: 90, height: 20, lineHeight: 20, fontSize: 13, backgroundColor: '#fff' }} numberOfLines={1}>Regular 13</Text>
-                                        <Text style={{ fontWeight: '400', width: 90, height: 20, lineHeight: 20, fontSize: 12, backgroundColor: '#eee' }} numberOfLines={1}>Regular 12</Text>
-                                        <Text style={{ fontWeight: '400', width: 90, height: 20, lineHeight: 20, fontSize: 11, backgroundColor: '#fff' }} numberOfLines={1}>Regular 11</Text>
-                                    </View>
-                                </View>
-                                <View flexDirection="row">
-                                    <View>
-                                        <Text style={{ fontWeight: '500', width: 90, height: 20, lineHeight: 20, fontSize: 15, backgroundColor: '#eee' }} numberOfLines={1}>Medium 15</Text>
-                                        <Text style={{ fontWeight: '500', width: 90, height: 20, lineHeight: 20, fontSize: 14, backgroundColor: '#fff' }} numberOfLines={1}>Medium 14</Text>
-                                        <Text style={{ fontWeight: '500', width: 90, height: 20, lineHeight: 20, fontSize: 13, backgroundColor: '#eee' }} numberOfLines={1}>Medium 13</Text>
-                                        <Text style={{ fontWeight: '500', width: 90, height: 20, lineHeight: 20, fontSize: 12, backgroundColor: '#fff' }} numberOfLines={1}>Medium 12</Text>
-                                        <Text style={{ fontWeight: '500', width: 90, height: 20, lineHeight: 20, fontSize: 11, backgroundColor: '#eee' }} numberOfLines={1}>Medium 11</Text>
-                                    </View>
-                                    <View>
-                                        <Text style={{ fontWeight: '700', width: 90, height: 20, lineHeight: 20, fontSize: 15, backgroundColor: '#fff' }} numberOfLines={1}>Bold 15</Text>
-                                        <Text style={{ fontWeight: '700', width: 90, height: 20, lineHeight: 20, fontSize: 14, backgroundColor: '#eee' }} numberOfLines={1}>Bold 14</Text>
-                                        <Text style={{ fontWeight: '700', width: 90, height: 20, lineHeight: 20, fontSize: 13, backgroundColor: '#fff' }} numberOfLines={1}>Bold 13</Text>
-                                        <Text style={{ fontWeight: '700', width: 90, height: 20, lineHeight: 20, fontSize: 12, backgroundColor: '#eee' }} numberOfLines={1}>Bold 12</Text>
-                                        <Text style={{ fontWeight: '700', width: 90, height: 20, lineHeight: 20, fontSize: 11, backgroundColor: '#fff' }} numberOfLines={1}>Bold 11</Text>
-                                    </View>
-                                    <View>
-                                        <Text style={{ fontWeight: '900', width: 90, height: 20, lineHeight: 20, fontSize: 15, backgroundColor: '#eee' }} numberOfLines={1}>Black 15</Text>
-                                        <Text style={{ fontWeight: '900', width: 90, height: 20, lineHeight: 20, fontSize: 14, backgroundColor: '#fff' }} numberOfLines={1}>Black 14</Text>
-                                        <Text style={{ fontWeight: '900', width: 90, height: 20, lineHeight: 20, fontSize: 13, backgroundColor: '#eee' }} numberOfLines={1}>Black 13</Text>
-                                        <Text style={{ fontWeight: '900', width: 90, height: 20, lineHeight: 20, fontSize: 12, backgroundColor: '#fff' }} numberOfLines={1}>Black 12</Text>
-                                        <Text style={{ fontWeight: '900', width: 90, height: 20, lineHeight: 20, fontSize: 11, backgroundColor: '#eee' }} numberOfLines={1}>Black 11</Text>
-                                    </View>
-                                </View>
-                                <View style={styles.container}>
-                                    <View style={styles.child} />
-                                </View>
-                            </View>
-                        </View>
-                    </SAnimated.View>
-                </SScrollView>
-            </>
+        variants.push(
+            <View marginVertical={10} marginHorizontal={16}>
+                <Text style={variant}>{style}: {variant.fontSize}/{variant.lineHeight} ∙ {variant.fontWeight}</Text>
+            </View>
         );
-    }
-}
+    });
+
+    return (
+        <SScrollView>
+            {variants}
+        </SScrollView>
+    );
+};
+
+export const Typography = withApp(TypographyComponent, { navigationAppearance: 'small-hidden' });
