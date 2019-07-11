3e8a5fe00e143825ecd7e57e143f47107ab58570
Sergey Lapin <lapanoid@gmail.com>
2019-07-11 09:26:53 +0300

    wip(web): bootstrap XRadio2

--- packages/openland-web/pages/account/components/RadioButtonsSelect.tsx
+++ packages/openland-web/pages/account/components/RadioButtonsSelect.tsx
@@ -1,49 +1,10 @@
 import * as React from 'react';
-import { XView } from 'react-mental';
-import { XSelect } from 'openland-x/XSelect';
-import ArrowIcon from 'openland-icons/ic-arrow-group-select.svg';
-import { css } from 'linaria';
-
-const DropdownItem = ({ title, label }: { title: string; label: string }) => {
-    return (
-        <XView flexDirection="column" marginTop={-3}>
-            <XView color="#1488f3" fontSize={12}>
-                {title}
-            </XView>
-            <XView fontSize={14} color="#000" marginTop={-4}>
-                {label}
-            </XView>
-        </XView>
-    );
-};
+import { XRadioItem } from 'openland-x/XRadio2';
 
 export interface RadioButtonsSelectOptions<T> {
     value: T;
     label: string;
 }
-
-const SelectGroupTypeClassName = css`
-    position: relative;
-    cursor: pointer;
-    & .Select-control > *,
-    & .Select-control > *:focus,
-    & .Select-control > *:active {
-        box-shadow: none !important;
-        border: none !important;
-        cursor: pointer !important;
-    }
-    & > .x {
-        position: absolute;
-        width: 100%;
-        top: 0px;
-        pointer-events: none;
-        cursor: pointer !important;
-    }
-    & .Select-control {
-        height: 52px !important;
-        opacity: 0;
-    }
-`;
 
 export function RadioButtonsSelect<T>({
     value,
@@ -66,32 +27,25 @@ export function RadioButtonsSelect<T>({
         }
     }, [innerValue]);
 
-    const currentItemOptions = selectOptions.find(
-        (item: RadioButtonsSelectOptions<T>) => item.value === innerValue.value,
-    )!!;
-
     return (
-        <div className={SelectGroupTypeClassName}>
-            <XSelect
-                searchable={false}
-                clearable={false}
-                withSubtitle={true}
-                value={innerValue.value}
-                onChange={setInnerValue as any}
-                options={selectOptions as any}
-            />
-            <XView
-                height={52}
-                paddingHorizontal={16}
-                backgroundColor="#f9f9f9"
-                borderRadius={8}
-                flexDirection="row"
-                justifyContent="space-between"
-                alignItems="center"
-            >
-                <DropdownItem title="" label={currentItemOptions.label} />
-                <ArrowIcon />
-            </XView>
-        </div>
+        <>
+            {selectOptions.map((item: RadioButtonsSelectOptions<T>, key: number) => {
+                return (
+                    <XRadioItem
+                        key={key}
+                        label={item.label}
+                        checked={item.value === innerValue.value}
+                        onChange={(checked: string) => {
+                            setInnerValue(
+                                selectOptions.find(
+                                    (foundItem: RadioButtonsSelectOptions<T>) =>
+                                        item.value === foundItem.value,
+                                )!!,
+                            );
+                        }}
+                    />
+                );
+            })}
+        </>
     );
 }
