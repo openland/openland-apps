c88db5e89275f2640f538746feede603b0f018c8
Sergey Lapin <lapanoid@gmail.com>
2019-07-11 07:57:09 +0300

    wip(web): wire profile form

--- packages/openland-web/pages/account/ProfileTab.tsx
+++ packages/openland-web/pages/account/ProfileTab.tsx
@@ -3,64 +3,92 @@ import { useForm } from 'openland-form/useForm';
 import { useClient } from 'openland-web/utils/useClient';
 import { useField } from 'openland-form/useField';
 import { StoredFileT, XAvatarFormFieldComponent } from 'openland-x/XAvatarUpload';
-import { XInput } from 'openland-x/XInput';
 import { XView } from 'react-mental';
-import {
-    SelectWithDropdown,
-    SelectWithDropdownOption,
-} from 'openland-web/pages/main/mail/SelectWithDropdown';
+import { SelectWithDropdown } from 'openland-web/pages/main/mail/SelectWithDropdown';
+import { sanitizeImageRef } from '../../utils/sanitizer';
 import { XButton } from 'openland-x/XButton';
 import { XTextArea } from 'openland-x/XTextArea';
+import { InputField } from 'openland-web/components/InputField';
 
 export const ProfileTab = () => {
     const form = useForm();
     const client = useClient();
 
-    const photo = '';
+    const profile = client.useProfile().profile!;
+    const user = client.useProfile().user!;
+    const organizations = client.useMyOrganizations();
 
-    let firstNameField = useField('input.firstName', '', form);
-    let secondNameField = useField('input.secondName', '', form);
+    let firstNameField = useField('input.firstName', profile.firstName || '', form);
+    let secondNameField = useField('input.secondName', profile.lastName || '', form);
     const avatarField = useField<StoredFileT | undefined | null>(
         'input.avatar',
-        { uuid: photo } as any,
+        sanitizeImageRef(profile.photoRef),
         form,
     );
-    let primaryOrganizationField = useField('input.primaryOrganization', '', form);
-    let aboutField = useField('input.about', '', form);
-    let locationField = useField('input.location', '', form);
-    let usernameField = useField('input.username', '', form);
-    let phoneNumberField = useField('input.phoneNumber', '', form);
-    let emailField = useField('input.email', '', form);
-    let websiteField = useField('input.website', '', form);
-    let linkedinField = useField('input.linkedin', '', form);
+    let primaryOrganizationField = useField(
+        'input.primaryOrganization',
+        profile.primaryOrganization && profile.primaryOrganization!!.id,
+        form,
+    );
+
+    let aboutField = useField('input.about', profile.about || '', form);
+    let locationField = useField('input.location', profile.location || '', form);
+    let usernameField = useField('input.username', user.shortname || '', form);
+    let phoneNumberField = useField('input.phoneNumber', profile.phone || '', form);
+    let emailField = useField('input.email', profile.email || '', form);
+    let websiteField = useField('input.website', profile.website || '', form);
+    let linkedinField = useField('input.linkedin', profile.linkedin || '', form);
 
     const doConfirm = React.useCallback(() => {
         form.doAction(async () => {
-            //
+            await client.mutateProfileUpdate({
+                input: {
+                    firstName: firstNameField.value,
+                    lastName: secondNameField.value,
+                    alphaPrimaryOrganizationId: primaryOrganizationField.value,
+                    about: aboutField.value,
+                    photoRef: sanitizeImageRef(avatarField.value),
+                    email: emailField.value,
+                    website: websiteField.value,
+                    alphaLinkedin: linkedinField.value,
+                    phone: phoneNumberField.value,
+                    location: locationField.value,
+                },
+            });
+
+            if (user.shortname !== usernameField.value) {
+                await client.mutateSetUserShortname({
+                    shortname: usernameField.value,
+                });
+            }
+
+            await client.refetchAccount();
+            await client.refetchMyOrganizations();
         });
     }, []);
-
-    // const primaryOrganizationSelectOption = [];
 
     return (
         <XView flexDirection="row">
             <XView>
-                <XInput title={'First name'} {...firstNameField.input} size="large" />
-                <XInput title={'Second name'} {...secondNameField.input} size="large" />
+                <InputField title={'First name'} field={firstNameField} size="large" />
+                <InputField title={'Second name'} field={secondNameField} size="large" />
 
-                {/* <SelectWithDropdown
+                <SelectWithDropdown
                     {...primaryOrganizationField.input}
                     title={'Primary organization'}
-                    selectOptions={primaryOrganizationSelectOption}
-                /> */}
+                    selectOptions={organizations.myOrganizations.map((org: any) => ({
+                        value: org.id,
+                        label: org.name,
+                    }))}
+                />
 
-                <XTextArea title="About" {...aboutField.input} resize={false} />
-                <XInput title={'Location'} {...locationField.input} size="large" />
-                <XInput title={'Username'} {...usernameField.input} size="large" />
-                <XInput title={'Phone number'} {...phoneNumberField.input} size="large" />
-                <XInput title={'Email'} {...emailField.input} size="large" />
-                <XInput title={'Website'} {...websiteField.input} size="large" />
-                <XInput {...linkedinField.input} size="large" />
+                <XTextArea mode="modern" title="About" {...aboutField.input} resize={false} />
+                <InputField title={'Location'} field={locationField} size="large" />
+                <InputField title={'Username'} field={usernameField} size="large" />
+                <InputField title={'Phone number'} field={phoneNumberField} size="large" />
+                <InputField title={'Email'} field={emailField} size="large" />
+                <InputField title={'Website'} field={websiteField} size="large" />
+                <InputField title={''} field={linkedinField} size="large" />
                 <XButton text="Save changes" style="primary" size="large" onClick={doConfirm} />
             </XView>
             <XAvatarFormFieldComponent
