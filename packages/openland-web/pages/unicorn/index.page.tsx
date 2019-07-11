8e07308243be7697f09ce4c2a6c3ec26444d2ec4
Steve Korshakov <steve@korshakov.com>
2019-07-11 17:41:34 +0300

    wip(web): Unicorn layout

--- packages/openland-web/pages/unicorn/index.page.tsx
+++ packages/openland-web/pages/unicorn/index.page.tsx
@@ -8,6 +8,7 @@ import { MessengerFragment } from 'openland-web/fragments/MessengerFragment';
 import { AppBarDesktop } from './navigation/AppBarDesktop';
 import { AppBarMobile } from './navigation/AppBarMobile';
 import { ThemeLightBlue } from 'openland-y-utils/themes';
+import { NotificationsButton, NewOptionsButton } from 'openland-web/components/NewOptionsButton';
 
 const Chat = (props: { id: string }) => {
     return (
@@ -23,15 +24,24 @@ const Root = () => {
         <XView width="100%" height="100%" flexDirection="column" alignItems="stretch">
             <XView
                 height={56}
-                paddingLeft={16}
+                paddingHorizontal={16}
                 paddingVertical={12}
                 backgroundColor="#fff"
                 fontSize={24}
                 lineHeight="32px"
                 fontWeight="600"
                 color={ThemeLightBlue.foregroundPrimary}
+                flexDirection="row"
             >
-                Chats
+                <XView flexGrow={1} minWidth={0} flexBasis={0}>
+                    Chats
+                </XView>
+                <XView flexDirection="row" alignItems="center" paddingLeft={12}>
+                    <XView marginRight={7} justifyContent="center" alignItems="center">
+                        <NotificationsButton />
+                    </XView>
+                    <NewOptionsButton />
+                </XView>
             </XView>
             <XView width="100%" minHeight={0} flexGrow={1} flexBasis={0}>
                 <DialogListFragment
