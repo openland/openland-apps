043b01cc0d833ff60fa9abb62dbe344cd5f4c018
Fred Morozov <morozov@fred.md>
2019-07-13 20:03:45 +0300

    chore(mobile): improve DevAvatars screen

--- packages/openland-mobile/pages/dev/Avatars.tsx
+++ packages/openland-mobile/pages/dev/Avatars.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { SScrollView } from 'react-native-s/SScrollView';
 import { withApp } from 'openland-mobile/components/withApp';
 import { PageProps } from 'openland-mobile/components/PageProps';
 import { ZAvatar, avatarSizes, ZAvatarSize } from 'openland-mobile/components/ZAvatar';
+import { ThemeContext } from 'openland-mobile/themes/ThemeContext';
 
 const mock1 = {
     src: 'https://ucarecdn.com/ce2508ab-8048-4e28-a1aa-8c2d64fdf3c7/-/crop/708x708/89,0/-/format/jpeg/-/scale_crop/80x80/center/-/quality/lighter/-/progressive/yes/',
@@ -17,22 +18,26 @@ const mock2 = {
 };
 
 export const AvatarsComponent = (props: PageProps) => {
+    const theme = React.useContext(ThemeContext);
     const variants: JSX.Element[] = [];
 
     Object.keys(avatarSizes).forEach((size: ZAvatarSize) => {
         variants.push(
-            <View flexDirection="row" flexWrap="wrap" marginHorizontal={16}>
-                <View marginVertical={10} marginRight={10}>
-                    <ZAvatar size={size} {...mock1} />
-                </View>
-                <View marginVertical={10} marginRight={10}>
-                    <ZAvatar size={size} {...mock1} online={true} />
-                </View>
-                <View marginVertical={10} marginRight={10}>
-                    <ZAvatar size={size} {...mock2} />
-                </View>
-                <View marginVertical={10}>
-                    <ZAvatar size={size} {...mock2} online={true} />
+            <View marginHorizontal={16}>
+                <Text style={{ color: theme.foregroundPrimary }}>{size}</Text>
+                <View flexDirection="row" flexWrap="wrap">
+                    <View marginVertical={10} marginRight={10}>
+                        <ZAvatar size={size} {...mock1} />
+                    </View>
+                    <View marginVertical={10} marginRight={10}>
+                        <ZAvatar size={size} {...mock1} online={true} />
+                    </View>
+                    <View marginVertical={10} marginRight={10}>
+                        <ZAvatar size={size} {...mock2} />
+                    </View>
+                    <View marginVertical={10}>
+                        <ZAvatar size={size} {...mock2} online={true} />
+                    </View>
                 </View>
             </View>
         );
