d36b9d0d72ebca7c410e695707e365ea6c54a948
Steve Korshakov <steve@korshakov.com>
2019-07-14 21:46:29 +0300

    feat(web): Fix selection of UListItem

--- packages/openland-web/components/unicorn/UListItem.tsx
+++ packages/openland-web/components/unicorn/UListItem.tsx
@@ -1,6 +1,21 @@
 import * as React from 'react';
-import { XView } from 'react-mental';
+import { XView, XViewSelectedContext } from 'react-mental';
 import { ThemeLightBlue } from 'openland-y-utils/themes';
+import { css } from 'linaria';
+
+const selectedStyle = css`
+ * path {
+    stroke: white; 
+ }
+`;
+
+const SelectableSVG = React.memo((props: { children?: any }) => {
+    let selected = React.useContext(XViewSelectedContext);
+    if (selected) {
+        return (<div className={selectedStyle}>{props.children}</div>);
+    }
+    return (<div>{props.children}</div>);
+});
 
 export const UListItem = React.memo((props: {
     text: string,
@@ -15,12 +30,15 @@ export const UListItem = React.memo((props: {
             alignItems="center"
             flexDirection="row"
             hoverBackgroundColor={ThemeLightBlue.backgroundPrimaryHover}
-            selectedBackgroundColor={ThemeLightBlue.backgroundPrimaryHover}
+            selectedBackgroundColor={ThemeLightBlue.accentPrimary}
+            selectedHoverBackgroundColor={ThemeLightBlue.accentPrimaryHover}
+            selectedColor="#fff"
             cursor="pointer"
             onClick={props.onClick}
             path={props.path}
+            linkSelectable={true}
         >
-            {props.icon && <XView marginRight={19}>{props.icon}</XView>}
+            {props.icon && <XView marginRight={19} width={24} height={24}><SelectableSVG>{props.icon}</SelectableSVG></XView>}
             <XView lineHeight="24px" fontSize={15}>
                 {props.text}
             </XView>
