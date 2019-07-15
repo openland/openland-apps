23b31c0f956f2a7a086fc7e8126cc4c2f64c9eb8
Max Volkov <wolkow_1996@mail.ru>
2019-07-15 14:46:16 +0300

    fix(web): copyrighting

--- packages/openland-landing/components/Footer.tsx
+++ packages/openland-landing/components/Footer.tsx
@@ -67,7 +67,7 @@ export const Footer = () => (
             </div>
         </Container>
         <div className={FooterStyles.info}>
-            &copy; 2018 Data Makes Perfect Inc.<span>Built in San Francisco</span>
+            &copy; 2019 Data Makes Perfect Inc.<span>Built in San Francisco</span>
         </div>
     </footer>
 );
