5a042f3be118738af60dc0f8f181549134f5f333
bogoslavskiy <luice96@yahoo.com>
2019-07-15 18:31:44 +0300

    ref(mobile): add example dev page - loader, toast

--- packages/openland-mobile/pages/dev/Loader.tsx
+++ packages/openland-mobile/pages/dev/Loader.tsx
@@ -1,13 +1,29 @@
 import * as React from 'react';
-import { View } from 'react-native';
-import { ZLoader } from '../../components/ZLoader';
+import { View, Text } from 'react-native';
+import { SScrollView } from 'react-native-s/SScrollView';
+import { SHeader } from 'react-native-s/SHeader';
+import { withApp } from 'openland-mobile/components/withApp';
+import { PageProps } from 'openland-mobile/components/PageProps';
+import LoaderSpinner from 'openland-mobile/components/LoaderSpinner';
 
-export class Loader extends React.PureComponent {
-    render() {
-        return (
-            <View width="100%" height="100%">
-                <ZLoader />
+export const LoadersComponent = (props: PageProps) => (
+    <SScrollView>
+        <SHeader title="Loader spinner" />
+        <View alignItems={'center'} paddingTop={20}>
+            <Text>Size: large</Text>
+            <View marginVertical={20}>
+                <LoaderSpinner size={'large'} />
             </View>
-        );
-    }
-}
+            <Text>Size: medium</Text>
+            <View marginVertical={20}>
+                <LoaderSpinner size={'medium'} />
+            </View>
+            <Text>Size: small</Text>
+            <View marginVertical={20}>
+                <LoaderSpinner size={'small'} />
+            </View>
+        </View>
+    </SScrollView>
+);
+
+export const Loader = withApp(LoadersComponent, { navigationAppearance: 'small' });
