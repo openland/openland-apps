bdd9ac6144dc20bc4d529faf703934a7f4e50120
Fred Morozov <morozov@fred.md>
2019-07-16 19:44:57 +0300

    wip(web): UListItem

--- packages/openland-web/components/unicorn/UListItem.tsx
+++ packages/openland-web/components/unicorn/UListItem.tsx
@@ -2,10 +2,16 @@ import * as React from 'react';
 import { XView, XViewSelectedContext } from 'react-mental';
 import { ThemeDefault } from 'openland-y-utils/themes';
 import { css } from 'linaria';
+import { TypeStyles } from 'openland-web/utils/TypeStyles';
 
 const selectedStyle = css`
-    * path {
-        stroke: white; 
+    svg {
+        path {
+            stroke: white; // Need to be ThemeDefault.contrastSpecial
+        }
+        circle {
+            fill: white; // Need to be ThemeDefault.contrastSpecial
+        }
     }
 `;
 
@@ -20,30 +26,34 @@ const SelectableSVG = React.memo((props: { children?: any }) => {
 interface UListItemProps {
     text: string;
     icon?: any;
+    avatar?: { photo?: string | null, key: string, title: string };
     onClick?: () => void;
     path?: string;
+    large?: boolean;
 }
 
 export const UListItem = React.memo((props: UListItemProps) => {
-    const { text, icon, onClick, path } = props;
+    const { text, icon, avatar, onClick, path, large } = props;
+    const height = large ? 80 : (!!avatar ? 56 : 48);
 
     return (
         <XView
-            height={48}
+            height={height}
             paddingHorizontal={16}
             alignItems="center"
             flexDirection="row"
             hoverBackgroundColor={ThemeDefault.backgroundPrimaryHover}
             selectedBackgroundColor={ThemeDefault.accentPrimary}
             selectedHoverBackgroundColor={ThemeDefault.accentPrimaryHover}
-            selectedColor="#fff"
+            selectedColor={ThemeDefault.contrastSpecial}
             cursor="pointer"
             onClick={onClick}
             path={path}
             linkSelectable={true}
         >
-            {icon && <XView marginRight={19} width={24} height={24}><SelectableSVG>{props.icon}</SelectableSVG></XView>}
-            <XView lineHeight="24px" fontSize={15}>
+            {!!icon && <XView marginRight={16} width={24} height={24}><SelectableSVG>{icon}</SelectableSVG></XView>}
+
+            <XView {...TypeStyles.body}>
                 {text}
             </XView>
         </XView>
