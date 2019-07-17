01eea9e6d3af971626d1ff0e9dea200d094a8559
Fred Morozov <morozov@fred.md>
2019-07-17 20:02:35 +0300

    wip(web): improve UPresence

--- packages/openland-web/components/unicorn/UPresence.tsx
+++ packages/openland-web/components/unicorn/UPresence.tsx
@@ -1,25 +1,27 @@
 import * as React from 'react';
 import { formatLastSeen } from 'openland-y-utils/formatTime';
 import { UserShort } from 'openland-api/Types';
-import { XView } from 'react-mental';
+import { XView, XViewSelectedContext } from 'react-mental';
 import { ThemeDefault } from 'openland-y-utils/themes';
 
 export const UPresence = ((props: { user: UserShort }) => {
     const { isBot, lastSeen, online } = props.user;
-
-    if (isBot) {
-        return (<XView color={ThemeDefault.accentPrimary}>bot</XView>);
-    }
+    const selected = React.useContext(XViewSelectedContext);
 
     let sub = undefined;
     let isOnline = false;
-    if (!online && lastSeen) {
+
+    if (isBot) {
+        sub = 'bot';
+        isOnline = true;
+    } else if (!online && lastSeen) {
         sub = formatLastSeen(lastSeen);
     } else if (online) {
         sub = 'online';
         isOnline = true;
     }
+
     return (
-        <XView color={isOnline ? ThemeDefault.accentPrimary : undefined}>{sub}</XView>
+        <XView color={isOnline && !selected ? ThemeDefault.accentPrimary : undefined}>{sub}</XView>
     );
 });
