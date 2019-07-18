29d49fecb45077250609cbb4660ae15fd411440f
Fred Morozov <morozov@fred.md>
2019-07-18 16:06:47 +0300

    wip(web): featured members in group profile

--- packages/openland-web/components/unicorn/templates/UUserView.tsx
+++ packages/openland-web/components/unicorn/templates/UUserView.tsx
@@ -1,17 +1,23 @@
 import * as React from 'react';
 import { UListItem } from 'openland-web/components/unicorn/UListItem';
-import { UserShort } from 'openland-api/Types';
+import { UserShort, UserBadge } from 'openland-api/Types';
 import { UPresence } from '../UPresence';
 
-export const UUserView = React.memo((props: { user: UserShort }) => {
-    const { id, photo, name, online, shortname, primaryOrganization } = props.user;
+interface UUserViewProps {
+    user: UserShort;
+    badge?: UserBadge | null;
+}
+
+export const UUserView = React.memo((props: UUserViewProps) => {
+    const { user, badge } = props;
+    const { id, photo, name, online, shortname, primaryOrganization } = user;
     const path = shortname ? '/' + shortname : '/' + id;
 
     return (
         <UListItem
             title={name}
             subtitle={primaryOrganization ? primaryOrganization.name : undefined}
-            description={<UPresence user={props.user} />}
+            description={badge ? badge.name : <UPresence user={props.user} />}
             avatar={{ photo, id, title: name, online }}
             useRadius={true}
             path={path}
--- packages/openland-web/fragments/group/GroupProfileFragment.tsx
+++ packages/openland-web/fragments/group/GroupProfileFragment.tsx
@@ -21,7 +21,7 @@ export const GroupProfileFragment = React.memo((props) => {
     if (!group || group.__typename === 'PrivateRoom') {
         return null;
     }
-
+    const featuredMembers = client.useRoomFeaturedMembers({ roomId: unicorn.id }, { fetchPolicy: 'cache-and-network' }).roomFeaturedMembers;
     const initialMembers = client.useRoomMembersPaginated({ roomId: unicorn.id, first: 15 }, { fetchPolicy: 'cache-and-network' }).members;
     const { id, membersCount, photo, title, description, organization, settings } = group;
 
@@ -78,6 +78,16 @@ export const GroupProfileFragment = React.memo((props) => {
                     <UOrganizationView organization={organization} />
                 </UListGroup>
             )}
+
+            <UListGroup header="Featured" counter={featuredMembers.length}>
+                {featuredMembers.map(member => (
+                    <UUserView
+                        key={'featured-member-' + member.user.id}
+                        user={member.user}
+                        badge={member.badge}
+                    />
+                ))}
+            </UListGroup>
 
             <UListHeader text="Members" counter={membersCount || 0} />
         </UFlatList>
