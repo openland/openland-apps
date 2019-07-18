d5837546a8da42a7cb4195acf614228b832a1e5a
Fred Morozov <morozov@fred.md>
2019-07-18 14:37:58 +0300

    wip(web): members in group profile

--- packages/openland-web/fragments/group/GroupProfileFragment.tsx
+++ packages/openland-web/fragments/group/GroupProfileFragment.tsx
@@ -9,6 +9,9 @@ import { UOrganizationView } from 'openland-web/components/unicorn/templates/UOr
 import { UNotificationsSwitch } from 'openland-web/components/unicorn/templates/UNotificationsSwitch';
 import { useUnicorn } from 'openland-unicorn/useUnicorn';
 import { UButton } from 'openland-web/components/unicorn/UButton';
+import { UFlatList } from 'openland-web/components/unicorn/UFlatList';
+import { UUserView } from 'openland-web/components/unicorn/templates/UUserView';
+import { UListHeader } from 'openland-web/components/unicorn/UListHeader';
 
 export const GroupProfileFragment = React.memo((props) => {
     const client = useClient();
@@ -19,10 +22,40 @@ export const GroupProfileFragment = React.memo((props) => {
         return null;
     }
 
+    const initialMembers = client.useRoomMembersPaginated({ roomId: unicorn.id, first: 15 }, { fetchPolicy: 'cache-and-network' }).members;
     const { id, membersCount, photo, title, description, organization, settings } = group;
 
+    const [ members, setMembers ] = React.useState(initialMembers);
+    const [ loading, setLoading ] = React.useState(false);
+
+    const handleLoadMore = React.useCallback(async () => {
+        if (membersCount && (members.length < membersCount && !loading)) {
+            setLoading(true);
+
+            const loaded = (await client.queryRoomMembersPaginated({
+                roomId: id,
+                first: 10,
+                after: members[members.length - 1].user.id,
+            }, { fetchPolicy: 'network-only' })).members;
+
+            setMembers([...members, ...loaded.filter(m => !members.find(m2 => m2.user.id === m.user.id))]);
+            setLoading(false);
+        }
+    }, [ membersCount, members, loading ]);
+
     return (
-        <Page padded={false}>
+        <UFlatList
+            loadMore={handleLoadMore}
+            items={members}
+            loading={loading}
+            renderItem={member => (
+                <UUserView
+                    key={'member-' + member.user.id}
+                    user={member.user}
+                />
+            )}
+            padded={false}
+        >
             <UListHero
                 title={title}
                 description={plural(membersCount || 0, ['member', 'members'])}
@@ -35,14 +68,18 @@ export const GroupProfileFragment = React.memo((props) => {
                     marginLeft={16}
                 />
             </UListHero>
+
             <UListGroup header="About">
                 {!!description && <UListField value={description} marginBottom={16} />}
             </UListGroup>
+
             {organization && (
                 <UListGroup header={organization.isCommunity ? 'Community' : 'Organization'}>
                     <UOrganizationView organization={organization} />
                 </UListGroup>
             )}
-        </Page>
+
+            <UListHeader text="Members" counter={membersCount || 0} />
+        </UFlatList>
     );
 });
