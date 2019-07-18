7609ab16a58472c9be2f010808c9270e506b0000
Fred Morozov <morozov@fred.md>
2019-07-18 18:00:49 +0300

    wip(web): modals in profiles

--- packages/openland-web/fragments/shortname/components/OrganizationManageButtons.tsx
+++ packages/openland-web/fragments/shortname/components/OrganizationManageButtons.tsx
@@ -4,14 +4,14 @@ import { UMoreButton } from 'openland-web/components/unicorn/templates/UMoreButt
 import { UListItem } from 'openland-web/components/unicorn/UListItem';
 import { showLeaveConfirmation } from 'openland-web/fragments/org/showLeaveConfirmation';
 import { showEditCommunityModal } from 'openland-web/fragments/account/components/EditCommunityModal';
+import { showDeleteOrganizationModal } from 'openland-web/fragments/account/components/modals';
 
 export const OrganizationManageButtons = React.memo((props: { organization: OrganizationWithoutMembers_organization }) => {
-    const { id, isCommunity, isOwner, isAdmin, isMine, } = props.organization;
+    const { id, isCommunity, isOwner, isAdmin, isMine, name } = props.organization;
 
     const canEdit = isOwner || isAdmin;
     const canLeave = isMine;
     const showButton = canEdit || canLeave;
-    const typeString = isCommunity ? 'community' : 'organization';
 
     if (!showButton) {
         return null;
@@ -20,8 +20,8 @@ export const OrganizationManageButtons = React.memo((props: { organization: Orga
     return (
         <UMoreButton>
             {canEdit && <UListItem title="Edit" onClick={() => showEditCommunityModal(id, isCommunity, isOwner)} />}
-            {canLeave && <UListItem title={'Leave ' + typeString} onClick={() => showLeaveConfirmation(id)} />}
-            {canEdit && <UListItem title="Delete" />}
+            {canLeave && <UListItem title="Leave" onClick={() => showLeaveConfirmation(id)} />}
+            {canEdit && <UListItem title="Delete" onClick={() => showDeleteOrganizationModal({ orgName: name, organizationId: id })} />}
         </UMoreButton>
     );
 });
