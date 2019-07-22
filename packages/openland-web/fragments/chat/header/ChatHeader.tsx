7ec1c511167426444d7a95466dffa4248093e62a
Steve Korshakov <steve@korshakov.com>
2019-07-22 13:30:24 +0300

    feat(web): Add members to to chat header

--- packages/openland-web/fragments/chat/header/ChatHeader.tsx
+++ packages/openland-web/fragments/chat/header/ChatHeader.tsx
@@ -6,6 +6,7 @@ import { css } from 'linaria';
 import { UAvatar } from 'openland-web/components/unicorn/UAvatar';
 import { useClient } from 'openland-web/utils/useClient';
 import { XDate } from 'openland-x/XDate';
+import { getChatOnlinesCount } from 'openland-y-utils/getChatOnlinesCount';
 
 const secondary = css`
     color: #969AA3;
@@ -42,6 +43,21 @@ const HeaderLastSeen = (props: { id: string }) => {
     } else {
         return null;
     }
+};
+
+const ChatOnlinesTitle = (props: { id: string }) => {
+    let client = useClient();
+    let [onlineCount, setOnlineCount] = React.useState<number>(0);
+
+    getChatOnlinesCount(props.id, client, count => setOnlineCount(count));
+
+    if (onlineCount <= 0) {
+        return null;
+    }
+
+    return (
+        <span>{', '}<span className={secondadyAcent}>{`${onlineCount} online`}</span></span>
+    );
 };
 
 export const ChatHeader = React.memo((props: { chat: ChatInfo }) => {
@@ -82,7 +98,15 @@ export const ChatHeader = React.memo((props: { chat: ChatInfo }) => {
                     fontWeight="600"
                     color={ThemeDefault.foregroundTertiary}
                 >
-                    {props.chat.__typename === 'PrivateRoom' ? <HeaderLastSeen id={props.chat.user.id} /> : null}
+                    {props.chat.__typename === 'PrivateRoom' && (
+                        <HeaderLastSeen id={props.chat.user.id} />
+                    )}
+                    {props.chat.__typename === 'SharedRoom' && props.chat.membersCount !== null && props.chat.membersCount !== 0 && (
+                        <span>
+                            {props.chat.membersCount >= 1 ? `${props.chat.membersCount} members` : `1 member`}
+                            <ChatOnlinesTitle id={props.chat.id} />
+                        </span>
+                    )}
                 </XView>
             </XView>
         </XView>
