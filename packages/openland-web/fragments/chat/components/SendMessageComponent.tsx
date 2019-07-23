5f9356f61917f8bbd89f6faf4f8c559e99fe85cc
Steve Korshakov <steve@korshakov.com>
2019-07-23 13:35:43 +0300

    wip(web): Working on autocomplete

--- packages/openland-web/fragments/chat/components/SendMessageComponent.tsx
+++ packages/openland-web/fragments/chat/components/SendMessageComponent.tsx
@@ -6,6 +6,7 @@ import { URickInput, URickInputInstance } from 'openland-web/components/unicorn/
 import PhotoIcon from 'openland-icons/ic-photo-2.svg';
 import FileIcon from 'openland-icons/ic-file-3.svg';
 import ShortcutsIcon from 'openland-icons/ic-attach-shortcuts-3.svg';
+import { UListItem } from 'openland-web/components/unicorn/UListItem';
 
 const attachButtonWrapper = css`
     display: flex;
@@ -68,6 +69,30 @@ const ButtonPartWrapper = (props: { leftContent: JSX.Element; rightContent: JSX.
     </XView>
 );
 
+const mentionsContainer = css`
+    position: absolute;
+    bottom: 100%;
+    left: 0px;
+    right: 0px;
+    box-shadow: 0px 0px 48px rgba(0, 0, 0, 0.04), 0px 8px 24px rgba(0, 0, 0, 0.08);
+    background-color: white;
+    border-radius: 8px;
+    padding-top: 8px;
+    padding-bottom: 8px;
+`;
+
+const AutoCompleteComponent = React.memo((props: { activeWord: string | null }) => {
+    if (!props.activeWord) {
+        return null;
+    }
+
+    return (
+        <div className={mentionsContainer}>
+            <UListItem title={props.activeWord} onClick={() => console.log(props.activeWord)} />
+        </div>
+    );
+});
+
 export const SendMessageComponent = React.memo((props: { onTextSent?: (text: string) => void }) => {
     const ref = React.useRef<URickInputInstance>(null);
     const onEnterPress = React.useCallback(() => {
@@ -82,37 +107,41 @@ export const SendMessageComponent = React.memo((props: { onTextSent?: (text: str
         }
     }, [props.onTextSent]);
 
+    const [activeWord, setActiveWord] = React.useState<string | null>(null);
     const onAutocompleteWordChange = React.useCallback((word: string) => {
-        console.log(word);
+        setActiveWord(word);
     }, []);
 
     return (
-        <XView flexGrow={1} flexShrink={1} maxHeight={250} paddingVertical={16}>
-            <XView flexGrow={1} flexShrink={1}>
-                <URickInput
-                    ref={ref}
-                    autocompletePrefixes={['@']}
-                    onAutocompleteWordChange={onAutocompleteWordChange}
-                    onEnterPress={onEnterPress}
-                    autofocus={true}
-                    placeholder="Write a message..."
-                />
+        <>
+            <XView flexGrow={1} flexShrink={1} maxHeight={250} paddingVertical={16} position="relative">
+                <AutoCompleteComponent activeWord={activeWord} />
+                <XView flexGrow={1} flexShrink={1}>
+                    <URickInput
+                        ref={ref}
+                        autocompletePrefixes={['@']}
+                        onAutocompleteWordChange={onAutocompleteWordChange}
+                        onEnterPress={onEnterPress}
+                        autofocus={true}
+                        placeholder="Write a message..."
+                    />
+                </XView>
+                <XView
+                    flexDirection="row"
+                    alignItems="center"
+                    justifyContent="space-between"
+                    marginTop={16}
+                >
+                    <ButtonPartWrapper
+                        leftContent={<AttachButton text="Photo" icon={<PhotoIcon />} />}
+                        rightContent={<AttachButton text="Document" icon={<FileIcon />} />}
+                    />
+                    <ButtonPartWrapper
+                        leftContent={<AttachButton text="Shortcuts" icon={<ShortcutsIcon />} />}
+                        rightContent={<UButton text="Send" onClick={onEnterPress} />}
+                    />
+                </XView>
             </XView>
-            <XView
-                flexDirection="row"
-                alignItems="center"
-                justifyContent="space-between"
-                marginTop={16}
-            >
-                <ButtonPartWrapper
-                    leftContent={<AttachButton text="Photo" icon={<PhotoIcon />} />}
-                    rightContent={<AttachButton text="Document" icon={<FileIcon />} />}
-                />
-                <ButtonPartWrapper
-                    leftContent={<AttachButton text="Shortcuts" icon={<ShortcutsIcon />} />}
-                    rightContent={<UButton text="Send" onClick={onEnterPress} />}
-                />
-            </XView>
-        </XView>
+        </>
     );
 });
