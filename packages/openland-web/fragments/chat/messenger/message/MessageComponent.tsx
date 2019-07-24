4771113fba87132d250c53e2b961bfcb78d27d2c
kor_ka <gleb.putintsev@openland.com>
2019-07-24 15:29:49 +0300

    wip(web): message header

--- packages/openland-web/fragments/chat/messenger/message/MessageComponent.tsx
+++ packages/openland-web/fragments/chat/messenger/message/MessageComponent.tsx
@@ -1,18 +1,40 @@
 import * as React from 'react';
 import { DataSourceWebMessageItem } from '../data/WebMessageItemDataSource';
-import { MessageContent } from './MessageContent';
 import { MessageReactions } from './reactions/MessageReactions';
+import { MessageContent } from './MessageContent';
+import { MAvatar } from './MAvatar';
+import { css, cx } from 'linaria';
+
+const messageContainerClass = css`
+    display: flex;
+    flex-direction: row;
+`;
+
+const messageContentAreaClass = css`
+    display: flex;
+    flex-direction: column;
+`;
+
+const noAvatarPlaceholder = css`
+    margin-left: 40px;
+`;
 
 export const MessageComponent = (props: { message: DataSourceWebMessageItem }) => {
     let { message } = props;
-    // place to header and all callbacks
-    return (
+
+    let content = (
         <>
             <MessageContent message={message} />
-            <>
-                {/* for buttons: reactions, comments */}
-                <MessageReactions messageId={message.id} reactions={message.reactions} />
-            </>
+            <MessageReactions messageId={message.id} reactions={message.reactions} />
         </>
+    );
+    return (
+        <div className={messageContainerClass}>
+            {!message.attachTop && <MAvatar senderPhoto={message.senderPhoto} senderNameEmojify={message.senderNameEmojify} senderName={message.senderName} senderId={message.senderId} />}
+            <div className={cx(messageContentAreaClass, message.attachTop && noAvatarPlaceholder)}>
+                {props.message.senderNameEmojify}
+                {content}
+            </div>
+        </div >
     );
 };
