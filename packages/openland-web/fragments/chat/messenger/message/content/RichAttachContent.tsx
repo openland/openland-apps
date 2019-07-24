1dea17ebac64a32d418d08a5ab2943bc499e88cc
Max Volkov <wolkow_1996@mail.ru>
2019-07-24 17:06:52 +0300

    fix(web): working from url augmentation component

--- packages/openland-web/fragments/chat/messenger/message/content/RichAttachContent.tsx
+++ packages/openland-web/fragments/chat/messenger/message/content/RichAttachContent.tsx
@@ -1,39 +1,138 @@
 import * as React from 'react';
-// import { css } from 'linaria';
+import { css } from 'linaria';
 import { FullMessage_GeneralMessage_attachments_MessageRichAttachment } from 'openland-api/Types';
 import { layoutMedia } from 'openland-web/utils/MediaLayout';
 
-// const richAttachWrapper = css`
-//     display: flex;
-//     flex-grow: 1;
-//     flex-shrink: 1;
-// `;
+const richWrapper = css`
+    display: flex;
+    flex-direction: row;
+    flex-grow: 1;
+    flex-shrink: 1;
+    max-width: 680px;
+    background-color: #f0f2f5;
+    border-radius: 8px;
+    overflow: hidden;
+`;
 
-export const RichAttachContent = (props: {
+const richImageContainer = css`
+    display: flex;
+    flex-shrink: 0;
+    flex-direction: row;
+    align-items: center;
+    min-height: 100%;
+`;
+
+const richImageStyle = css`
+    object-fit: cover;
+    flex-shrink: 0;
+    min-width: 100%;
+    min-height: 100%;
+`;
+
+const richContentContainer = css`
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    flex-shrink: 1;
+    padding-top: 16px;
+    padding-bottom: 16px;
+    padding-left: 16px;
+    padding-right: 16px;
+`;
+
+const siteIconContainer = css`
+    width: 15px;
+    height: 15px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 6px;
+`;
+
+const siteIconImageStyle = css`
+    max-width: 100%;
+    max-height: 100%;
+    object-fit: contain;
+`;
+
+const linkHostnameContainer = css`
+    display: flex;
+    align-items: center;
+    font-size: 13px;
+    color: #676d7a;
+`;
+
+const titleStyle = css`
+    font-size: 17px;
+    line-height: 24px;
+    font-weight: 600;
+    color: #171b1f;
+    margin-top: 2px;
+`;
+
+const textStyle = css`
+    font-size: 15px;
+    line-height: 24px;
+    color: #171b1f;
+    margin-top: 2px;
+`;
+
+export const RichAttachContent = ({
+    attach,
+}: {
     attach: FullMessage_GeneralMessage_attachments_MessageRichAttachment;
 }) => {
-    // console.log(props);
-    let img = undefined;
-    if (props.attach.image && props.attach.image.metadata) {
+    let img = null;
+    let siteIcon = null;
+    const text = attach.text && attach.text.length > 150 ? attach.text.slice(0, 130) + '...' : null;
+    if (attach.image && attach.image.metadata) {
         let layout = layoutMedia(
-            props.attach.image.metadata.imageWidth || 0,
-            props.attach.image.metadata.imageHeight || 0,
-            680,
-            360,
+            attach.image.metadata.imageWidth || 0,
+            attach.image.metadata.imageHeight || 0,
+            300,
+            200,
             24,
             24,
         );
         img = (
-            <div style={{ width: layout.width, height: layout.height, position: 'relative' }}>
-                <img width={layout.width} height={layout.height} src={props.attach.image.url} />
+            <div className={richImageContainer}>
+                <img
+                    className={richImageStyle}
+                    width={layout.width}
+                    height={layout.height}
+                    src={attach.image.url}
+                />
+            </div>
+        );
+    }
+    if (attach.icon && attach.icon.url) {
+        siteIcon = (
+            <div className={siteIconContainer}>
+                <img className={siteIconImageStyle} src={attach.icon.url} />
             </div>
         );
     }
     return (
-        <div>
-            <span>{props.attach.title}</span>
-            <span>{props.attach.subTitle}</span>
+        <div className={richWrapper}>
             {img}
+            <div className={richContentContainer}>
+                {(siteIcon || attach.titleLinkHostname) && (
+                    <div className={linkHostnameContainer}>
+                        {siteIcon}
+                        <span>{attach.titleLinkHostname}</span>
+                    </div>
+                )}
+                {attach.title && (
+                    <div className={titleStyle}>
+                        <span>{attach.title}</span>
+                    </div>
+                )}
+                {text && (
+                    <div className={textStyle}>
+                        <span>{text}</span>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
