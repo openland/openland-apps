ecd9ccbb0758c97851d5321eff3bf231a8d97618
Max Volkov <wolkow_1996@mail.ru>
2019-07-24 21:07:42 +0300

    fix(web): working from message image component

--- packages/openland-web/fragments/chat/messenger/message/content/ImageContent.tsx
+++ packages/openland-web/fragments/chat/messenger/message/content/ImageContent.tsx
@@ -2,14 +2,36 @@ import * as React from 'react';
 import { FullMessage_GeneralMessage_attachments_MessageAttachmentFile } from 'openland-api/Types';
 import { DataSourceWebMessageItem } from '../../data/WebMessageItemDataSource';
 import { layoutMedia } from 'openland-web/utils/MediaLayout';
-import { css } from 'linaria';
+import { css, cx } from 'linaria';
 
 const imgContainer = css`
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     min-height: 72px;
     min-width: 72px;
+    max-width: 680px;
+    max-height: 500px;
     overflow: hidden;
     border-radius: 8px;
     background-color: #f0f2f5;
+    
+    @media (max-width: 1300px) {
+        max-width: 100%;
+        height: auto !important;
+    }
+`;
+
+const imgMediaClass = css`
+    @media (max-width: 1300px) {
+        position: static !important;
+        object-fit: contain;
+        display: block;
+        max-width: 100%;
+        max-height: 100%;
+        height: auto;
+    }
 `;
 
 const imgPreviewClass = css`
@@ -45,7 +67,7 @@ export const ImageContent = React.memo(
                 if (imgRef.current) {
                     if (delta < 50) {
                         // show image instantly if loaded fast enough
-                        imgRef.current.className = imgAppearInstantClass;
+                        imgRef.current.className = cx(imgAppearInstantClass, imgMediaClass);
                     } else {
                         // animate loaded via transition
                         imgRef.current.style.opacity = '1';
@@ -58,7 +80,7 @@ export const ImageContent = React.memo(
             [placeholderRef.current, imgRef.current],
         );
 
-        let layout = layoutMedia(
+        const layout = layoutMedia(
             props.file.fileMetadata.imageWidth || 0,
             props.file.fileMetadata.imageHeight || 0,
             680,
@@ -78,10 +100,10 @@ export const ImageContent = React.memo(
         let opsRetina = `scale_crop/${layoutWidth * 2}x${layoutHeight * 2}/center/ 2x`;
 
         return (
-            <div className={imgContainer} style={{ width: layoutWidth, height: layoutHeight, position: 'relative' }}>
+            <div className={imgContainer} style={{ width: layoutWidth, height: layoutHeight }}>
                 <img
                     ref={placeholderRef}
-                    className={imgPreviewClass}
+                    className={cx(imgPreviewClass, imgMediaClass)}
                     width={layoutWidth}
                     height={layoutHeight}
                     src={props.file.filePreview || undefined}
@@ -90,7 +112,7 @@ export const ImageContent = React.memo(
                 <img
                     ref={imgRef}
                     onLoad={onLoad}
-                    className={imgAppearClass}
+                    className={cx(imgAppearClass, imgMediaClass)}
                     width={layoutWidth}
                     height={layoutHeight}
                     src={url + ops}
