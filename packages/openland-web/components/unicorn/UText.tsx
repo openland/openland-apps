cb817e52a376f7dd4188acb66fb8861eeca74a60
Fred Morozov <morozov@fred.md>
2019-07-24 16:50:21 +0300

    wip(web): ULink for internal links without XView

--- packages/openland-web/components/unicorn/UText.tsx
+++ packages/openland-web/components/unicorn/UText.tsx
@@ -1,18 +1,7 @@
 import * as React from 'react';
 import { preprocessText } from 'openland-y-utils/TextProcessor';
 import { ThemeDefault } from 'openland-y-utils/themes';
-import { isInternalLink } from 'openland-web/utils/isInternalLink';
-import { XViewRouterContext } from 'react-mental';
-import { makeInternalLinkRelative } from 'openland-web/utils/makeInternalLinkRelative';
-import { css } from 'linaria';
-
-const internalLinkClass = css`
-    cursor: pointer;
-
-    &:hover {
-        text-decoration: underline;
-    }
-`;
+import { ULink } from './ULink';
 
 interface UTextProps {
     text?: string | null;
@@ -21,43 +10,21 @@ interface UTextProps {
 
 export const UText = React.memo((props: UTextProps) => {
     const { text, linkify = true } = props;
-    const router = React.useContext(XViewRouterContext);
 
     if (text) {
         const preprocessed = preprocessText(text);
         const parts = preprocessed.map((v, i) => {
             if (v.type === 'new_line') {
                 return <br key={'new-line-' + i} />;
-            } else if (v.type === 'link') {
-                if (v.link && isInternalLink(v.link)) {
-                    return (
-                        <span
-                            key={'link-' + i}
-                            className={ linkify ? internalLinkClass : undefined }
-                            style={{
-                                color: linkify ? ThemeDefault.accentPrimary : undefined,
-                            }}
-                            onClick={() => {
-                                if (router) {
-                                    router.navigate(makeInternalLinkRelative(v.link!));
-                                }
-                            }}
-                        >
-                            {v.text}
-                        </span>
-                    );
-                } else {
-                    return (
-                        <a
-                            key={'link-' + i}
-                            href={v.link}
-                            style={{ color: linkify ? ThemeDefault.accentPrimary : undefined }}
-                            target="_blank"
-                        >
-                            {v.text}
-                        </a>
-                    );
-                }
+            } else if (v.type === 'link' && v.link && linkify) {
+                return (
+                    <ULink
+                        key={'link-' + i}
+                        link={v.link}
+                        text={v.text || v.link}
+                        color={ThemeDefault.accentPrimary}
+                    />
+                );
             } else {
                 return <span key={'text-' + i}>{v.text}</span>;
             }
