a88fcdda5a80df64b80487d0718b06970f3cba94
Fred Morozov <morozov@fred.md>
2019-07-25 00:14:41 +0300

    fix(*): improve serachMentions

--- packages/openland-engines/mentions/searchMentions.ts
+++ packages/openland-engines/mentions/searchMentions.ts
@@ -5,12 +5,17 @@ export const searchMentions = (word: string, members: RoomMembers_members[]): Ro
     const searchString = word.replace('@', '').toLowerCase();
 
     members.map(member => {
-        const { firstName, lastName, shortname } = member.user;
-        const hasInFirstName = firstName.toLowerCase().startsWith(searchString);
-        const hasInLastName = lastName ? lastName.toLowerCase().startsWith(searchString) : false;
-        const hasInShortname = shortname ? shortname.toLowerCase().startsWith(searchString) : false;
+        const { name, lastName, shortname } = member.user;
+        const lowerName = name.toLowerCase();
+        const lowerLastName = lastName ? lastName.toLowerCase() : '';
+        const lowerShortname = shortname ? shortname.toLowerCase() : '';
 
-        if (hasInFirstName || hasInLastName || hasInShortname) {
+        const inName = lowerName.startsWith(searchString);
+        const inLastName = lowerLastName.startsWith(searchString);
+        const inShortname = lowerShortname.startsWith(searchString);
+        const inNameAndLastName = searchString.length >= 2 && lowerName.startsWith(searchString[0]) && lowerLastName.startsWith(searchString.substr(1));
+
+        if (inName || inLastName || inShortname || inNameAndLastName) {
             res.push(member);
         }
     });
