a4c5304d2d6a6ae24864af0a69735070c8672df2
Fred Morozov <morozov@fred.md>
2019-07-25 17:23:19 +0300

    wip(web): comments

--- packages/openland-web/fragments/message/MessageFragment.tsx
+++ packages/openland-web/fragments/message/MessageFragment.tsx
@@ -7,6 +7,10 @@ import { UHeader } from 'openland-unicorn/UHeader';
 import { CommentInput } from './components/CommentInput';
 import { CommentsList } from './components/CommentsList';
 import { MessageView } from './components/MessageView';
+import { URickTextValue } from 'openland-web/components/unicorn/URickInput';
+import { findSpans } from 'openland-y-utils/findSpans';
+import { prepareLegacyMentionsForSend } from 'openland-engines/legacy/legacymentions';
+import UUID from 'uuid/v4';
 
 const wrapper = css`
     display: flex;
@@ -23,16 +27,31 @@ const MessageFragmentInner = React.memo((props: { messageId: string }) => {
         return null;
     }
 
+    const handleCommentSent = React.useCallback((data: URickTextValue, replyId?: string) => {
+        const text = data.text.trim();
+
+        if (text.length > 0) {
+            client.mutateAddMessageComment({
+                messageId,
+                repeatKey: UUID(),
+                mentions: prepareLegacyMentionsForSend(text, data.mentions),
+                message: text,
+                spans: findSpans(text),
+                replyComment: replyId
+            });
+        }
+    }, [messageId]);
+
     const groupId = message.source && message.source.__typename === 'MessageSourceChat' && message.source.chat.__typename === 'SharedRoom' ? message.source.chat.id : undefined;
 
     return (
         <div className={wrapper}>
-            <XScrollView3 flexGrow={1} flexBasis={0} flexShrink={1} alignItems="flex-start">
+            <XScrollView3 flexGrow={1} flexBasis={0} flexShrink={1} alignItems="flex-start" paddingHorizontal={32}>
                 <MessageView message={message} />
-                <CommentsList messageId={messageId} />
+                <CommentsList messageId={messageId} groupId={groupId} onSent={handleCommentSent} />
             </XScrollView3>
 
-            <CommentInput messageId={messageId} groupId={groupId} />
+            <CommentInput onSent={handleCommentSent} groupId={groupId} />
         </div>
     );
 });
