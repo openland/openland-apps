62bc4ed2f687d04429d2183d23b9b938df971321
Fred Morozov <morozov@fred.md>
2019-07-25 20:01:10 +0300

    wip(web): comments

--- packages/openland-web/fragments/message/components/CommentTools.tsx
+++ packages/openland-web/fragments/message/components/CommentTools.tsx
@@ -1,13 +1,27 @@
 import * as React from 'react';
 import { URelativeDate } from 'openland-web/components/unicorn/URelativeDate';
-import { css } from 'linaria';
+import { css, cx } from 'linaria';
+import { TextCaption } from 'openland-web/utils/TextStyles';
 
-const wrapper = css`
+const wrapperClass = css`
     display: flex;
     flex-direction: row;
+    margin-top: 8px;
 `;
 
 const dateClass = css`
+    color: #676D7A; // ThemeDefault.foregroundSecondary
+`;
+
+const replyClass = css`
+    cursor: pointer;
+    margin-left: 8px;
+    color: #0C7FF2; // ThemeDefault.accentPrimary
+`;
+
+const deleteClass = css`
+    cursor: pointer;
+    margin-left: 8px;
     color: #676D7A; // ThemeDefault.foregroundSecondary
 `;
 
@@ -22,13 +36,13 @@ export const CommentTools = React.memo((props: CommentToolsProps) => {
     const { date, deleted, onReplyClick, onDeleteClick } = props;
 
     return (
-        <div className={wrapper}>
-            <URelativeDate date={date} className={dateClass} />
+        <div className={wrapperClass}>
+            <URelativeDate date={date} className={cx(TextCaption, dateClass)} />
 
             {!deleted && (
                 <>
-                    <div onClick={onReplyClick}>Reply</div>
-                    <div onClick={onDeleteClick}>Delete</div>
+                    <div className={cx(TextCaption, replyClass)} onClick={onReplyClick}>Reply</div>
+                    <div className={cx(TextCaption, deleteClass)} onClick={onDeleteClick}>Delete</div>
                 </>
             )}
         </div>
