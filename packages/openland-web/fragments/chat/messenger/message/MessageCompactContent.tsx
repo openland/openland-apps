798a5ca3bb15c8e8f84c7b33526a64ab15d46bff
Steve Korshakov <steve@korshakov.com>
2019-07-26 13:28:53 +0300

    fix(web): Fix build

--- packages/openland-web/fragments/chat/messenger/message/MessageCompactContent.tsx
+++ packages/openland-web/fragments/chat/messenger/message/MessageCompactContent.tsx
@@ -3,6 +3,5 @@ import { DataSourceWebMessageItem } from '../data/WebMessageItemDataSource';
 
 export const MessageComponent = React.memo((props: { message: DataSourceWebMessageItem }) => {
 
-
     return null;
 });
