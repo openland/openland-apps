63e9d270b8df511805c97140e20790e2163931e9
bogoslavskiy <luice96@yahoo.com>
2019-07-30 22:22:43 +0300

    ref(mobile): migrate to useForm in EditCommunity

--- packages/openland-mobile/pages/main/EditCommunity.tsx
+++ packages/openland-mobile/pages/main/EditCommunity.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { PageProps } from '../../components/PageProps';
 import { withApp } from '../../components/withApp';
 import { SHeader } from 'react-native-s/SHeader';
-import { ZForm } from '../../components/ZForm';
 import { ZListItemGroup } from '../../components/ZListItemGroup';
 import { SHeaderButton } from 'react-native-s/SHeaderButton';
 import { sanitizeImageRef } from 'openland-y-utils/sanitizeImageRef';
@@ -10,68 +9,76 @@ import { getClient } from 'openland-mobile/utils/graphqlClient';
 import { XMemo } from 'openland-y-utils/XMemo';
 import { ZAvatarPicker } from 'openland-mobile/components/ZAvatarPicker';
 import { ZInput } from 'openland-mobile/components/ZInput';
-import { startLoader, stopLoader } from 'openland-mobile/components/ZGlobalLoader';
 import { ZPickField } from 'openland-mobile/components/ZPickField';
 import { ZSelect } from 'openland-mobile/components/ZSelect';
+import { useForm } from 'openland-form/useForm';
+import { useField } from 'openland-form/useField';
+import { SScrollView } from 'react-native-s/SScrollView';
+import Toast from 'openland-mobile/components/Toast';
+
+const Loader = Toast.loader();
 
 const EditCommunityComponent = XMemo<PageProps>((props) => {
-    const ref = React.useRef<ZForm | null>(null);
+    const organizationId = props.router.params.id;
     const client = getClient();
-    const organization = getClient().useOrganizationWithoutMembers({ organizationId: props.router.params.id }, { fetchPolicy: 'network-only' }).organization;
-    const profile = getClient().useOrganizationProfile({ organizationId: props.router.params.id }, { fetchPolicy: 'network-only' }).organizationProfile;
+    const organization = getClient().useOrganizationWithoutMembers({ organizationId }, { fetchPolicy: 'network-only' }).organization;
+    const profile = getClient().useOrganizationProfile({ organizationId }, { fetchPolicy: 'network-only' }).organizationProfile;
+
+    const form = useForm();
+    const nameField = useField('name', profile.name || '', form);
+    const photoField = useField('photoRef', profile.photoRef, form);
+    const aboutField = useField('about', profile.about || '', form);
 
     const changeType = React.useCallback(async (isPrivate: boolean) => {
-        startLoader();
+        Loader.show();
 
         try {
             await client.mutateUpdateOrganization({ organizationId: organization.id, input: { alphaIsPrivate: isPrivate }});
-            await client.refetchOrganizationProfile({ organizationId: props.router.params.id });
-            await client.refetchOrganization({ organizationId: props.router.params.id });
+            await client.refetchOrganizationProfile({ organizationId });
+            await client.refetchOrganization({ organizationId });
         } catch (e) {
             console.warn(e);
         } finally {
-            stopLoader();
+            Loader.hide();
         }
     }, [organization, profile]);
+
+    const handleSave = () => 
+        form.doAction(async () => {
+            try {
+                await client.mutateUpdateOrganization({
+                    organizationId,
+                    input: {
+                        name: nameField.value,
+                        photoRef: sanitizeImageRef(photoField.value),
+                        about: aboutField.value,
+                    }
+                });
+                await client.refetchOrganizationProfile({ organizationId });
+                await client.refetchOrganization({ organizationId });
+            } catch (e) {
+                console.warn(e);
+            } 
+
+            props.router.back();
+        });
 
     return (
         <>
             <SHeader title="Edit community" />
-            <SHeaderButton title="Save" onPress={() => { ref.current!.submitForm(); }} />
-            <ZForm
-                ref={ref}
-                action={async src => {
-                    await client.mutateUpdateOrganization(src);
-                    await client.refetchOrganizationProfile({ organizationId: props.router.params.id });
-                    await client.refetchOrganization({ organizationId: props.router.params.id });
-                }}
-                defaultData={{
-                    input: {
-                        name: profile.name,
-                        photoRef: sanitizeImageRef(
-                            profile.photoRef,
-                        ),
-                        about: profile.about,
-                    },
-                }}
-                staticData={{
-                    organizationId: props.router.params.id,
-                }}
-                onSuccess={() => {
-                    props.router.back();
-                }}
-            >
+            <SHeaderButton title="Save" onPress={handleSave} />
+            <SScrollView>
                 <ZListItemGroup header={null} alignItems="center">
-                    <ZAvatarPicker size="xx-large" field="input.photoRef" />
+                    <ZAvatarPicker size="xx-large" field={photoField} />
                 </ZListItemGroup>
 
                 <ZListItemGroup header="Info" headerMarginTop={0}>
                     <ZInput
                         placeholder="Community name"
-                        field="input.name"
+                        field={nameField}
                     />
                     <ZInput
-                        field="input.about"
+                        field={aboutField}
                         placeholder="About"
                         multiline={true}
                         description="Publicly describe this community for all to see see"
@@ -100,7 +107,7 @@ const EditCommunityComponent = XMemo<PageProps>((props) => {
                         description="People will be able to find your community by this shortname"
                     />
                 </ZListItemGroup>
-            </ZForm>
+            </SScrollView>
         </>
     );
 });
