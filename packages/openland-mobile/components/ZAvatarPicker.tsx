17c39d6fe2dd18cbcbe0269cc0c72f52aa70ff01
bogoslavskiy <luice96@yahoo.com>
2019-07-30 20:29:05 +0300

    ref(mobile): useForm support for ZAvatarPicker

--- packages/openland-mobile/components/ZAvatarPicker.tsx
+++ packages/openland-mobile/components/ZAvatarPicker.tsx
@@ -3,14 +3,13 @@ import { View, Image, TouchableOpacity } from 'react-native';
 import ImagePicker, { Image as PickerImage } from 'react-native-image-crop-picker';
 import { UploadCareDirectUploading } from '../utils/UploadCareDirectUploading';
 import { UploadStatus } from 'openland-engines/messenger/types';
-import { XStoreContext } from 'openland-y-store/XStoreContext';
-import { XStoreState } from 'openland-y-store/XStoreState';
 import { ZAvatar, ZAvatarSize, avatarSizes } from './ZAvatar';
 import { handlePermissionDismiss } from 'openland-mobile/utils/permissions/handlePermissionDismiss';
 import { XMemo } from 'openland-y-utils/XMemo';
 import { ThemeContext } from 'openland-mobile/themes/ThemeContext';
 import { ThemeGlobal } from 'openland-y-utils/themes/ThemeGlobal';
 import LoaderSpinner from './LoaderSpinner';
+import { FormField } from 'openland-form/useField';
 
 interface AvatarImageRef {
     uuid: string;
@@ -27,7 +26,7 @@ export interface ZAvatarPickerRenderProps {
 export interface ZAvatarPickerProps {
     size?: ZAvatarSize;
     initialUrl?: string;
-    field?: string;
+    field?: FormField<AvatarImageRef | null>;
     valueStoreKey?: string;
     value?: AvatarImageRef | null;
     onChanged?: (value: AvatarImageRef | null) => void;
@@ -38,29 +37,24 @@ export interface ZAvatarPickerProps {
     };
 }
 
-class ZAvatarPickerComponent extends React.PureComponent<ZAvatarPickerProps & { store?: XStoreState, theme: ThemeGlobal }, { loading: boolean, localPath?: string }> {
+const ZAvatarPickerComponent = (props: ZAvatarPickerProps & { theme: ThemeGlobal }) => {
+    const [file, setFile] = React.useState<string | undefined>(undefined);
+    const [localPath, setLocalPath] = React.useState<string | undefined>(undefined);
+    const [loading, setLoading] = React.useState<boolean>(false);
+    
+    let currentIteration = 0;
 
-    state = {
-        file: undefined,
-        loading: false,
-
-        localPath: undefined,
-    };
-
-    private currentIteration = 0;
-
-    upload = (data: { path: string, width?: number, height?: number }) => {
-        // startLoader();
-        this.setState({ loading: true });
-        let up = ++this.currentIteration;
+    const upload = (data: { path: string, width?: number, height?: number }) => {
+        setLoading(true);
+        let up = ++currentIteration;
         let uploading;
         uploading = new UploadCareDirectUploading('photo.jpg', data.path);
 
         uploading.watch((v) => {
-            if (up === this.currentIteration) {
+            if (up === currentIteration) {
                 if (v.status === UploadStatus.COMPLETED) {
-                    if (this.props.onChanged) {
-                        this.props.onChanged({
+                    if (props.onChanged) {
+                        props.onChanged({
                             uuid: v.uuid!!,
                             ...data.width && data.height ?
                                 {
@@ -73,37 +67,22 @@ class ZAvatarPickerComponent extends React.PureComponent<ZAvatarPickerProps & { 
                                 } : {}
                         });
                     }
-                    if (this.props.valueStoreKey || this.props.field) {
-                        this.props.store!!.writeValue(this.props.valueStoreKey || ('fields.' + this.props.field), {
-                            uuid: v.uuid!!,
-                            ...data.width && data.height ?
-                                {
-                                    crop: {
-                                        x: 0,
-                                        y: 0,
-                                        w: data!!.width!!,
-                                        h: data!!.height!!
-                                    }
-                                } : {}
-                        });
-                    }
-                    this.setState({ loading: false });
-                    // stopLoader();
+                
+                    setLoading(false);
                 } else if (v.status === UploadStatus.FAILED) {
-                    this.setState({ loading: false });
-                    // stopLoader();
+                    setLoading(false);
                 }
             }
         });
-    }
+    };
 
-    private handlePicker = async () => {
+    const handlePicker = async () => {
         try {
             let res: PickerImage | null = null;
             try {
                 let r = await ImagePicker.openPicker({
-                    width: this.props.pickSize ? this.props.pickSize.width : 1024,
-                    height: this.props.pickSize ? this.props.pickSize.height : 1024,
+                    width: props.pickSize ? props.pickSize.width : 1024,
+                    height: props.pickSize ? props.pickSize.height : 1024,
                     cropping: true
                 });
                 if (!Array.isArray(r)) {
@@ -121,101 +100,96 @@ class ZAvatarPickerComponent extends React.PureComponent<ZAvatarPickerProps & { 
                 // Ignore
             }
             if (res) {
-                this.setState({
-                    localPath: res.path
-                });
-
-                this.upload(res);
+                setLocalPath(res.path);
+                upload(res);
             }
         } catch (e) {
             console.log(e);
         }
-    }
-    componentDidMount() {
-        if (this.props.initialUrl) {
-            this.upload({ path: this.props.initialUrl });
-        }
-    }
-    componentWillUnmount() {
-        // Reset watcher
-        this.currentIteration++;
-    }
+    };
 
-    render() {
-        let { value, theme } = this.props;
-        if (this.props.field || this.props.valueStoreKey) {
-            let existing = this.props.store!!.readValue(this.props.valueStoreKey || ('fields.' + this.props.field));
-            if (existing) {
-                value = existing;
+    React.useEffect(() => {
+        if (props.initialUrl) {
+            upload({ path: props.initialUrl });
+        }
+
+        return () => {
+            currentIteration++;
+        };
+    }, []);
+
+    let { value, theme } = props;
+    let valueUrl = undefined;
+
+    valueUrl = props.initialUrl;
+
+    if (value && typeof value.uuid === 'string') {
+        if (value.uuid.startsWith('https://ucarecdn.com/')) {
+            valueUrl = value.uuid;
+        } else {
+            valueUrl = 'https://ucarecdn.com/' + value.uuid + '/';
+            if (value.crop) {
+                valueUrl += `-/crop/${value.crop.w}x${value.crop.h}/${value.crop.x},${value.crop.y}/`;
             }
         }
-        let valueUrl = undefined;
+    }
 
-        valueUrl = this.props.initialUrl;
+    if (localPath) {
+        valueUrl = localPath;
+    }
 
-        if (value && typeof value.uuid === 'string') {
-            if (value.uuid.startsWith('https://ucarecdn.com/')) {
-                valueUrl = value.uuid;
-            } else {
-                valueUrl = 'https://ucarecdn.com/' + value.uuid + '/';
-                if (value.crop) {
-                    valueUrl += `-/crop/${value.crop.w}x${value.crop.h}/${value.crop.x},${value.crop.y}/`;
-                }
-            }
-        }
+    let size = avatarSizes[props.size || 'x-large'].size;
+    return props.render ? <props.render url={valueUrl} file={file} loading={loading} showPicker={handlePicker} /> : (
+        <TouchableOpacity onPress={handlePicker}>
+            <View width={size} height={size} borderRadius={size / 2} backgroundColor={theme.backgroundTertiary}>
+                {!valueUrl && !loading && (
+                    <View position="absolute" alignItems="center" justifyContent="center" style={{ width: size, height: size, borderRadius: size / 2 }}>
+                        <Image source={require('assets/ic-picker-48.png')} style={{ tintColor: theme.foregroundTertiary, width: 48, height: 48 }} />
+                    </View>
+                )}
+                {!!valueUrl && (
+                    <View width={size} height={size}>
+                        <ZAvatar src={valueUrl} size={props.size || 'x-large'} />
 
-        if (this.state.localPath) {
-            valueUrl = this.state.localPath;
-        }
-
-        let size = avatarSizes[this.props.size || 'x-large'].size;
-        return this.props.render ? <this.props.render url={valueUrl} file={this.state.file} loading={this.state.loading} showPicker={this.handlePicker} /> : (
-            <TouchableOpacity onPress={this.handlePicker}>
-                <View width={size} height={size} borderRadius={size / 2} backgroundColor={theme.backgroundTertiary}>
-                    {!valueUrl && !this.state.loading && (
-                        <View position="absolute" alignItems="center" justifyContent="center" style={{ width: size, height: size, borderRadius: size / 2 }}>
-                            <Image source={require('assets/ic-picker-48.png')} style={{ tintColor: theme.foregroundTertiary, width: 48, height: 48 }} />
-                        </View>
-                    )}
-                    {!!valueUrl && (
-                        <View width={size} height={size}>
-                            <ZAvatar src={valueUrl} size={this.props.size || 'x-large'} />
-
-                            {!this.state.loading && (
-                                <View position="absolute" bottom={2} right={2} width={28} height={28} padding={2} backgroundColor={theme.backgroundPrimary} borderRadius={14}>
-                                    <View width={24} height={24} backgroundColor={theme.accentPrimary} borderRadius={12} alignItems="center" justifyContent="center">
-                                        <Image source={require('assets/ic-picker-selected-16.png')} style={{ tintColor: theme.contrastSpecial, width: 16, height: 16 }} />
-                                    </View>
+                        {!loading && (
+                            <View position="absolute" bottom={2} right={2} width={28} height={28} padding={2} backgroundColor={theme.backgroundPrimary} borderRadius={14}>
+                                <View width={24} height={24} backgroundColor={theme.accentPrimary} borderRadius={12} alignItems="center" justifyContent="center">
+                                    <Image source={require('assets/ic-picker-selected-16.png')} style={{ tintColor: theme.contrastSpecial, width: 16, height: 16 }} />
                                 </View>
-                            )}
-                        </View>
-                    )}
-                    {this.state.loading && (
-                        <View position="absolute" alignItems="center" justifyContent="center" width={size} height={size} borderRadius={size / 2} backgroundColor={theme.overlayLight}>
-                            <LoaderSpinner />
-                        </View>
-                    )}
-                </View>
-            </TouchableOpacity>
-        );
-    }
-}
+                            </View>
+                        )}
+                    </View>
+                )}
+                {loading && (
+                    <View position="absolute" alignItems="center" justifyContent="center" width={size} height={size} borderRadius={size / 2} backgroundColor={theme.overlayLight}>
+                        <LoaderSpinner />
+                    </View>
+                )}
+            </View>
+        </TouchableOpacity>
+    );
+};
 
 export const ZAvatarPicker = XMemo<ZAvatarPickerProps>(props => {
+    const { field } = props;
     const theme = React.useContext(ThemeContext);
-
-    if (props.field || props.valueStoreKey) {
+    
+    if (field && field.input) {
         return (
-            <XStoreContext.Consumer>
-                {store => {
-                    if (!store) {
-                        throw Error('No store!');
+            <ZAvatarPickerComponent 
+                {...props} 
+                theme={theme}
+                value={field.input.value}
+                onChanged={(ref) => {
+                    field.input.onChange(ref);
+
+                    if (props.onChanged) {
+                        props.onChanged(ref);
                     }
-                    return <ZAvatarPickerComponent store={store} theme={theme} {...props} />;
                 }}
-            </XStoreContext.Consumer>
+            />
         );
     }
-
+    
     return <ZAvatarPickerComponent theme={theme} {...props} />;
 });
