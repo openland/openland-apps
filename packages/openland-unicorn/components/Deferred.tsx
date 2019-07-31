d780f2f227237c655e39bc83ea7b257de20a3975
Steve Korshakov <steve@korshakov.com>
2019-07-31 15:18:19 +0300

    fix(web): Fix build

--- packages/openland-unicorn/components/Deferred.tsx
+++ packages/openland-unicorn/components/Deferred.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 export const Deferred = React.memo((props: { children?: any }) => {
-    let [inited, setInited] = React.useState(false);
+    // let [inited, setInited] = React.useState(false);
 
     // React.useLayoutEffect(() => {
     //     let active = true;
