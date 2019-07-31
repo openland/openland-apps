74ab06774637ddb29957ae55801cb76a1f8eb227
Fred Morozov <morozov@fred.md>
2019-07-31 15:58:12 +0300

    fix(web): sender name emojify in MessageAction

--- packages/openland-web/fragments/chat/messenger/message/MessageCompactContent.tsx
+++ packages/openland-web/fragments/chat/messenger/message/MessageCompactContent.tsx
@@ -12,11 +12,9 @@ const singleLine = css`
 `;
 
 export const MessageCompactComponent = React.memo((props: { message: DataSourceMessageItem }) => {
-    // let imageAttachWithPreview = (props.message.attachments || []).filter(a => a.__typename === 'MessageAttachmentFile' && a.fileMetadata.isImage && a.filePreview)[0];
     return (
         <>
-            {/* {imageAttachWithPreview && <img style={{ objectFit: 'contain' }} width={20} height={20} src={imageAttachWithPreview.__typename === 'MessageAttachmentFile' && imageAttachWithPreview.filePreview || undefined} />} */}
-            <span className={cx(TextLabel1, singleLine)}>{props.message.senderName}</span>
+            <span className={cx(TextLabel1, singleLine)}>{emoji(props.message.senderName)}</span>
             <span className={cx(TextBody, singleLine)}>{emoji(props.message.fallback)}</span>
         </>
     );
