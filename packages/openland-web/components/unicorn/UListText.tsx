19454bb3e3fb971dd08c18156240eed88200ec70
Fred Morozov <morozov@fred.md>
2019-08-01 17:56:43 +0300

    fix(web): new lines in about fields in profiles + emojify

--- packages/openland-web/components/unicorn/UListText.tsx
+++ packages/openland-web/components/unicorn/UListText.tsx
@@ -2,6 +2,26 @@ import * as React from 'react';
 import { XView, XViewProps } from 'react-mental';
 import { TextStyles } from 'openland-web/utils/TextStyles';
 import { ThemeDefault } from 'openland-y-utils/themes';
+import { emoji } from 'openland-y-utils/emoji';
+import { css } from 'linaria';
+
+const wrapper = css`
+    span {
+        display: inline;
+    }
+`;
+
+const getRows = (value: string) => {
+    const processedRows: (JSX.Element | string)[] = [];
+    const rows = value.split('\n');
+
+    rows.map(row => {
+        processedRows.push(emoji(row));
+        processedRows.push(<br />);
+    });
+
+    return processedRows;
+}
 
 interface UListTextProps extends XViewProps {
     value?: string | JSX.Element;
@@ -19,7 +39,10 @@ export const UListText = (props: UListTextProps) => {
             flexGrow={1}
             flexShrink={1}
         >
-            {value}
+            <div className={wrapper}>
+                {typeof value === 'string' && getRows(value)}
+                {typeof value !== 'string' && value}
+            </div>
         </XView>
     );
 };
