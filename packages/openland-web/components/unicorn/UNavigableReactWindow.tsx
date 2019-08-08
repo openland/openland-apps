3c4a4d1d70d08d52ca8d83194e51a1f5eb11b91b
kor_ka <gleb.putintsev@openland.com>
2019-08-08 14:16:41 +0300

    fix(web): recover autocomplete focus by default

--- packages/openland-web/components/unicorn/UNavigableReactWindow.tsx
+++ packages/openland-web/components/unicorn/UNavigableReactWindow.tsx
@@ -9,14 +9,14 @@ export interface UNavigableListRef {
     onPressEnter(): boolean;
 }
 
-export const UNavigableReactWindow = React.memo(React.forwardRef(<T extends {}>(props: { data: T[], itemSize: number, width: number | string, height: number, overscanCount?: number, onSelected: (item: T) => void, renderItem: (item: T) => JSX.Element }, ref: React.Ref<UNavigableListRef>) => {
+export const UNavigableReactWindow = React.memo(React.forwardRef(<T extends {}>(props: { data: T[], focusedByDefault?: boolean, itemSize: number, width: number | string, height: number, overscanCount?: number, onSelected: (item: T) => void, renderItem: (item: T) => JSX.Element }, ref: React.Ref<UNavigableListRef>) => {
     let { data, itemSize, width, height, overscanCount, renderItem } = props;
 
     let [state, dispatch] = React.useReducer((oldState: { index: number, length: number }, action: { delta?: number, length?: number, reset?: boolean }) => {
         let delta = action.delta !== undefined ? action.delta : 0;
         let length = action.length !== undefined ? action.length : oldState.length;
-        return { index: action.reset ? -1 : Math.max(Math.min(length - 1, oldState.index + delta), 0), length };
-    }, { index: -1, length: data.length });
+        return { index: action.reset ? (props.focusedByDefault === false ? -1 : 0) : Math.max(Math.min(length - 1, oldState.index + delta), 0), length };
+    }, { index: props.focusedByDefault === false ? -1 : 0, length: data.length });
 
     if (data.length !== state.length) {
         dispatch({ length: data.length });
