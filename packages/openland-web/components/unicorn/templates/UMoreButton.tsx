a46c7e9da844d2072a307ad4cfa1ab852892cb23
Fred Morozov <morozov@fred.md>
2019-08-13 18:19:49 +0300

    fix(web): remove UMoreButton with XPopper && add MenuBuilder in GroupMemberMenu

--- packages/openland-web/components/unicorn/templates/UMoreButton.tsx
+++ packages/openland-web/components/unicorn/templates/UMoreButton.tsx
@@ -1,91 +1,9 @@
 import * as React from 'react';
-import XPopper, { Placement } from 'openland-x/XPopper';
 import { UIconButton } from '../UIconButton';
 import ManageVerticalIcon from 'openland-icons/ic-more-v.svg';
 import { XViewProps } from 'react-mental';
-import { css, cx } from 'linaria';
 import { UPopperController } from '../UPopper';
 import { usePopper } from '../usePopper';
-
-const iconWrapper = css`
-    svg {
-        path {
-            fill: #676D7A; // Need to be ThemeDefault.foregroundSecondary
-        }
-    }
-`;
-
-interface UMoreButtonDeprecatedProps extends XViewProps {
-    placement?: Placement;
-    show?: boolean;
-    showOnHover?: boolean;
-    onClickOutside?: () => void;
-    children: any;
-}
-
-export class UMoreButtonDeprecated extends React.PureComponent<UMoreButtonDeprecatedProps, { show: boolean }> {
-    refComp?: Element;
-
-    constructor(props: UMoreButtonDeprecatedProps) {
-        super(props);
-
-        this.state = {
-            show: false,
-        };
-    }
-
-    switch = (e: any) => {
-        e.stopPropagation();
-        const { show } = this.props;
-
-        if (typeof show === 'undefined') {
-            this.setState({ show: !this.state.show });
-        }
-    }
-
-    handleClose = () => {
-        const { show, onClickOutside } = this.props;
-        if (typeof show === 'undefined') {
-            this.setState({ show: false });
-        } else if (onClickOutside !== undefined) {
-            onClickOutside();
-        }
-    }
-
-    createRef = (el: any) => {
-        this.refComp = el;
-    }
-
-    render() {
-        const { placement = 'bottom-end', show, showOnHover, onClickOutside, children, ...other } = this.props;
-        let computedShow: boolean | undefined =
-            typeof show === 'undefined' ? this.state.show : show;
-
-        if (showOnHover) {
-            computedShow = undefined;
-        }
-
-        return (
-            <XPopper
-                show={computedShow}
-                content={children}
-                arrow={null}
-                placement={placement}
-                onClickOutside={this.handleClose}
-                showOnHover={showOnHover}
-            >
-                <div className={cx(iconWrapper)}>
-                    <UIconButton
-                        icon={<ManageVerticalIcon />}
-                        active={computedShow}
-                        onClick={this.switch}
-                        {...other}
-                    />
-                </div>
-            </XPopper>
-        );
-    }
-}
 
 interface UMoreButtonProps extends XViewProps {
     menu: (ctx: UPopperController) => JSX.Element;
--- packages/openland-web/fragments/group/components/GroupMemberMenu.tsx
+++ packages/openland-web/fragments/group/components/GroupMemberMenu.tsx
@@ -1,53 +1,105 @@
 import * as React from 'react';
 import { RoomFullWithoutMembers_SharedRoom, RoomMembersPaginated_members } from 'openland-api/Types';
-import { UListItem } from 'openland-web/components/unicorn/UListItem';
-import { UMoreButtonDeprecated } from 'openland-web/components/unicorn/templates/UMoreButton';
+import { UMoreButton } from 'openland-web/components/unicorn/templates/UMoreButton';
 import StarIcon from 'openland-icons/s/ic-star-24.svg';
 import LeaveIcon from 'openland-icons/s/ic-leave-24.svg';
-import { useRole } from 'openland-x-permissions/XWithRole';
-import { showRemoveMemberModal } from 'openland-web/fragments/chat/RemoveMemberModal';
 import { showMakeFeaturedModal } from 'openland-web/fragments/account/components/modals';
+import { MenuItem, UPopperMenuBuilder } from 'openland-web/components/unicorn/UPopperMenuBuilder';
+import { AppConfig } from 'openland-y-runtime/AppConfig';
+import { showLeaveChatConfirmation } from 'openland-web/fragments/account/components/groupProfileModals';
+import { OpenlandClient } from 'openland-api/OpenlandClient';
+import { useClient } from 'openland-web/utils/useClient';
+import { UPopperController } from 'openland-web/components/unicorn/UPopper';
+import { AlertBlanketBuilder } from 'openland-x/AlertBlanket';
 
-interface GroupMemberMenu {
+interface MenuContentOpts {
+    group: RoomFullWithoutMembers_SharedRoom;
+    memberRef: React.MutableRefObject<RoomMembersPaginated_members>;
+    client: OpenlandClient;
+}
+
+const getMenuContent = (opts: MenuContentOpts) => {
+    const res: MenuItem[] = [];
+
+    const { group, memberRef, client } = opts;
+    const { id, isChannel } = group;
+    const { user, badge, canKick } = memberRef.current;
+
+    const typeString = isChannel ? 'channel' : 'group';
+
+    if (AppConfig.isSuperAdmin()) {
+        res.push({
+            title: badge ? 'Edit featured status' : 'Make featured',
+            icon: <StarIcon />,
+            onClick: () => showMakeFeaturedModal(id, user.id)
+        });
+    }
+
+    if (user.isYou) {
+        res.push({
+            title: `Leave ${typeString}`,
+            icon: <LeaveIcon />,
+            onClick: () => showLeaveChatConfirmation(client, id)
+        });
+    }
+
+    if (!user.isYou && canKick) {
+        res.push({
+            title: `Remove from ${typeString}`,
+            icon: <LeaveIcon />,
+            onClick: () => {
+                const builder = new AlertBlanketBuilder;
+
+                builder.title(`Remove ${user.name} from ${group.title}`);
+                builder.message(`Are you sure you want to remove ${user.firstName}? They will no longer be able to participate in the discussion.`);
+                builder.action(`Remove`, async () => {
+                    await client.mutateRoomKick({
+                        userId: user.id,
+                        roomId: group.id,
+                    });
+
+                    await client.refetchRoom({ id: group.id });
+                }, 'danger');
+
+                builder.show();
+            }
+        });
+    }
+
+    return res;
+};
+
+const MenuComponent = React.memo((props: { ctx: UPopperController, items: MenuItem[] }) => (
+    new UPopperMenuBuilder().items(props.items).build(props.ctx)
+));
+
+interface GroupMemberMenuProps {
     group: RoomFullWithoutMembers_SharedRoom;
     member: RoomMembersPaginated_members;
 }
 
-export const GroupMemberMenu = React.memo((props: GroupMemberMenu) => {
+export const GroupMemberMenu = React.memo((props: GroupMemberMenuProps) => {
+    const client = useClient();
     const { group, member } = props;
-    const { id, title, isChannel } = group;
-    const { user, badge, canKick } = member;
 
-    const typeString = isChannel ? 'channel' : 'group';
-    const showButton = useRole('super-admin') || canKick || user.isYou;
+    // Sorry universe
+    const memberRef = React.useRef(member);
+    memberRef.current = member;
 
-    const handleFeaturedClick = React.useCallback((e: React.MouseEvent) => {
-        e.preventDefault();
+    const menuContent = getMenuContent({ group, memberRef, client });
 
-        showMakeFeaturedModal(id, user.id);
-    }, []);
-
-    const handleLeaveClick = React.useCallback((e: React.MouseEvent) => {
-        e.preventDefault();
-
-        showRemoveMemberModal({ roomId: id, roomTitle: title, memberId: user.id, memberName: user.name });
-    }, []);
-
-    const handleRemoveClick = React.useCallback((e: React.MouseEvent) => {
-        e.preventDefault();
-
-        showRemoveMemberModal({ roomId: id, roomTitle: title, memberId: user.id, memberName: user.name });
-    }, []);
-
-    if (!showButton) {
+    if (menuContent.length <= 0) {
         return null;
     }
 
     return (
-        <UMoreButtonDeprecated>
-            {useRole('super-admin') && <UListItem title={badge ? 'Edit featured status' : 'Make featured'} icon={<StarIcon />} onClick={handleFeaturedClick} />}
-            {user.isYou && <UListItem title={`Leave ${typeString}`} icon={<LeaveIcon />} onClick={handleLeaveClick} />}
-            {!user.isYou && <UListItem title={`Remove from ${typeString}`} icon={<LeaveIcon />} onClick={handleRemoveClick} />}
-        </UMoreButtonDeprecated>
+        <UMoreButton
+            menu={ctx => (
+                <MenuComponent
+                    ctx={ctx}
+                    items={getMenuContent({ group, memberRef, client })}
+                />
+            )}
+        />
     );
 });
