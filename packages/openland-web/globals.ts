c8edf47e7861c82127e5867f58523cc444b2b818
Fred Morozov <morozov@fred.md>
2019-08-15 13:18:05 +0300

    fix(web): fix navigating to comment after user clicks on push notification

--- packages/openland-web/globals.ts
+++ packages/openland-web/globals.ts
@@ -1,6 +1,4 @@
 import { canUseDOM } from 'openland-y-utils/canUseDOM';
-import { AppNotifications } from 'openland-y-runtime-web/AppNotifications';
-import { Router } from './routes';
 import { Howler } from 'howler';
 import { disableTag } from 'mental-log';
 import 'openland-x-routing/NextRouting';
@@ -12,5 +10,4 @@ disableTag('GraphQL-Direct');
 // Is it a good place?
 if (canUseDOM) {
     Howler.mobileAutoEnable = true;
-    AppNotifications.setRouter(Router);
 }
