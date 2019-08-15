85601e505cd7097f4a3e4113fd6773fb498b7aea
kor_ka <gleb.putintsev@openland.com>
2019-08-15 13:07:32 +0300

    fix(web): fix build

--- packages/openland-web/fragments/dialogs/components/DialogListView.tsx
+++ packages/openland-web/fragments/dialogs/components/DialogListView.tsx
@@ -40,7 +40,7 @@ export const DialogListView = XMemo<DialogListViewProps>(props => {
     if (!ds) {
         ds = dialogListWebDataSource(messenger.dialogList.dataSource);
     }
-    let dataSource = ds;
+    let dataSource = React.useMemo(() => new DataSourceWindow(ds!, 20), [ds]);
     let [query, setQuery] = React.useState('');
     let isSearching = query.trim().length > 0;
     let router = React.useContext(XViewRouterContext);
