40b6136930917aa5c5a5a01fb0f1e880eedf2594
Fred Morozov <morozov@fred.md>
2019-08-15 20:40:12 +0300

    fix(mobile): add spaces between timestamps and AM/PM

--- packages/openland-mobile/utils/formatDate.ts
+++ packages/openland-mobile/utils/formatDate.ts
@@ -22,7 +22,7 @@ export function formatDate(date: number) {
     if (now.getFullYear() === dt.getFullYear() && now.getMonth() === dt.getMonth() && now.getDate() === dt.getDate()) {
         // return <span>Today</span>;
         let hours = dt.getHours();
-        let ampm = dt.getHours() < 12 ? 'AM' : 'PM';
+        let ampm = dt.getHours() < 12 ? ' AM' : ' PM';
         hours = hours > 12 ? hours - 12 : hours === 0 ? 12 : hours;
         return hours + ':' + ('0' + dt.getMinutes()).substr(-2) + ampm;
     }
