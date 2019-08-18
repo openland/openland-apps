d1fb99a2e54da38404c22c510cf74506d07fd6d6
bogoslavskiy <luice96@yahoo.com>
2019-08-18 22:02:15 +0300

    fix(mobile): impove newMessagesDivider

--- packages/openland-mobile/messenger/components/AsyncNewMessageDivider.tsx
+++ packages/openland-mobile/messenger/components/AsyncNewMessageDivider.tsx
@@ -10,13 +10,14 @@ export const AsyncNewMessageDivider = React.memo((props) => {
     let theme = useThemeGlobal();
     
     return (
-        <ASFlex flexDirection={'column'} alignItems="center" justifyContent="center" backgroundColor={theme.backgroundPrimary} marginLeft={12} marginRight={12} height={32} marginBottom={-6}>
-            <ASFlex width={screenWidth - 24} height={0.5} backgroundColor={theme.foregroundQuaternary} />
-            <ASFlex marginTop={-8} backgroundColor={theme.backgroundPrimary}>
+        <ASFlex width={screenWidth} flexDirection={'row'} alignItems={'center'} justifyContent={'center'} backgroundColor={theme.backgroundPrimary} marginLeft={12} marginRight={12} height={32}>
+            <ASFlex height={0.5} backgroundColor={theme.foregroundQuaternary} flexGrow={1} flexShrink={1} flexBasis={0} alignSelf="stretch" />
+            <ASFlex backgroundColor={theme.backgroundPrimary}>
                 <ASText color={theme.foregroundSecondary} fontSize={13} marginLeft={12} marginRight={12}>
                     New messages
                 </ASText>
             </ASFlex>
+            <ASFlex height={0.5} backgroundColor={theme.foregroundQuaternary} flexGrow={1} flexShrink={1} flexBasis={0} alignSelf="stretch" />
         </ASFlex>
     );
 });
