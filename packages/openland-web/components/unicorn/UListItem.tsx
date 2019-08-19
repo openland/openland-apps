03ca0d47226902c890b92d9fce7f869585a96150
Max Volkov <wolkow_1996@mail.ru>
2019-08-19 13:26:38 +0300

    fix(web): missing ellipsis in UListItem

--- packages/openland-web/components/unicorn/UListItem.tsx
+++ packages/openland-web/components/unicorn/UListItem.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { css } from 'linaria';
 import { XView, XViewSelectedContext, XViewProps } from 'react-mental';
 import { TextStyles } from 'openland-web/utils/TextStyles';
 import { UAvatar } from './UAvatar';
@@ -7,18 +8,26 @@ import { UIcon } from './UIcon';
 const SelectableSVG = React.memo((props: { icon: JSX.Element }) => {
     const selected = React.useContext(XViewSelectedContext);
 
-    return <UIcon icon={props.icon} color={selected ? 'var(--foregroundInverted)' : 'var(--foregroundSecondary)'} />;
+    return (
+        <UIcon
+            icon={props.icon}
+            color={selected ? 'var(--foregroundInverted)' : 'var(--foregroundSecondary)'}
+        />
+    );
 });
+
+const ellipsisText = css`
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+`;
 
 export const SelectableText = React.memo((props: XViewProps) => {
     const selected = React.useContext(XViewSelectedContext);
 
     return (
-        <XView
-            {...props}
-            color={selected ? props.selectedColor : props.color}
-        >
-            {props.children}
+        <XView {...props} maxWidth="100%" color={selected ? props.selectedColor : props.color}>
+            <span className={ellipsisText}>{props.children}</span>
         </XView>
     );
 });
@@ -32,7 +41,7 @@ export interface UListItemProps {
     icon?: JSX.Element;
     iconBackground?: string;
     iconColor?: string;
-    avatar?: { photo?: string | null, id: string, title: string, online?: boolean };
+    avatar?: { photo?: string | null; id: string; title: string; online?: boolean };
     onClick?: (event: React.MouseEvent) => void;
     path?: string;
     large?: boolean;
@@ -45,8 +54,27 @@ export interface UListItemProps {
 }
 
 export const UListItem = React.memo((props: UListItemProps) => {
-    const { title, titleIcon, subtitle, description, descriptionColor, icon, iconBackground, iconColor, avatar, onClick, path, large, useRadius, textRight, rightElement, hovered, disabled, paddingHorizontal = 16 } = props;
-    const height = large ? 80 : ((!!avatar || !!iconBackground) ? 56 : 48);
+    const {
+        title,
+        titleIcon,
+        subtitle,
+        description,
+        descriptionColor,
+        icon,
+        iconBackground,
+        iconColor,
+        avatar,
+        onClick,
+        path,
+        large,
+        useRadius,
+        textRight,
+        rightElement,
+        hovered,
+        disabled,
+        paddingHorizontal = 16,
+    } = props;
+    const height = large ? 80 : !!avatar || !!iconBackground ? 56 : 48;
 
     const titleFont = !!description ? TextStyles.Label1 : TextStyles.Body;
     const subtitleFont = TextStyles.Caption;
@@ -54,58 +82,99 @@ export const UListItem = React.memo((props: UListItemProps) => {
     const textRightFont = TextStyles.Body;
 
     const containerRef = React.useRef<HTMLDivElement>(null);
-    React.useEffect(() => {
-        if (containerRef.current && hovered) {
-            containerRef.current.scrollIntoView({ behavior: 'auto', block: 'nearest' });
-        }
-    }, [hovered]);
+    React.useEffect(
+        () => {
+            if (containerRef.current && hovered) {
+                containerRef.current.scrollIntoView({ behavior: 'auto', block: 'nearest' });
+            }
+        },
+        [hovered],
+    );
 
     const content = (
         <>
-            {!!icon && !iconBackground && <XView marginRight={16} width={24} height={24} alignItems="center" justifyContent="center"><SelectableSVG icon={icon} /></XView>}
-            {!!icon && !!iconBackground && <XView marginRight={16} width={40} height={40} borderRadius={20} backgroundColor={iconBackground} alignItems="center" justifyContent="center"><UIcon icon={icon} color={iconColor || '#FFFFFF'} /></XView>}
-            {!!avatar && !icon && (
-                <XView marginRight={16}>
-                    <UAvatar {...avatar} size={large ? 'large' : 'medium'} />
-                </XView>
-            )}
+            {!!icon &&
+                !iconBackground && (
+                    <XView
+                        marginRight={16}
+                        width={24}
+                        height={24}
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+                        <SelectableSVG icon={icon} />
+                    </XView>
+                )}
+            {!!icon &&
+                !!iconBackground && (
+                    <XView
+                        marginRight={16}
+                        width={40}
+                        height={40}
+                        borderRadius={20}
+                        backgroundColor={iconBackground}
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+                        <UIcon icon={icon} color={iconColor || '#FFFFFF'} />
+                    </XView>
+                )}
+            {!!avatar &&
+                !icon && (
+                    <XView marginRight={16}>
+                        <UAvatar {...avatar} size={large ? 'large' : 'medium'} />
+                    </XView>
+                )}
 
             <XView flexDirection="column" flexGrow={1} flexShrink={1} flexBasis={0}>
                 <XView flexDirection="row" alignItems="center" overflow="hidden">
-                    {!!titleIcon && (
-                        <XView>
-                            {titleIcon}
-                        </XView>
-                    )}
-                    <SelectableText {...titleFont} color="var(--foregroundPrimary)" selectedColor="var(--foregroundInverted)">
+                    {!!titleIcon && <XView>{titleIcon}</XView>}
+                    <SelectableText
+                        {...titleFont}
+                        color="var(--foregroundPrimary)"
+                        selectedColor="var(--foregroundInverted)"
+                    >
                         {title}
                     </SelectableText>
 
                     {!!subtitle && (
-                        <SelectableText {...subtitleFont} color="var(--foregroundTertiary)" selectedColor="var(--foregroundInverted)" marginLeft={8}>
+                        <SelectableText
+                            {...subtitleFont}
+                            color="var(--foregroundTertiary)"
+                            selectedColor="var(--foregroundInverted)"
+                            marginLeft={8}
+                        >
                             {subtitle}
                         </SelectableText>
                     )}
                 </XView>
 
                 {!!description && (
-                    <SelectableText {...descriptionFont} color={descriptionColor ? descriptionColor : 'var(--foregroundTertiary)'} selectedColor="var(--foregroundInverted)" textOverflow="ellipsis" height={descriptionFont.lineHeight} white-space="nowrap" overflow="hidden">
+                    <SelectableText
+                        {...descriptionFont}
+                        color={descriptionColor ? descriptionColor : 'var(--foregroundTertiary)'}
+                        selectedColor="var(--foregroundInverted)"
+                        textOverflow="ellipsis"
+                        height={descriptionFont.lineHeight}
+                        white-space="nowrap"
+                        overflow="hidden"
+                    >
                         {description}
                     </SelectableText>
                 )}
             </XView>
 
             {!!textRight && (
-                <SelectableText {...textRightFont} color="var(--foregroundTertiary)" selectedColor="var(--foregroundInverted)">
+                <SelectableText
+                    {...textRightFont}
+                    color="var(--foregroundTertiary)"
+                    selectedColor="var(--foregroundInverted)"
+                >
                     {textRight}
                 </SelectableText>
             )}
 
-            {!!rightElement && (
-                <XView marginRight={-8}>
-                    {rightElement}
-                </XView>
-            )}
+            {!!rightElement && <XView marginRight={-8}>{rightElement}</XView>}
         </>
     );
 
