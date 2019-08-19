e4cebc892a42dbf4ece0f53a9050ace8f02cd61e
Fred Morozov <morozov@fred.md>
2019-08-19 10:04:12 +0300

    fix(ios): reduce duration for longpress event (use Apple default value - 0.5 s)

--- ios/RNTouchableNode.swift
+++ ios/RNTouchableNode.swift
@@ -31,7 +31,7 @@ class RNTouchableNode: ASControlNode, UIGestureRecognizerDelegate {
     
     let longTap = UILongPressGestureRecognizer(target: self, action: #selector(self.longPressHandler))
     longTap.delegate = self
-    longTap.minimumPressDuration = 0.6
+    longTap.minimumPressDuration = 0.5
     view.addGestureRecognizer(longTap)
     let singleTap = UITapGestureRecognizer(target: self, action: #selector(self.handler))
     singleTap.require(toFail: longTap)
