a2b229d9a187a25db49e461f37d239175ec5b69d
Fred Morozov <morozov@fred.md>
2019-08-21 12:27:25 +0300

    fix(web): Change capitalization: BOT → bot

--- packages/openland-web/fragments/chat/messenger/message/MessageComponent.tsx
+++ packages/openland-web/fragments/chat/messenger/message/MessageComponent.tsx
@@ -114,7 +114,7 @@ interface MessageSenderContentProps {
 export const MessageSenderContent = (props: MessageSenderContentProps) => (
     <div className={senderContainer}>
         <MessageSenderName sender={props.sender} senderNameEmojify={props.senderNameEmojify} />
-        {props.sender.isBot && <div className={botBadgeStyle}>BOT</div>}
+        {props.sender.isBot && <div className={botBadgeStyle}>bot</div>}
         {props.senderBadgeNameEmojify && (
             <MessageSenderBadge senderBadgeNameEmojify={props.senderBadgeNameEmojify} />
         )}
