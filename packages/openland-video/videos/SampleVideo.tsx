b6f9fbaf829a5cd117d5a8753c0a7dafa12fcc85
Steve Korshakov <steve@korshakov.com>
2019-08-21 09:32:29 -0700

    feat(web): Improve video animations

--- packages/openland-video/videos/SampleVideo.tsx
+++ packages/openland-video/videos/SampleVideo.tsx
@@ -1,22 +1,14 @@
 import * as React from 'react';
 import { XView } from 'react-mental';
 import { Slide } from './components/Slide';
+import { useClampedTime } from './components/useClampedTime';
 
-const BaseSlide = (props: { index: number, children?: any }) => {
+const BaseSlide = (props: { index: number, children?: any, background?: string, color?: string }) => {
     return (
         <Slide
-            start={props.index}
-            duration={1}
+            start={props.index * 5}
+            duration={5}
         >
-            <img
-                src="https://ucarecdn.com/6ca08ef7-c5a1-425a-bc40-7b554a6d8b46/Backwhite.png"
-                style={{
-                    position: 'absolute',
-                    top: 0, bottom: 0, left: 0, right: 0,
-                    width: 375, height: 375,
-                    objectFit: 'cover'
-                }}
-            />
             <XView
                 position="absolute"
                 top={0}
@@ -30,6 +22,7 @@ const BaseSlide = (props: { index: number, children?: any }) => {
                 paddingBottom={32}
                 flexDirection="column"
                 justifyContent="flex-end"
+                color={props.color || 'black'}
             >
                 {props.children}
             </XView>
@@ -37,31 +30,98 @@ const BaseSlide = (props: { index: number, children?: any }) => {
     );
 };
 
-export const SampleVideoDuration = 5;
+const Background = React.memo(() => {
+    const entering = useClampedTime(0, 0.3);
+    const main = useClampedTime(5 - 0.3, 5);
+    const leaving = useClampedTime(5 * 5 - 0.3, 5 * 5);
+
+    return (
+        <>
+            <img
+                src={'https://ucarecdn.com/12f683de-3d25-45db-ae0d-e450a3d78ae7/'}
+                style={{
+                    position: 'absolute',
+                    top: 0, bottom: 0, left: 0, right: 0,
+                    width: 375, height: 375,
+                    objectFit: 'cover'
+                }}
+            />
+            <img
+                src={'https://ucarecdn.com/6ca08ef7-c5a1-425a-bc40-7b554a6d8b46/'}
+                style={{
+                    position: 'absolute',
+                    top: 0, bottom: 0, left: 0, right: 0,
+                    width: 375, height: 375,
+                    objectFit: 'cover',
+                    opacity: main
+                }}
+            />
+            <div
+                style={{
+                    position: 'absolute',
+                    top: -40, left: -40, right: 0,
+                    width: 170, height: 170,
+                    opacity: main,
+                    background: 'linear-gradient(214.05deg, #EDEDED 29.35%, #E2E2E2 93.66%)',
+                    borderRadius: 85
+                }}
+            />
+            <img
+                src={'https://ucarecdn.com/4703d1b4-ebe7-4567-a588-c85513c96619/'}
+                style={{
+                    position: 'absolute',
+                    top: 0, bottom: 0, left: 0, right: 0,
+                    width: 375, height: 375,
+                    objectFit: 'cover',
+                    opacity: leaving
+                }}
+            />
+        </>
+    );
+});
+
+export const SampleVideoDuration = 6 * 5;
 
 export const SampleVideo = React.memo(() => {
     return (
-        <XView width="100%" height="100%" backgroundColor="red">
-            <BaseSlide index={0}>
+        <XView width="100%" height="100%">
+            <Background />
+            <BaseSlide
+                index={0}
+                color="white"
+            >
+                Become friends <br />
+                with more founders
+            </BaseSlide>
+            <BaseSlide index={1}>
                 To raise a round <br />
                 you need 100+ intros
             </BaseSlide>
-            <BaseSlide index={1}>
+            <BaseSlide index={2}>
                 Most people will <br />
                 only make 1-2 intros
             </BaseSlide>
-            <BaseSlide index={2}>
+            <BaseSlide index={3}>
                 To win, you need <br />
                 50+ successful <br />
                 founders helping you
             </BaseSlide>
-            <BaseSlide index={3}>
+            <BaseSlide index={4}>
                 If your founder <br />
                 network is small, <br />
                 time to make new <br />
                 friends!
             </BaseSlide>
-            <BaseSlide index={4}>
+            <BaseSlide
+                index={5}
+                color="white"
+            >
+                <img
+                    src="https://ucarecdn.com/5595be37-0f00-4f44-afe7-5dfdc4bd2968/"
+                    style={{
+                        borderRadius: 20
+                    }}
+                />
                 Place where founders <br />
                 become friends
             </BaseSlide>
--- packages/openland-video/videos/components/Slide.tsx
+++ packages/openland-video/videos/components/Slide.tsx
@@ -7,9 +7,9 @@ export const Slide = React.memo((props: { children?: any, start: number, duratio
     // const time = useTime();
     // const visible = (time >= props.start - 0.3) && (time <= props.start + props.duration + 0.3);
     const startAnimation = Easings.standart(useClampedTime(props.start - 0.3, props.start));
-    // const endAnimation = useClampedTime(props.start + props.duration - 0.3, props.start + props.duration);
+    const endAnimation = useClampedTime(props.start + props.duration - 0.3, props.start + props.duration);
 
-    const offset = Math.floor((1 - startAnimation) * 100);
+    const offset = Math.floor((1 - startAnimation) * 100 - endAnimation * 100);
     // const offset = Math.floor((endAnimation) * 100);
 
     return (
@@ -18,7 +18,8 @@ export const Slide = React.memo((props: { children?: any, start: number, duratio
                 display: 'flex',
                 position: 'absolute',
                 top: 0, right: 0, left: 0, bottom: 0,
-                opacity: startAnimation,
+                opacity: startAnimation * (1 - endAnimation),
+                scale: startAnimation * (1 - endAnimation),
                 transform: `translateX(${offset}px)`
             }}
         >
