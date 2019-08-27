55767c1bac037db13a7f6af0609278feade38f2b
Steve Korshakov <steve@korshakov.com>
2019-08-26 18:39:39 -0700

    wip(videos): Working on react native renderer

--- packages/openland-mobile/videos/NativeRenderer.tsx
+++ packages/openland-mobile/videos/NativeRenderer.tsx
@@ -1,11 +1,37 @@
 import * as React from 'react';
 import { VideoRendererInt } from 'openland-video/videos/components/renderers';
-import { View } from 'react-native';
+import { Animated } from 'react-native';
+import { Animation, TimingAnimation, SequenceAnimation } from 'openland-video/videos/components/Animation';
 
+function convertAnimation(delay: number, value: Animated.Value, animation: Animation): Animated.CompositeAnimation {
+    if (animation instanceof TimingAnimation) {
+        return Animated.timing(value, {
+            toValue: animation._to,
+            delay: delay + animation._delay,
+        });
+    } else if (animation instanceof SequenceAnimation) {
+        return Animated.sequence(animation._animations.map((v) => convertAnimation(delay, value, v)));
+    } else {
+        throw Error();
+    }
+}
 export const NativeRenderer: VideoRendererInt = {
     renderView: (props) => {
+        const transform: any[] = [];
+        if (typeof props.translateY === 'object') {
+            const value = new Animated.Value(0);
+            const anim = convertAnimation(props.delay, value, props.translateY);
+            anim.start();
+            transform.push({ translateY: value });
+        }
+        if (typeof props.translateX === 'object') {
+            const value = new Animated.Value(0);
+            const anim = convertAnimation(props.delay, value, props.translateX);
+            anim.start();
+            transform.push({ translateX: value });
+        }
         return (
-            <View
+            <Animated.View
                 style={{
                     marginTop: props.marginTop,
                     marginBottom: props.marginBottom,
@@ -18,10 +44,11 @@ export const NativeRenderer: VideoRendererInt = {
                     width: props.width,
                     height: props.height,
                     backgroundColor: props.backgroundColor,
+                    transform
                 }}
             >
                 {props.children}
-            </View>
+            </Animated.View>
         );
     }
 };
