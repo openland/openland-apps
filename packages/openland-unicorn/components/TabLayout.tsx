215af2459d582100127f412df74f19beee4a89ba
Max Volkov <wolkow_1996@mail.ru>
2019-08-28 14:33:13 +0300

    fix(web): Vertical separator (between dialogs list and chat) color fix

--- packages/openland-unicorn/components/TabLayout.tsx
+++ packages/openland-unicorn/components/TabLayout.tsx
@@ -140,13 +140,13 @@ export const TabLayout = React.memo((props: { router: TabRouter }) => {
                                 {props.router.stacks.map((v, i) => (
                                     <div key={'tab-' + i} className={selectedMounted === i ? visibleContainer : invisibleContainer}>
                                         <XView width="100%" height="100%" flexDirection="row" overflow="hidden" paddingLeft={64}>
-                                            <XView key="sep1" width={1} backgroundColor="rgba(120, 128, 143, 0.08)" height="100%" />
+                                            <XView key="sep1" width={1} backgroundColor="#E7E8EA" height="100%" />
                                             <XView key="root" maxWidth={370} flexShrink={1} flexGrow={1} height="100%" flexDirection="column">
                                                 <XView width="100%" height="100%" position="relative" alignItems="flex-start" backgroundColor="#fff">
                                                     <TabContainer index={i} router={props.router} />
                                                 </XView>
                                             </XView>
-                                            <XView key="sep2" width={1} height="100%" backgroundColor="rgba(0, 0, 0, 0.08)" />
+                                            <XView key="sep2" width={1} height="100%" backgroundColor="#EBEBEB" />
                                             <StackLayout key="stack" className={containerDesktop} router={v} />
                                         </XView>
                                     </div>
