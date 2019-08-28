0d11a248c6d6d462c99daa57e7176ea35340b75a
Steve Korshakov <steve@korshakov.com>
2019-08-28 11:09:14 -0700

    fix(video): Bundle bezier because of importing issues

--- packages/openland-video/components/Easing.ts
+++ packages/openland-video/components/Easing.ts
@@ -1,4 +1,4 @@
-import BezierEasing from 'bezier-easing';
+import { bezier } from './util/bezier';
 
 export class Easing {
     readonly x1: number;
@@ -12,6 +12,6 @@ export class Easing {
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
-        this.interpolate = BezierEasing(x1, y1, x2, y2);
+        this.interpolate = bezier(x1, y1, x2, y2);
     }
 }
