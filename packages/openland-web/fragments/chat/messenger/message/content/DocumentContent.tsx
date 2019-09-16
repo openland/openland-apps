6d4ab69e11951d0ba96be221c4ef902e6fc50ebb
Max Volkov <wolkow_1996@mail.ru>
2019-09-16 16:13:43 +0300

    fix(web): file component

--- packages/openland-web/fragments/chat/messenger/message/content/DocumentContent.tsx
+++ packages/openland-web/fragments/chat/messenger/message/content/DocumentContent.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
 import { css, cx } from 'linaria';
 import { formatBytes } from 'openland-y-utils/formatBytes';
-import IconFile from 'openland-icons/ic-file.svg';
 import { TextDensed, TextLabel1 } from 'openland-web/utils/TextStyles';
 import { XLoader } from 'openland-x/XLoader';
+import { UIcon } from 'openland-web/components/unicorn/UIcon';
+import IcDownload from 'openland-icons/s/ic-down-24.svg';
+import IcBlue from 'openland-icons/files/blue.svg';
+import IcGreen from 'openland-icons/files/green.svg';
+import IcRed from 'openland-icons/files/red.svg';
+import IcViolet from 'openland-icons/files/violet.svg';
+import IcYellow from 'openland-icons/files/yellow.svg';
 
 const fileContainer = css`
     display: flex;
@@ -28,6 +34,26 @@ const fileContainer = css`
         text-decoration: none;
         background-color: var(--backgroundTertiaryHover);
     }
+    
+    & .icon-info .format-text {
+        transition: all 150ms;
+        transform: translateY(0);
+        opacity: 1;
+    }
+    
+    & .icon-info .download-icon {
+        transition: all 150ms;
+        transform: translateY(-20px);
+        opacity: 0;
+    }
+    &:hover .icon-info .download-icon {
+        opacity: 1;
+        transform: translateY(8px);
+    }
+    &:hover .icon-info .format-text {
+        opacity: 0;
+        transform: translateY(20px);
+    }
 `;
 
 const infoContent = css`
@@ -40,13 +66,30 @@ const iconContainer = css`
     position: relative;
     width: 40px;
     height: 40px;
-    border-radius: 40px;
-    background-color: #fff;
     display: flex;
     align-items: center;
     justify-content: center;
     flex-shrink: 0;
     margin-right: 12px;
+    & > svg {
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+`;
+
+const iconInfo = css`
+    position: relative;
+    flex-grow: 1;
+    flex-shrink: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: 10px;
+    font-weight: 600;
+    line-height: 12px;
+    color: #fff;
 `;
 
 const metadataContainer = css`
@@ -95,6 +138,39 @@ const VideoContent = React.memo(
     },
 );
 
+const fileIcon = {
+    FILE: <IcBlue />,
+    PDF: <IcRed />,
+    ZIP: <IcViolet />,
+    VIDEO: <IcViolet />,
+    DOCX: <IcBlue />,
+    XLSX: <IcGreen />,
+    PPTX: <IcYellow />,
+};
+
+const fileFormat = (name: string) => {
+    let format = 'FILE';
+    if (name.endsWith('.pdf')) {
+        format = 'PDF';
+    }
+    if (name.endsWith('.zip')) {
+        format = 'ZIP';
+    }
+    if (name.endsWith('.mp4') || name.endsWith('.mov')) {
+        format = 'VIDEO';
+    }
+    if (name.endsWith('.doc') || name.endsWith('.docx') || name.endsWith('.pages')) {
+        format = 'DOCX';
+    }
+    if (name.endsWith('.xls') || name.endsWith('.xlsx') || name.endsWith('.numbers')) {
+        format = 'XLSX';
+    }
+    if (name.endsWith('.ppt') || name.endsWith('.pptx') || name.endsWith('.key')) {
+        format = 'PPTX';
+    }
+    return format;
+};
+
 export const DocumentContent = React.memo(
     (props: {
         file: {
@@ -105,16 +181,13 @@ export const DocumentContent = React.memo(
         onClick?: (ev: React.MouseEvent) => void;
     }) => {
         const { file } = props;
-
+        const { name, size } = file.fileMetadata;
         if (
             file.fileMetadata.mimeType &&
-            (!!file.fileMetadata.mimeType.match('video') ||
-                !!file.fileMetadata.mimeType.match('mp4') ||
-                !!file.fileMetadata.name.match('.mp4'))
+            (!!file.fileMetadata.mimeType.match('video') || fileFormat(name) === 'VIDEO')
         ) {
             return <VideoContent file={props.file} />;
         }
-
         const onClick = React.useCallback((ev: React.MouseEvent) => {
             if (props.onClick) {
                 props.onClick(ev);
@@ -135,19 +208,18 @@ export const DocumentContent = React.memo(
             >
                 <div className={infoContent}>
                     <div className={iconContainer}>
-                        {file.uri ? (
-                            <XLoader size="small" transparentBackground={true} />
-                        ) : (
-                            <IconFile />
+                        {fileIcon[fileFormat(name)]}
+                        {file.uri && (
+                            <XLoader size="small" color="#fff" transparentBackground={true} />
                         )}
+                        <div className={cx(iconInfo, 'icon-info')}>
+                            <UIcon icon={<IcDownload/>} color="#fff" size={16} className="download-icon"/>
+                            <div className="format-text">{fileFormat(name)}</div>
+                        </div>
                     </div>
                     <div className={metadataContainer}>
-                        <div className={cx(title + ' title', TextLabel1)}>
-                            {file.fileMetadata.name}
-                        </div>
-                        <div className={cx(subtitle, TextDensed)}>
-                            {formatBytes(file.fileMetadata.size)}
-                        </div>
+                        <div className={cx(title + ' title', TextLabel1)}>{name}</div>
+                        <div className={cx(subtitle, TextDensed)}>{formatBytes(size)}</div>
                     </div>
                 </div>
             </a>
