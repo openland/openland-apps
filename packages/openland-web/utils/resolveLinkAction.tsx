c2fe25c5c73f7173bb377cb143b0fba98f246020
Max Volkov <wolkow_1996@mail.ru>
2019-09-16 20:04:27 +0300

    fix(web): Analytics: set up events for Billy Bot messages

--- packages/openland-web/utils/resolveLinkAction.tsx
+++ packages/openland-web/utils/resolveLinkAction.tsx
@@ -5,25 +5,38 @@ import { DiscoverOnLocalState } from 'openland-web/pages/onboarding/discover.pag
 import { showWriteFirstMessageModal } from 'openland-web/fragments/onboarding/showWriteFirstMessageModal';
 import { InviteFriendsComponent } from 'openland-web/fragments/account/SettingsInviteFriendsFragment';
 import { DownloadAppsComponent } from 'openland-web/fragments/account/SettingsDownloadAppsFragment';
+import { trackEvent } from 'openland-x-analytics';
 
 export const resolveLinkAction = (url: string) => {
     if (url === '/onboarding_invite') {
+        trackEvent('billy_bot_button_action', {
+            action_type: 'invite_friends'
+        });
         showModalBox({ fullScreen: true }, ctx => (
             <XScrollView3 flexGrow={1} flexShrink={1} useDefaultScroll>
                 <InviteFriendsComponent modalContext={ctx} />
             </XScrollView3>
         ));
     } else if (url === '/onboarding_apps') {
+        trackEvent('billy_bot_button_action', {
+            action_type: 'install_apps'
+        });
         showModalBox({ fullScreen: true }, () => (
             <XScrollView3 flexGrow={1} flexShrink={1} useDefaultScroll>
                 <DownloadAppsComponent />
             </XScrollView3>
         ));
     } else if (url === '/onboarding_discover') {
+        trackEvent('billy_bot_button_action', {
+            action_type: 'complete_chat_navigator'
+        });
         showModalBox({ fullScreen: true }, ctx => (
             <DiscoverOnLocalState fullHeight={true} onJoinChats={ctx.hide} />
         ));
     } else if (url === '/onboarding_send_first_message') {
+        trackEvent('billy_bot_button_action', {
+            action_type: 'write_first_message'
+        });
         showWriteFirstMessageModal();
     }
 };
