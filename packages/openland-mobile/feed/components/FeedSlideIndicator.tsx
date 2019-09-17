a807027948a6248d74706dea520d5576623bcce6
Fred Morozov <morozov@fred.md>
2019-09-17 17:41:38 +0300

    fix(mobile): add opacity for FeedSlideIndicator with media style

--- packages/openland-mobile/feed/components/FeedSlideIndicator.tsx
+++ packages/openland-mobile/feed/components/FeedSlideIndicator.tsx
@@ -25,10 +25,11 @@ export const FeedSlideIndicator = React.memo((props: FeedSlideIndicatorProps) =>
     const { current, items, style } = props;
 
     const color = style === 'default' ? theme.foregroundPrimary : theme.foregroundContrast;
+    const opacity = style === 'default' ? 1 : 0.56;
 
     return (
         <View style={styles.box}>
-            <Text style={[styles.text, { color }]} allowFontScaling={false}>
+            <Text style={[styles.text, { color, opacity }]} allowFontScaling={false}>
                 {current} / {items}
             </Text>
         </View>
