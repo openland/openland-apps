ed83cc550c10b5efb67d722c8867e47f950e88ad
Fred Morozov <morozov@fred.md>
2019-09-18 19:27:33 +0300

    fix(mobile): improve report form

--- packages/openland-mobile/components/showReportForm.tsx
+++ packages/openland-mobile/components/showReportForm.tsx
@@ -1,16 +1,31 @@
 import { ActionSheetBuilder } from './ActionSheet';
 import { useClient } from 'openland-mobile/utils/useClient';
+import { startLoader, stopLoader } from './ZGlobalLoader';
+import Toast from './Toast';
+
+const report = async (id: string, type: 'SPAM' | 'INAPPROPRIATE') => {
+    startLoader();
+
+    try {
+        await useClient().mutateReportContent({ contentId: id, type });
+
+        Toast.success({ text: 'Thank you for report', duration: 1000 }).show();
+    } catch (e) {
+        Toast.failure({ text: 'Something went wrong', duration: 1000 }).show();
+    } finally {
+        stopLoader();
+    }
+}
 
 export const showReportForm = (id: string) => {
-    const client = useClient();
     const builder = new ActionSheetBuilder();
 
     builder.action('It\'s Spam', () => {
-        client.mutateReportContent({ contentId: id, type: 'SPAM' });
+        report(id, 'SPAM');
     });
 
     builder.action('It\'s Inappropriate', () => {
-        client.mutateReportContent({ contentId: id, type: 'INAPPROPRIATE' });
+        report(id, 'INAPPROPRIATE');
     });
 
     builder.show();
