9f8420cdff5f025a872c2330bfefee1b94f43384
Fred Morozov <morozov@fred.md>
2019-09-19 06:44:34 +0300

    wip(ios): try to detect error after video select on real device

--- packages/openland-mobile/files/showAttachMenu.tsx
+++ packages/openland-mobile/files/showAttachMenu.tsx
@@ -1,5 +1,5 @@
 import { ActionSheetBuilder } from 'openland-mobile/components/ActionSheet';
-import { Platform } from 'react-native';
+import { Platform, Alert } from 'react-native';
 import { checkPermissions } from 'openland-mobile/utils/permissions/checkPermissions';
 import Picker from 'react-native-image-picker';
 import { handlePermissionDismiss } from 'openland-mobile/utils/permissions/handlePermissionDismiss';
@@ -65,7 +65,7 @@ export const showAttachMenu = (callback?: (type: 'document' | 'photo' | 'video',
                 },
                 (response) => {
                     if (response.error) {
-                        throw Error(response.error);
+                        Alert.alert(response.error);
 
                         handlePermissionDismiss('gallery');
                         return;
