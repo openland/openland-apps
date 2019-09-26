f55a3afc7cb8aa94a9a72eefaa57275c05a78f05
Max Volkov <wolkow_1996@mail.ru>
2019-09-26 15:29:14 +0300

    fix(web): kill unused files

--- packages/openland-web/fragments/group/components/GroupMemberMenu.tsx
+++ packages/openland-web/fragments/group/components/GroupMemberMenu.tsx
@@ -3,7 +3,6 @@ import { RoomFullWithoutMembers_SharedRoom, RoomMembersPaginated_members } from 
 import { UMoreButton } from 'openland-web/components/unicorn/templates/UMoreButton';
 import StarIcon from 'openland-icons/s/ic-star-24.svg';
 import LeaveIcon from 'openland-icons/s/ic-leave-24.svg';
-import { showMakeFeaturedModal } from 'openland-web/fragments/account/components/modals';
 import { MenuItem, UPopperMenuBuilder } from 'openland-web/components/unicorn/UPopperMenuBuilder';
 import { AppConfig } from 'openland-y-runtime/AppConfig';
 import { showLeaveChatConfirmation } from 'openland-web/fragments/account/components/groupProfileModals';
@@ -11,6 +10,105 @@ import { OpenlandClient } from 'openland-api/OpenlandClient';
 import { useClient } from 'openland-web/utils/useClient';
 import { UPopperController } from 'openland-web/components/unicorn/UPopper';
 import { AlertBlanketBuilder } from 'openland-x/AlertBlanket';
+import { XModalController } from 'openland-x/showModal';
+import { useForm } from 'openland-form/useForm';
+import { useField } from 'openland-form/useField';
+import { XView } from 'react-mental';
+import { XModalFooter } from 'openland-web/components/XModalFooter';
+import { InputField } from 'openland-web/components/InputField';
+import { XErrorMessage } from 'openland-x/XErrorMessage';
+import { XModalFooterButton } from 'openland-web/components/XModalFooterButton';
+import { XModalContent } from 'openland-web/components/XModalContent';
+import { XCheckbox } from 'openland-x/XCheckbox';
+import { showModalBox } from 'openland-x/showModalBox';
+
+const MakeFeaturedModal = (props: {
+    ctx: XModalController;
+    roomId: string;
+    userId: string;
+}) => {
+    const { ctx, roomId, userId } = props;
+    const client = useClient();
+    const form = useForm();
+    const userBadge = client.useSuperBadgeInRoom(
+        { roomId, userId },
+        { fetchPolicy: 'cache-and-network' },
+    ).superBadgeInRoom;
+    const [featured, setFeatured] = React.useState<boolean>(!!userBadge);
+    const descriptionField = useField('input.description', userBadge ? userBadge.name : '', form, [
+        {
+            checkIsValid: value => value.trim().length > 0,
+            text: "Description can't be empty",
+        },
+        {
+            checkIsValid: value => value.trim().length <= 40,
+            text: 'Max length: 40 characters',
+        },
+    ]);
+
+    const onSave = () => {
+        form.doAction(async () => {
+            if (featured) {
+                await client.mutateSuperBadgeCreateToRoom({
+                    name: descriptionField.value,
+                    userId,
+                    roomId,
+                });
+            } else {
+                if (userBadge) {
+                    await client.mutateSuperBadgeUnsetToRoom({
+                        userId,
+                        roomId,
+                        badgeId: userBadge.id,
+                    });
+                }
+            }
+
+            ctx.hide();
+        });
+    };
+
+    return (
+        <>
+            {form.error && <XErrorMessage message={form.error} />}
+            <XView flexDirection="column" borderRadius={8} overflow="hidden">
+                <XModalContent>
+                    <XCheckbox
+                        label={featured ? 'Featured' : 'Not featured'}
+                        checked={featured}
+                        onChange={() => setFeatured(!featured)}
+                        switcher={true}
+                    />
+                    {featured && (
+                        <XView marginTop={20}>
+                            <InputField
+                                title="Description"
+                                field={descriptionField}
+                                setFocusOnError={true}
+                                maxLength={40}
+                            />
+                        </XView>
+                    )}
+                </XModalContent>
+                <XModalFooter>
+                    <XModalFooterButton text="Cancel" style="ghost" onClick={() => ctx.hide()} />
+                    <XModalFooterButton text="Save" style="primary" onClick={onSave} />
+                </XModalFooter>
+            </XView>
+        </>
+    );
+};
+
+export const showMakeFeaturedModal = (roomId: string, userId: string) => {
+    showModalBox(
+        {
+            title: 'Member featuring',
+        },
+        ctx => {
+            return <MakeFeaturedModal ctx={ctx} userId={userId} roomId={roomId} />;
+        },
+    );
+};
 
 interface GroupMemberMenuProps {
     group: RoomFullWithoutMembers_SharedRoom;
