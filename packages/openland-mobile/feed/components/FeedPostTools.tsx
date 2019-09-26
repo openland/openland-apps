f5b58f9ed38561c183efa4e9a1031b7a5d1623ce
Fred Morozov <morozov@fred.md>
2019-09-26 12:26:28 +0300

    fix(mobile): improve likesCount in FeedItem screen

--- packages/openland-mobile/feed/components/FeedPostTools.tsx
+++ packages/openland-mobile/feed/components/FeedPostTools.tsx
@@ -38,15 +38,21 @@ export const FeedPostTools = React.memo((props: FeedPostToolsProps) => {
     const likesCount = likes.length ? likes[0].count : 0;
     const myLike = likes.length ? likes[0].my : false;
 
+    const handleLikeCountPress = React.useCallback(() => {
+        if (likesCount > 0) {
+            showReactionsList(reactions);
+        } else {
+            FeedHandlers.Like(item);
+        }
+    }, [item, likesCount, reactions]);
+
     return (
         <View style={styles.box}>
             <ZIconButton src={myLike ? require('assets/ic-like-filled-24.png') : require('assets/ic-like-24.png')} style={myLike ? 'danger' : 'default'} onPress={() => FeedHandlers.Like(item)} />
             <View style={styles.counterWrapper}>
-                {likesCount > 0 && (
-                    <Text style={[styles.counter, { color: myLike ? theme.accentNegative : theme.foregroundSecondary }]} allowFontScaling={false} onPress={() => showReactionsList(reactions)}>
-                        {plural(likesCount, ['like', 'likes'])}
-                    </Text>
-                )}
+                <Text style={[styles.counter, { color: myLike ? theme.accentNegative : theme.foregroundSecondary }]} allowFontScaling={false} onPress={handleLikeCountPress}>
+                    {likesCount > 0 ? plural(likesCount, ['like', 'likes']) : 'Like'}
+                </Text>
             </View>
             <ZIconButton src={require('assets/ic-forward-24.png')} onPress={() => FeedHandlers.Share(id)} />
             <ZIconButton src={require('assets/ic-more-24.png')} onPress={() => FeedHandlers.Manage(id, canEdit)} />
