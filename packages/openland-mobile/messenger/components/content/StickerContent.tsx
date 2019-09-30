01a716429f3d110e0ce955df8208771ba5571173
Fred Morozov <morozov@fred.md>
2019-09-30 12:59:09 +0300

    fix(mobile): increase sticker size

--- packages/openland-mobile/messenger/components/content/StickerContent.tsx
+++ packages/openland-mobile/messenger/components/content/StickerContent.tsx
@@ -6,6 +6,17 @@ import { DownloadManagerInstance } from 'openland-mobile/files/DownloadManager';
 import { ASImage } from 'react-native-async-view/ASImage';
 import { paddedText } from '../AsyncMessageContentView';
 import { DataSourceMessageItem } from 'openland-engines/messenger/ConversationEngine';
+import { PixelRatio } from 'react-native';
+
+export const STICKER_SIZE = 150;
+export const layoutSticker = () => {
+    const ratio = PixelRatio.get();
+
+    return {
+        width: Math.floor(STICKER_SIZE * ratio),
+        height: Math.floor(STICKER_SIZE * ratio)
+    };
+};
 
 interface StickerContentProps {
     sticker: MyStickers_stickers_packs_stickers;
@@ -18,15 +29,15 @@ export const StickerContent = React.memo<StickerContentProps>((props) => {
     const [downloadState, setDownloadState] = React.useState<DownloadState | undefined>(undefined);
 
     React.useEffect(() => {
-        DownloadManagerInstance.watch(sticker.image.uuid, { width: 200, height: 200 }, setDownloadState);
+        DownloadManagerInstance.watch(sticker.image.uuid, layoutSticker(), setDownloadState);
     }, [sticker]);
 
     return (
         <ASFlex flexDirection="column">
             <ASImage
                 source={{ uri: (downloadState && downloadState.path) ? ('file://' + downloadState.path) : undefined }}
-                width={100}
-                height={100}
+                width={STICKER_SIZE}
+                height={STICKER_SIZE}
             />
             {padded && paddedText(message.isOut)}
         </ASFlex>
