827f338f531117edc71bdc6528a4e4cc62265118
kor_ka <gleb.putintsev@openland.com>
2019-09-30 11:57:19 +0300

    fix(mobile): new messages divider layout

--- packages/openland-mobile/messenger/components/AsyncNewMessageDivider.tsx
+++ packages/openland-mobile/messenger/components/AsyncNewMessageDivider.tsx
@@ -4,20 +4,18 @@ import { ASText } from 'react-native-async-view/ASText';
 import { useThemeGlobal } from 'openland-mobile/themes/ThemeContext';
 import { Dimensions } from 'react-native';
 
-const screenWidth = Dimensions.get('screen').width;
-
 export const AsyncNewMessageDivider = React.memo((props) => {
     const theme = useThemeGlobal();
 
     return (
-        <ASFlex width={screenWidth} flexDirection="row" backgroundColor={theme.backgroundPrimary} marginLeft={12} marginRight={12} height={32}>
-            <ASFlex height={0.5} backgroundColor={theme.foregroundQuaternary} flexGrow={1} flexShrink={1} flexBasis={0} alignSelf="stretch" marginTop={8} />
+        <ASFlex alignSelf="stretch" alignItems="center" flexDirection="row" marginLeft={12} marginRight={12} height={32} marginTop={4}>
+            <ASFlex height={0.5} backgroundColor={theme.foregroundQuaternary} flexGrow={1} flexShrink={1} flexBasis={0} alignSelf="center" />
             <ASFlex backgroundColor={theme.backgroundPrimary}>
                 <ASText color={theme.foregroundSecondary} fontSize={13} marginLeft={12} marginRight={12}>
                     New messages
                 </ASText>
             </ASFlex>
-            <ASFlex height={0.5} backgroundColor={theme.foregroundQuaternary} flexGrow={1} flexShrink={1} flexBasis={0} alignSelf="stretch" marginTop={8} />
+            <ASFlex height={0.5} backgroundColor={theme.foregroundQuaternary} flexGrow={1} flexShrink={1} flexBasis={0} alignSelf="center" />
         </ASFlex>
     );
 });
