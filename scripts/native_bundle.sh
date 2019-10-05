05ceadd58e664df40f190b07f499db8e1d5bc7f4
Steve Korshakov <steve@korshakov.com>
2019-10-04 23:48:50 -0700

    fix(mobile): Disable manual bundling

--- scripts/native_bundle.sh
+++ scripts/native_bundle.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 yarn native:build
-rm -fr ./build/bundle-ios
-rm -fr ./build/bundle-android 
-mkdir -p ./build/bundle-ios
-mkdir -p ./build/bundle-ios/threads
-mkdir -p ./build/bundle-android
-mkdir -p ./build/bundle-android/threads
-./node_modules/.bin/react-native bundle --platform ios --entry-file index.js --bundle-output ./build/bundle-ios/main.jsbundle --assets-dest ./build/bundle-ios/ --dev false
-./node_modules/.bin/react-native bundle --platform android --entry-file index.js --bundle-output ./build/bundle-android/main.jsbundle --assets-dest ./build/bundle-android/ --dev false
+# rm -fr ./build/bundle-ios
+# rm -fr ./build/bundle-android 
+# mkdir -p ./build/bundle-ios
+# mkdir -p ./build/bundle-ios/threads
+# mkdir -p ./build/bundle-android
+# mkdir -p ./build/bundle-android/threads
+# ./node_modules/.bin/react-native bundle --platform ios --entry-file index.js --bundle-output ./build/bundle-ios/main.jsbundle --assets-dest ./build/bundle-ios/ --dev false
+# ./node_modules/.bin/react-native bundle --platform android --entry-file index.js --bundle-output ./build/bundle-android/main.jsbundle --assets-dest ./build/bundle-android/ --dev false
