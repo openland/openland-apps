63c2985818099e876fe3fe6446de5c63403491df
Steve Korshakov <steve@korshakov.com>
2019-10-13 13:21:15 -0700

    fix(*): Fix build

--- packages/openland-mobile/components/AndroidSplashView.tsx
+++ packages/openland-mobile/components/AndroidSplashView.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
-import { requireNativeComponent, Platform, StyleProp, ViewStyle } from 'react-native';
+import { requireNativeComponent, StyleProp, ViewStyle } from 'react-native';
 
 export const AndroidSplashView = requireNativeComponent<{ style?: StyleProp<ViewStyle> }>('AndroidSplashView');
