af910600ae090c8ea3fe7e0230c14c4269f6d9b2
Max Volkov <wolkow_1996@mail.ru>
2019-10-14 18:10:10 +0300

    fix(web): member profile component

--- packages/openland-web/fragments/group/MemberProfileFragment.tsx
+++ packages/openland-web/fragments/group/MemberProfileFragment.tsx
@@ -7,35 +7,41 @@ import { UListGroup } from 'openland-web/components/unicorn/UListGroup';
 import { useUnicorn } from 'openland-unicorn/useUnicorn';
 import { UButton } from 'openland-web/components/unicorn/UButton';
 import { UListText } from 'openland-web/components/unicorn/UListText';
+import { UserInfoContext } from 'openland-web/components/UserInfo';
 
 export const MemberProfileFragment = React.memo(() => {
     const client = useClient();
     const unicorn = useUnicorn();
+    const userContext = React.useContext(UserInfoContext)!!;
     const chatId = unicorn && unicorn.query.id;
-    const data = client.useMatchmakingRoom({ peerId: chatId }).matchmakingRoom;
+    const userId = unicorn && unicorn.query.uid;
+    const userProfile = client.useMatchmakingProfile({ peerId: chatId, uid: userId }).matchmakingProfile;
 
-    const myProfile = data && data.myProfile && data.myProfile.user;
-    const myAnswers = data && data.myProfile && data.myProfile.answers;
-
-    if (!myProfile || !myAnswers) {
+    if (!userProfile || !userContext.user) {
         return null;
     }
 
-    const { photo, id, name } = myProfile;
+    const isMyProfile = userProfile.user.id === userContext.user.id;
+
+    const userAnswers = userProfile.answers;
+
+    const { photo, id, name, primaryOrganization } = userProfile.user;
+
+    const headerText = isMyProfile ? 'My member profile' : 'Details';
+    const buttonPath = isMyProfile ? `/group/${chatId}/myprofile/edit` : `/mail/${userId}`;
+    const buttonText = isMyProfile ? 'Edit' : 'Message';
 
     return (
-        <Page track="my_member_profile" padded={true}>
-            <UHeader title="My member profile" />
+        <Page track="member_profile" padded={true}>
+            <UHeader title={headerText} />
             <UListHero
-                title={myProfile.name}
-                description={
-                    myProfile.primaryOrganization ? myProfile.primaryOrganization.name : undefined
-                }
+                title={name}
+                description={primaryOrganization ? primaryOrganization.name : undefined}
                 avatar={{ photo, id, title: name }}
             >
-                <UButton text="Edit" path={`/group/${chatId}/myprofile/edit`} />
+                <UButton text={buttonText} path={buttonPath} />
             </UListHero>
-            {myAnswers.map(i => (
+            {userAnswers.map(i => (
                 <UListGroup header={i.question.title} key={i.question.id}>
                     <UListText
                         value={
