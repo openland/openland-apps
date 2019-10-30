de1d1f7d8e441f048da3bc38d9c174c307149922
Max Volkov <wolkow_1996@mail.ru>
2019-10-30 20:13:50 +0300

    fix(web): Account Design Fixes

--- packages/openland-web/fragments/account/components/FormFooter.tsx
+++ packages/openland-web/fragments/account/components/FormFooter.tsx
@@ -2,5 +2,5 @@ import * as React from 'react';
 import { XView } from 'react-mental';
 
 export const FormFooter = ({ children }: { children: any }) => {
-    return <XView marginTop={8}>{children}</XView>;
+    return <XView marginTop={16}>{children}</XView>;
 };
