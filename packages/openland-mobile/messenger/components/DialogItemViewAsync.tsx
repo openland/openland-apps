509cdc359eed646006660fb1bc04a4712b3fc8a3
metreniuk <metreniuk@gmail.com>
2019-11-18 17:02:56 +0300

    fix(mobile): Adjust margins for secret chat icon

--- packages/openland-mobile/messenger/components/DialogItemViewAsync.tsx
+++ packages/openland-mobile/messenger/components/DialogItemViewAsync.tsx
@@ -60,7 +60,7 @@ const DialogItemViewAsyncRender = React.memo<{ theme: ThemeGlobal, item: DialogD
             </ASFlex>
             <ASFlex marginLeft={paddingHorizontal} marginRight={paddingHorizontal} marginTop={6} marginBottom={6} flexDirection="column" flexGrow={1} flexBasis={0} alignItems="stretch">
                 <ASFlex height={24} alignItems="center">
-                    {isGroup && <ASFlex alignItems="center" marginRight={2} marginTop={4}><ASImage opacity={CompensationAlpha} tintColor={theme.accentPositive} width={16} height={16} source={require('assets/ic-lock-16.png')} marginBottom={Platform.OS === 'android' ? 4 : 0} /></ASFlex>}
+                    {isGroup && <ASFlex alignItems="center" marginRight={4} marginTop={4}><ASImage opacity={CompensationAlpha} tintColor={theme.accentPositive} width={16} height={16} source={require('assets/ic-lock-16.png')} marginBottom={Platform.OS === 'android' ? 4 : 0} /></ASFlex>}
                     <ASText {...TextStylesAsync.Label1} color={isGroup ? theme.accentPositive : theme.foregroundPrimary} flexGrow={1} flexBasis={0}>{item.title}</ASText>
                     {item.date !== undefined && <ASText {...TextStylesAsync.Caption} marginLeft={10} color={theme.foregroundTertiary} marginTop={2}>{formatDate(item.date)}</ASText>}
                 </ASFlex>
--- packages/openland-mobile/pages/main/components/ChatHeader.tsx
+++ packages/openland-mobile/pages/main/components/ChatHeader.tsx
@@ -46,7 +46,7 @@ const SharedChatHeaderContent = XMemo<{ room: Room_room_SharedRoom, typing?: str
     return (
         <View flexDirection="column" alignItems="flex-start" alignSelf="center" justifyContent="center" pointerEvents="box-none" height={44} minWidth={0} flexBasis={0} flexShrink={1} flexGrow={1}>
             <View flexDirection="row">
-                {room.kind === 'GROUP' && (<View height={18} alignItems="center" justifyContent="center" marginRight={2}><Image opacity={CompensationAlpha} source={require('assets/ic-lock-16.png')} style={{ tintColor: theme.accentPositive, marginTop: 3 }} /></View>)}
+                {room.kind === 'GROUP' && (<View height={18} alignItems="center" justifyContent="center" marginRight={4}><Image opacity={CompensationAlpha} source={require('assets/ic-lock-16.png')} style={{ tintColor: theme.accentPositive, marginTop: 3 }} /></View>)}
                 <Text style={[styles.title, { color: theme.foregroundPrimary }, room.kind === 'GROUP' && { color: theme.accentPositive }]} numberOfLines={1} ellipsizeMode="tail" allowFontScaling={false}>{title}</Text>
             </View>
             <Text style={[styles.subTitle, { color: accent ? theme.accentPrimary : theme.foregroundSecondary }]} numberOfLines={1} ellipsizeMode="tail" allowFontScaling={false}>
