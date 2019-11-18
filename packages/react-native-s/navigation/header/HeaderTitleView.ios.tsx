bef59e1fdca4c3883faeffd13107b4c38ecd4ddb
metreniuk <metreniuk@gmail.com>
2019-11-18 16:21:43 +0300

    fix(mobile): Make search interactive on touch

--- packages/react-native-s/navigation/header/HeaderTitleView.ios.tsx
+++ packages/react-native-s/navigation/header/HeaderTitleView.ios.tsx
@@ -2,13 +2,14 @@ import * as React from 'react';
 import { HeaderPage } from './HeaderPage';
 import { SAnimated } from '../../SAnimated';
 import { SDevice } from '../../SDevice';
-import { View, Text, TouchableWithoutFeedback, Image, TextInput, StyleSheet, TextStyle, Dimensions, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableHighlight, Image, TextInput, StyleSheet, TextStyle, Dimensions, TouchableOpacity } from 'react-native';
 import { SBackButton } from '../../SBackButton';
 import { NavigationManager } from '../NavigationManager';
 import { SNavigationViewStyle } from '../../SNavigationView';
 import { SEquisiteCentered } from '../../SExquisiteCentered';
 import { SCloseButton } from '../../SCloseButton';
 import { TextStyles, RadiusStyles } from 'openland-mobile/styles/AppStyles';
+import { ThemeContext } from 'openland-mobile/themes/ThemeContext';
 
 const styles = StyleSheet.create({
     title: {
@@ -27,7 +28,11 @@ const styles = StyleSheet.create({
         textAlignVertical: 'center',
         paddingLeft: 16,
         paddingRight: 16
-    } as TextStyle
+    } as TextStyle,
+    passiveSearchInput: {
+        flex: 1,
+        borderRadius: RadiusStyles.Large
+    }
 });
 
 const MAX_SIZE = Math.max(Dimensions.get('window').height, Dimensions.get('window').width);
@@ -115,12 +120,16 @@ export class HeaderTitleView extends React.PureComponent<HeaderTitleViewProps, {
                                 <View style={{ flexDirection: 'row', height: 36, marginLeft: 16, marginRight: 16, alignItems: 'center' }}>
                                     <SAnimated.View name={'header-search-input--' + v.page.key} style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, backgroundColor: this.props.style.searchBackground, height: 36, borderRadius: RadiusStyles.Large }} />
                                     {!v.config.searchActive && (
-                                        <TouchableWithoutFeedback onPress={v.config.searchPress}>
-                                            <View style={{ flexDirection: 'row', height: 36, alignItems: 'center', flexGrow: 1 }}>
-                                                <Image source={require('assets/ic-search-16.png')} style={{ width: 16, height: 16, marginLeft: 12, marginRight: 8, tintColor: this.props.style.searchColor }} />
-                                                <Text style={{ fontSize: 17, marginTop: -1, color: this.props.style.searchColor, lineHeight: 22 }} allowFontScaling={false}>Search</Text>
-                                            </View>
-                                        </TouchableWithoutFeedback>
+                                        <ThemeContext.Consumer>
+                                            {theme => (
+                                                <TouchableHighlight style={styles.passiveSearchInput} underlayColor={theme.backgroundTertiaryActive} delayPressIn={0} onPress={v.config.searchPress}>
+                                                    <View style={{ flexDirection: 'row', height: 36, alignItems: 'center', flexGrow: 1 }}>
+                                                        <Image source={require('assets/ic-search-16.png')} style={{ width: 16, height: 16, marginLeft: 12, marginRight: 8, tintColor: this.props.style.searchColor }} />
+                                                        <Text style={{ fontSize: 17, marginTop: -1, color: this.props.style.searchColor, lineHeight: 22 }} allowFontScaling={false}>Search</Text>
+                                                    </View>
+                                                </TouchableHighlight>
+                                            )}
+                                        </ThemeContext.Consumer>
                                     )}
                                     {v.config.searchActive && (
                                         <View style={{ flexDirection: 'row', height: 36, alignItems: 'center', flexGrow: 1, marginRight: 70 }}>
