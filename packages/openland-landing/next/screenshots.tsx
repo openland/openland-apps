a2037d878b10db0ed4e0f5386255e31465c6b117
uyouthe <melvoloskov@ya.ru>
2019-11-27 14:22:53 +0300

    wip(web): responsive screenshots

--- packages/openland-landing/next/screenshots.tsx
+++ packages/openland-landing/next/screenshots.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { css } from 'linaria';
+import { css, cx } from 'linaria';
 import Block from './block';
 import Heading from './heading';
 
@@ -10,16 +10,33 @@ const header = css`
     align-items: center;
 `;
 
-const content = css`
-    margin-left: 32px;
-`;
+const content = css``;
 
 const listWrapper = css`
     margin-top: 11px;
+
+    @media (min-width: 1140px) {
+        font-size: 26px;
+        line-height: 38px;
+    }
+
+    @media (min-width: 960px) and (max-width: 1140px) {
+        font-size: 22px;
+        line-height: 35px;
+    }
+
+    @media (min-width: 768px) and (max-width: 960px) {
+        font-size: 22px;
+        line-height: 35px;
+    }
+
+    @media (max-width: 768px) {
+        font-size: 18px;
+        line-height: 30px;
+    }
 `;
 
 const list = css`
-    font-size: 26px;
     list-style-type: none;
     display: inline;
 `;
@@ -36,7 +53,7 @@ const item = css`
 `;
 
 const link = css`
-    font-size: 26px;
+    font-size: inherit;
     color: inherit;
     font-weight: bold;
 
@@ -45,18 +62,60 @@ const link = css`
         text-decoration: none;
         color: inherit;
     }
+
+    @media (max-width: 768px) {
+        display: block;
+    }
 `;
 
 const screenshot = css`
+    width: 100%;
+    max-width: 1140px;
+    height: auto;
     margin-top: 54px;
     filter: drop-shadow(0px 13px 55px rgba(20, 64, 86, 0.05));
+    @media (max-width: 768px) {
+        display: none;
+    }
+`;
+
+const hide = css`
+    @media (min-width: 768px) and (max-width: 960px) {
+        display: none;
+    }
+`;
+
+const logo = css`
+    margin-right: 32px;
+
+    @media (max-width: 768px) {
+        display: none;
+    }
+`;
+
+const screenshotMobile = css`
+    display: none;
+    width: 100%;
+    max-width: 560px;
+    height: auto;
+    margin-top: 54px;
+    margin-left: auto;
+    margin-right: auto;
+    @media (max-width: 768px) {
+        display: block;
+    }
 `;
 
 export default () => (
     <div className={root}>
         <Block>
             <div className={header}>
-                <img src="/static/landing/logo-screenshots.svg" width="100" height="100" />
+                <img
+                    src="/static/landing/logo-screenshots.svg"
+                    width="100"
+                    height="100"
+                    className={logo}
+                />
                 <div className={content}>
                     <Heading>All-in-one platform you'll love</Heading>
                     <div className={listWrapper}>
@@ -64,8 +123,9 @@ export default () => (
                             <ul className={item}>Member profiles</ul>
                             <ul className={item}>Live sessions</ul>
                             <ul className={item}>Chats</ul>
-                            <ul className={item}>Ultra-fast apps.</ul>
-                        </li>{' '}
+                            <ul className={cx(item, hide)}>Ultra-fast apps</ul>
+                        </li>
+                        {'. '}
                         <a href="#" className={link}>
                             All features
                         </a>
@@ -77,6 +137,12 @@ export default () => (
                 className={screenshot}
                 width="1140"
                 height="667"
+            />
+            <img
+                src="/static/landing/screenshots-mobile.png"
+                className={screenshotMobile}
+                width="560"
+                height="1188"
             />
         </Block>
     </div>
