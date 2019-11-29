a87abdcda094981dc7e5b900056b2ae36396a115
uyouthe <melvoloskov@ya.ru>
2019-11-29 11:54:53 +0300

    wip(web): mobile header menu

--- packages/openland-landing/next/header.tsx
+++ packages/openland-landing/next/header.tsx
@@ -51,7 +51,8 @@ const menu = css`
     @media (min-width: 768px) and (max-width: 960px) {
     }
 
-    @media (max-width: 768px) {
+    @media (max-width: 767px) {
+        display: none;
     }
     z-index: 10;
 `;
@@ -178,14 +179,199 @@ const popupCloser = css`
     left: 0;
 `;
 
+const mobileMenu = css`
+    z-index: 20;
+`;
+
+const mobileMenuTrigger = css`
+    cursor: pointer;
+    display: none;
+
+    @media (max-width: 767px) {
+        display: initial;
+    }
+`;
+
+const mobileMenuInner = css`
+    position: fixed;
+    background-color: white;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+`;
+
+const mobileMenuHeader = css`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px;
+    padding-top: 22px;
+`;
+
+const mobileMenuClose = css`
+    cursor: pointer;
+`;
+
+const mobileMenuList = css`
+    margin-top: 16px;
+`;
+
+const mobileMenuItem = css`
+    padding: 0 20px;
+    line-height: 2.4;
+    font-size: 20px;
+`;
+
+const mobileMenuLink = css`
+    color: #272750;
+    &:hover,
+    &:active {
+        text-decoration: none;
+    }
+`;
+
+const mobileMenuFooter = css`
+    position: absolute;
+    bottom: 0;
+`;
+
+const blue = css`
+    &,
+    &:hover,
+    &:active {
+        color: #248bf2;
+    }
+`;
+
+const appsHeading = css`
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 17px;
+    color: #9393a7;
+    margin-left: 20px;
+`;
+
+const apps = css`
+    display: grid;
+    grid-template-columns: auto auto;
+    grid-gap: 14px;
+    margin: 12px 20px;
+`;
+
+const appsLink = css``;
+
+const mobileMenuOpenIcon = css``;
+const mobileMenuCloseIcon = css``;
+
 export default ({ isGrey }: { isGrey?: boolean }) => {
     const [isOpen, setOpen] = React.useState<boolean>(false);
+    const [isMenuOpen, setMenuOpen] = React.useState<boolean>(false);
 
     return (
         <div className={isGrey ? grey : root}>
             <Block>
                 <div className={header}>
                     <img className={logo} src="/static/landing/logo.svg" width="155" height="48" />
+
+                    <div className={mobileMenu}>
+                        <span className={mobileMenuTrigger} onClick={() => setMenuOpen(true)}>
+                            <img
+                                src="/static/landing/icons/open.svg"
+                                className={mobileMenuOpenIcon}
+                                width="24"
+                                height="24"
+                                alt="Open menu"
+                            />
+                        </span>
+                        {isMenuOpen && (
+                            <div className={mobileMenuInner}>
+                                <div className={mobileMenuHeader}>
+                                    <img
+                                        className={logo}
+                                        src="/static/landing/logo.svg"
+                                        width="155"
+                                        height="48"
+                                    />
+                                    <span
+                                        className={mobileMenuClose}
+                                        onClick={() => setMenuOpen(false)}
+                                    >
+                                        <img
+                                            src="/static/landing/icons/close.svg"
+                                            className={mobileMenuCloseIcon}
+                                            width="24"
+                                            height="24"
+                                            alt="Close menu"
+                                        />
+                                    </span>
+                                </div>
+
+                                <ul className={mobileMenuList}>
+                                    <li className={mobileMenuItem}>
+                                        <a href="#" className={mobileMenuLink}>
+                                            Discover
+                                        </a>
+                                    </li>
+                                    <li className={mobileMenuItem}>
+                                        <a href="#" className={mobileMenuLink}>
+                                            Start community
+                                        </a>
+                                    </li>
+                                    <li className={mobileMenuItem}>
+                                        <a href="#" className={mobileMenuLink}>
+                                            About
+                                        </a>
+                                    </li>
+                                    <li className={mobileMenuItem}>
+                                        <a href="#" className={mobileMenuLink}>
+                                            Careers
+                                        </a>
+                                    </li>
+                                    <li className={mobileMenuItem}>
+                                        <a href="#" className={mobileMenuLink}>
+                                            Chat with us
+                                        </a>
+                                    </li>
+                                </ul>
+
+                                <ul className={mobileMenuList}>
+                                    <li className={mobileMenuItem}>
+                                        <a href="#" className={cx(mobileMenuLink, blue)}>
+                                            Sign up
+                                        </a>
+                                    </li>
+                                    <li className={mobileMenuItem}>
+                                        <a href="#" className={mobileMenuLink}>
+                                            Login
+                                        </a>
+                                    </li>
+                                </ul>
+
+                                <div className={mobileMenuFooter}>
+                                    <h2 className={appsHeading}>Install the app</h2>
+
+                                    <div className={apps}>
+                                        <a href="#" className={appsLink}>
+                                            <img
+                                                src="/static/landing/apps-ios.svg"
+                                                width="120"
+                                                height="40"
+                                            />
+                                        </a>
+                                        <a href="#" className={appsLink}>
+                                            <img
+                                                src="/static/landing/apps-android.svg"
+                                                width="130"
+                                                height="40"
+                                            />
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+                    </div>
 
                     <div className={menu}>
                         <a className={menuItem} href="#">
