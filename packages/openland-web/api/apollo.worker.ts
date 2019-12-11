917e816f080e447c25382e0225ed9d7674d36559
Steve Korshakov <steve@korshakov.com>
2019-12-10 16:35:52 -0800

    feat(web): Enable SpaceX

--- packages/openland-web/api/apollo.worker.ts
+++ packages/openland-web/api/apollo.worker.ts
@@ -1,3 +1,5 @@
+import { Operations } from './../../openland-api/Client';
+import { SpaceXWebClient } from './../../openland-graphql/spacex/SpaceXWebClient';
 import { disableTag, disableAll } from 'mental-log';
 disableAll();
 disableTag('GraphQL-Direct');
@@ -22,19 +24,19 @@ const initHandler = (ev: MessageEvent) => {
 
     ctx.removeEventListener('message', initHandler);
 
-    let token = msg.token;
-    let client = buildClient({
-        token: token,
-        endpoint: msg.endpoint,
-        wsEndpoint: msg.wsEndpoint,
-    });
+    // let token = msg.token;
+    // // let client = buildClient({
+    // //     token: token,
+    // //     endpoint: msg.endpoint,
+    // //     wsEndpoint: msg.wsEndpoint,
+    // // });
 
     let workerInterface: WorkerInterface = {
         post: src => ctx.postMessage(src),
         setHandler: handler => ctx.addEventListener('message', (src: any) => handler(src.data)),
     };
 
-    host = new WorkerApolloHost(new DirectApollolClient(client), workerInterface);
+    host = new WorkerApolloHost(new SpaceXWebClient(Operations, msg.wsEndpoint, msg.token), workerInterface);
 };
 
 ctx.addEventListener('message', initHandler);
