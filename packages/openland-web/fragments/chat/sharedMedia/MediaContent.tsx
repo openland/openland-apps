fdaa4585b012f1836cc41b679fc5e2015088f9f9
kor_ka <kor_ka@openland.com>
2019-12-12 15:20:26 +0300

    feat(web): shared media images blured preview

--- packages/openland-web/fragments/chat/sharedMedia/MediaContent.tsx
+++ packages/openland-web/fragments/chat/sharedMedia/MediaContent.tsx
@@ -4,11 +4,13 @@ import { SharedItemFile } from './SharedMediaFragment';
 import { showImageModal } from '../messenger/message/content/ImageContent';
 import { layoutMedia, uploadcareOptions } from 'openland-y-utils/MediaLayout';
 import { emoji } from 'openland-y-utils/emoji';
+import { ImgWithRetry } from 'openland-web/components/ImgWithRetry';
 
 const MediaItemClass = css`
 display: flex;
 width: 25%;    
 position: relative;
+overflow: hidden;
 @media (max-width: 750px) {
     width: calc(100% / 3);    
 }
@@ -20,15 +22,41 @@ position: relative;
 }
 `;
 const MediaItemContentClass = css`
-position: absolute;
-top:1px;
-left: 1px;
+    position: absolute;
+    top:1px;
+    left: 1px;
 
-display: block;
-width: calc(100% - 2px);
+    display: block;
+    width: calc(100% - 2px);
+    opacity: 0;
+    transition: opacity 150ms cubic-bezier(0.4, 0, 0.2, 1);
+    will-change: opacity;
+`;
+
+const ImgPreviewClass = css`
+    position: absolute;
+    top: -5px;
+    left: -5px;
+    width: calc(100% + 10px);
+    height: calc(100% + 10px);
+    filter: blur(5px);
+`;
+
+const ImgPreviewContainerClass = css`
+    position: absolute;
+    top: 1px;
+    left: 1px;
+    bottom: 1px;
+    right: 1px;
+
+    overflow: hidden;
+
+    transition: opacity 150ms cubic-bezier(0.4, 0, 0.2, 1);
+    will-change: opacity;
 `;
 export const MediaContent = (props: { item: SharedItemFile }) => {
-
+    const imgRef = React.useRef<HTMLImageElement>(null);
+    const placeholderRef = React.useRef<HTMLDivElement>(null);
     const onClick = React.useCallback(() => {
 
         const layoutModal = layoutMedia(
@@ -54,9 +82,28 @@ export const MediaContent = (props: { item: SharedItemFile }) => {
             date: parseInt(props.item.dateRaw, 10),
         });
     }, []);
+
+    const onLoad = React.useCallback(() => {
+        if (imgRef.current && placeholderRef.current) {
+            imgRef.current.style.opacity = "1";
+            placeholderRef.current.style.opacity = "0";
+        }
+    }, []);
     return (
         <div className={MediaItemClass} onClick={onClick}>
-            <img src={`https://ucarecdn.com/${props.item.attach.fileId}/-/format/auto/-/scale_crop/138x138/smart/`} className={MediaItemContentClass} />
+            <div className={ImgPreviewContainerClass} ref={placeholderRef}>
+                <ImgWithRetry
+                    className={ImgPreviewClass}
+                    src={props.item.attach.filePreview || undefined}
+                />
+            </div>
+
+            <ImgWithRetry
+                ref={imgRef}
+                className={MediaItemContentClass}
+                src={`https://ucarecdn.com/${props.item.attach.fileId}/-/format/auto/-/scale_crop/138x138/smart/`}
+                onLoad={onLoad}
+            />
         </div>
     );
 };
