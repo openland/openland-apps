bd44200a5e6093ca0f8c5ae0e5e824916c342da0
Steve Korshakov <steve@korshakov.com>
2019-12-23 06:49:30 -0800

    feat(android): Use Thrusted socket

--- android/app/src/main/java/com/openland/spacex/transport/net/DispatchedWebSocketClient.kt
+++ android/app/src/main/java/com/openland/spacex/transport/net/DispatchedWebSocketClient.kt
@@ -1,9 +1,13 @@
 package com.openland.spacex.transport.net
 
 import com.openland.spacex.utils.DispatchQueue
+import com.openland.spacex.utils.xLog
 import okhttp3.*
 
 class DispatchedWebSocketClient(val url: String, val queue: DispatchQueue) {
+    companion object {
+        val client = OkHttpClient()
+    }
 
     var onOpen: (() -> Unit)? = null
     var onClose: (() -> Unit)? = null
@@ -13,10 +17,10 @@ class DispatchedWebSocketClient(val url: String, val queue: DispatchQueue) {
     private var socket: WebSocket? = null
     private var isClosed = false
     private var isStarted = false
-    private val client = OkHttpClient()
 
     val callback = object : WebSocketListener() {
         override fun onOpen(webSocket: WebSocket, response: Response?) {
+            xLog("SpaceX-Apollo", "Socket:onOpen")
             queue.async {
                 if (!this@DispatchedWebSocketClient.isClosed) {
                     this@DispatchedWebSocketClient.isStarted = true
@@ -26,6 +30,7 @@ class DispatchedWebSocketClient(val url: String, val queue: DispatchQueue) {
         }
 
         override fun onMessage(webSocket: WebSocket, text: String) {
+            xLog("SpaceX-Apollo", "Socket:onMessage")
             queue.async {
                 if (!this@DispatchedWebSocketClient.isClosed && this@DispatchedWebSocketClient.isStarted) {
                     this@DispatchedWebSocketClient.onMessage?.invoke(text)
@@ -34,6 +39,18 @@ class DispatchedWebSocketClient(val url: String, val queue: DispatchQueue) {
         }
 
         override fun onClosed(webSocket: WebSocket, code: Int, reason: String?) {
+            xLog("SpaceX-Apollo", "Socket:onClosed")
+            queue.async {
+                if (!this@DispatchedWebSocketClient.isClosed) {
+                    this@DispatchedWebSocketClient.isClosed = true
+                    this@DispatchedWebSocketClient.socket = null
+                    this@DispatchedWebSocketClient.onClose?.invoke()
+                }
+            }
+        }
+
+        override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+            xLog("SpaceX-Apollo", "Socket:onFailure")
             queue.async {
                 if (!this@DispatchedWebSocketClient.isClosed) {
                     this@DispatchedWebSocketClient.isClosed = true
@@ -49,27 +66,27 @@ class DispatchedWebSocketClient(val url: String, val queue: DispatchQueue) {
                 .addHeader("Sec-WebSocket-Protocol", "graphql-ws")
                 .addHeader("Cookie", "")
                 .build()
-        this.socket = client.newWebSocket(request, callback)
+        this.socket = DispatchedWebSocketClient.client.newWebSocket(request, callback)
     }
 
     fun post(text: String) {
-        this.queue.async {
-            if (!this.isClosed) {
-                if (!this@DispatchedWebSocketClient.isStarted) {
-                    throw Error("Socket is not started yet!")
-                }
-                this.socket!!.send(text)
+        this.queue.requreQueue()
+
+        if (!this.isClosed) {
+            if (!this@DispatchedWebSocketClient.isStarted) {
+                throw Error("Socket is not started yet!")
             }
+            this.socket!!.send(text)
         }
     }
 
     fun close() {
-        this.queue.async {
-            if (!this.isClosed) {
-                this.isClosed = true
-                this.socket!!.close(1000, null)
-                this.socket = null
-            }
+        this.queue.requreQueue()
+
+        if (!this.isClosed) {
+            this.isClosed = true
+            this.socket!!.close(1000, null)
+            this.socket = null
         }
     }
 }
