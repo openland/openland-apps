63d46fb6161c5eed1a83990f56523e6d65a7058a
uyouthe <melvoloskov@ya.ru>
2019-12-26 12:03:35 +0300

    wip(mobile): about page gradient

--- packages/openland-mobile/pages/main/SettingsAbout.tsx
+++ packages/openland-mobile/pages/main/SettingsAbout.tsx
@@ -1,21 +1,48 @@
 import * as React from 'react';
 import { withApp } from 'openland-mobile/components/withApp';
-import { SHeader } from 'react-native-s/SHeader';
 import { PageProps } from 'openland-mobile/components/PageProps';
 import { SScrollView } from 'react-native-s/SScrollView';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 // import { ThemeContext } from 'openland-mobile/themes/ThemeContext';
+import { ZLinearGradient } from 'openland-mobile/components/visual/ZLinearGradient.native';
+
+const styles = StyleSheet.create({
+    // image: {
+    //     width: 240,
+    //     height: 150,
+    //     resizeMode: 'cover'
+    // } as ImageStyle,
+    // title: {
+    //     ...TextStyles.Title2,
+    //     marginTop: 16,
+    //     textAlign: 'center'
+    // } as TextStyle,
+    // subtitle: {
+    //     ...TextStyles.Body,
+    //     marginTop: 8,
+    //     marginBottom: 24,
+    //     textAlign: 'center'
+    // } as TextStyle
+    hero: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 242
+    }
+});
 
 const SettingsAboutComponent = React.memo<PageProps>((props) => {
     // const theme = React.useContext(ThemeContext);
 
     return (
-        <>
-            <SHeader title="About" />
-            <SScrollView>
-                <Text>Hello!</Text>
-            </SScrollView>
-        </>
+        <SScrollView>
+            <ZLinearGradient colors={['rgba(242, 243, 245, 0)', '#F2F3F5']} fallbackColor="#F2F3F5" start={{ x: 0, y: 0 }} end={{ x: 0, y: 1 }}>
+                <View style={styles.hero}>
+                    <Text>Hello!</Text>
+                </View>
+            </ZLinearGradient>
+
+        </SScrollView>
     );
 });
 
