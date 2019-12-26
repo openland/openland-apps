303ed0f16b89174779396694057067ca00660b79
Steve Korshakov <steve@korshakov.com>
2019-12-25 17:14:11 -0800

    fix(android): Fix timers

---
+++ android/app/src/main/java/com/openland/react/KeepAliveModule.kt
@@ -0,0 +1,44 @@
+package com.openland.react
+
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.jstasks.HeadlessJsTaskContext
+import com.facebook.react.modules.core.Timing
+import java.util.concurrent.atomic.AtomicBoolean
+
+class KeepAliveModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+    override fun getName(): String {
+        return "RNKeepAlive"
+    }
+
+    init {
+        // Hack Timers
+        val headlessContext = HeadlessJsTaskContext.getInstance(reactContext)
+        try {
+            val mActiveTasks = HeadlessJsTaskContext::class.java.getDeclaredField("mActiveTasks")
+            mActiveTasks.isAccessible = true
+            val tasks = mActiveTasks.get(headlessContext) as MutableSet<Int>
+            tasks.add(-1)
+        } catch (e: NoSuchFieldException) {
+            e.printStackTrace()
+        } catch (e: IllegalAccessException) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun initialize() {
+        super.initialize()
+
+        val module = reactApplicationContext.getNativeModule(Timing::class.java)
+        try {
+            val isRunningTasks = Timing::class.java.getDeclaredField("isRunningTasks")
+            isRunningTasks.isAccessible = true
+            val v = isRunningTasks.get(module) as AtomicBoolean
+            v.set(true)
+        } catch (e: NoSuchFieldException) {
+            e.printStackTrace()
+        } catch (e: IllegalAccessException) {
+            e.printStackTrace()
+        }
+    }
+}
--- android/app/src/main/java/com/openland/react/RNSPackage.kt
+++ android/app/src/main/java/com/openland/react/RNSPackage.kt
@@ -6,6 +6,7 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.jstasks.HeadlessJsTaskContext
+import com.facebook.react.modules.core.Timing
 import com.facebook.react.uimanager.ReactShadowNode
 import com.facebook.react.uimanager.ViewManager
 import com.openland.react.async.AsyncConfigManager
@@ -26,20 +27,8 @@ class RNSPackage : ReactPackage {
 
     override fun createNativeModules(reactContext: ReactApplicationContext): MutableList<NativeModule> {
 
-        // Hack Timers
-        val headlessContext = HeadlessJsTaskContext.getInstance(reactContext)
-        try {
-            val mActiveTasks = HeadlessJsTaskContext::class.java.getDeclaredField("mActiveTasks")
-            mActiveTasks.isAccessible = true
-            val tasks = mActiveTasks.get(headlessContext) as MutableSet<Int>
-            tasks.add(-1)
-        } catch (e: NoSuchFieldException) {
-            e.printStackTrace()
-        } catch (e: IllegalAccessException) {
-            e.printStackTrace()
-        }
-
         return mutableListOf(
+                KeepAliveModule(reactContext),
                 AsyncConfigManager(reactContext),
                 AsyncDataViewManager(reactContext),
                 RNSAnimatedViewManager(reactContext),
