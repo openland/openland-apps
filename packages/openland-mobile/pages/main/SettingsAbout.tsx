d81d20a809792126f90aac0486b5060a822bc0ed
uyouthe <melvoloskov@ya.ru>
2019-12-26 15:37:21 +0300

    wip(mobile): about section

--- packages/openland-mobile/pages/main/SettingsAbout.tsx
+++ packages/openland-mobile/pages/main/SettingsAbout.tsx
@@ -2,48 +2,89 @@ import * as React from 'react';
 import { withApp } from 'openland-mobile/components/withApp';
 import { PageProps } from 'openland-mobile/components/PageProps';
 import { SScrollView } from 'react-native-s/SScrollView';
-import { View, Text, StyleSheet } from 'react-native';
-// import { ThemeContext } from 'openland-mobile/themes/ThemeContext';
+import { View, Text, StyleSheet, Image, Linking, TouchableOpacity, ImageSourcePropType } from 'react-native';
+import { ThemeContext } from 'openland-mobile/themes/ThemeContext';
 import { ZLinearGradient } from 'openland-mobile/components/visual/ZLinearGradient.native';
+import { TextStyles } from 'openland-mobile/styles/AppStyles';
 
-const styles = StyleSheet.create({
-    // image: {
-    //     width: 240,
-    //     height: 150,
-    //     resizeMode: 'cover'
-    // } as ImageStyle,
-    // title: {
-    //     ...TextStyles.Title2,
-    //     marginTop: 16,
-    //     textAlign: 'center'
-    // } as TextStyle,
-    // subtitle: {
-    //     ...TextStyles.Body,
-    //     marginTop: 8,
-    //     marginBottom: 24,
-    //     textAlign: 'center'
-    // } as TextStyle
-    hero: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: 242
-    }
+interface ItemProps {
+    path: string;
+    icon: ImageSourcePropType;
+    children: string;
+}
+
+const Item = React.memo((props: ItemProps) => {
+    const theme = React.useContext(ThemeContext);
+
+    const styles = StyleSheet.create({
+        text: {
+            ...TextStyles.Body,
+            color: theme.foregroundPrimary
+        },
+        icon: {
+            tintColor: theme.foregroundSecondary
+        }
+    });
+
+    return (
+        <TouchableOpacity onPress={() => Linking.openURL(props.path)}>
+            <View flexDirection="row" alignItems="center" paddingLeft={16} paddingRight={16} paddingTop={12} paddingBottom={12}>
+                <View marginRight={16}>
+                    <Image source={props.icon} style={styles.icon} />
+                </View>
+                <Text allowFontScaling={false} style={styles.text}>
+                    {props.children}
+                </Text>
+            </View>
+        </TouchableOpacity>
+    );
 });
 
-const SettingsAboutComponent = React.memo<PageProps>((props) => {
-    // const theme = React.useContext(ThemeContext);
+const SettingsAboutComponent = React.memo((props: PageProps) => {
+    const theme = React.useContext(ThemeContext);
+
+    const styles = StyleSheet.create({
+        image: {
+            width: 192,
+            height: 192,
+            resizeMode: 'cover'
+        },
+        title: {
+            ...TextStyles.Title2,
+            marginTop: -16,
+            textAlign: 'center',
+            color: theme.foregroundPrimary
+        },
+        subtitle: {
+            ...TextStyles.Subhead,
+            marginTop: 4,
+            marginBottom: 32,
+            textAlign: 'center',
+            color: theme.foregroundTertiary
+        },
+        hero: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        }
+    });
 
     return (
         <SScrollView>
-            <ZLinearGradient colors={['rgba(242, 243, 245, 0)', '#F2F3F5']} fallbackColor="#F2F3F5" start={{ x: 0, y: 0 }} end={{ x: 0, y: 1 }}>
+            <ZLinearGradient colors={[theme.gradient0to100Start, theme.gradient0to100End]} fallbackColor={theme.gradient0to100Start} start={{ x: 0, y: 0 }} end={{ x: 0, y: 1 }}>
                 <View style={styles.hero}>
-                    <Text>Hello!</Text>
+                    <Image style={styles.image} source={require('assets/ic-app-icon-192.png')} />
+                    <Text style={styles.title} allowFontScaling={false}>Openland</Text>
+                    <Text style={styles.subtitle} allowFontScaling={false}>Version 1.2.3.4</Text>
                 </View>
             </ZLinearGradient>
-
+            <View marginTop={16}>
+                <Item path="https://openland.com/about" icon={require('assets/ic-info-24.png')}>About Openland</Item>
+                <Item path="https://openland.com/terms" icon={require('assets/ic-terms-24.png')}>Terms of service</Item>
+                <Item path="https://openland.com/privacy" icon={require('assets/ic-lock-24.png')}>Privacy policy</Item>
+            </View>
         </SScrollView>
     );
 });
 
-export const SettingsAbout = withApp(SettingsAboutComponent);
+export const SettingsAbout = withApp(SettingsAboutComponent, { navigationAppearance: 'small' });
