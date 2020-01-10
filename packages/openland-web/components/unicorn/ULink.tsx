20c213c8bdcdcedcf3358da630beda452242df4d
uyouthe <melvoloskov@ya.ru>
2020-01-10 17:31:23 +0300

    fix(web): suspense for invite resolving

--- packages/openland-web/components/unicorn/ULink.tsx
+++ packages/openland-web/components/unicorn/ULink.tsx
@@ -7,18 +7,18 @@ import { useClient } from 'openland-web/utils/useClient';
 const ULinkInternal = React.memo((props: { link: string; color?: string, className?: string, children?: any }) => {
     const { link, children, className, color } = props;
     const router = React.useContext(XViewRouterContext);
-    // const client = useClient();
+    const client = useClient();
 
-    // const linkSegments = link.split('/');
-    // const inviteId = linkSegments.includes('invite') ? linkSegments[linkSegments.length - 1] : '';
-    // const invite = client.useResolvedInvite({ key: inviteId });
+    const linkSegments = link.split('/');
+    const key = linkSegments.includes('invite') ? linkSegments[linkSegments.length - 1] : '';
+    const invite = client.useResolvedInvite({ key });
 
     let finalLink = link;
 
-    // if (invite.invite && invite.invite.__typename === 'RoomInvite' && invite.invite.room.membership === 'MEMBER') {
-    //     const roomId = invite.invite.room.id!;
-    //     finalLink = `/mail/${roomId}`;
-    // }
+    if (invite.invite && invite.invite.__typename === 'RoomInvite' && invite.invite.room.membership === 'MEMBER') {
+        const roomId = invite.invite.room.id!;
+        finalLink = `/mail/${roomId}`;
+    }
 
     return (
         <a
@@ -47,11 +47,27 @@ interface ULinkProps {
     children?: any;
 }
 
+const ULinkFallback = React.memo((props: ULinkProps) => (
+    <a
+        href={props.href}
+        className={props.className}
+        style={{ color: props.color }}
+        target="_blank"
+        onClick={(e) => {
+            e.stopPropagation();
+        }}
+    >
+        {props.children}
+    </a>
+));
+
 export const ULink = React.memo((props: ULinkProps) => {
     const { children, href, path, color, className } = props;
+    const fallback = <ULinkFallback {...props}>{children}</ULinkFallback>;
 
     if (path) {
         return (
+            <React.Suspense fallback={fallback}>
             <ULinkInternal
                 link={path}
                 color={color}
@@ -59,35 +75,26 @@ export const ULink = React.memo((props: ULinkProps) => {
             >
                 {children}
             </ULinkInternal>
+            </React.Suspense>
         );
     }
 
     if (href) {
         if (isInternalLink(href)) {
             return (
-                <ULinkInternal
-                    link={makeInternalLinkRelative(href)}
-                    color={color}
-                    className={className}
-                >
-                    {children}
-                </ULinkInternal>
+                <React.Suspense fallback={fallback}>
+                    <ULinkInternal
+                        link={makeInternalLinkRelative(href)}
+                        color={color}
+                        className={className}
+                    >
+                        {children}
+                    </ULinkInternal>
+                </React.Suspense>
             );
         }
 
-        return (
-            <a
-                href={href}
-                className={className}
-                style={{ color }}
-                target="_blank"
-                onClick={(e) => {
-                    e.stopPropagation();
-                }}
-            >
-                {children}
-            </a>
-        );
+        return fallback;
     }
 
     return null;
