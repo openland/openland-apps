2a0865f4cd6cddbed789d407269a2c63b29c59b9
Fred Morozov <morozov@fred.md>
2020-01-20 18:52:24 +0300

    wip(web): add funds modal layout

--- packages/openland-web/fragments/wallet/components/showAddFunds.tsx
+++ packages/openland-web/fragments/wallet/components/showAddFunds.tsx
@@ -8,6 +8,9 @@ import { UCheckbox } from 'openland-web/components/unicorn/UCheckbox';
 import { UButton } from 'openland-web/components/unicorn/UButton';
 import uuid from 'uuid';
 import { backoff } from 'openland-y-utils/timer';
+import { UListGroup } from 'openland-web/components/unicorn/UListGroup';
+import { UAddItem } from 'openland-web/components/unicorn/templates/UAddButton';
+import { showAddCard } from './showAddCard';
 
 const token = 'pk_test_y80EsXGYQdMKMcJ5lifEM4jx';
 
@@ -53,38 +56,42 @@ const AddFundsComponent = React.memo((props: { ctx: XModalController }) => {
 
     return (
         <XView flexDirection="column">
-            <XView height={120}>
-                <USelect
-                    value={amount}
-                    searchable={false}
-                    options={[
-                        { value: 1000, label: '10$' },
-                        { value: 2000, label: '20$' },
-                        { value: 5000, label: '50$' }
-                    ]}
-                    onChange={(v) => setAmount((v as any).value)}
-                />
+            <XView paddingHorizontal={8}>
+                <XView paddingHorizontal={16} paddingTop={12}>
+                    <USelect
+                        placeholder="Amount"
+                        value={amount}
+                        searchable={false}
+                        options={[
+                            { value: 1000, label: '$10' },
+                            { value: 2000, label: '$20' },
+                            { value: 5000, label: '$50' }
+                        ]}
+                        onChange={(v) => setAmount((v as any).value)}
+                    />
+                </XView>
+                <UListGroup header="Payment method" action={{ title: 'Add card', onClick: () => showAddCard() }}>
+                    {cards.myCards.map((v) => (
+                        <XView key={v.id}>
+                            <UCheckbox
+                                label={v.last4}
+                                checked={v.id === currentCard}
+                                onChange={(v2) => v2 ? setCurrentCard(v.id) : setCurrentCard(undefined)}
+                            />
+                        </XView>
+                    ))}
+                </UListGroup>
             </XView>
-            <XView>
-                {cards.myCards.map((v) => (
-                    <XView key={v.id}>
-                        <UCheckbox
-                            label={v.last4}
-                            checked={v.id === currentCard}
-                            onChange={(v2) => v2 ? setCurrentCard(v.id) : setCurrentCard(undefined)}
-                        />
-                    </XView>
-                ))}
-            </XView>
-            <XView>
-                <UButton text="Add" onClick={onSubmit} />
+            <XView marginTop={24} paddingVertical={16} paddingHorizontal={24} backgroundColor="var(--backgroundTertiary)" justifyContent="flex-end" flexDirection="row">
+                <UButton text="Cancel" onClick={() => props.ctx.hide()} style="secondary" size="large" />
+                <UButton text={`Top up $${amount / 100}`} onClick={onSubmit} style="pay" size="large" />
             </XView>
         </XView>
     );
 });
 
 export function showAddFunds() {
-    showModalBox({ title: 'Add Funds' }, (ctx) => {
+    showModalBox({ title: 'Add funds' }, (ctx) => {
         return (
             <AddFundsComponent ctx={ctx} />
         );
