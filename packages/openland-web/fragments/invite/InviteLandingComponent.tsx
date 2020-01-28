034b54f0b693c9f10a687edccd19d44b71beef57
uyouthe <melvoloskov@ya.ru>
2020-01-28 15:01:54 +0300

    fix(web, mobile): invite landing analytics

--- packages/openland-web/fragments/invite/InviteLandingComponent.tsx
+++ packages/openland-web/fragments/invite/InviteLandingComponent.tsx
@@ -436,7 +436,7 @@ export const InviteLandingComponent = ({ signupRedirect }: { signupRedirect?: st
     return (
         <>
             <XTrack
-                event="invite_landing_view"
+                event={loggedIn ? "invite_landing_view_logged_in" : "invite_landing_view"}
                 params={{ invite_type: whereToInvite.toLowerCase() }}
             />
             <InviteLandingComponentLayout
