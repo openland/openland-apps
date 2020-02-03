ffe858bfe4c8c2192a14a9f3c525736fc08cc4a3
Steve Korshakov <steve@korshakov.com>
2020-02-02 18:49:38 -0800

    ref(web): Remove old code

--- packages/openland-e2e-perf/constants.ts
+++
@@ -1,7 +0,0 @@
-export const prodUrl = 'https://openland.com/mail';
-export const nextUrl = 'https://next.openland.com/mail';
-export const baseUrl = 'http://localhost:3000/mail';
-
-// TODO
-export const chromeProfile = 'NO PROFILE FOR NOW';
-export const dialogsPair = ['@openland/frontend', '@openland/backend'];
