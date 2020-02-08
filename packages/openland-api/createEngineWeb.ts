f4c1b6ff632a6903557d7e9180b62b3997b6184e
Steve Korshakov <steve@korshakov.com>
2020-02-08 09:00:08 -0800

    chore(web): Enforce openland protocol

--- packages/openland-api/createEngineWeb.ts
+++ packages/openland-api/createEngineWeb.ts
@@ -9,7 +9,8 @@ export const createEngineWeb = (endpoint: string, token?: string) => {
     } else {
         return new WebEngine(Definitions, {
             endpoint: endpoint,
-            connectionParams: { ['x-openland-token']: token }
+            connectionParams: { ['x-openland-token']: token },
+            protocol: 'openland'
         });
     }
 };
