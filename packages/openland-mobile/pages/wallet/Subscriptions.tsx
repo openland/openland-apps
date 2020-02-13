dc43bc5a955658d8fa44b31bb7ad3fcf2293667e
uyouthe <melvoloskov@ya.ru>
2020-02-13 12:45:06 +0300

    wip(mobile): subscription list modals

--- packages/openland-mobile/pages/wallet/Subscriptions.tsx
+++ packages/openland-mobile/pages/wallet/Subscriptions.tsx
@@ -7,6 +7,13 @@ import { ZListGroup } from 'openland-mobile/components/ZListGroup';
 import { ZListItem } from 'openland-mobile/components/ZListItem';
 import { useClient } from 'openland-api/useClient';
 import { WalletSubscriptionState, Subscriptions_subscriptions_product_WalletSubscriptionProductGroup } from 'openland-api/spacex.types';
+import { ActionSheetBuilder } from 'openland-mobile/components/ActionSheet';
+import { View, Text } from 'react-native';
+import { ZAvatar } from 'openland-mobile/components/ZAvatar';
+import { ThemeContext } from 'openland-mobile/themes/ThemeContext';
+import { TextStyles } from 'openland-mobile/styles/AppStyles';
+import LinearGradient from 'react-native-linear-gradient';
+import { ZButton } from 'openland-mobile/components/ZButton';
 
 interface NormalizedSubscription {
     id: string;
@@ -48,9 +55,112 @@ const generateSubTitle = (subscription: NormalizedSubscription) => {
     }
 };
 
+const SubscriptionView = React.memo((props: NormalizedSubscription) => {
+
+    const theme = React.useContext(ThemeContext);
+
+    const showModal = React.useCallback(() => {
+        const builder = new ActionSheetBuilder('Done');
+
+        builder.view(ctx => (
+            <View
+                paddingTop={16}
+                paddingBottom={16}
+                paddingLeft={32}
+                paddingRight={32}
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <ZAvatar
+                    src={props.photo}
+                    placeholderTitle={props.title}
+                    placeholderKey={props.id}
+                    size='xx-large'
+                />
+                <View marginTop={16}>
+                    <Text allowFontScaling={false} style={{ ...TextStyles.Title2, color: theme.foregroundPrimary }}>
+                        { props.title }
+                    </Text>
+                </View>
+                <View marginTop={4}>
+                    <Text allowFontScaling={false} style={{ ...TextStyles.Body, color: theme.foregroundTertiary }}>
+                        { generateSubTitle(props) }
+                    </Text>
+                </View>
+            </View>
+        ));
+
+        builder.show();
+    }, []);
+
+    const showModalWithCancel = React.useCallback(() => {
+        const builder = new ActionSheetBuilder('Done');
+
+        builder.view(ctx => (
+            <>
+                <LinearGradient
+                    colors={[theme.gradient0to100Start, theme.gradient0to100End]}
+                    paddingTop={16}
+                    paddingLeft={32}
+                    paddingRight={32}
+                    paddingBottom={32}
+                    alignItems="center"
+                    justifyContent="center"
+                    flexDirection="column"
+                >
+                    <ZAvatar
+                        src={props.photo}
+                        placeholderTitle={props.title}
+                        placeholderKey={props.id}
+                        size='xx-large'
+                    />
+                    <View marginTop={16}>
+                        <Text allowFontScaling={false} style={{ ...TextStyles.Title2, color: theme.foregroundPrimary }}>
+                            { props.title }
+                        </Text>
+                    </View>
+                    <View marginTop={4}>
+                        <Text allowFontScaling={false} style={{ ...TextStyles.Body, color: theme.foregroundTertiary }}>
+                            { generateSubTitle(props) }
+                        </Text>
+                    </View>
+                </LinearGradient>
+                <View
+                    paddingTop={32}
+                    paddingBottom={16}
+                    paddingLeft={32}
+                    paddingRight={32}
+                    flexDirection="column"
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    <ZButton title="Cancel subscription" style="secondary" />
+                    <View marginTop={16}>
+                        <Text allowFontScaling={false} style={{ ...TextStyles.Caption, color: theme.foregroundSecondary, textAlign: 'center' }}>
+                            If you cancel now, you can still access {"\n"} the group until {displayDate(props.expires)}
+                        </Text>
+                    </View>
+                </View>
+            </>
+        ));
+
+        builder.show();
+    }, []);
+
+    return (
+        <ZListItem
+            text={props.title}
+            subTitle={generateSubTitle(props)}
+            leftAvatar={{ photo: props.photo, key: props.id, title: props.title }}
+            onPress={props.state === WalletSubscriptionState.STARTED ? showModalWithCancel : showModal}
+        />
+    );
+});
+
 const SubscriptionsComponent = React.memo<PageProps>((props) => {
     const client = useClient();
-    const subscriptions = client.useSubscriptions();
+    const subscriptions = client.useSubscriptions({ fetchPolicy: 'network-only' });
     const groupSubscriptions = subscriptions.subscriptions.filter(subscription => subscription.product.__typename === 'WalletSubscriptionProductGroup');
     const normalizedSubscriptions: NormalizedSubscription[] = groupSubscriptions.map(subscription => {
         const group = (subscription.product as Subscriptions_subscriptions_product_WalletSubscriptionProductGroup).group;
@@ -82,11 +192,7 @@ const SubscriptionsComponent = React.memo<PageProps>((props) => {
                 { activeSubscriptions.length > 0 && (
                     <ZListGroup header="Active">
                         { activeSubscriptions.map(subscription => (
-                            <ZListItem
-                                text={subscription.title}
-                                subTitle={generateSubTitle(subscription)}
-                                leftAvatar={{ photo: subscription.photo, key: subscription.id, title: subscription.title }}
-                            />
+                            <SubscriptionView {...subscription} />
                         ))}
                     </ZListGroup>
                 )}
@@ -94,11 +200,7 @@ const SubscriptionsComponent = React.memo<PageProps>((props) => {
                 { expiredSubscriptions.length > 0 && (
                     <ZListGroup header="Active">
                         { expiredSubscriptions.map(subscription => (
-                            <ZListItem
-                                text={subscription.title}
-                                subTitle={generateSubTitle(subscription)}
-                                leftAvatar={{ photo: subscription.photo, key: subscription.id, title: subscription.title }}
-                            />
+                            <SubscriptionView {...subscription} />
                         ))}
                     </ZListGroup>
                 )}
