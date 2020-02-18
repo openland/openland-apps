96190384f021c0bbdc588118e97e3a144aca09a1
Fred Morozov <morozov@fred.md>
2020-02-18 13:15:39 +0300

    fix(*): adaptive cents for money-formatter

--- packages/openland-y-utils/wallet/Money.tsx
+++ packages/openland-y-utils/wallet/Money.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export const formatMoney = (amount: number) => {
+export const formatMoney = (amount: number, adaptiveCents?: boolean) => {
     let a = amount < 0 ? -amount : amount; // Division of incorrect numbers is incorrect in CPU
     let d = Math.floor(a / 100);
     let c = a % 100;
@@ -9,7 +9,7 @@ export const formatMoney = (amount: number) => {
     if (c < 10) {
         cs = '0' + cs;
     }
-    return '$' + (amount < 0 ? '-' : '') + (d.toString() + '.' + cs);
+    return '$' + (amount < 0 ? '-' : '') + (d.toString() + ((adaptiveCents && cs === '00') ? '' : ('.' + cs)));
 };
 
-export const Money = React.memo((props: { amount: number }) => <>{formatMoney(props.amount)}</>);
+export const Money = React.memo((props: { amount: number, adaptiveCents?: boolean }) => <>{formatMoney(props.amount, props.adaptiveCents)}</>);
