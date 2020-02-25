d6c11656ed4e72b4a5d744fcd89a5609be314a7b
uyouthe <melvoloskov@ya.ru>
2020-02-25 12:24:30 +0300

    fix(web): correct timezone for wallet

--- packages/openland-y-utils/wallet/dateTime.ts
+++ packages/openland-y-utils/wallet/dateTime.ts
@@ -1,9 +1,14 @@
-export const extractDateTime = (unixTime: string): { date: string, time: string, isToday: boolean } => {
+import { formatTime } from 'openland-y-utils/formatTime';
+
+export const extractDateTime = (
+    unixTime: string,
+): { date: string; time: string; isToday: boolean } => {
     const date = new Date(parseInt(unixTime, 10));
     const utc = date.toUTCString();
     const segments = utc.split(' ');
-    const time = segments[4].split(':').slice(0, 2).join(':');
-    const isToday = ((new Date()).toDateString() === date.toDateString());
+    const isToday = new Date().toDateString() === date.toDateString();
 
-    return { date: `${segments[2]} ${segments[1]}`, time, isToday };
+    const localTime = formatTime(parseInt(unixTime, 10));
+
+    return { date: `${segments[2]} ${segments[1]}`, time: localTime, isToday };
 };
