f759b4408ed55ec807f1d23ef25aa5b3008d4c88
Max Volkov <qwertynestor40@gmail.com>
2020-03-02 20:51:06 +0300

    fix(web): test

--- packages/openland-web/pages/dev/choice.page.tsx
+++ packages/openland-web/pages/dev/choice.page.tsx
@@ -1,28 +1,28 @@
 import * as React from 'react';
 import { withApp } from '../../components/withApp';
-import { UButton } from 'openland-web/components/unicorn/UButton';
+// import { UButton } from 'openland-web/components/unicorn/UButton';
 import { DevToolsScaffold } from './components/DevToolsScaffold';
-import { useClient } from 'openland-api/useClient';
-import { SuperAdminRole } from 'openland-api/spacex.types';
+// import { useClient } from 'openland-api/useClient';
+// import { SuperAdminRole } from 'openland-api/spacex.types';
 import { XView } from 'react-mental';
-import { XModalFooter } from 'openland-web/components/XModalFooter';
-import { showModalBox } from 'openland-x/showModalBox';
-import { useForm } from 'openland-form/useForm';
-import { useField } from 'openland-form/useField';
-import { TextLabel1, TextTitle1, TextLabel2 } from 'openland-web/utils/TextStyles';
+// import { XModalFooter } from 'openland-web/components/XModalFooter';
+// import { showModalBox } from 'openland-x/showModalBox';
+// import { useForm } from 'openland-form/useForm';
+// import { useField } from 'openland-form/useField';
+// import { TextLabel1, TextTitle1, TextLabel2 } from 'openland-web/utils/TextStyles';
 import { XScrollView3 } from 'openland-x/XScrollView3';
-import { UUserView } from 'openland-web/components/unicorn/templates/UUserView';
+// import { UUserView } from 'openland-web/components/unicorn/templates/UUserView';
 
 export default withApp('Super Admins', ['super-admin', 'software-developer'], () => {
-    const client = useClient();
-    const superAdmins = client.useSuperAdmins().superAdmins;
+    // const client = useClient();
+    // const superAdmins = client.useSuperAdmins().superAdmins;
     return (
         <DevToolsScaffold title="Super Admins">
             <XView flexDirection="row" alignItems="center">
-                <XView flexGrow={1}>
-                    <span className={TextTitle1}>Super Admins</span>
-                    <span className={TextLabel2}>{superAdmins.length + ' total'}</span>
-                </XView>
+                {/*<XView flexGrow={1}>*/}
+                {/*    <span className={TextTitle1}>Super Admins</span>*/}
+                {/*    <span className={TextLabel2}>{superAdmins.length + ' total'}</span>*/}
+                {/*</XView>*/}
             </XView>
             <XScrollView3>
                 <XView maxWidth={600}>
