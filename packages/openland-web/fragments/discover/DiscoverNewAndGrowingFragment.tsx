af4fd703ba1ff3704c3a3debff79254233979c1c
uyouthe <melvoloskov@ya.ru>
2020-03-03 10:26:09 +0300

    fix(web): autoloaded new and growing (I did it!)

--- packages/openland-web/fragments/discover/DiscoverNewAndGrowingFragment.tsx
+++ packages/openland-web/fragments/discover/DiscoverNewAndGrowingFragment.tsx
@@ -1,19 +1,50 @@
 import * as React from 'react';
 import { useClient } from 'openland-api/useClient';
-import { Page } from 'openland-unicorn/Page';
 import { UHeader } from 'openland-unicorn/UHeader';
-import { Listing } from './components/Listing';
+import { UFlatList } from 'openland-web/components/unicorn/UFlatList';
+import { UGroupView } from 'openland-web/components/unicorn/templates/UGroupView';
+import { DiscoverSharedRoom } from 'openland-api/spacex.types';
 
 export const DiscoverNewAndGrowingFragment = React.memo(() => {
     const client = useClient();
-    // temporary seed
+
+    // initial items
     const newAndGrowing = client.useDiscoverNewAndGrowing({ first: 20, seed: 123 });
-    const newAndGrowingItems = newAndGrowing.discoverNewAndGrowing.items || [];
+    const { items: initialItems, cursor: initialCursor } = newAndGrowing.discoverNewAndGrowing;
+
+    const [loading, setLoading] = React.useState<boolean>(false);
+    const [after, setAfter] = React.useState<string | null>(initialCursor);
+    const [displayItems, setDisplayItems] = React.useState<DiscoverSharedRoom[]>(initialItems);
+
+    const handleLoadMore = React.useCallback(async () => {
+        if (loading || !after) {
+            return;
+        }
+        setLoading(true);
+        const first = 10;
+
+        const loaded = await client.queryDiscoverNewAndGrowing({ first, seed: 123, after });
+        const { items, cursor } = loaded.discoverNewAndGrowing;
+
+        setAfter(cursor);
+        setDisplayItems(prev => prev.concat(items));
+        setLoading(false);
+
+    }, [after, loading]);
 
     return (
-        <Page track="discover_new_and_growing">
+        <>
             <UHeader title="New and growing" />
-            <Listing items={newAndGrowingItems} />
-        </Page>
+            <UFlatList
+                track="discover_new_and_growing"
+                title="New and growing"
+                loading={loading}
+                loadMore={handleLoadMore}
+                items={displayItems}
+                renderItem={item => (
+                    <UGroupView group={item as DiscoverSharedRoom} />
+                )}
+            />
+        </>
     );
 });
