47bb0e523a46a702c716c8178b5c323d4d255554
kor_ka <gleb@openland.com>
2020-03-13 13:49:50 +0300

    fix(ios): RNAsyncCell update debaunce

--- ios/RNAsyncCell.swift
+++ ios/RNAsyncCell.swift
@@ -9,16 +9,24 @@
 import Foundation
 
 let range = ASSizeRange(min: CGSize(width: UIScreen.main.bounds.width, height: 0), max: CGSize(width: UIScreen.main.bounds.width, height: 10000))
+let q = DispatchQueue(label: "CellUpdate", qos: .userInitiated)
 
 class RNAsyncCell: ASCellNode {
   let context: RNAsyncViewContext
   var spec: AsyncViewSpec
-  var node: ASLayoutElement!
-  var applyModes: [String] = []
+  var applyModes: [String]
   
-  init(spec: AsyncViewSpec, context: RNAsyncViewContext) {
+  var pendingSpec: AsyncViewSpec?
+  var pendingApplyModes: [String]?
+  
+  var node: ASLayoutElement!
+  
+  let lock = NSObject()
+  var updating = true;
+  init(spec: AsyncViewSpec, context: RNAsyncViewContext, applyModes: [String]) {
     self.context = context
     self.spec = spec
+    self.applyModes = applyModes
     super.init()
     self.node = resolveNode(spec: spec, modesToApply: self.applyModes, context: self.context)
     self.automaticallyManagesSubnodes = true
@@ -28,19 +36,57 @@ class RNAsyncCell: ASCellNode {
   
   // We are updating cell always from background thread
   func setSpec(spec: AsyncViewSpec) {
-    self.spec = spec
-    updateNode()
+    updateNode(spec: spec, modesToApply: self.applyModes)
   }
   
   func applyModes(modesToApply: [String]){
-    self.applyModes = modesToApply
-    updateNode()
+    if(self.applyModes != modesToApply){
+      self.applyModes = modesToApply
+      updateNode(spec: self.spec, modesToApply: modesToApply)
+    }
   }
   
-  func updateNode(){
-    self.node = resolveNode(spec: self.spec, modesToApply: self.applyModes, context: self.context)
-    self.layoutThatFits(range)
-    self.setNeedsLayout()
+  func updateNode(spec: AsyncViewSpec, modesToApply: [String]){
+      openland.lock(self.lock) {
+        if(self.updating || self.preDisplay){
+          self.pendingApplyModes = modesToApply
+          self.pendingSpec = spec
+          return
+        }
+        self.updating = true
+        q.async {
+           self.spec = spec
+           self.applyModes = modesToApply
+           self.node = resolveNode(spec: spec, modesToApply: modesToApply, context: self.context)
+           self.setNeedsLayout()
+           self.layoutThatFits(range)
+         }
+      }
+  }
+  
+  func updateIfPending(){
+    var pendingSpec: AsyncViewSpec? = nil;
+    var pendingApplyModes: [String]? = nil;
+    openland.lock(self.lock) {
+      if(self.pendingSpec != nil || self.pendingApplyModes != nil){
+         pendingSpec = self.pendingSpec
+         pendingApplyModes = self.pendingApplyModes
+         self.pendingSpec = nil
+         self.pendingApplyModes = nil
+      }
+    }
+    if(pendingSpec != nil || pendingApplyModes != nil){
+      self.updateNode(spec: pendingSpec ?? self.spec, modesToApply: pendingApplyModes ?? self.applyModes)
+    }
+  }
+  
+  override func layoutDidFinish() {
+    DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(50)) {
+         openland.lock(self.lock) {
+           self.updating = false
+         }
+         self.updateIfPending();
+     }
   }
   
   override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
