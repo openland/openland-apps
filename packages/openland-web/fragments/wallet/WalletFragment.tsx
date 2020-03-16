5d328fadd12aed5e23a259e5832b40796b9e775e
metreniuk <metreniuk@gmail.com>
2020-03-16 13:51:25 +0200

    fix(web): add withdraw modal

--- packages/openland-web/fragments/wallet/WalletFragment.tsx
+++ packages/openland-web/fragments/wallet/WalletFragment.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { UHeader } from 'openland-unicorn/UHeader';
-import { XView } from 'react-mental';
+import { XView, XViewRouterContext } from 'react-mental';
 import { useClient } from 'openland-api/useClient';
 import { showAddCard } from './modals/showAddCard';
 import { UListGroup } from 'openland-web/components/unicorn/UListGroup';
@@ -13,9 +13,24 @@ import { TextStyles } from 'openland-web/utils/TextStyles';
 import { MessengerContext } from 'openland-engines/MessengerEngine';
 import { UFlatList } from 'openland-web/components/unicorn/UFlatList';
 import { UListHeader } from 'openland-web/components/unicorn/UListHeader';
+import { UButton } from 'openland-web/components/unicorn/UButton';
+import { css, cx } from 'linaria';
+import { showWithdrawFunds } from './components/WithdrawFunds';
+
+const balanceWrapper = css`
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 16px;
+    margin: 16px 16px 0;
+    height: 80px;
+    border-radius: 8px;
+    background: linear-gradient(180deg, rgba(242, 243, 245, 0.56) 0%, #F2F3F5 100%);
+`;
 
 export const WalletFragment = React.memo(() => {
     const client = useClient();
+    const router = React.useContext(XViewRouterContext)!;
     const messenger = React.useContext(MessengerContext);
     const walletEngine = messenger.wallet;
     const walletState = walletEngine.state.useState();
@@ -33,16 +48,22 @@ export const WalletFragment = React.memo(() => {
             renderItem={transaction => <TransactionView key={transaction.id} item={transaction} />}
         >
             <UHeader title="Wallet" maxWidth={550} />
-            <UListGroup header="Your balance" action={{ title: 'Top up', onClick: () => showAddFunds() }}>
-                <XView
-                    {...TextStyles.Title1}
-                    color={balance === 0 ? 'var(--foregroundTertiary)' : 'var(--accentPrimary)'}
-                    paddingHorizontal={16}
-                    paddingBottom={16}
-                >
-                    <Money amount={balance} />
+            <div className={cx('x', balanceWrapper)}>
+                <XView flexDirection="column">
+                    <XView
+                        {...TextStyles.Title2}
+                        color={balance === 0 ? 'var(--foregroundTertiary)' : 'var(--foregroundPrimary)'}
+                    >
+                        <Money amount={balance} />
+                    </XView>
+                    <XView {...TextStyles.Subhead} color="var(--foregroundSecondary)">Your balance</XView>
                 </XView>
-            </UListGroup>
+            
+                <XView flexDirection="row">
+                    <UButton text="Withdraw" style="secondary" marginRight={8} onClick={() => showWithdrawFunds(router)} />
+                    <UButton text="Add funds" onClick={showAddFunds} />
+                </XView>
+            </div>
             <UListGroup
                 header="Payments methods"
                 action={cards.length > 0 ? { title: 'Add card', onClick: () => showAddCard() } : undefined}
