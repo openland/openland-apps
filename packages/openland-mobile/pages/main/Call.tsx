d9ea80c278633483f51b4f0b613e9d83ea98987c
kor_ka <gleb@openland.com>
2020-04-01 15:02:48 +0300

    feat(mobile): call - layout video fullscreen

--- packages/openland-mobile/pages/main/Call.tsx
+++ packages/openland-mobile/pages/main/Call.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { getClient } from 'openland-mobile/utils/graphqlClient';
-import { View, Text, TouchableOpacity, Image, BackHandler, Dimensions, ScrollView } from 'react-native';
+import { View, Text, TouchableOpacity, Image, BackHandler, Dimensions, StatusBar, Platform } from 'react-native';
 import { ASSafeAreaView } from 'react-native-async-view/ASSafeAreaView';
 import { XMemo } from 'openland-y-utils/XMemo';
 import { SStatusBar } from 'react-native-s/SStatusBar';
@@ -27,7 +27,7 @@ import { AppUserMediaStreamNative } from 'openland-y-runtime-native/AppUserMedia
 import { RTCView } from 'react-native-webrtc';
 import { AppConfig } from 'openland-y-runtime-native/AppConfig';
 
-const VideoView = React.memo((props: { peer: Conference_conference_peers, mediaSession: MediaSessionManager }) => {
+const VideoView = React.memo((props: { peer: Conference_conference_peers, mediaSession: MediaSessionManager, h: number }) => {
     let [stream, setStream] = React.useState<string>();
     let streamManager = props.mediaSession.useStreamManager(props.peer.id);
 
@@ -45,11 +45,10 @@ const VideoView = React.memo((props: { peer: Conference_conference_peers, mediaS
         }
 
     }, [streamManager]);
-    let w = Dimensions.get('window').width / 2;
     return (
-        <View width={w} height={w} backgroundColor="black">
-            {stream && <RTCView streamURL={stream} style={{ flex: 1 }} objectFit="cover" />}
-            <View position="absolute" left={6} bottom={6}>
+        <View flexGrow={1} height={props.h} backgroundColor="gray">
+            {stream && <RTCView streamURL={stream} style={{ flexGrow: 1 }} objectFit="cover" />}
+            <View position="absolute" left={6} top={6}>
                 <ZAvatar size="medium" id={props.peer.user.id} title={props.peer.user.name} photo={props.peer.user.photo} />
             </View>
         </View>
@@ -75,7 +74,6 @@ let Content = XMemo<{ id: string, hide: () => void }>((props) => {
         return () => {
             RNSDevice.proximityDisable();
             SStatusBar.setBarStyle(theme.statusBar);
-
         };
     }, []);
 
@@ -132,50 +130,90 @@ let Content = XMemo<{ id: string, hide: () => void }>((props) => {
 
     }, [timer, initialTime, status]);
 
+    // layout video order
+    let peers = conference ? conference.conference.peers : [];
+    let peerSlice = peers.reduce((all, peer, i, array) => {
+        let index =
+            i === 1 ? 0
+                : i === 2 && array.length > 3 ? 1
+                    : i % 2;
+        all[index].push(peer);
+        return all;
+    }, [[], []] as Conference_conference_peers[][]);
+    let w = Dimensions.get('screen').width;
+    let h = Dimensions.get('screen').height;
+    let h1 = h / peerSlice[0].length;
+    let h2 = h / peerSlice[1].length;
+
     let mediaSession = calls.getMediaSession();
+    let videoEnabled = !!(callsState.videoEnabled && mediaSession);
+
+    // animate controls hide/show
+    const [uiHidden, setHideUi] = React.useState(false);
+    const uiHiddenRef = React.useRef(false);
+    React.useEffect(() => setHideUi(videoEnabled), [videoEnabled]);
+    let switchUi = React.useCallback(() => setHideUi(v => !v), []);
+    const key = React.useMemo(() => randomKey(), []);
+    React.useEffect(() => {
+        if (uiHiddenRef.current === uiHidden) {
+            return;
+        }
+        uiHiddenRef.current = uiHidden;
+        SAnimated.beginTransaction();
+        if (uiHidden) {
+            SAnimated.timing(`call-controls-${key}`, { property: 'translateY', easing: { bezier: [0.23, 1, 0.32, 1] }, from: 0, to: 150, duration: 0.15 });
+        } else {
+            SAnimated.timing(`call-controls-${key}`, { property: 'translateY', easing: 'material', from: 150, to: 0, duration: 0.25 });
+        }
+        SAnimated.commitTransaction();
+    }, [uiHidden]);
 
     return (
-        <ASSafeAreaView flexDirection="column" alignItems="stretch" height="100%">
-            <ScrollView >
-                <View alignItems="center" justifyContent="center" flexDirection="column" marginBottom={40}>
-                    <View marginTop={67} flexDirection="row" borderWidth={10} borderRadius={120} borderColor="rgba(0, 0, 0, 0.05)">
-                        <ZAvatar size="xx-large" id={room.id} title={title} photo={photo} />
-                    </View>
-                    <Text
-                        style={{ fontSize: 28, fontWeight: FontStyles.Weight.Medium, color: 'white', textAlign: 'center', marginTop: 25 }}
-                        numberOfLines={4}
-                    >
-                        {title}
-                    </Text>
-                    <Text
-                        style={{ fontSize: 16, color: 'white', textAlign: 'center', marginTop: 8, opacity: 0.8 }}
-                    >
-                        {
-                            status === 'end' ? 'Call ended ' :
-                                status === 'waiting' ? 'Waiting...' :
-                                    status === 'connected' ? '' : 'Connecting...'
-                        }
-                        {['connected', 'end'].includes(status) ? formatTimerTime(timer) : ''}
-                    </Text>
-                </View>
+        <TouchableOpacity delayPressIn={10} delayPressOut={10} activeOpacity={1} onPress={switchUi}>
+            {Platform.OS === 'ios' && <StatusBar hidden={!!videoEnabled} />}
+            {videoEnabled ?
+                <View flexDirection="row" alignItems="flex-start" width={w}>
+                    <View flexDirection="column" justifyContent="flex-start" flexGrow={1}>{peerSlice[0].map(p => <VideoView key={p.id} peer={p} mediaSession={mediaSession!} h={h1} />)}</View>
+                    <View flexDirection="column" justifyContent="flex-start" flexGrow={peerSlice[1].length ? 1 : 0}>{peerSlice[1].map(p => <VideoView key={p.id} peer={p} mediaSession={mediaSession!} h={h2} />)}</View>
+                </View> :
 
-                {room.__typename === 'SharedRoom' &&
-                    <View flexDirection="row" alignItems="center" flexWrap="wrap" marginHorizontal={callsState.videoEnabled ? 0 : 18}>
-                        {conference && conference.conference.peers.map(p => {
-                            return <View key={p.id} margin={callsState.videoEnabled ? 0 : 10} >
-                                {(!callsState.videoEnabled || !mediaSession) ? <ZAvatar size="medium" id={p.user.id} title={p.user.name} photo={p.user.photo} /> :
-                                    <VideoView peer={p} mediaSession={mediaSession} />
-                                }
-                            </View>;
+                <ASSafeAreaView flexDirection="column" alignItems="stretch" height="100%">
+                    {!videoEnabled && <View alignItems="center" justifyContent="center" flexDirection="column" marginBottom={40}>
+                        <View marginTop={67} flexDirection="row" borderWidth={10} borderRadius={120} borderColor="rgba(0, 0, 0, 0.05)">
+                            <ZAvatar size="xx-large" id={room.id} title={title} photo={photo} />
+                        </View>
+                        <Text
+                            style={{ fontSize: 28, fontWeight: FontStyles.Weight.Medium, color: 'white', textAlign: 'center', marginTop: 25 }}
+                            numberOfLines={4}
+                        >
+                            {title}
+                        </Text>
+                        <Text
+                            style={{ fontSize: 16, color: 'white', textAlign: 'center', marginTop: 8, opacity: 0.8 }}
+                        >
+                            {
+                                status === 'end' ? 'Call ended ' :
+                                    status === 'waiting' ? 'Waiting...' :
+                                        status === 'connected' ? '' : 'Connecting...'
+                            }
+                            {['connected', 'end'].includes(status) ? formatTimerTime(timer) : ''}
+                        </Text>
+                    </View>}
 
-                        })}
-                        <View height={100} width={1} />
-                    </View>
-                }
-            </ScrollView>
+                    {room.__typename === 'SharedRoom' &&
+                        <View flexDirection="row" alignItems="center" flexWrap="wrap" marginHorizontal={18}>
+                            {conference && conference.conference.peers.map(p => {
+                                return <View key={p.id} margin={10} >
+                                    <ZAvatar size="medium" id={p.user.id} title={p.user.name} photo={p.user.photo} />
+                                </View>;
 
-            <View justifyContent="space-around" alignItems="center" bottom={56} flexDirection="row" position="absolute" width="100%">
+                            })}
+                            <View height={100} width={1} />
+                        </View>
+                    }
+                </ASSafeAreaView>}
 
+            <SAnimated.View name={`call-controls-${key}`} style={{ justifyContent: 'space-around', alignItems: 'center', bottom: 56, flexDirection: 'row', position: 'absolute', width: '100%' }}>
                 {AppConfig.isNonProduction() && <TouchableOpacity
                     onPress={props.hide}
                     style={{ width: 56, height: 56 }}
@@ -229,8 +267,9 @@ let Content = XMemo<{ id: string, hide: () => void }>((props) => {
                         <Image source={mute ? require('assets/ic-mic-off-30.png') : require('assets/ic-mic-on-30.png')} style={{ tintColor: mute ? 'black' : 'white' }} />
                     </View>
                 </TouchableOpacity>
-            </View>
-        </ASSafeAreaView>
+            </SAnimated.View>
+        </TouchableOpacity>
+
     );
 });
 
