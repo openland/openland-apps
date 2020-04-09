e11fcffed43963d29684162d889c6e1f3c3ee68e
metreniuk <metreniuk@gmail.com>
2020-04-09 21:15:09 +0300

    wip(web): adjust call settings modal

--- packages/openland-web/modules/conference/CallControls.tsx
+++ packages/openland-web/modules/conference/CallControls.tsx
@@ -12,6 +12,10 @@ import { TextStyles } from 'openland-web/utils/TextStyles';
 import MediaDevicesManager from 'openland-web/utils/MediaDevicesManager';
 import { showModalBox } from 'openland-x/showModalBox';
 import { USelect } from 'openland-web/components/unicorn/USelect';
+import { XModalFooter } from 'openland-web/components/XModalFooter';
+import { UButton } from 'openland-web/components/unicorn/UButton';
+import { XModalController } from 'openland-x/showModal';
+import { VideoPeer, VideoPeerProps } from './VideoPeer';
 
 const wrapper = css`
     position: absolute;
@@ -57,29 +61,68 @@ const ControlItem = (props: {icon: JSX.Element, text: string, onClick: React.Mou
     );
 };
 
-const SettingsModal = React.memo((props: {}) => {
-    let [devices, input, ouput, setInput, setOutput] = MediaDevicesManager.instance().useMediaDevices();
+const SettingsModal = React.memo((props: {ctx: XModalController, videoProps?: VideoPeerProps}) => {
+    let {devices, input, output, videoInput, selectInput, selectOutput, selectVideoInput} = MediaDevicesManager.instance().useMediaDevices();
+    let prevValues = React.useRef<{input?: MediaDeviceInfo, output?: MediaDeviceInfo, videoInput?: MediaDeviceInfo}>({input, output, videoInput}).current;
 
     let outputs = devices.filter(d => d.kind === 'audiooutput');
     let inputs = devices.filter(d => d.kind === 'audioinput');
+    let videoInputs = devices.filter(d => d.kind === 'videoinput');
 
     let setInputDevice = React.useCallback((val) => {
         let device = devices.find(d => d.deviceId === val.value);
-        setInput(device);
+        selectInput(device);
     }, [devices]);
     let setOutputDevice = React.useCallback((val) => {
         let dev = devices.find(d => d.deviceId === val.value);
-        setOutput(dev);
+        selectOutput(dev);
+    }, [devices]);
+    let setVideoInputDevice = React.useCallback((val) => {
+        let dev = devices.find(d => d.deviceId === val.value);
+        selectVideoInput(dev);
     }, [devices]);
 
+    const resetSettings = () => {
+        selectInput(prevValues.input);
+        selectOutput(prevValues.output);
+        selectVideoInput(prevValues.videoInput);
+    };
+
     return (
-        <XView height={500} justifyContent="flex-start">
-            <XView paddingHorizontal={16} paddingVertical={8}>
-                <USelect searchable={false} onChange={setInputDevice} placeholder="Microphone" value={input?.deviceId} options={inputs.map(o => ({ value: o.deviceId, label: o.label }))} />
+        <XView>
+            <XView paddingHorizontal={24} flexDirection="row" paddingTop={12}>
+                <XView width={170} height={128} borderRadius={8}>
+                    {props.videoProps ? <VideoPeer {...props.videoProps} compact={true} /> : null}
+                </XView>
+                <XView flexDirection="column" marginLeft={16} flexGrow={1}>
+                <XView marginBottom={16}>
+                        <USelect searchable={false} onChange={setVideoInputDevice} placeholder="Camera" value={videoInput?.deviceId} options={videoInputs.map(o => ({ value: o.deviceId, label: o.label }))} />
+                    </XView>
+                    <XView marginBottom={16}>
+                        <USelect searchable={false} onChange={setInputDevice} placeholder="Microphone" value={input?.deviceId} options={inputs.map(o => ({ value: o.deviceId, label: o.label }))} />
+                    </XView>
+                    <XView marginBottom={24}>
+                        <USelect searchable={false} onChange={setOutputDevice} placeholder="Speakers" value={output?.deviceId} options={outputs.map(o => ({ value: o.deviceId, label: o.label }))} />
+                    </XView>
+                </XView>
             </XView>
-            <XView paddingHorizontal={16} paddingVertical={8}>
-                <USelect searchable={false} onChange={setOutputDevice} placeholder="Speakers" value={ouput?.deviceId} options={outputs.map(o => ({ value: o.deviceId, label: o.label }))} />
-            </XView>
+            <XModalFooter>
+                <UButton
+                    text="Cancel"
+                    style="tertiary"
+                    size="large"
+                    onClick={() => {
+                        props.ctx.hide();
+                        resetSettings();
+                    }}
+                />
+                <UButton
+                    text="Save"
+                    style="primary"
+                    size="large"
+                    onClick={() => props.ctx.hide()}
+                />
+            </XModalFooter>
         </XView>
     );
 });
@@ -93,12 +136,13 @@ interface CallControlsProps {
     onMute: React.MouseEventHandler;
     onCameraClick: React.MouseEventHandler;
     onScreenClick: React.MouseEventHandler;
+    videoProps?: VideoPeerProps;
 }
 
 export const CallControls = (props: CallControlsProps) => {
     let showSettings = React.useCallback(() => {
-        showModalBox({ title: 'Audio setting' }, () => <SettingsModal />);
-    }, []);
+        showModalBox({ title: 'Settings', width: 560, overflowVisible: true }, (ctx) => <SettingsModal ctx={ctx} videoProps={props.videoProps} />);
+    }, [props.videoProps]);
     return (
         <XView 
             width={64} 
