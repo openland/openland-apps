595da825ff5861f48d4bb680ab77e913f5e59c0b
kor_ka <gleb@openland.com>
2020-04-12 01:37:43 +0300

    wip(web): call space draw

--- packages/openland-web/modules/conference/VolumeSpace.tsx
+++ packages/openland-web/modules/conference/VolumeSpace.tsx
@@ -6,6 +6,9 @@ import { MediaSessionManager } from 'openland-engines/media/MediaSessionManager'
 import { AppUserMediaStreamWeb } from 'openland-y-runtime-web/AppUserMedia';
 import { VideoComponent } from './ScreenShareModal';
 import { UAvatar } from 'openland-web/components/unicorn/UAvatar';
+import uuid from 'uuid';
+import { bezierPath } from './smooth';
+import { UCheckbox } from 'openland-web/components/unicorn/UCheckbox';
 
 let VolumeSpaceContainerStyle = css`
     width: 100%;
@@ -18,6 +21,22 @@ let VolumeSpaceInnerContainerStyle = css`
     width: 3000px;
     height: 3000px;
 `;
+let VolumeSpaceDrawContainerStyle = css`
+    position: absolute;
+    width: 3000px;
+    height: 3000px;
+    top: 0;
+    left: 0;
+    pointer-events: none;
+`;
+
+let VolumeSpaceDrawListener = css`
+    position: absolute;
+    width: 3000px;
+    height: 3000px;
+    top: 0;
+    left: 0;
+`;
 let VolumeSpaceItemStyle = css`
     position: absolute;
 `;
@@ -27,6 +46,14 @@ let VolumeSpaceVideoStyle = css`
     height: 72px;
     border-radius: 72px;
     background-color: var(--foregroundSecondary);
+`;
+let DrawControlsContainerStyle = css`
+    position: absolute;
+    bottom: 16px;
+    right: 16px;
+    width: 160px;
+    border-radius: 8px;
+    background-color: var(--backgroundTertiary);
 `;
 
 const VolumeSpaceAvatar = React.memo((props: Conference_conference_peers & { mediaSession: MediaSessionManager }) => {
@@ -63,9 +90,18 @@ const VolumeSpaceAvatar = React.memo((props: Conference_conference_peers & { med
 export const VolumeSpace = React.memo((props: { mediaSession: MediaSessionManager, peers: Conference_conference_peers[] }) => {
     let containerRef = React.useRef<HTMLDivElement>(null);
     let innerContainerRef = React.useRef<HTMLDivElement>(null);
+    let drawListenerRef = React.useRef<HTMLDivElement>(null);
     let selfRef = React.useRef<HTMLDivElement>(null);
+    let [selfPaths, setSelfPaths] = React.useState<Map<string, string>>(new Map());
+    let [erase, setEraseInner] = React.useState(false);
+    let eraseRef = React.useRef(false);
+    let setErase = React.useCallback((er: boolean) => {
+        eraseRef.current = er;
+        setEraseInner(er);
+    }, []);
     useJsDrag(selfRef, selfRef, props.mediaSession.volumeSpace.onSelfMoved, props.mediaSession.volumeSpace.getSelfCoords(), undefined, undefined, undefined, undefined, [props.peers]);
     React.useEffect(() => {
+        // scroll to center
         if (containerRef.current) {
             let centerx = (3000 - containerRef.current.clientWidth) / 2;
             let centery = (3000 - containerRef.current.clientHeight) / 2;
@@ -73,7 +109,7 @@ export const VolumeSpace = React.memo((props: { mediaSession: MediaSessionManage
             console.warn(centerx, centery);
             containerRef.current.scrollBy(centerx, centery);
         }
-
+        // listen obj updates
         return props.mediaSession.volumeSpace.listenPeers(peers => {
             if (innerContainerRef.current) {
                 for (let i = 0; i < innerContainerRef.current.childElementCount; i++) {
@@ -85,20 +121,79 @@ export const VolumeSpace = React.memo((props: { mediaSession: MediaSessionManage
                     }
                 }
             }
-            // for (let [peerId, coords] of Object.entries(peers)) {
-
-            // }
         });
+    }, []);
+
+    React.useEffect(() => {
+        // draw
+        let drawObjs = new Map<string, string>();
+        let drawPaths = new Map<string, number[][]>();
+
+        let down = false;
+        let path: number[][] = [];
+        let id = '';
+        const eraseDisatance = 10;
+        let onMove = (ev: any) => {
+            if (!down) {
+                return;
+            }
+            let coords = [ev.offsetX, ev.offsetY];
+            if (eraseRef.current) {
+                let deleted = false;
+                for (let [key, pth] of drawPaths) {
+                    if (pth.find(p => Math.pow(Math.pow(coords[0] - p[0], 2) + Math.pow(coords[1] - p[1], 2), 0.5) < eraseDisatance)) {
+                        drawPaths.delete(key);
+                        drawObjs.delete(key);
+                        deleted = true;
+                    }
+                }
+                setSelfPaths(new Map(drawObjs));
+            } else {
+                path.push(coords);
+                drawObjs.set(id, bezierPath(path));
+                drawPaths.set(id, path);
+                setSelfPaths(new Map(drawObjs));
+            }
+
+        };
+        let onStart = () => {
+            drawListenerRef.current!.addEventListener('mousemove', onMove);
+            id = `${props.mediaSession.getPeerId()}_path_${uuid()}`;
+            path = [];
+            down = true;
+        };
+
+        let onStop = () => {
+            drawListenerRef.current!.removeEventListener('mousemove', onMove);
+            down = false;
+        };
+        if (drawListenerRef.current) {
+            drawListenerRef.current.addEventListener('mousedown', onStart);
+
+            // drawListenerRef.current.addEventListener('mouseout', onStop);
+            // drawListenerRef.current.addEventListener('mouseleave', onStop);
+            drawListenerRef.current.addEventListener('mouseup', onStop);
+        }
+
     }, []);
     return (
         <div className={VolumeSpaceContainerStyle} ref={containerRef}>
+
             <div className={VolumeSpaceInnerContainerStyle} ref={innerContainerRef}>
+
+                <div className={VolumeSpaceDrawListener} ref={drawListenerRef} />
                 {props.peers.map(p =>
                     <div className={VolumeSpaceItemStyle} key={p.id} id={p.id} ref={p.id === props.mediaSession.getPeerId() ? selfRef : undefined}>
                         <VolumeSpaceAvatar {...p} mediaSession={props.mediaSession} />
                     </div>
                 )}
                 {/* <div style={{ width: 20, height: 20, backgroundColor: 'red', position: 'absolute', left: 1490, top: 1490 }} /> */}
+                <svg viewBox={'0 0 3000 3000'} className={VolumeSpaceDrawContainerStyle}>
+                    {[...selfPaths.entries()].map(e => <path key={e[0]} d={e[1]} stroke="black" fill="transparent" />)}
+                </svg>
+            </div>
+            <div className={DrawControlsContainerStyle}>
+                <UCheckbox onChange={setErase} label="Erase" checked={erase} asSwitcher={true} />
             </div>
         </div>
     );
---
+++ packages/openland-web/modules/conference/smooth.ts
@@ -0,0 +1,54 @@
+const line = (pointA: number[], pointB: number[]) => {
+    const lengthX = pointB[0] - pointA[0];
+    const lengthY = pointB[1] - pointA[1];
+    return {
+        length: Math.sqrt(Math.pow(lengthX, 2) + Math.pow(lengthY, 2)),
+        angle: Math.atan2(lengthY, lengthX)
+    };
+};
+
+const controlPoint = (current: number[], previous: number[], next: number[], reverse?: boolean) => {
+    // When 'current' is the first or last point of the array
+    // 'previous' or 'next' don't exist.
+    // Replace with 'current'
+    const p = previous || current;
+    const n = next || current;
+    // The smoothing ratio
+    const smoothing = 0.2;
+    // Properties of the opposed-line
+    const o = line(p, n);
+    // If is end-control-point, add PI to the angle to go backward
+    const angle = o.angle + (reverse ? Math.PI : 0);
+    const length = o.length * smoothing;
+    // The control point position is relative to the current point
+    const x = current[0] + Math.cos(angle) * length;
+    const y = current[1] + Math.sin(angle) * length;
+    return [x, y, length];
+};
+
+const bezier = (point: number[], prev: number[], prevPrev: number[], next: number[]) => {
+    // start control point
+    const [cpsX, cpsY, len1] = controlPoint(prev, prevPrev, point);
+    // end control point
+    const [cpeX, cpeY, len2] = controlPoint(point, prev, next, true);
+    return ((len1 + len2 > 7) || !prev || !next) ? `C ${cpsX},${cpsY} ${cpeX},${cpeY} ${point[0]},${point[1]}` : undefined;
+};
+
+export const bezierPath = (points: number[][]) => {
+    // build the d attributes by looping over the points
+    let shift = 0;
+    let prev: number[][] = [];
+    return points.reduce((acc, point, i, a) => {
+        if (i === 0) {
+            prev.push(point);
+            return `M ${point[0]},${point[1]}`;
+        } else {
+            let b = bezier(point, prev[prev.length - 1], prev[prev.length - 2], a[i + 1]);
+            if (b) {
+                prev.push(point);
+            }
+            shift -= b ? 0 : 1;
+            return b ? `${acc} ${b}` : acc;
+        }
+    }, '');
+};
