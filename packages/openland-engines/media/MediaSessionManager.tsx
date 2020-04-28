dec8ded3e66b8efaba95c157ba9517102b56ad92
kor_ka <gleb@openland.com>
2020-04-28 20:43:49 +0300

    fix(calls): MediaSessionState - local receiver

--- packages/openland-engines/media/MediaSessionManager.tsx
+++ packages/openland-engines/media/MediaSessionManager.tsx
@@ -69,7 +69,6 @@ export class MediaSessionManager {
         // Initialize reducer
         this.state = new Reducer(reduceState, {
             sender: {
-                id: null,
                 audioEnabled: this.audioEnabled,
                 videoEnabled: this.videoEnabled,
                 screencastEnabled: this.screencastEnabled,
@@ -78,7 +77,8 @@ export class MediaSessionManager {
                 videoTrack: null,
                 screencastTrack: null
             },
-            receivers: {}
+            receivers: {},
+            receiversIds: {},
         });
         this.connectionsInvalidateSync = new InvalidateSync(this.handleState);
         this.doInit();
@@ -280,7 +280,7 @@ export class MediaSessionManager {
             this.iceServers = joinConference.conference.iceServers;
             this.conferenceId = conferenceId;
             this.peerId = joinConference.peerId;
-            this.state.dispatch({ command: 'sender', sender: { id: this.peerId } });
+            this.state.dispatch({ command: 'localPeerId', peerId: this.peerId });
 
             // TODO: Reimplement
             // this.onStatusChange(this.isPrivate ? 'waiting' : 'connected', !this.isPrivate ? joinConference.conference.startTime : undefined);
