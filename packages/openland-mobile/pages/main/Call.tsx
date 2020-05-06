a9ac4cc6d9ae241e04aaba2b8c1948a15c2a8d19
metreniuk <metreniuk@gmail.com>
2020-05-06 22:01:15 +0300

    wip(mobile): add peer info to calls

--- packages/openland-mobile/pages/main/Call.tsx
+++ packages/openland-mobile/pages/main/Call.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { getClient } from 'openland-mobile/utils/graphqlClient';
-import { BackHandler, TouchableOpacity, StatusBar, Dimensions, View, Platform, Image } from 'react-native';
+import { BackHandler, TouchableOpacity, StatusBar, Dimensions, View, Platform, Image, Text } from 'react-native';
 import { XMemo } from 'openland-y-utils/XMemo';
 import { SStatusBar } from 'react-native-s/SStatusBar';
 import { ZModalController, showModal } from 'openland-mobile/components/ZModal';
@@ -22,6 +22,9 @@ import { AppMediaStreamTrack } from 'openland-y-runtime-api/AppMediaStream';
 import { AppUserMediaStreamTrackNative } from 'openland-y-runtime-native/AppUserMedia';
 import { MediaSessionState } from 'openland-engines/media/MediaSessionState';
 import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
+import { TextStyles } from 'openland-mobile/styles/AppStyles';
+import { ThemeGlobal } from 'openland-y-utils/themes/ThemeGlobal';
+import { LoaderSpinner } from 'openland-mobile/components/LoaderSpinner';
 
 export interface PeerMedia {
     videoTrack: AppMediaStreamTrack | null;
@@ -29,9 +32,27 @@ export interface PeerMedia {
     screencastTrack: AppMediaStreamTrack | null;
 }
 
+// @ts-ignore
+const PeerInfoGradient = (props: { children: any }) => {
+    let theme = React.useContext(ThemeContext);
+    return (
+        <ZLinearGradient
+            flexGrow={1}
+            alignSelf="stretch"
+            fallbackColor={theme.transparent}
+            colors={[theme.transparent, theme.overlayMedium]}
+            start={{ x: 0.5, y: 0 }}
+            end={{ x: 0.5, y: 1 }}
+        >
+            {props.children}
+        </ZLinearGradient>
+    );
+};
+
 interface VideoViewProps extends PeerMedia {
     peer: Conference_conference_peers;
     mirror?: boolean;
+    theme: ThemeGlobal;
 }
 
 const VideoView = React.memo((props: VideoViewProps) => {
@@ -42,8 +63,18 @@ const VideoView = React.memo((props: VideoViewProps) => {
     let stream = React.useMemo(() => track && new MediaStream([(track as AppUserMediaStreamTrackNative).track]), [track]);
 
     let colors = getPlaceholderColors(props.peer.user.id);
+    const iconColor = props.theme.foregroundContrast;
+    // @ts-ignore
+    const iconByStatus = {
+        loading: <LoaderSpinner size="small" color={iconColor} />,
+        muted: <Image source={require('assets/ic-muted-bold-16.png')} style={{ tintColor: iconColor }} />,
+        speaking: <Image source={require('assets/ic-speaking-bold-16.png')} style={{ tintColor: iconColor }} />,
+    };
+    let icon = null;
+    let InfoWrapper = React.Fragment;
+    // let InfoWrapper = props.peer.user.photo ? PeerInfoGradient : React.Fragment;
     return (
-        <View flexGrow={1} backgroundColor="gray">
+        <View flexGrow={1} backgroundColor="gray" position="relative">
             {stream && <RTCView streamURL={stream.toURL()} style={{ flexGrow: 1 }} objectFit="cover" mirror={props.mirror} />}
             {stream && <View position="absolute" left={6} top={6}>
                 <ZAvatar size="medium" id={props.peer.user.id} title={props.peer.user.name} photo={props.peer.user.photo} />
@@ -60,9 +91,36 @@ const VideoView = React.memo((props: VideoViewProps) => {
                     <View alignSelf="stretch" flexGrow={1} justifyContent="center" alignItems="center">
                         <ZAvatar size="x-large" id={props.peer.user.id} title={props.peer.user.name} photo={props.peer.user.photo} />
                     </View>
-
                 </ZLinearGradient>
             }
+            <View
+                position="absolute"
+                bottom={0}
+                left={0}
+                right={0}
+            >
+                <InfoWrapper>
+                    <View
+                        flexGrow={1}
+                        paddingHorizontal={16}
+                        paddingVertical={14}
+                        flexDirection="row"
+                        alignItems="center"
+                    >
+                        <Text
+                            style={{
+                                ...TextStyles.Label2,
+                                color: props.theme.foregroundContrast,
+                                flexShrink: 1,
+                            }}
+                            numberOfLines={1}
+                        >
+                            {props.peer.user.name}
+                        </Text>
+                        {icon && <View alignItems="center" marginLeft={8}>{icon}</View>}
+                    </View>
+                </InfoWrapper>
+            </View>
         </View>
     );
 });
@@ -71,7 +129,7 @@ let Content = XMemo<{ id: string, hide: () => void }>((props) => {
     let theme = React.useContext(ThemeContext);
     let [mute, setMute] = React.useState(false);
     let [speaker, setSpeaker] = React.useState(false);
-    
+
     React.useLayoutEffect(() => {
         SStatusBar.setBarStyle('light-content');
         InCallManager.start({ media: 'audio' });
@@ -166,6 +224,7 @@ let Content = XMemo<{ id: string, hide: () => void }>((props) => {
                                 peer={p}
                                 {...media}
                                 mirror={isLocal}
+                                theme={theme}
                             />;
 
                         })}
