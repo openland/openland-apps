334e89fdc92272cce6c96156144e1ab3e7f3d640
Steve Korshakov <steve@korshakov.com>
2020-06-03 14:58:38 -0700

    feat(spacex): Fix retry engine

--- packages/openland-api/createEngineRetry.ts
+++ packages/openland-api/createEngineRetry.ts
@@ -4,15 +4,25 @@ import { GraphqlEngine, GraphqlError, RetryEngine } from '@openland/spacex';
 export function createEngineRetry(engine: GraphqlEngine): GraphqlEngine {
     return new RetryEngine({
         engine: engine,
+        minRetry: 1000,
         errorHandler: (src) => {
+
+            console.warn('Handler: Received error', src);
 
             if (src instanceof GraphqlError) {
 
                 // If retry required
                 for (let e of src.errors) {
                     if (e.shouldRetry) {
-                        console.warn('Unexpected error: asked to try again.');
-                        return null;
+                        let delay: number;
+                        if (typeof e.retryDelay === 'number') {
+                            delay = e.retryDelay;
+                        } else {
+                            delay = Math.floor(1000 + Math.random() * 8000);
+                        }
+
+                        console.warn('Unexpected error: asked to try again. Retrying in ' + delay + ' ms');
+                        return { type: 'retry', delay };
                     }
                 }
 
@@ -32,12 +42,12 @@ export function createEngineRetry(engine: GraphqlEngine): GraphqlEngine {
                         }
                     }
                     console.warn('API Error: ' + message, src.errors);
-                    return new ApiError(message, invalidFields);
+                    return { type: 'error', error: new ApiError(message, invalidFields) };
                 }
             }
 
             console.warn('Unexpected error: try again.');
-            return null;
+            return { type: 'unknown' };
         }
     });
 }
