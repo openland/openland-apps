44ac6ad69269424ceedbd6037c6d6e2fe3140b01
Steve Korshakov <steve@korshakov.com>
2020-06-03 17:23:56 -0700

    feat(posts): Add images,h1,h2 rendering to post view

--- packages/openland-web/fragments/discussions/components/ParagraphComponent.tsx
+++ packages/openland-web/fragments/discussions/components/ParagraphComponent.tsx
@@ -1,12 +1,48 @@
 import * as React from 'react';
 import { XView } from 'react-mental';
 import { ParagraphSimple } from 'openland-api/spacex.types';
+import { XCloudImage } from 'openland-x/XCloudImage';
+import { layoutMedia } from 'openland-y-utils/MediaLayout';
 
 export const ParagraphComponent = React.memo((props: { data: ParagraphSimple }) => {
     if (props.data.__typename === 'TextParagraph') {
         return (
             <XView>
                 {props.data.text}
+            </XView>
+        );
+    }
+    if (props.data.__typename === 'H1Paragraph') {
+        return (
+            <XView>
+                {props.data.text}
+            </XView>
+        );
+    }
+    if (props.data.__typename === 'H2Paragraph') {
+        return (
+            <XView>
+                {props.data.text}
+            </XView>
+        );
+    }
+    if (props.data.__typename === 'ImageParagraph') {
+        const layoutModal = layoutMedia(
+            props.data.fileMetadata!.imageWidth!,
+            props.data.fileMetadata!.imageHeight!,
+            600,
+            600,
+            32,
+            32,
+        );
+        return (
+            <XView>
+                <XCloudImage
+                    photoRef={{ uuid: props.data.image.uuid }}
+                    width={layoutModal.width}
+                    height={layoutModal.height}
+                    resize="fill"
+                />
             </XView>
         );
     }
