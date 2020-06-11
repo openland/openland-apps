a2831e53b8f3de96616d4dd8c93a797d0dd9c269
metreniuk <metreniuk@gmail.com>
2020-06-11 16:42:11 +0300

    fix(web): update invite friends UI

--- packages/openland-web/fragments/account/SettingsInviteFriendsFragment.tsx
+++ packages/openland-web/fragments/account/SettingsInviteFriendsFragment.tsx
@@ -1,59 +1,56 @@
 import * as React from 'react';
 import copy from 'copy-to-clipboard';
-import { XView } from 'react-mental';
+import { XView, XViewProps } from 'react-mental';
 import { css, cx } from 'linaria';
 import { useClient } from 'openland-api/useClient';
-import { XImage } from 'react-mental';
-import LinkedInIcon from 'openland-icons/linkedin-2.svg';
-import TwitterIcon from 'openland-icons/twitter-2.svg';
-import FacebookIcon from 'openland-icons/ic-fb.svg';
-import { XModalController } from 'openland-x/showModal';
+import LinkedInIcon from 'openland-icons/s/ic-linkedin-24.svg';
+import TwitterIcon from 'openland-icons/s/ic-twitter-24.svg';
+import FacebookIcon from 'openland-icons/s/ic-facebook-24.svg';
+import DoneIcon from 'openland-icons/s/ic-done-bold-16.svg';
 import { useLayout } from 'openland-unicorn/components/utils/LayoutContext';
 import { UButton } from 'openland-web/components/unicorn/UButton';
 import { OwnerLinkComponent } from 'openland-web/fragments/invite/OwnerLinkComponent';
-import { TextBody } from 'openland-web/utils/TextStyles';
+import { TextBody, TextStyles, TextTitle1 } from 'openland-web/utils/TextStyles';
 import { Page } from 'openland-unicorn/Page';
 import { UHeader } from 'openland-unicorn/UHeader';
 import { trackEvent } from 'openland-x-analytics';
-
-const textAlignCenterClassName = css`
-    text-align: center;
-`;
+import { plural } from 'openland-y-utils/plural';
+import { UIcon } from 'openland-web/components/unicorn/UIcon';
 
 const socialTextStyle = css`
     width: 100%;
-    padding: 16px;
+    padding: 8px 16px;
     border-radius: 8px;
-    background-color: var(--backgroundTertiary);
+    background-color: var(--backgroundTertiaryTrans);
     color: var(--foregroundPrimary);
     white-space: pre-wrap;
 `;
 
 const SocialButtonClassName = css`
-    width: 36px;
-    height: 36px;
-    background-color: #e8f4ff;
+    width: 40px;
+    height: 40px;
+    background-color: var(--backgroundTertiaryTrans);
     border-radius: 50px;
     display: flex !important;
     align-items: center;
     justify-content: center;
     transition: all 0.2s ease;
     cursor: pointer;
+
     & svg {
-        height: 18px;
-        width: 18px;
+        height: 24px;
+        width: 24px;
     }
     & svg path {
         opacity: 1;
         transition: all 0.2s ease;
-        fill: #1790ff;
+        fill: var(--foregroundTertiary);
     }
     &:hover {
-        background-color: #1790ff;
+        background-color: var(--backgroundTertiaryHoverTrans);
     }
-    &:hover svg path {
-        opacity: 1;
-        fill: #fff;
+    &:not(:last-child) {
+        margin-right: 8px;
     }
 `;
 
@@ -71,7 +68,7 @@ const SocialButton = (props: SocialButtonProps) => (
 const WritePostBlock = (props: { inviteKey: string; isMobile: boolean }) => {
     const sharingUrl = 'https://openland.com/invite/' + props.inviteKey;
     const sharingText =
-        "Check out Openland, a new community app. There are groups for almost every industry, professional role, skill, interest, and location. Openland is a great place to make new friends, expand your professional network, learn from pros, and get help for any challenge you face.\nInvite to join:\n";
+        "Check out Openland, a new community app. There are groups for almost every industry, professional role, skill, interest, and location. Openland is a great place to make new friends, expand your professional network, learn from pros, and get help for any challenge you face.\nInvite to join: ";
     const sharingTextFull = sharingText + sharingUrl;
     const [copied, setCopied] = React.useState(false);
     const copyText = () => {
@@ -100,14 +97,11 @@ const WritePostBlock = (props: { inviteKey: string; isMobile: boolean }) => {
     };
     return (
         <XView flexDirection="column" alignItems="center" flexGrow={0} alignSelf="stretch">
-            <XView fontSize={18} color="#000" fontWeight="600" marginBottom={20}>
-                <span className={textAlignCenterClassName}>Write a post</span>
-            </XView>
             <XView flexGrow={1} alignSelf="stretch">
                 <div className={cx(socialTextStyle, TextBody)}>{sharingTextFull}</div>
             </XView>
             <XView
-                marginTop={24}
+                marginTop={16}
                 alignSelf="stretch"
                 flexDirection="row"
                 alignItems="center"
@@ -120,15 +114,12 @@ const WritePostBlock = (props: { inviteKey: string; isMobile: boolean }) => {
                     onClick={copyText}
                 />
                 <XView flexDirection="row" alignItems="center">
-                    <XView fontSize={14} color="#000" marginRight={16}>
+                    <XView {...TextStyles.Label1} color="var(--foregroundPrimary)" marginRight={16}>
                         {props.isMobile ? 'Share' : 'Share on'}
                     </XView>
                     <XView
                         flexDirection="row"
                         alignItems="center"
-                        width={132}
-                        paddingTop={3}
-                        justifyContent="space-between"
                     >
                         <SocialButton
                             icon={<FacebookIcon />}
@@ -149,95 +140,101 @@ const WritePostBlock = (props: { inviteKey: string; isMobile: boolean }) => {
     );
 };
 
-const InviteAcceptedBlock = (props: { accepted: number }) => (
-    <XView
-        alignSelf="center"
-        alignItems="center"
-        flexDirection="row"
-        backgroundColor="#EFF9F1"
-        flexGrow={0}
-        borderRadius={40}
-        paddingVertical={4}
-        paddingHorizontal={14}
-        color="#5ccc6e"
-        fontSize={14}
-        fontWeight="600"
-        marginTop={18}
-    >
-        {props.accepted} {`${props.accepted === 1 ? 'invite' : 'invites'} accepted`}
-    </XView>
-);
+const headerTitleStyle = cx(TextTitle1, css`
+    align-self: center;
+    flex: 1;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    color: var(--foregroundPrimary);
+`);
 
-interface InviteFriendsFragmentProps {
-    modalContext?: XModalController;
-    onSettingPage?: boolean;
-}
+const headerTitleFullscreenStyle = css`
+    flex-grow: 0;
+    flex-basis: auto;
+    text-align: center;
+    margin-bottom: 16px;
+`;
 
-export const InviteFriendsComponent = (props: InviteFriendsFragmentProps) => {
-    const client = useClient(),
-        inviteCount = client.useMySuccessfulInvitesCount(),
-        { invite: openlandInvite } = client.useAccountAppInvite(),
-        isMobile = useLayout() === 'mobile';
+const headerIconStyle = css`
+    margin-right: 8px;
+`;
 
+export const InviteFriendsContent = (props: XViewProps) => {
+    const client = useClient();
+    const { invite: openlandInvite } = client.useAccountAppInvite();
+    const isMobile = useLayout() === 'mobile';
     return (
-        <XView
-            flexDirection="row"
-            position="relative"
-            flexGrow={1}
-            flexShrink={1}
-            justifyContent="center"
-            paddingLeft={isMobile ? 20 : 0}
-            paddingRight={isMobile ? 20 : 0}
-            backgroundColor="#fff"
-        >
-            {props.modalContext && (
-                <XView position={isMobile ? 'absolute' : 'fixed'} top={19} left={32}>
-                    <XImage src="/static/landing/logotype.svg" width={145} height={42} />
-                </XView>
-            )}
+        <XView flexGrow={1} marginTop={16} {...props}>
             <XView
-                flexDirection="column"
-                alignItems="center"
-                width={isMobile ? '100%' : 420}
-                maxWidth={420}
-                marginTop={isMobile ? (props.onSettingPage ? undefined : 120) : 32}
-                paddingBottom={40}
+                {...TextStyles.Title3}
+                paddingVertical={12}
+                color="var(--foregroundPrimary)"
             >
-                <XView fontSize={24} fontWeight="600" color="#000" flexGrow={0}>
-                    <span className={textAlignCenterClassName}>Invite friends to Openland</span>
-                </XView>
-                {inviteCount.mySuccessfulInvitesCount > 0 && (
-                    <InviteAcceptedBlock accepted={inviteCount.mySuccessfulInvitesCount} />
-                )}
-                <XView flexGrow={0} alignSelf="stretch" marginTop={142} marginBottom={78}>
-                    <XView
-                        flexDirection="row"
-                        alignItems="center"
-                        fontSize={18}
-                        fontWeight="600"
-                        color="#000"
-                        alignSelf="center"
-                        marginBottom={16}
-                    >
-                        <span className={textAlignCenterClassName}>Invite link</span>
-                    </XView>
-                    <OwnerLinkComponent
-                        inviteKey={openlandInvite}
-                        isGroup={false}
-                        isOrganization={false}
-                        hideRevoke={true}
-                        isAppInvite={true}
-                    />
-                </XView>
-                <WritePostBlock inviteKey={openlandInvite} isMobile={isMobile} />
+                Send link
+            </XView>
+            <OwnerLinkComponent
+                inviteKey={openlandInvite}
+                isGroup={false}
+                isOrganization={false}
+                hideRevoke={true}
+                isAppInvite={true}
+            />
+            <XView
+                {...TextStyles.Title3}
+                paddingVertical={12}
+                marginTop={32}
+                color="var(--foregroundPrimary)"
+            >
+                Write post
+            </XView >
+            <WritePostBlock inviteKey={openlandInvite} isMobile={isMobile} />
+        </XView >
+    );
+};
+
+export const InviteFriendsFullscreen = () => {
+    return (
+        <XView flexGrow={1} flexShrink={1}>
+            <XView flexGrow={1} flexShrink={1} alignSelf="center" justifyContent="center" width="100%" maxWidth={600} paddingHorizontal={16}>
+                <div className={cx(headerTitleStyle, headerTitleFullscreenStyle)}>
+                    Invite friends
+                </div>
+                <InviteFriendsContent flexGrow={0} justifyContent="center" />
             </XView>
         </XView>
     );
 };
 
-export const InviteFriendsFragment = React.memo(() => (
-    <Page track="account_invite">
-        <UHeader documentTitle="Invite" />
-        <InviteFriendsComponent onSettingPage={true} />
-    </Page>
-));
+export const InviteFriendsFragment = React.memo(() => {
+    const client = useClient();
+    const inviteCount = client.useMySuccessfulInvitesCount();
+    return (
+        <Page track="account_invite">
+            <UHeader
+                documentTitle="Invite friends"
+                titleView={(
+                    <>
+                        <div className={headerTitleStyle}>
+                            Invite friends
+                        </div>
+                        {inviteCount.mySuccessfulInvitesCount > 0 && (
+                            <XView
+                                {...TextStyles.Label1}
+                                alignSelf="center"
+                                flexDirection="row"
+                                alignItems="center"
+                                color="var(--accentPositive)"
+                                marginLeft={8}
+                            >
+                                <UIcon className={headerIconStyle} icon={<DoneIcon />} size={16} color="var(--accentPositive)" />
+                                {plural(inviteCount.mySuccessfulInvitesCount, ['invite', 'invites'])} accepted
+                            </XView>
+                        )}
+                    </>
+                )}
+            />
+            <InviteFriendsContent />
+        </Page>
+    );
+});
