00b54371cc5d108e08d3c9bbe29e5f15f83564f5
metreniuk <metreniuk@gmail.com>
2020-07-03 15:51:56 +0300

    fix(web): redirect on /signin for app invite

--- packages/openland-web/pages/auth/create-new-account.page.tsx
+++ packages/openland-web/pages/auth/create-new-account.page.tsx
@@ -9,6 +9,7 @@ import { useWithWidth } from 'openland-web/hooks/useWithWidth';
 import { XRouterContext } from 'openland-x-routing/XRouterContext';
 import { AuthHeaderConfig } from './root.page';
 import { AskAuthDataProps } from './ask-auth-data.page';
+import Cookies from 'js-cookie';
 
 type AuthMechanism = AskAuthDataProps & {
     loginWith: (phone: boolean) => void;
@@ -17,12 +18,14 @@ type AuthMechanism = AskAuthDataProps & {
 const SignUpAuthMechanism = (props: AuthMechanism) => {
     const { loginWith } = props;
     const [width] = useWithWidth();
+    const hasAppInvite = !!Cookies.get('x-openland-app-invite');
+    const inviterName = hasAppInvite ? localStorage.getItem('app-inviter-name') : null;
     return (
         <FormLayout>
             <XView alignItems="center" marginBottom={16}>
                 <Unicorn width="96" height="96" />
             </XView>
-            <Title text="Openland" />
+            <Title text={inviterName ? `${inviterName} invites you to join Openland` : 'Openland'} />
             <Subtitle text="Modern social network built&nbsp;for&nbsp;you,&nbsp;not&nbsp;advertisers" />
 
             <XView alignSelf="center" width={width && width < 400 ? '100%' : 240} marginTop={32}>
