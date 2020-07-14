fd4bbcd9b0502bcca6410264830a2c309319c3b2
Steve Korshakov <steve@korshakov.com>
2020-07-13 18:34:28 -0700

    wip(core): Add sparse index

---
+++ packages/openland-engines/new/SparseIndex.spec.ts
@@ -0,0 +1,133 @@
+import { SparseIndex, SparseIndexState } from './SparseIndex';
+describe('SparseIndex', () => {
+    it('should create window', () => {
+        let index = new SparseIndex(SparseIndex.EMPTY);
+
+        // Empty state
+        let state: SparseIndexState = { windows: [] };
+        expect(index.state).toMatchObject(state);
+
+        // Initial apply
+        index.apply({ min: 10, max: 20, items: [{ id: '1', sortKey: 11 }, { id: '2', sortKey: 12 }, { id: '3', sortKey: 13 }] });
+        state = { windows: [{ min: 10, max: 20, items: [{ id: '1', sortKey: 11 }, { id: '2', sortKey: 12 }, { id: '3', sortKey: 13 }] }] };
+        expect(index.state).toMatchObject(state);
+
+        // Second separate window
+        index.apply({ min: 1, max: 8, items: [{ id: '4', sortKey: 1 }, { id: '5', sortKey: 2 }, { id: '6', sortKey: 3 }] });
+        state = {
+            windows: [
+                { min: 1, max: 8, items: [{ id: '4', sortKey: 1 }, { id: '5', sortKey: 2 }, { id: '6', sortKey: 3 }] },
+                { min: 10, max: 20, items: [{ id: '1', sortKey: 11 }, { id: '2', sortKey: 12 }, { id: '3', sortKey: 13 }] }]
+        };
+        expect(index.state).toMatchObject(state);
+
+        // Merge two windows
+        index.apply({ min: 8, max: 10, items: [{ id: '7', sortKey: 10 }] });
+        state = {
+            windows: [
+                {
+                    min: 1, max: 20,
+                    items: [
+                        { id: '4', sortKey: 1 },
+                        { id: '5', sortKey: 2 },
+                        { id: '6', sortKey: 3 },
+                        { id: '7', sortKey: 10 },
+                        { id: '1', sortKey: 11 },
+                        { id: '2', sortKey: 12 },
+                        { id: '3', sortKey: 13 }
+                    ]
+                }
+            ]
+        };
+        expect(index.state).toMatchObject(state);
+    });
+
+    it('should merge windows', () => {
+        let index = new SparseIndex(SparseIndex.EMPTY);
+
+        // First segment
+        index.apply({ min: 10, max: 20, items: [{ id: '1', sortKey: 11 }, { id: '2', sortKey: 12 }, { id: '3', sortKey: 13 }] });
+
+        // Second segment
+        index.apply({ min: 100, max: 120, items: [{ id: '10', sortKey: 101 }, { id: '20', sortKey: 102 }, { id: '30', sortKey: 103 }] });
+
+        // Third segment
+        index.apply({ min: 25, max: 28, items: [{ id: '4', sortKey: 26 }, { id: '5', sortKey: 27 }, { id: '6', sortKey: 28 }] });
+
+        // Forth segment
+        index.apply({ min: 50, max: 60, items: [{ id: '40', sortKey: 55 }] });
+
+        let state: SparseIndexState = {
+            windows: [
+                { min: 10, max: 20, items: [{ id: '1', sortKey: 11 }, { id: '2', sortKey: 12 }, { id: '3', sortKey: 13 }] },
+                { min: 25, max: 28, items: [{ id: '4', sortKey: 26 }, { id: '5', sortKey: 27 }, { id: '6', sortKey: 28 }] },
+                { min: 50, max: 60, items: [{ id: '40', sortKey: 55 }] },
+                { min: 100, max: 120, items: [{ id: '10', sortKey: 101 }, { id: '20', sortKey: 102 }, { id: '30', sortKey: 103 }] },
+            ]
+        };
+        expect(index.state).toMatchObject(state);
+
+        // Merge
+        index.apply({ min: 25, max: 51, items: [{ id: '44', sortKey: 25 }, { id: '45', sortKey: 51 }] });
+
+        state = {
+            windows: [
+                { min: 10, max: 20, items: [{ id: '1', sortKey: 11 }, { id: '2', sortKey: 12 }, { id: '3', sortKey: 13 }] },
+                { min: 25, max: 60, items: [{ id: '44', sortKey: 25 }, { id: '4', sortKey: 26 }, { id: '5', sortKey: 27 }, { id: '6', sortKey: 28 }, { id: '45', sortKey: 51 }, { id: '40', sortKey: 55 }] },
+                { min: 100, max: 120, items: [{ id: '10', sortKey: 101 }, { id: '20', sortKey: 102 }, { id: '30', sortKey: 103 }] },
+            ]
+        };
+        expect(index.state).toMatchObject(state);
+
+        // Merge with empty items
+        index.apply({ min: 1, max: 120, items: [] });
+        state = {
+            windows: [
+                {
+                    min: 1, max: 120, items: [
+                        { id: '1', sortKey: 11 },
+                        { id: '2', sortKey: 12 },
+                        { id: '3', sortKey: 13 },
+                        { id: '44', sortKey: 25 },
+                        { id: '4', sortKey: 26 },
+                        { id: '5', sortKey: 27 },
+                        { id: '6', sortKey: 28 },
+                        { id: '45', sortKey: 51 },
+                        { id: '40', sortKey: 55 },
+                        { id: '10', sortKey: 101 },
+                        { id: '20', sortKey: 102 },
+                        { id: '30', sortKey: 103 }
+                    ]
+                }
+            ]
+        };
+        expect(index.state).toMatchObject(state);
+
+        // Append
+        index.apply({ min: SparseIndex.MIN, max: 11, items: [{ id: '33', sortKey: 0 }] });
+        state = {
+            windows: [
+                {
+                    min: SparseIndex.MIN,
+                    max: 120,
+                    items: [
+                        { id: '33', sortKey: 0 },
+                        { id: '1', sortKey: 11 },
+                        { id: '2', sortKey: 12 },
+                        { id: '3', sortKey: 13 },
+                        { id: '44', sortKey: 25 },
+                        { id: '4', sortKey: 26 },
+                        { id: '5', sortKey: 27 },
+                        { id: '6', sortKey: 28 },
+                        { id: '45', sortKey: 51 },
+                        { id: '40', sortKey: 55 },
+                        { id: '10', sortKey: 101 },
+                        { id: '20', sortKey: 102 },
+                        { id: '30', sortKey: 103 }
+                    ]
+                }
+            ]
+        };
+        expect(index.state).toMatchObject(state);
+    });
+});
