b6130d67e743c02c85bed118cff96a00795c87bd
Max <qwertynestor40@gmail.com>
2020-07-16 00:37:08 +0300

    feat(mobile): work from contacts

--- packages/openland-mobile/pages/main/Contacts.tsx
+++ packages/openland-mobile/pages/main/Contacts.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import { PageProps } from 'openland-mobile/components/PageProps';
-import { withApp } from 'openland-mobile/components/withApp';
-import { Text, Image } from 'react-native';
 import { SHeader } from 'react-native-s/SHeader';
+import { SSearchControler } from 'react-native-s/SSearchController';
+import ActionSheet from 'openland-mobile/components/ActionSheet';
+import { withApp } from 'openland-mobile/components/withApp';
+import { GlobalSearch } from './components/globalSearch/GlobalSearch';
+import { getClient } from 'openland-mobile/utils/graphqlClient';
+import { Text, Image } from 'react-native';
 import { ASSafeAreaView } from 'react-native-async-view/ASSafeAreaView';
 import { TextStyles } from 'openland-mobile/styles/AppStyles';
+import { UserView } from './components/UserView';
 import { ThemeContext } from 'openland-mobile/themes/ThemeContext';
+import { ZLoader } from 'openland-mobile/components/ZLoader';
+import { SFlatList } from 'react-native-s/SFlatList';
+import { SDeferred } from 'react-native-s/SDeferred';
+import { MyContacts_myContacts_items_user } from 'openland-api/spacex.types';
 
 const ContactsStub = React.memo(() => {
     let theme = React.useContext(ThemeContext);
@@ -47,11 +56,92 @@ const ContactsStub = React.memo(() => {
     );
 });
 
+const ContactsList = React.memo((props: PageProps) => {
+    const client = getClient();
+    const initialContacts = client.useMyContacts({ first: 20 }, { fetchPolicy: 'network-only' }).myContacts;
+    const [contacts, setContacts] = React.useState(initialContacts.items);
+    const [loading, setLoading] = React.useState(false);
+    const [loadedFull, setLoadedFull] = React.useState(false);
+
+    const handleContactLongPress = React.useCallback(
+        (user: MyContacts_myContacts_items_user) => {
+            const builder = ActionSheet.builder();
+            builder.cancelable(false);
+            builder.action(
+                'Send message',
+                () => props.router.push('Conversation', { id: user.id }),
+                false,
+                require('assets/ic-message-24.png'),
+            );
+
+            builder.show(true);
+        },
+        [],
+    );
+
+    const handleLoadMore = React.useCallback(async () => {
+        if (contacts.length && !loading && !loadedFull) {
+            setLoading(true);
+            const loaded = (
+                await client.queryMyContacts(
+                    {
+                        first: 10,
+                        after: initialContacts.cursor,
+                    },
+                    { fetchPolicy: 'network-only' },
+                )
+            ).myContacts;
+
+            setContacts((current) => [
+                ...current,
+                ...loaded.items.filter(c => !current.find(c2 => c2.user.id === c.user.id)),
+            ]);
+            if (!loaded.cursor) {
+                setLoadedFull(true);
+            }
+            setLoading(false);
+        }
+    }, [contacts, loading, loadedFull]);
+
+    return (
+        <SFlatList
+            data={contacts}
+            onEndReached={handleLoadMore}
+            refreshing={loading}
+            keyExtractor={(item, index) => index + '-' + item.user.id}
+            renderItem={({ item }) => (
+                <UserView
+                    user={item.user}
+                    showOrganization={false}
+                    onPress={() => props.router.push('ProfileUser', { id: item.user.id })}
+                    onLongPress={() => handleContactLongPress(item.user)}
+                />
+            )}
+        />
+    );
+});
+
 const ContactsPage = React.memo((props: PageProps) => {
+    const client = getClient();
+    const contacts = client.useMyContacts({ first: 1 }, { fetchPolicy: 'network-only' }).myContacts.items;
+    const isEmpty = contacts.length === 0;
+    const content = isEmpty ? <ContactsStub /> : <ContactsList {...props} />;
     return (
         <>
             <SHeader title="Contacts" />
-            <ContactsStub />
+            <SSearchControler
+                searchRender={(p) => (
+                    <GlobalSearch
+                        query={p.query}
+                        router={props.router}
+                        onUserPress={(id: string) => props.router.push('ProfileUser', { id: id })}
+                    />
+                )}
+            >
+                <React.Suspense fallback={<ZLoader />}>
+                    <SDeferred>{content}</SDeferred>
+                </React.Suspense>
+            </SSearchControler>
         </>
     );
 });
--- packages/openland-mobile/pages/main/components/UserView.tsx
+++ packages/openland-mobile/pages/main/components/UserView.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react';
-import { UserShort, RoomMemberRole, OrganizationMemberRole, UserBadge } from 'openland-api/spacex.types';
+import {
+    UserShort,
+    RoomMemberRole,
+    OrganizationMemberRole,
+    UserBadge,
+} from 'openland-api/spacex.types';
 import { ZListItemBase } from 'openland-mobile/components/ZListItemBase';
 import { View, Text, Image } from 'react-native';
 import { PresenceComponent } from './PresenceComponent';
@@ -17,27 +22,126 @@ interface UserViewProps {
     subtitle?: string;
     subtitleColor?: string;
     paddingRight?: number;
+    showOrganization?: boolean;
 }
 
 export const UserView = (props: UserViewProps) => {
-    const { user, memberRole, badge, enabled, onPress, onLongPress, subtitle, subtitleColor, paddingRight } = props;
+    const {
+        user,
+        memberRole,
+        badge,
+        enabled,
+        onPress,
+        onLongPress,
+        subtitle,
+        subtitleColor,
+        paddingRight,
+        showOrganization = true,
+    } = props;
     const theme = React.useContext(ThemeContext);
 
     let showCrown = memberRole === 'OWNER' || memberRole === 'ADMIN';
     let isOwner = memberRole === 'OWNER';
 
     return (
-        <ZListItemBase key={user.id} separator={false} height={56} onPress={onPress} onLongPress={onLongPress} enabled={enabled}>
-            <View paddingLeft={16} paddingRight={16} alignSelf="center" opacity={enabled === false ? 0.5 : 1}>
-                <ZAvatar size="medium" photo={user.photo} id={user.id} title={user.name} online={user.online} />
+        <ZListItemBase
+            key={user.id}
+            separator={false}
+            height={56}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            enabled={enabled}
+        >
+            <View
+                paddingLeft={16}
+                paddingRight={16}
+                alignSelf="center"
+                opacity={enabled === false ? 0.5 : 1}
+            >
+                <ZAvatar
+                    size="medium"
+                    photo={user.photo}
+                    id={user.id}
+                    title={user.name}
+                    online={user.online}
+                />
             </View>
-            <View alignSelf="center" flexGrow={1} flexBasis={0} alignItems="flex-start" justifyContent="center" flexDirection="column" opacity={enabled === false ? 0.5 : 1} paddingRight={paddingRight || 16}>
+            <View
+                alignSelf="center"
+                flexGrow={1}
+                flexBasis={0}
+                alignItems="flex-start"
+                justifyContent="center"
+                flexDirection="column"
+                opacity={enabled === false ? 0.5 : 1}
+                paddingRight={paddingRight || 16}
+            >
                 <View flexDirection="row">
-                    {showCrown && <Image source={require('assets/ic-crown-16.png')} style={{ tintColor: (isOwner ? '#fbc139' : '#c0c0c0'), alignSelf: 'center', marginRight: 3 }} />}
-                    {!!badge && <Image source={require('assets/ic-star-admin-16.png')} style={{ tintColor: theme.accentPrimary, alignSelf: 'center', marginRight: 3 }} />}
-                    <Text style={{ ...TextStyles.Label1, color: theme.foregroundPrimary, flexGrow: 1, flexBasis: 0 }} numberOfLines={1} ellipsizeMode="tail" allowFontScaling={false}>{user.name}{user.primaryOrganization && <Text style={{ ...TextStyles.Label2, color: theme.foregroundTertiary }}> {user.primaryOrganization.name}</Text>}</Text>
+                    {showCrown && (
+                        <Image
+                            source={require('assets/ic-crown-16.png')}
+                            style={{
+                                tintColor: isOwner ? '#fbc139' : '#c0c0c0',
+                                alignSelf: 'center',
+                                marginRight: 3,
+                            }}
+                        />
+                    )}
+                    {!!badge && (
+                        <Image
+                            source={require('assets/ic-star-admin-16.png')}
+                            style={{
+                                tintColor: theme.accentPrimary,
+                                alignSelf: 'center',
+                                marginRight: 3,
+                            }}
+                        />
+                    )}
+                    <Text
+                        style={{
+                            ...TextStyles.Label1,
+                            color: theme.foregroundPrimary,
+                            flexGrow: 1,
+                            flexBasis: 0,
+                        }}
+                        numberOfLines={1}
+                        ellipsizeMode="tail"
+                        allowFontScaling={false}
+                    >
+                        {user.name}
+                        {showOrganization && user.primaryOrganization && (
+                            <Text style={{ ...TextStyles.Label2, color: theme.foregroundTertiary }}>
+                                {' '}
+                                {user.primaryOrganization.name}
+                            </Text>
+                        )}
+                    </Text>
                 </View>
-                {subtitle ? <Text style={{ ...TextStyles.Subhead, color: subtitleColor ? subtitleColor : theme.foregroundTertiary }} numberOfLines={1} ellipsizeMode="tail" allowFontScaling={false}>{subtitle}</Text> : <PresenceComponent isBot={user.isBot} uid={user.id} lastSeen={user.lastSeen} online={user.online} style={{ ...TextStyles.Subhead, color: theme.foregroundTertiary }} onlineStyle={{ color: theme.accentPrimary }} numberOfLines={1} ellipsizeMode="tail" allowFontScaling={false} />}
+                {subtitle ? (
+                    <Text
+                        style={{
+                            ...TextStyles.Subhead,
+                            color: subtitleColor ? subtitleColor : theme.foregroundTertiary,
+                        }}
+                        numberOfLines={1}
+                        ellipsizeMode="tail"
+                        allowFontScaling={false}
+                    >
+                        {subtitle}
+                    </Text>
+                ) : (
+                    <PresenceComponent
+                        isBot={user.isBot}
+                        uid={user.id}
+                        lastSeen={user.lastSeen}
+                        online={user.online}
+                        style={{ ...TextStyles.Subhead, color: theme.foregroundTertiary }}
+                        onlineStyle={{ color: theme.accentPrimary }}
+                        numberOfLines={1}
+                        ellipsizeMode="tail"
+                        allowFontScaling={false}
+                    />
+                )}
             </View>
         </ZListItemBase>
     );
