3604afd5d92afeb9554d777bb92b8a3a47605120
Fred Morozov <morozov@fred.md>
2020-07-25 07:39:19 +0300

    fix(landing): fix avatars borders

--- packages/openland-landing/components/TiltPreview.tsx
+++ packages/openland-landing/components/TiltPreview.tsx
@@ -100,6 +100,7 @@ const avatar = css`
         overflow: hidden;
         border-radius: 23px;
         border: 1px solid var(--border);
+        width: 100%; height: 100%;
 
         @media (min-width: 768px) and (max-width: 1199px) {
             border-radius: 15px;
