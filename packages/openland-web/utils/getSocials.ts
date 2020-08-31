badc697778540d222480057551424d4902ecb1a1
Arseniy Nikitochkin <nikitochkinav@mos-team.ru>
2020-08-31 17:08:48 +0300

    fix(web): profiles socials fix

--- packages/openland-web/utils/getSocials.ts
+++
@@ -1,9 +0,0 @@
-export function getSocialId(url: string) {
-    const result = url.replace(/@|in\//g, '').split('.com/');
-
-    return result.length === 1 ? result[0] : result[1];
-}
-
-export function beautifyUrl(url: string) {
-    return url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "");
-}
