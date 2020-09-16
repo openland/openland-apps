ee795caab3c9ed16cb047922f6335006f2332190
krigga <krigga7@gmail.com>
2020-09-16 23:24:45 +0300

    fix(mobile): sticker picker horizontal scroll

--- packages/openland-mobile/pages/main/components/stickers/StickerPicker.tsx
+++ packages/openland-mobile/pages/main/components/stickers/StickerPicker.tsx
@@ -203,6 +203,7 @@ export const StickerPicker = React.memo((props: StickerPickerProps) => {
                                 }
                             }
                         }}
+                        directionalLockEnabled={true}
                     />
                     <FlatList
                         ref={stickerPackButtonListRef}
