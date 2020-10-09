f2c7ac02f43b0ddf28290d389304ccd60016eaf3
metreniuk <metreniuk@gmail.com>
2020-10-09 16:10:45 +0300

    fix(mobile): improve files sending

--- packages/openland-mobile/files/showAttachMenu.tsx
+++ packages/openland-mobile/files/showAttachMenu.tsx
@@ -1,11 +1,42 @@
 import { ActionSheetBuilder } from 'openland-mobile/components/ActionSheet';
 import { Platform } from 'react-native';
 import { checkPermissions } from 'openland-mobile/utils/permissions/checkPermissions';
-import Picker from 'react-native-image-crop-picker';
+import Picker, { ImageOrVideo } from 'react-native-image-crop-picker';
 
 import { handlePermissionDismiss } from 'openland-mobile/utils/permissions/handlePermissionDismiss';
 import { checkFileIsPhoto } from 'openland-y-utils/checkFileIsPhoto';
 import { DocumentPicker, DocumentPickerUtil } from 'react-native-document-picker';
+
+type PickerMedia = {
+    type: 'document' | 'photo' | 'video',
+    name: string,
+    path: string,
+    size: number,
+};
+
+const normalizeMedia = (data: ImageOrVideo[]) => {
+    let filesMeta = data.reduce((acc, response) => {
+        const isPhoto = checkFileIsPhoto(response.path);
+        if (isPhoto) {
+            acc.images.push({ type: 'photo', name: 'image.jpg', path: response.path, size: response.size });
+        } else {
+            acc.videos.push({ type: 'video', name: 'video.mp4', path: response.path, size: response.size });
+        }
+        return acc;
+    }, { images: [] as PickerMedia[], videos: [] as PickerMedia[] });
+
+    let imagesBatches = filesMeta.images.reduce((acc, image, i) => {
+        let current = acc[acc.length - 1];
+        if (!current || current && current.length === 4) {
+            acc.push([image]);
+        } else if (current && current.length < 4) {
+            current.push(image);
+        }
+        return acc;
+    }, [] as PickerMedia[][]);
+
+    return { imagesBatches, videos: filesMeta.videos };
+};
 
 export const showAttachMenu = (fileCallback?: (filesMeta: ({ type: 'document' | 'photo' | 'video', name: string, path: string, size: number })[]) => void, donationCb?: () => void) => {
     let builder = new ActionSheetBuilder();
@@ -55,14 +86,18 @@ export const showAttachMenu = (fileCallback?: (filesMeta: ({ type: 'document' | 
                 compressImageQuality: 1,
                 compressVideoPreset: 'MediumQuality',
                 cropping: false,
+                maxFiles: Platform.select({ ios: 20, android: undefined }),
                 mediaType: Platform.select({ ios: 'any', android: 'photo', default: 'photo' })
             }).then(pickerResponse => {
-                let filesMeta = pickerResponse.map(response => {
-                    const isPhoto = checkFileIsPhoto(response.path);
-                    return { type: isPhoto ? 'photo' : 'video' as 'document' | 'photo' | 'video', name: isPhoto ? 'image.jpg' : 'video.mp4', path: response.path, size: response.size };
-                });
                 if (fileCallback) {
-                    fileCallback(filesMeta);
+                    let { imagesBatches, videos } = normalizeMedia(pickerResponse);
+
+                    if (imagesBatches.length > 0) {
+                        imagesBatches.forEach(x => fileCallback(x));
+                    }
+                    if (videos.length > 0) {
+                        videos.forEach(x => fileCallback([x]));
+                    }
                 }
             }).catch(e => {
                 if (e.code === 'E_PERMISSION_MISSING') {
@@ -84,12 +119,15 @@ export const showAttachMenu = (fileCallback?: (filesMeta: ({ type: 'document' | 
                     cropping: false,
                     mediaType: 'video'
                 }).then(pickerResponse => {
-                    let filesMeta = pickerResponse.map(response => {
-                        const isPhoto = checkFileIsPhoto(response.path);
-                        return { type: isPhoto ? 'photo' : 'video' as 'document' | 'photo' | 'video', name: isPhoto ? 'image.jpg' : 'video.mp4', path: response.path, size: response.size };
-                    });
                     if (fileCallback) {
-                        fileCallback(filesMeta);
+                        let { imagesBatches, videos } = normalizeMedia(pickerResponse);
+
+                        if (imagesBatches.length > 0) {
+                            imagesBatches.forEach(x => fileCallback(x));
+                        }
+                        if (videos.length > 0) {
+                            videos.forEach(x => fileCallback([x]));
+                        }
                     }
                 }).catch(e => {
                     if (e.code === 'E_PERMISSION_MISSING') {
