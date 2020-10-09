5917b0b8fded57c529351705406379d081c12ba7
Fred Morozov <morozov@fred.md>
2020-10-09 06:41:55 +0300

    fix(web): UListHero: increase title lines count

--- packages/openland-web/components/unicorn/UListHero.tsx
+++ packages/openland-web/components/unicorn/UListHero.tsx
@@ -9,9 +9,11 @@ import { ProfileLayoutContext } from 'openland-web/components/ProfileLayout';
 import { useWithWidth } from 'openland-web/hooks/useWithWidth';
 
 const titleStyle = css`
-    white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
 `;
 
 const textStyle = css`
@@ -48,6 +50,7 @@ export const UListHero = (props: UListHeroProps) => {
     const [width] = useWithWidth();
 
     const isSmallMobile = width! < 400;
+    const titleTextStyle = compactView ? TextStyles.Title3 : TextStyles.Title2;
 
     return (
         <XView paddingHorizontal={16} flexDirection={compactView ? 'row' : 'column'} flexShrink={1}>
@@ -73,14 +76,17 @@ export const UListHero = (props: UListHeroProps) => {
                 justifyContent="center"
             >
                 <XView
-                    {...(compactView ? TextStyles.Title3 : TextStyles.Title2)}
+                    {...titleTextStyle}
                     color="var(--foregroundPrimary)"
                     flexDirection="row"
-                    alignItems="center"
                 >
-                    {titleIcon}
+                    <XView height={titleTextStyle.lineHeight} justifyContent="center">
+                        {titleIcon}
+                    </XView>
                     <span className={titleStyle}>{titleEmojify}</span>
-                    {titleRightIcon}
+                    <XView height={titleTextStyle.lineHeight} justifyContent="center">
+                        {titleRightIcon}
+                    </XView>
                 </XView>
 
                 {!!description && (
