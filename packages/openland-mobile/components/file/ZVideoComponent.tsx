debaca8df8f778e056b490790ac6a18d00f54f10
Max <qwertynestor40@gmail.com>
2020-11-18 21:55:22 +0300

    fix(mobile): Video sound not playing in device silence mode

--- packages/openland-mobile/components/file/ZVideoComponent.tsx
+++ packages/openland-mobile/components/file/ZVideoComponent.tsx
@@ -4,32 +4,44 @@ import { View } from 'react-native';
 import Video from 'react-native-video';
 import { LoaderSpinner } from '../LoaderSpinner';
 
-export const ZVideoComponent = React.memo((props: { uuid: string, name: string, completed: boolean }) => {
-    const { uuid, completed } = props;
-    const [path, setPath] = React.useState('');
+export const ZVideoComponent = React.memo(
+    (props: { uuid: string; name: string; completed: boolean }) => {
+        const { uuid, completed } = props;
+        const [path, setPath] = React.useState('');
 
-    React.useEffect(() => {
-        if (completed && path.length <= 0) {
-            (async () => {
-                const filePathWithExtension = await DownloadManagerInstance.getFilePathWithRealName(uuid, null, `${uuid}.mp4`);
+        React.useEffect(() => {
+            if (completed && path.length <= 0) {
+                (async () => {
+                    const filePathWithExtension = await DownloadManagerInstance.getFilePathWithRealName(
+                        uuid,
+                        null,
+                        `${uuid}.mp4`,
+                    );
 
-                if (filePathWithExtension) {
-                    setPath(filePathWithExtension);
-                }
-            })();
-        }
-    }, [completed, path]);
+                    if (filePathWithExtension) {
+                        setPath(filePathWithExtension);
+                    }
+                })();
+            }
+        }, [completed, path]);
 
-    return (
-        <View flexGrow={1}>
-            {path.length > 0 && completed && (
-                <Video source={{ uri: path }} style={{ flexGrow: 1 }} controls={true} playWhenInactive={true} />
-            )}
-            {(path.length <= 0 || !completed) && (
-                <View flexGrow={1} alignItems="center" justifyContent="center">
-                    <LoaderSpinner />
-                </View>
-            )}
-        </View>
-    );
-});
+        return (
+            <View flexGrow={1}>
+                {path.length > 0 && completed && (
+                    <Video
+                        source={{ uri: path }}
+                        style={{ flexGrow: 1 }}
+                        controls={true}
+                        playWhenInactive={true}
+                        ignoreSilentSwitch="ignore"
+                    />
+                )}
+                {(path.length <= 0 || !completed) && (
+                    <View flexGrow={1} alignItems="center" justifyContent="center">
+                        <LoaderSpinner />
+                    </View>
+                )}
+            </View>
+        );
+    },
+);
