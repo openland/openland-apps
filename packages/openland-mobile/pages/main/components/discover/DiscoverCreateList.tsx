317c24333fc1ed9f76d7f37e5db42c02decd3087
Alexandr Metreniuc <metreniuk@gmail.com>
2020-11-30 16:44:01 +0200

    feat(mobile): udpate discover and add new section

---
+++ packages/openland-mobile/pages/main/components/discover/DiscoverCreateList.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import { ZListGroup } from 'openland-mobile/components/ZListGroup';
+import { View, ScrollView, Text, TouchableWithoutFeedback, Animated, Image } from 'react-native';
+import { TextStyles, RadiusStyles } from 'openland-mobile/styles/AppStyles';
+import { useTheme } from 'openland-mobile/themes/ThemeContext';
+import { SRouterContext } from 'react-native-s/SRouterContext';
+import { usePressableView } from './usePressableView';
+import { ZButton } from 'openland-mobile/components/ZButton';
+
+interface CreateItemProps {
+    title: string;
+    description: string;
+    image: NodeRequire;
+    buttonText: string;
+    bgColor: string;
+    onPress: () => void;
+}
+
+const CreateItem = (props: CreateItemProps) => {
+    const theme = useTheme();
+    const { title, description, buttonText, image, bgColor, onPress } = props;
+    const { styles, delayPressIn, handlePressIn, handlePressOut } = usePressableView();
+
+    return (
+        <Animated.View style={{ width: 343, marginRight: 8, ...styles }}>
+            <TouchableWithoutFeedback delayPressIn={delayPressIn} onPress={onPress} onPressIn={handlePressIn} onPressOut={handlePressOut}>
+                <View flexDirection="row" borderRadius={RadiusStyles.Large} padding={24} backgroundColor={bgColor}>
+                    <View marginRight={8} flexGrow={1} flexShrink={1} flexDirection="column">
+                        <Text style={{ ...TextStyles.Title2, color: theme.foregroundPrimary, marginBottom: 8 }} allowFontScaling={false}>{title}</Text>
+                        <Text style={{ ...TextStyles.Body, color: theme.foregroundPrimary, opacity: 0.75, marginBottom: 24, }} allowFontScaling={false}>
+                            {description}
+                        </Text>
+                        <ZButton title={buttonText} onPress={onPress} onPressIn={handlePressIn} onPressOut={handlePressOut} />
+                    </View>
+                    <Image source={image} style={{ width: 108, height: 108, alignSelf: 'flex-end' }} />
+                </View>
+            </TouchableWithoutFeedback>
+        </Animated.View>
+    );
+};
+
+export const DiscoverCreateList = () => {
+    const router = React.useContext(SRouterContext)!;
+
+    return (
+        <ZListGroup header="Create">
+            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} paddingLeft={16} paddingBottom={8} pagingEnabled={true} decelerationRate="fast" snapToInterval={351}>
+                <CreateItem
+                    title="Community"
+                    description="A hub for your chats and channels"
+                    buttonText="New community"
+                    image={require('assets/art-create-community.png')}
+                    bgColor="#F4ECF5"
+                    onPress={() => router.push('NewOrganization')}
+                />
+                <CreateItem
+                    title="Chat"
+                    description="Public, secret, or paid group chat"
+                    buttonText="New chat"
+                    image={require('assets/art-create-chat.png')}
+                    bgColor="#F8F2E1"
+                    onPress={() => router.push('CreateGroupAttrs')}
+                />
+                <CreateItem
+                    title="Channel"
+                    description="Only admins write, others comment"
+                    buttonText="New channel"
+                    image={require('assets/art-create-channel.png')}
+                    bgColor="#E1EEF8"
+                    onPress={() => router.push('CreateGroupAttrs', { isChannel: true })}
+                />
+                <View width={24} />
+            </ScrollView>
+        </ZListGroup>
+    );
+};
