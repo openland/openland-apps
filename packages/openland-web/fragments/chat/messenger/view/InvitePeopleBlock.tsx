40f99d8c7b2347c9b6838a3956c423aa0be17819
Arseniy Nikitochkin <arseniynikitochkin@ya.ru>
2020-12-16 19:38:30 +0300

    feat(web): invite people block image for dark mode

--- packages/openland-web/fragments/chat/messenger/view/InvitePeopleBlock.tsx
+++ packages/openland-web/fragments/chat/messenger/view/InvitePeopleBlock.tsx
@@ -7,15 +7,24 @@ import { UButton } from 'openland-web/components/unicorn/UButton';
 import { showAddMembersModal } from 'openland-web/fragments/chat/showAddMembersModal';
 import { GroupMember } from 'openland-y-utils/members/EntityMembersManager';
 import { MessengerContext } from 'openland-engines/MessengerEngine';
+import { useTheme } from 'openland-x-utils/useTheme';
 import { useClient } from 'openland-api/useClient';
 import { ChatInit_room_SharedRoom } from 'openland-api/spacex.types';
 
-const findFriendsImgSrc = css`
-    height: 184px;
-    width: 186px;
-    margin-bottom: 24px;
+const artAddPeopleImgStyle = css`
+  height: 184px;
+  width: 186px;
+  margin-bottom: 24px;
+`;
+
+const artAddPeopleImgSrc = css`
     background: url(https://cdn.openland.com/shared/art/art-add-people.png) center center no-repeat;
     background-image: -webkit-image-set(url(https://cdn.openland.com/shared/art/art-add-people.png) 1x, url(https://cdn.openland.com/shared/art/art-add-people@2x.png) 2x, url(https://cdn.openland.com/shared/art/art-add-people@3x.png) 3x);
+`;
+
+const artAddPeopleImgSrcDark = css`
+    background: url(https://cdn.openland.com/shared/art/art-add-people-dark.png) center center no-repeat;
+    background-image: -webkit-image-set(url(https://cdn.openland.com/shared/art/art-add-people-dark.png) 1x, url(https://cdn.openland.com/shared/art/art-add-people-dark@2x.png) 2x, url(https://cdn.openland.com/shared/art/art-add-people-dark@3x.png) 3x);
 `;
 
 const titleClassName = css`
@@ -35,6 +44,7 @@ interface InvitePeopleBlockProps {
 export const InvitePeopleBlock = React.memo((props: InvitePeopleBlockProps) => {
     const onlines = React.useContext(MessengerContext).getOnlines();
     const client = useClient();
+    const theme = useTheme().theme;
     
     const { id, isChannel } = props.room;
 
@@ -47,9 +57,14 @@ export const InvitePeopleBlock = React.memo((props: InvitePeopleBlockProps) => {
         showAddMembersModal({ id, isChannel, isGroup: true, isOrganization: false, onGroupMembersAdd });
     }, [id, isChannel]);
 
+    const artStyles = cx(
+        artAddPeopleImgStyle,
+        theme === 'dark' ? artAddPeopleImgSrcDark : artAddPeopleImgSrc
+    );
+
     return (
         <XView alignItems="center" marginBottom={48}>
-            <div className={findFriendsImgSrc} />
+            <div className={artStyles} />
             <div className={cx(TextTitle1, titleClassName)}>Add people</div>
             <div className={cx(TextBody, textClassName)}>
                 Invite people to join {isChannel ? 'channel' : 'conversation'}
