bfcbda163a133d366ae2e9bf47bdf8d4ce306464
Max <qwertynestor40@gmail.com>
2021-01-21 19:50:12 +0300

    feat(web): add dialogs filters

--- packages/openland-web/components/unicorn/USideHeader.tsx
+++ packages/openland-web/components/unicorn/USideHeader.tsx
@@ -1,9 +1,38 @@
 import * as React from 'react';
+import { css, cx } from 'linaria';
 import { XView } from 'react-mental';
 import { TextStyles } from 'openland-web/utils/TextStyles';
+import { UIcon } from 'openland-web/components/unicorn/UIcon';
+import IcDropdown from 'openland-icons/s/ic-dropdown-16.svg';
+
+const titleContainer = css`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    cursor: pointer;
+    user-select: none;
+`;
+
+const titleIcon = css`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin-left: 8px;
+`;
+
+const titleActive = css`
+    transform: rotate(180deg);
+`;
 
 interface USideHeaderProps {
-    title: string;
+    title:
+        | string
+        | {
+              title: string;
+              active: boolean;
+              action: (element: HTMLElement | React.MouseEvent<unknown, MouseEvent>) => void;
+          };
     children?: any;
 }
 
@@ -19,8 +48,24 @@ export const USideHeader = React.memo((props: USideHeaderProps) => {
             flexDirection="row"
             backgroundColor="var(--backgroundPrimary)"
         >
-            <XView {...TextStyles.Title1} flexGrow={1} minWidth={0} flexBasis={0} color="var(--foregroundPrimary)">
-                {title}
+            <XView
+                {...TextStyles.Title1}
+                flexGrow={1}
+                minWidth={0}
+                flexBasis={0}
+                color="var(--foregroundPrimary)"
+                flexDirection="row"
+            >
+                {typeof title === 'string' ? (
+                    title
+                ) : (
+                    <div className={titleContainer} onClick={title.action}>
+                        {title.title}{' '}
+                        <div className={cx(titleIcon, title.active && titleActive)}>
+                            <UIcon icon={<IcDropdown />} />
+                        </div>
+                    </div>
+                )}
             </XView>
             <XView flexDirection="row" alignItems="center">
                 {children}
