f3531dfe498ddd0e556c019f920f03a44de9805a
Alexandr Metreniuc <metreniuk@gmail.com>
2021-02-12 19:31:24 +0200

    wip(mobile): add raise hand btn for rooms

--- packages/openland-mobile/pages/rooms/CreateRoom.tsx
+++ packages/openland-mobile/pages/rooms/CreateRoom.tsx
@@ -50,8 +50,16 @@ const showRoomInvite = ({ link, theme }: { link: string, theme: ThemeGlobal }) =
         .show();
 };
 
-const ControlItem = React.memo((props: { theme: ThemeGlobal, text?: string, icon: NodeRequire, iconColor: string, bgColor: string, onPress?: () => void }) => {
-    const { theme, text, icon, iconColor, bgColor, onPress } = props;
+const ControlItem = React.memo((props: {
+    theme: ThemeGlobal,
+    text?: string,
+    icon: NodeRequire | string,
+    iconColor?: string,
+    bgColor: string,
+    counter?: number,
+    onPress?: () => void
+}) => {
+    const { theme, text, icon, iconColor, bgColor, counter, onPress } = props;
     const size = text ? 56 : 78;
     const iconSize = text ? 24 : 36;
     return (
@@ -59,11 +67,43 @@ const ControlItem = React.memo((props: { theme: ThemeGlobal, text?: string, icon
             <TouchableOpacity
                 activeOpacity={0.6}
                 onPress={onPress}
-                style={{ width: size, height: size, marginBottom: 8 }}
+                style={{ width: size, height: size, marginBottom: 8, position: 'relative' }}
             >
                 <View style={{ backgroundColor: bgColor, width: size, height: size, borderRadius: 100, alignItems: 'center', justifyContent: 'center' }}>
-                    <Image source={icon} style={{ width: iconSize, height: iconSize, tintColor: iconColor }} />
+                    {typeof icon === 'string' ? (
+                        <Text style={{ fontSize: iconSize, color: iconColor }}>{icon}</Text>
+                    ) : (
+                            <Image source={icon} style={{ width: iconSize, height: iconSize, tintColor: iconColor }} />
+                        )}
                 </View>
+                {counter && (
+                    <View
+                        style={{
+                            position: "absolute",
+                            top: -3,
+                            right: -10,
+                            borderRadius: 100,
+                            backgroundColor: theme.foregroundTertiary,
+                            paddingHorizontal: 4,
+                            paddingVertical: 2,
+                            borderWidth: 1,
+                            borderColor: theme.backgroundSecondary,
+                            justifyContent: "center",
+                            alignItems: "center",
+                            overflow: "hidden",
+                        }}
+                    >
+                        <Text
+                            style={{
+                                color: theme.foregroundContrast,
+                                ...TextStyles.Detail,
+                                textAlign: 'center'
+                            }}
+                        >
+                            {counter}
+                        </Text>
+                    </View>
+                )}
             </TouchableOpacity>
             {text
                 ? <Text style={{ ...TextStyles.Label3, color: theme.foregroundPrimary, textAlign: 'center' }}>{text}</Text>
@@ -73,10 +113,64 @@ const ControlItem = React.memo((props: { theme: ThemeGlobal, text?: string, icon
     );
 });
 
-const RoomControls = React.memo((props: { theme: ThemeGlobal }) => {
+const ControlMute = React.memo((props: { theme: ThemeGlobal }) => {
     const { theme } = props;
+    const [muted, setMuted] = React.useState(true);
+    const toggleMute = React.useCallback(() => setMuted(x => !x), []);
+
+    return (
+        <ControlItem
+            theme={theme}
+            icon={muted ? require('assets/ic-mute-glyph-36.png') : require('assets/ic-microphone-36.png')}
+            iconColor={theme.foregroundContrast}
+            bgColor={muted ? theme.tintOrange : theme.tintBlue}
+            onPress={toggleMute}
+        />
+    );
+});
+
+const ControlRaiseHand = React.memo((props: { theme: ThemeGlobal }) => {
+    const { theme } = props;
+    const [raised, setRaised] = React.useState(false);
+    const toggleRaised = React.useCallback(() => setRaised(x => !x), []);
+
+    return (
+        <ControlItem
+            theme={theme}
+            icon={raised ? '🖐' : '🤚'}
+            bgColor={raised ? theme.accentPositive : theme.incomingBackgroundPrimary}
+            onPress={toggleRaised}
+        />
+    );
+});
+
+const ControlRaisedHandsCount = React.memo((props: { theme: ThemeGlobal, raisedCount?: number }) => {
+    const { theme, raisedCount } = props;
+
+    return (
+        <ControlItem
+            theme={theme}
+            icon={require('assets/ic-hand-24.png')}
+            iconColor={theme.foregroundSecondary}
+            text="Raised"
+            bgColor={theme.incomingBackgroundPrimary}
+            counter={raisedCount}
+        />
+    );
+});
+
+const RoomControls = React.memo((props: { theme: ThemeGlobal, role?: 'member' | 'admin' | 'speaker' }) => {
+    const { theme, role } = props;
     const client = useClient();
     const user = client.useProfile({ suspense: false })?.user;
+
+    const roleButtons = role === 'admin' ? (
+        <>
+            <ControlRaisedHandsCount theme={theme} raisedCount={124} />
+            <ControlMute theme={theme} />
+        </>
+    ) : role === 'speaker' ? <ControlMute theme={theme} />
+            : <ControlRaiseHand theme={theme} />;
 
     return (
         <View style={{ marginTop: 20, paddingHorizontal: 38, flexDirection: 'row', justifyContent: 'space-between' }}>
@@ -84,7 +178,7 @@ const RoomControls = React.memo((props: { theme: ThemeGlobal }) => {
                 theme={theme}
                 text="Leave"
                 icon={require('assets/ic-door-leave-24.png')}
-                iconColor={theme.tintRed}
+                iconColor={theme.accentNegative}
                 bgColor="rgba(242, 48, 81, 0.12)"
             />
             <ControlItem
@@ -95,12 +189,7 @@ const RoomControls = React.memo((props: { theme: ThemeGlobal }) => {
                 bgColor={theme.incomingBackgroundPrimary}
                 onPress={() => showRoomInvite({ theme, link: user ? `https://openland.com/${user.shortname || user.id}` : 'Try again' })}
             />
-            <ControlItem
-                theme={theme}
-                icon={require('assets/ic-mute-glyph-36.png')}
-                iconColor={theme.foregroundContrast}
-                bgColor={theme.tintOrange}
-            />
+            {roleButtons}
         </View>
     );
 });
