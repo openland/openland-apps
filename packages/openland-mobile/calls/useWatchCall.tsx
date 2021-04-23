a042ef0c4834ef9503991418c97e2c9ab7f38a50
Alexandr Metreniuc <metreniuk@gmail.com>
2021-04-23 16:38:58 +0300

    fix(mobile): try to fix loading indicators

--- packages/openland-mobile/calls/useWatchCall.tsx
+++ packages/openland-mobile/calls/useWatchCall.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import { getClient } from 'openland-mobile/utils/graphqlClient';
 import { reliableWatcher } from 'openland-api/reliableWatcher';
+import { ConferenceFull, ConferenceWatch } from 'openland-api/spacex.types';
 
-export function useWatchCall(id?: string | null) {
+export function useWatchCall(id?: string | null, onUpdate?: (update: ConferenceFull) => void) {
     React.useEffect(() => {
         if (!id) {
             return;
         }
-        return reliableWatcher((handler) => getClient().subscribeConferenceWatch({ id: id }, handler), () => {
-            // Nothing to do;
+        return reliableWatcher<ConferenceWatch>((handler) => getClient().subscribeConferenceWatch({ id: id }, handler), (update) => {
+            if (onUpdate) {
+                onUpdate(update.alphaConferenceWatch);
+            }
         });
     }, [id]);
 }
