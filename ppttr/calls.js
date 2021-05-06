93e1aa0e7b155be4f06016f2f773ab8f085a1860
max <qwertynestor40@gmail.com>
2021-05-07 00:23:47 +0300

    fix(web): puppeteer user registration timeout

--- ppttr/calls.js
+++ ppttr/calls.js
@@ -1,12 +1,12 @@
-const puppeteer = require('puppeteer')
-const readline = require('readline')
+const puppeteer = require('puppeteer');
+const readline = require('readline');
 
 // ids: joinRoomAuth, authEmail, authEmailNext, authEmailInput, authCodeInput-${i}, authCodeNext, authName, authNameNext, joinRoom
-const isDev = process.env.NODE_ENV === 'dev'
-const urlPath = isDev ? 'http://localhost:3000/' : 'https://next.openland.com/'
+const isDev = process.env.NODE_ENV === 'dev';
+const urlPath = isDev ? 'http://localhost:3000/' : 'https://next.openland.com/';
 
 async function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function askQuestion(query) {
@@ -15,79 +15,81 @@ async function askQuestion(query) {
         output: process.stdout,
     });
 
-    return new Promise(resolve => rl.question(query, ans => {
-        rl.close();
-        resolve(ans);
-    }))
+    return new Promise((resolve) =>
+        rl.question(query, (ans) => {
+            rl.close();
+            resolve(ans);
+        }),
+    );
 }
 
 const main = async (shortname, userNumber) => {
     const userEmailCode = ('000000' + userNumber.toString()).slice(-6);
-    const userCode = userEmailCode[5].toString().repeat(6)
-    const userName = `test${userEmailCode}`
-    const userEmail = `${userName}@openland.com`
+    const userCode = userEmailCode[5].toString().repeat(6);
+    const userName = `test${userEmailCode}`;
+    const userEmail = `${userName}@openland.com`;
 
     // const browser = await puppeteer.launch({ headless: isDev ? false : undefined })
     const browser = await puppeteer.connect({
         browserWSEndpoint: 'wss://chrome.browserless.io?token=6c11b2ba-17e0-49cf-85f7-0644d15cc1fa',
-    })
-    const page = await browser.newPage()
-    await page.goto(`${urlPath}${shortname.trim()}`)
+    });
+    const page = await browser.newPage();
+    await page.goto(`${urlPath}${shortname.trim()}`);
 
-    console.log('page loaded')
+    console.log('page loaded');
 
-    const click = (id) => page.waitForSelector(id, { visible: true }).then(d => d.click());
-    const loaded = () => page.waitForNavigation({ waitUntil: "domcontentloaded" });
+    const click = (id) => page.waitForSelector(id, { visible: true }).then((d) => d.click());
+    const loaded = () => page.waitForNavigation({ waitUntil: 'domcontentloaded' });
 
-    await click('#joinRoomAuth')
-    await loaded()
-    console.log('web login')
+    await click('#joinRoomAuth');
+    await loaded();
+    console.log('web login');
 
-    await click('#authEmail')
-    await loaded()
+    await click('#authEmail');
+    await loaded();
 
-    console.log('email login')
-    const emailInput = await page.waitForSelector('#authEmailInput')
+    console.log('email login');
+    const emailInput = await page.waitForSelector('#authEmailInput');
     await emailInput.focus();
-    await delay(500)
-    await emailInput.type(userEmail, { delay: 50 })
-    await click('#authEmailNext')
-    await loaded()
+    await delay(500);
+    await emailInput.type(userEmail, { delay: 50 });
+    await click('#authEmailNext');
+    await loaded();
 
-    console.log('code page')
-    const codeInput = await page.waitForSelector('#authCodeInput-0')
-    await codeInput.focus()
-    await delay(500)
-    await codeInput.type(userCode, { delay: 50 })
-    await loaded()
+    console.log('code page');
+    const codeInput = await page.waitForSelector('#authCodeInput-0');
+    await codeInput.focus();
+    await delay(500);
+    await codeInput.type(userCode, { delay: 50 });
+    await loaded();
 
     if (page.url().search('createProfile') !== -1) {
-        console.log('create user page')
-        await page.waitForSelector('#authName')
-        const nameInput = await page.waitForSelector('#authName')
+        console.log('create user page');
+        await page.waitForSelector('#authName', { timeout: 300000 });
+        const nameInput = await page.waitForSelector('#authName');
         await nameInput.focus();
-        await delay(500)
-        await nameInput.type(userName, { delay: 50 })
-        await click('#authNameNext')
-        await loaded()
+        await delay(500);
+        await nameInput.type(userName, { delay: 50 });
+        await click('#authNameNext');
+        await loaded();
     }
 
-    console.log('navigate to user')
-    await page.waitForSelector('#joinRoom', { visible: true })
-    await click('#joinRoom')
-    await loaded()
-    console.log('joined success')
-}
+    console.log('navigate to user');
+    await page.waitForSelector('#joinRoom', { visible: true });
+    await click('#joinRoom');
+    await loaded();
+    console.log('joined success');
+};
 
 let userNumber = 1;
 
 (async () => {
-    const iterations = await askQuestion('how many iterations?:')
-    const lastUserNumber = await askQuestion('last user number?:')
+    const iterations = await askQuestion('how many iterations?:');
+    const lastUserNumber = await askQuestion('last user number?:');
     userNumber = Number(lastUserNumber);
-    const username = await askQuestion('user shortname with chat:')
+    const username = await askQuestion('user shortname with chat:');
     for (let i = 0; i < Number(iterations || 1); i++) {
         main(username, userNumber || 1);
         userNumber++;
     }
-})()
+})();
