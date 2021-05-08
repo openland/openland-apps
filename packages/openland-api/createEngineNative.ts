58877306f26adad7c53dd4091beb6bca63379cb6
Steve Korshakov <steve@korshakov.com>
2021-05-08 13:22:07 +0300

    chore(mobile): Restrore native client, but disable offline mode

--- packages/openland-api/createEngineNative.ts
+++ packages/openland-api/createEngineNative.ts
@@ -1,10 +1,15 @@
 import { Definitions } from './spacex.web';
-// import { NativeEngine } from './native/NativeEngine';
+import { NativeEngine } from './native/NativeEngine';
 import { GraphqlEngine, WebEngine } from '@openland/spacex';
 import { API_HOST } from 'openland-y-utils/api';
 
+const ENABLE_STORAGE = false;
+const ENABLE_NATIVE = true;
+
 export function createEngineNative(storageKey?: string, token?: string): GraphqlEngine {
-    // return new NativeEngine(storageKey, token);
+    if (ENABLE_NATIVE) {
+        return new NativeEngine(ENABLE_STORAGE ? storageKey : undefined, token);
+    }
     return new WebEngine(Definitions, {
         endpoint: 'wss://' + API_HOST + '/api',
         connectionParams: { ['x-openland-token']: token },
