86cb7c0cee636afe5c3a9ea8d184d7c11ab3047f
Alexandr Metreniuc <metreniuk@gmail.com>
2021-05-24 14:35:20 +0300

    fix(mobile): audio fixes

--- packages/openland-mobile/utils/mediaExtension.ts
+++ packages/openland-mobile/utils/mediaExtension.ts
@@ -7,6 +7,11 @@ export const extractExtension = (name: string) => {
     return undefined;
 };
 
+export const removeExtension = (name: string) => {
+    let splitted = name.toLowerCase().split('.');
+    return splitted.slice(0, splitted.length - 1).join('');
+};
+
 export const isAudio = (name: string) => ['mp3', 'wav', 'm4a'].includes(extractExtension(name)!);
 
 export const isVideo = (name: string) => ['mp4', 'mov'].includes(extractExtension(name)!);
